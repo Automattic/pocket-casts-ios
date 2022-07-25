0ef6782997ee29ce75d9261086e79bb427666b6f
John Caruso <johncaruso@gmail.com>
2022-07-25 15:21:03 -0400

    add tests to PodcastSorterTests

--- Modules/Utils/Sources/PocketCastsUtils/Sorting/PodcastSorter.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Sorting/PodcastSorter.swift
@@ -1,7 +1,12 @@
 import Foundation
 
-public struct PodcastSorter {
-    /// TODO: Documentation
+public enum PodcastSorter {
+    /**
+     A case insensitive string comparison that ignores the word "The" at the start of the title.
+     - Parameter param title1 String
+     - Parameter  title2 String
+     - Returns true when title1 is alphabetically before title2, false otherwise
+     */
     public static func titleSort(title1: String, title2: String) -> Bool {
         let convertedTitle1 = title1.trimmingThePrefix()
         let convertedTitle2 = title2.trimmingThePrefix()
@@ -9,12 +14,22 @@ public struct PodcastSorter {
         return convertedTitle1.localizedLowercase.compare(convertedTitle2.localizedLowercase) == .orderedAscending
     }
 
-    /// TODO: Documentation
+    /**
+     A simple integer comparison function
+     - Parameter  order1 Int32
+     - Parameter  order2 Int32
+     - Returns true when order2 is greater than order1, false otherwise
+     */
     public static func customSort(order1: Int32, order2: Int32) -> Bool {
         return order2 > order1
     }
 
-    /// TODO: Documentation
+    /**
+     A simple date comparison function
+     - Parameter  order1 Int32
+     - Parameter  order2 Int32
+     - Returns true when date2 is greater than date1, false otherwise
+     */
     public static func dateAddedSort(date1: Date, date2: Date) -> Bool {
         return date1.compare(date2) == .orderedAscending
     }
