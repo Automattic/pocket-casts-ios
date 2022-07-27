7ca66a2983ef06dd51f264c21ae7fc77325a0958
Leandro Alonso <contato@leandroalonso.com>
2022-07-27 13:06:46 -0300

    Add PocketCastsDataModel as a dependency of unit tests target

--- PocketCastsTests/Podcasts/Helpers/HomeGridDataHelperTests.swift
+++ PocketCastsTests/Podcasts/Helpers/HomeGridDataHelperTests.swift
@@ -1,7 +1,11 @@
 import XCTest
 
 @testable import podcasts
+@testable import PocketCastsDataModel
 
 class HomeGridDataHelperTests: XCTestCase {
-    
+    func testLatestEpisodeSort() {
+        let folder = Folder()
+        let podcast = Podcast()
+    }
 }
