9b12400bb471a44e754314384f9cc297b712458b
emilylaguna <emily.laguna@automattic.com>
2022-08-02 16:43:40 -0400

    Prefix the database table field names with the table name to prepare them to be used in a JOIN

--- Modules/DataModel/Sources/PocketCastsDataModel/Public/PlaylistHelper.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Public/PlaylistHelper.swift
@@ -2,11 +2,11 @@ import Foundation
 
 public class PlaylistHelper {
     public class func queryFor(filter: EpisodeFilter, episodeUuidToAdd: String?, limit: Int) -> String {
-        var queryString = "archived = 0 "
+        var queryString = "\(field("archived")) = 0 "
         var addedUuid = false
         
         if let episodeUuidToAdd = episodeUuidToAdd {
-            queryString += "AND ((uuid = '\(episodeUuidToAdd)') OR ("
+            queryString += "AND ((\(field("uuid")) = '\(episodeUuidToAdd)') OR ("
             addedUuid = true
         }
         else {
@@ -18,17 +18,17 @@ public class PlaylistHelper {
         if !(filter.filterUnplayed && filter.filterPartiallyPlayed && filter.filterFinished), filter.filterUnplayed || filter.filterPartiallyPlayed || filter.filterFinished {
             queryString += "("
             if filter.filterUnplayed {
-                queryString += "playingStatus = \(PlayingStatus.notPlayed.rawValue) "
+                queryString += "\(field("playingStatus")) = \(PlayingStatus.notPlayed.rawValue) "
             }
             if filter.filterPartiallyPlayed {
                 if filter.filterUnplayed { queryString += "OR " }
                 
-                queryString += "playingStatus = \(PlayingStatus.inProgress.rawValue) "
+                queryString += "\(field("playingStatus")) = \(PlayingStatus.inProgress.rawValue) "
             }
             if filter.filterFinished {
                 if filter.filterUnplayed || filter.filterPartiallyPlayed { queryString += "OR " }
                 
-                queryString += "playingStatus = \(PlayingStatus.completed.rawValue)"
+                queryString += "\(field("playingStatus")) = \(PlayingStatus.completed.rawValue)"
             }
             
             queryString += ") "
@@ -38,14 +38,14 @@ public class PlaylistHelper {
         // Audio & Video
         if filter.filterAudioVideoType == AudioVideoFilter.videoOnly.rawValue {
             if haveStartedWhere { queryString += "AND " }
-            
-            queryString += "fileType LIKE 'video%' "
+
+            queryString += "\(field("fileType")) LIKE 'video%' "
             haveStartedWhere = true
         }
         if filter.filterAudioVideoType == AudioVideoFilter.audioOnly.rawValue {
             if haveStartedWhere { queryString += "AND " }
             
-            queryString += "fileType LIKE 'audio%' "
+            queryString += "\(field("fileType")) LIKE 'audio%' "
             haveStartedWhere = true
         }
         
@@ -54,16 +54,16 @@ public class PlaylistHelper {
             if haveStartedWhere { queryString += "AND " }
             queryString += "("
             if filter.filterDownloaded {
-                queryString += "episodeStatus = \(DownloadStatus.downloaded.rawValue) "
+                queryString += "\(field("episodeStatus")) = \(DownloadStatus.downloaded.rawValue) "
             }
             if filter.filterDownloading {
                 if filter.filterDownloaded { queryString += "OR " }
                 
-                queryString += "episodeStatus = \(DownloadStatus.queued.rawValue) OR episodeStatus = \(DownloadStatus.downloading.rawValue) "
+                queryString += "\(field("episodeStatus")) = \(DownloadStatus.queued.rawValue) OR \(field("episodeStatus")) = \(DownloadStatus.downloading.rawValue) "
             }
             if filter.filterNotDownloaded {
                 if filter.filterDownloaded || filter.filterDownloading { queryString += "OR " }
-                queryString += "episodeStatus = \(DownloadStatus.notDownloaded.rawValue) OR episodeStatus = \(DownloadStatus.downloadFailed.rawValue) OR episodeStatus = \(DownloadStatus.waitingForWifi.rawValue) "
+                queryString += "\(field("episodeStatus")) = \(DownloadStatus.notDownloaded.rawValue) OR \(field("episodeStatus")) = \(DownloadStatus.downloadFailed.rawValue) OR \(field("episodeStatus")) = \(DownloadStatus.waitingForWifi.rawValue) "
             }
             queryString += ") "
             haveStartedWhere = true
@@ -77,7 +77,7 @@ public class PlaylistHelper {
             // we add 59s here to account for how iOS doesn't show "10m" until you get to 10*60 seconds, that way our visual representation lines up with the filter times
             let shorterThanTime = (filter.shorterThan * 60) + 59
             
-            queryString += "(duration >= \(longerThanTime) AND duration <= \(shorterThanTime)) "
+            queryString += "(\(field("duration")) >= \(longerThanTime) AND \(field("duration")) <= \(shorterThanTime)) "
             
             haveStartedWhere = true
         }
@@ -86,16 +86,16 @@ public class PlaylistHelper {
         if filter.filterStarred {
             if haveStartedWhere { queryString += "AND " }
             
-            queryString += "keepEpisode = 1 "
+            queryString += "\(field("keepEpisode")) = 1 "
             haveStartedWhere = true
         }
         
         // particular podcasts only
         if !filter.filterAllPodcasts, filter.podcastUuids.count > 0, filter.podcastUuids != "null" {
             if haveStartedWhere { queryString += "AND " }
-            
+
             let podcastUuidArr = filter.podcastUuids.components(separatedBy: ",")
-            queryString += " podcastUuid in ("
+            queryString += " \(field("podcastUuid")) in ("
             for (index, uuid) in podcastUuidArr.enumerated() {
                 queryString += "\(index > 0 ? "," : "")'\(uuid)'"
             }
@@ -108,7 +108,7 @@ public class PlaylistHelper {
         if unsubscribedUuids.count > 0 {
             if haveStartedWhere { queryString += "AND " }
             
-            queryString += " podcastUuid NOT IN ("
+            queryString += " \(field("podcastUuid")) NOT IN ("
             for (index, uuid) in unsubscribedUuids.enumerated() {
                 queryString += "\(index > 0 ? "," : "")'\(uuid)'"
             }
@@ -119,8 +119,8 @@ public class PlaylistHelper {
         // time based filtering
         if filter.filterHours > 0 {
             if haveStartedWhere { queryString += "AND " }
-            
-            queryString += "publishedDate > \(filterTimeFor(hours: filter.filterHours)) "
+
+            queryString += "\(field("publishedDate")) > \(filterTimeFor(hours: filter.filterHours)) "
             // haveStartedWhere = true
         }
         
@@ -129,18 +129,18 @@ public class PlaylistHelper {
         queryString = queryString.replacingOccurrences(of: "OR ()", with: "OR (1)")
         
         if addedUuid { queryString += ")" }
-        
+
         if filter.sortType == PlaylistSort.oldestToNewest.rawValue {
-            queryString += " ORDER BY publishedDate ASC, addedDate ASC"
+            queryString += " ORDER BY \(field("publishedDate")) ASC, \(field("addedDate")) ASC"
         }
         else if filter.sortType == PlaylistSort.newestToOldest.rawValue {
-            queryString += " ORDER BY publishedDate DESC, addedDate DESC"
+            queryString += " ORDER BY \(field("publishedDate")) DESC, \(field("addedDate")) DESC"
         }
         else if filter.sortType == PlaylistSort.shortestToLongest.rawValue {
-            queryString += " ORDER BY duration ASC, addedDate ASC"
+            queryString += " ORDER BY \(field("duration")) ASC, \(field("addedDate")) ASC"
         }
         else if filter.sortType == PlaylistSort.longestToShortest.rawValue {
-            queryString += " ORDER BY duration DESC, addedDate DESC"
+            queryString += " ORDER BY \(field("duration")) DESC, \(field("addedDate")) DESC"
         }
         
         if limit > 0 {
@@ -154,5 +154,14 @@ public class PlaylistHelper {
         let changedTime = Date(timeIntervalSinceNow: TimeInterval(hours * -3600))
         
         return changedTime.timeIntervalSince1970
+    }
+}
+
+private extension PlaylistHelper {
+    /// Returns a table field name prefixed with the episode table name
+    /// archived -> SJEpisode.archived
+    /// This makes sure the right field is being used if there is a JOIN in the query
+    static func field(_ name: String) -> String {
+        return "\(DataManager.episodeTableName).\(name)"
     }
 }
