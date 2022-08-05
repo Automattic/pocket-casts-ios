50addb181b0cbff9a9671971a1eeeaa3fd33c467
emilylaguna <emily.laguna@automattic.com>
2022-08-05 16:31:54 -0400

    Add a helper toString var on some server enums

--- Modules/Server/Sources/PocketCastsServer/Public/ServerEnums.swift
+++ Modules/Server/Sources/PocketCastsServer/Public/ServerEnums.swift
@@ -6,14 +6,51 @@ public enum SubscriptionStatus: Int {
 
 public enum SubscriptionPlatform: Int {
     case none = 0, iOS = 1, android = 2, web = 3, gift = 4
+
+    public var toString: String {
+        switch self {
+        case .none:
+            return "none"
+        case .iOS:
+            return "ios"
+        case .android:
+            return "android"
+        case .web:
+            return "web"
+        case .gift:
+            return "gift"
+        }
+    }
 }
 
 public enum SubscriptionFrequency: Int {
     case none = 0, monthly = 1, yearly = 2
+
+    public var toString: String {
+        switch self {
+        case .none:
+            return "none"
+        case .monthly:
+            return "monthly"
+        case .yearly:
+            return "yearly"
+        }
+    }
 }
 
 public enum SubscriptionType: Int {
     case none = 0, plus = 1, supporter = 2
+
+    public var toString: String {
+        switch self {
+        case .none:
+            return "none"
+        case .plus:
+            return "plus"
+        case .supporter:
+            return "supporter"
+        }
+    }
 }
 
 public enum UpdateStatus: Int {
