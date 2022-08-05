63d76d88626d21b31c8b651335294fcf00439cd4
emilylaguna <emily.laguna@automattic.com>
2022-08-05 16:40:53 -0400

    Add an Analytics class for tracking

---
+++ podcasts/Analytics/Analytics.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+protocol AnalyticsAdapter {
+    func track(name: String, properties: [AnyHashable: Any]?)
+}
+
+class Analytics {
+    private static let shared = Analytics()
+    private var adapters: [AnalyticsAdapter]?
+
+    static func register(adapters: [AnalyticsAdapter]) {
+        Self.shared.adapters = adapters
+    }
+
+    static func track(_ event: AnalyticsEvent, properties: [AnyHashable: Any]? = nil) {
+        Self.shared.adapters?.forEach {
+            $0.track(name: event.eventName, properties: properties)
+        }
+    }
+}
