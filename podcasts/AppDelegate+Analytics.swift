876c7494bf81994cb0b407a4ef40ffbf9257dc81
emilylaguna <emily.laguna@automattic.com>
2022-08-16 10:10:46 -0400

    Remove debug item

--- podcasts/AppDelegate+Analytics.swift
+++ podcasts/AppDelegate+Analytics.swift
@@ -1,8 +1,5 @@
 extension AppDelegate {
     func setupAnalytics() {
         Analytics.register(adapters: [AnalyticsLoggingAdapter(), TracksAdapter()])
-
-        #warning("TODO: Remove this")
-        Analytics.track(.applicationOpened)
     }
 }
