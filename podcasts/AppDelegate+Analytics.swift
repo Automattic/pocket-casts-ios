492374646f8764c65e5f8d685b6abaf9a34ec64f
emilylaguna <emily.laguna@automattic.com>
2022-08-15 20:18:26 -0400

    Rename the TracksLogginAdapter to AnalyticsLoggingAdapter

--- podcasts/AppDelegate+Analytics.swift
+++ podcasts/AppDelegate+Analytics.swift
@@ -1,6 +1,6 @@
 extension AppDelegate {
     func setupAnalytics() {
-        Analytics.register(adapters: [TracksLoggingAdapter(), TracksAdapter()])
+        Analytics.register(adapters: [AnalyticsLoggingAdapter(), TracksAdapter()])
 
         #warning("TODO: Remove this")
         Analytics.track(.applicationOpened)
