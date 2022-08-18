543c809a3c1df4f738a626449a3d782f771512b1
emilylaguna <emily.laguna@automattic.com>
2022-08-17 23:35:57 -0400

    Updates the analytics class to make it easier to test

--- podcasts/Analytics/Analytics.swift
+++ podcasts/Analytics/Analytics.swift
@@ -5,15 +5,20 @@ protocol AnalyticsAdapter {
 }
 
 class Analytics {
-    private static let shared = Analytics()
+    static let shared = Analytics()
     private var adapters: [AnalyticsAdapter]?
 
     static func register(adapters: [AnalyticsAdapter]) {
         Self.shared.adapters = adapters
     }
 
+    /// Convenience method to call Analytics.shared.track*
     static func track(_ event: AnalyticsEvent, properties: [AnyHashable: Any]? = nil) {
-        Self.shared.adapters?.forEach {
+        Self.shared.track(event, properties: properties)
+    }
+
+    func track(_ event: AnalyticsEvent, properties: [AnyHashable: Any]? = nil) {
+        adapters?.forEach {
             $0.track(name: event.eventName, properties: properties)
         }
     }
