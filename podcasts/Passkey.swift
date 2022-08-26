181ce918df845e1c7a63ca3cdb7a2553cdddd8ff
Leandro Alonso <contato@leandroalonso.com>
2022-08-26 17:39:51 -0300

    Adapt Passkey to be available for iOS 14/15

--- podcasts/Passkey.swift
+++ podcasts/Passkey.swift
@@ -9,12 +9,12 @@ extension NSNotification.Name {
     static let ModalSignInSheetCanceled = Notification.Name("ModalSignInSheetCanceledNotification")
 }
 
-@available(iOS 16, *)
 class Passkey: NSObject, ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate {
     var authenticationAnchor: ASPresentationAnchor?
     var isPerformingModalReqest = false
 
-    func signInWith(anchor: ASPresentationAnchor, preferImmediatelyAvailableCredentials: Bool) async {
+    @available(iOS 16, *)
+    func signInWith(anchor: ASPresentationAnchor, preferImmediatelyAvailableCredentials: Bool) {
         self.authenticationAnchor = anchor
         let publicKeyCredentialProvider = ASAuthorizationPlatformPublicKeyCredentialProvider(relyingPartyIdentifier: domain)
 
@@ -48,6 +48,7 @@ class Passkey: NSObject, ASAuthorizationControllerPresentationContextProviding, 
         isPerformingModalReqest = true
     }
 
+    @available(iOS 16, *)
     func beginAutoFillAssistedPasskeySignIn(anchor: ASPresentationAnchor) {
         self.authenticationAnchor = anchor
 
@@ -64,7 +65,8 @@ class Passkey: NSObject, ASAuthorizationControllerPresentationContextProviding, 
         authController.performAutoFillAssistedRequests()
     }
 
-    func signUpWith(userName: String, anchor: ASPresentationAnchor) async {
+    @available(iOS 16, *)
+    func signUpWith(userName: String, anchor: ASPresentationAnchor) {
         self.authenticationAnchor = anchor
         let publicKeyCredentialProvider = ASAuthorizationPlatformPublicKeyCredentialProvider(relyingPartyIdentifier: domain)
 
@@ -73,7 +75,7 @@ class Passkey: NSObject, ASAuthorizationControllerPresentationContextProviding, 
         let challenge = Data()
         let userID = Data(UUID().uuidString.utf8)
 
-        let registrationRequest = publicKeyCredentialProvider.createCredentialRegistrationRequest(challenge: userID,
+        let registrationRequest = publicKeyCredentialProvider.createCredentialRegistrationRequest(challenge: challenge,
                                                                                                   name: userName, userID: userID)
 
         // Use only ASAuthorizationPlatformPublicKeyCredentialRegistrationRequests or
@@ -87,34 +89,38 @@ class Passkey: NSObject, ASAuthorizationControllerPresentationContextProviding, 
 
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         let logger = Logger()
-        switch authorization.credential {
-        case let credentialRegistration as ASAuthorizationPlatformPublicKeyCredentialRegistration:
-            logger.log("A new passkey was registered: \(credentialRegistration)")
-            // Verify the attestationObject and clientDataJSON with your service.
-            // The attestationObject contains the user's new public key to store and use for subsequent sign-ins.
-            // let attestationObject = credentialRegistration.rawAttestationObject
-            // let clientDataJSON = credentialRegistration.rawClientDataJSON
+        if #available(iOS 16.0, *) {
+            switch authorization.credential {
+            case let credentialRegistration as ASAuthorizationPlatformPublicKeyCredentialRegistration:
+                logger.log("A new passkey was registered: \(credentialRegistration)")
+                // Verify the attestationObject and clientDataJSON with your service.
+                // The attestationObject contains the user's new public key to store and use for subsequent sign-ins.
+                // let attestationObject = credentialRegistration.rawAttestationObject
+                // let clientDataJSON = credentialRegistration.rawClientDataJSON
 
-            // After the server verifies the registration and creates the user account, sign in the user with the new account.
-        case let credentialAssertion as ASAuthorizationPlatformPublicKeyCredentialAssertion:
-            logger.log("A passkey was used to sign in: \(credentialAssertion)")
-            // Verify the below signature and clientDataJSON with your service for the given userID.
-            // let signature = credentialAssertion.signature
-            // let clientDataJSON = credentialAssertion.rawClientDataJSON
-            // let userID = credentialAssertion.userID
+                // After the server verifies the registration and creates the user account, sign in the user with the new account.
+            case let credentialAssertion as ASAuthorizationPlatformPublicKeyCredentialAssertion:
+                logger.log("A passkey was used to sign in: \(credentialAssertion)")
+                // Verify the below signature and clientDataJSON with your service for the given userID.
+                // let signature = credentialAssertion.signature
+                // let clientDataJSON = credentialAssertion.rawClientDataJSON
+                // let userID = credentialAssertion.userID
 
-            // After the server verifies the assertion, sign in the user.
-            didFinishSignIn()
-        case let passwordCredential as ASPasswordCredential:
-            logger.log("A password was provided: \(passwordCredential)")
-            // Verify the userName and password with your service.
-            // let userName = passwordCredential.user
-            // let password = passwordCredential.password
+                // After the server verifies the assertion, sign in the user.
+                didFinishSignIn()
+            case let passwordCredential as ASPasswordCredential:
+                logger.log("A password was provided: \(passwordCredential)")
+                // Verify the userName and password with your service.
+                // let userName = passwordCredential.user
+                // let password = passwordCredential.password
 
-            // After the server verifies the userName and password, sign in the user.
-            didFinishSignIn()
-        default:
-            fatalError("Received unknown authorization type.")
+                // After the server verifies the userName and password, sign in the user.
+                didFinishSignIn()
+            default:
+                fatalError("Received unknown authorization type.")
+            }
+        } else {
+            // Fallback on earlier versions
         }
 
         isPerformingModalReqest = false
