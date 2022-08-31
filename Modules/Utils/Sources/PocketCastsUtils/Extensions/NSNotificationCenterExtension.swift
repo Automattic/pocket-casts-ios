f9ed9a85073644d3b746eab307e2ac03778c7dce
emilylaguna <emily.laguna@automattic.com>
2022-08-31 14:02:00 -0400

    Refactor the postOnMainThread method and add unit tests for it

--- Modules/Utils/Sources/PocketCastsUtils/Extensions/NSNotificationCenterExtension.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Extensions/NSNotificationCenterExtension.swift
@@ -1,25 +1,14 @@
 import Foundation
 
 public extension NotificationCenter {
-    static func postOnMainThread(notification: Notification.Name) {
-        if Thread.isMainThread {
-            NotificationCenter.default.post(name: notification, object: nil)
-        }
-        else {
-            DispatchQueue.main.sync { () in
-                NotificationCenter.default.post(name: notification, object: nil)
+    static func postOnMainThread(notification: Notification.Name, object: Any? = nil, userInfo: [AnyHashable: Any]? = nil) {
+        guard Thread.isMainThread else {
+            DispatchQueue.main.sync {
+                Self.postOnMainThread(notification: notification, object: object, userInfo: userInfo)
             }
+            return
         }
-    }
-    
-    static func postOnMainThread(notification: Notification.Name, object: Any?) {
-        if Thread.isMainThread {
-            NotificationCenter.default.post(name: notification, object: object)
-        }
-        else {
-            DispatchQueue.main.sync { () in
-                NotificationCenter.default.post(name: notification, object: object)
-            }
-        }
+
+        NotificationCenter.default.post(name: notification, object: object, userInfo: userInfo)
     }
 }
