443c7b110b67b1f97cbfad2659d40d4bb070574d
emilylaguna <emily.laguna@automattic.com>
2022-09-01 11:51:58 -0400

    Adds a feature flag for enabling tracks

--- podcasts/AppDelegate+Analytics.swift
+++ podcasts/AppDelegate+Analytics.swift
@@ -3,6 +3,10 @@ import PocketCastsUtils
 
 extension AppDelegate {
     func setupAnalytics() {
+        guard FeatureFlag.tracksEnabled else {
+            return
+        }
+
         Analytics.register(adapters: [AnalyticsLoggingAdapter(), TracksAdapter()])
     }
 }
