756081c006f63b060f078eb22a02bb9935d25bd6
emilylaguna <emily.laguna@automattic.com>
2022-09-02 12:10:55 -0400

    Improve readability of the post on main thread method

--- Modules/Utils/Sources/PocketCastsUtils/Extensions/NSNotificationCenterExtension.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Extensions/NSNotificationCenterExtension.swift
@@ -2,13 +2,14 @@ import Foundation
 
 public extension NotificationCenter {
     static func postOnMainThread(notification: Notification.Name, object: Any? = nil, userInfo: [AnyHashable: Any]? = nil) {
-        guard Thread.isMainThread else {
-            DispatchQueue.main.sync {
-                Self.postOnMainThread(notification: notification, object: object, userInfo: userInfo)
-            }
+        if Thread.isMainThread {
+            NotificationCenter.default.post(name: notification, object: object, userInfo: userInfo)
             return
         }
 
-        NotificationCenter.default.post(name: notification, object: object, userInfo: userInfo)
+        // Force the notification to be posted on the main thread
+        DispatchQueue.main.sync {
+            Self.postOnMainThread(notification: notification, object: object, userInfo: userInfo)
+        }
     }
 }
