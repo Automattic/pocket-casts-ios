a7b6f8a74031960691529b31d117576360b2ec7e
Leandro Alonso <contato@leandroalonso.com>
2022-09-08 13:10:01 -0300

    Refactor to use a protocol instead of PCViewController

--- podcasts/Analytics/AnalyticsPlaybackHelper.swift
+++ podcasts/Analytics/AnalyticsPlaybackHelper.swift
@@ -1,10 +1,15 @@
 import UIKit
 
+protocol PlaybackSource {
+    /// Used for analytics purpose when playing/pausing
+    var playbackSource: String { get }
+}
+
 /// Helper used to track playback
 class AnalyticsPlaybackHelper {
     #if !os(watchOS)
     func play() {
-        let playBackSource = (getTopViewController() as? PCViewController)?.playbackSource ?? "unknown"
+        let playBackSource = (getTopViewController() as? PlaybackSource)?.playbackSource ?? "unknown"
         Analytics.track(.play, properties: ["source": playBackSource])
     }
 
