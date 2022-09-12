20cebaf310db310d9d60fd5ad65c9c9726763e4d
Leandro Alonso <contato@leandroalonso.com>
2022-09-12 10:42:27 -0300

    Add AppIconWidget code

--- WidgetExtension/App Icon/AppIconWidget.swift
+++ WidgetExtension/App Icon/AppIconWidget.swift
@@ -1,1 +1,51 @@
-import Foundation
+import SwiftUI
+import WidgetKit
+
+struct StaticWidgetProvider: TimelineProvider {
+    func placeholder(in context: Context) -> StaticEntry {
+        StaticEntry(date: Date())
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (StaticEntry) -> Void) {
+        completion(StaticEntry(date: Date()))
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<StaticEntry>) -> Void) {
+        completion(Timeline(entries: [StaticEntry(date: Date())], policy: .never))
+    }
+}
+
+struct StaticEntry: TimelineEntry {
+    let date: Date
+}
+
+
+struct AppIconWidget: Widget {
+    var body: some WidgetConfiguration {
+        if #available(iOSApplicationExtension 16.0, *) {
+            return StaticConfiguration(kind: "App_Icon_Widget", provider: StaticWidgetProvider()) { entry in
+                AppIconWidgetEntryView(entry: entry)
+            }
+            .configurationDisplayName("App Icon Widget")
+            .description("Launch Pocket Casts")
+            .supportedFamilies([.accessoryCircular])
+        } else {
+            return EmptyWidgetConfiguration()
+        }
+    }
+}
+
+struct AppIconWidgetEntryView: View {
+    @State var entry: StaticWidgetProvider.Entry
+
+    var body: some View {
+        ZStack {
+            Color.black
+            Image("logo-transparent-medium")
+                .resizable()
+                .frame(width: 55, height: 55)
+                .foregroundColor(.white)
+        }
+        .widgetURL(URL(string: "pktc://last_opened"))
+    }
+}
