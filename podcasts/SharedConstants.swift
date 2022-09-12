af9706518702e6d63215a912c039f503ae3cb0fd
Leandro Alonso <contato@leandroalonso.com>
2022-09-12 15:45:02 -0300

    Store the number of episodes in up next

--- podcasts/SharedConstants.swift
+++ podcasts/SharedConstants.swift
@@ -2,6 +2,7 @@ enum SharedConstants {
     enum GroupUserDefaults {
         public static let groupContainerId = "group.au.com.shiftyjelly.pocketcasts"
         public static let upNextItems = "upNextItems"
+        public static let upNextItemsCount = "upNextItemsCount"
         public static let siriSearchItems = "siriSearchItems"
         public static let topFilterName = "topFilterTitle"
         public static let topFilterItems = "topFilterItems"
