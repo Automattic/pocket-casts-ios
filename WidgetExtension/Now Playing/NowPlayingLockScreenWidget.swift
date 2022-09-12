4a4c82ebb58624f17e1eb519efce6e345e4b275b
Leandro Alonso <contato@leandroalonso.com>
2022-09-12 10:05:20 -0300

    Add now playing lock screen initial code

--- WidgetExtension/Now Playing/NowPlayingLockScreenWidget.swift
+++ WidgetExtension/Now Playing/NowPlayingLockScreenWidget.swift
@@ -1,1 +1,27 @@
-import Foundation
+import SwiftUI
+import WidgetKit
+
+struct NowPlayingLockScreenWidget: Widget {
+    var body: some WidgetConfiguration {
+        if #available(iOSApplicationExtension 16.0, *) {
+            return StaticConfiguration(kind: "Now_Playing_Lock_Screen_Widget", provider: NowPlayingProvider()) { entry in
+                NowPlayingLockscreenWidgetEntryView(entry: entry)
+            }
+            .configurationDisplayName(L10n.nowPlaying)
+            .description(L10n.widgetsNowPlayingDesc)
+            .supportedFamilies([.accessoryRectangular])
+        } else {
+            return EmptyWidgetConfiguration()
+        }
+    }
+}
+
+struct NowPlayingLockscreenWidgetEntryView: View {
+    @State var entry: NowPlayingProvider.Entry
+
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
