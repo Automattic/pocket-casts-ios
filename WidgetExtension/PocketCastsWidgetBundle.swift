6ea857537d9824fdcc7cfa495f2ca16c8d067543
emilylaguna <emily.laguna@automattic.com>
2022-09-13 11:50:36 -0400

    Enable the widget

--- WidgetExtension/PocketCastsWidgetBundle.swift
+++ WidgetExtension/PocketCastsWidgetBundle.swift
@@ -9,5 +9,6 @@ struct PocketCastsWidgetBundle: WidgetBundle {
         NowPlayingLockScreenWidget()
         AppIconWidget()
         UpNextLockScreenWidget()
+        UpNextEpisodeLockScreenWidget()
     }
 }
