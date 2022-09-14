f984d4c2a9cea4a180c0332dbfa52ee76404ca89
Leandro Alonso <contato@leandroalonso.com>
2022-09-14 14:56:48 -0300

    Test with a single episode played 2 minutes

--- PocketCastsTests/Podcasts/Helpers/PlaybackTimeHelperTests.swift
+++ PocketCastsTests/Podcasts/Helpers/PlaybackTimeHelperTests.swift
@@ -13,12 +13,45 @@ class PlaybackTimeHelperTests: XCTestCase {
 
         XCTAssertEqual(playbackTime, 0)
     }
+
+    func testLastSevenDaysPlaytimeWhenPlayedASingleEpisode() {
+        let dataManagerMock = DataManagerMock()
+        let playbackTimeHelper = PlaybackTimeHelper(dataManager: dataManagerMock)
+        dataManagerMock.episodesToReturn = [
+            EpisodeBuilder()
+                .with(playedUpTo: 120)
+                .with(lastPlaybackInteractionDate: Date())
+                .build()
+        ]
+
+        let playbackTime = playbackTimeHelper.playtimeLastSevenDaysInSeconds()
+
+        XCTAssertEqual(playbackTime, 2)
+    }
 }
 
 class DataManagerMock: DataManager {
     var episodesToReturn: [Episode] = []
 
     override func findEpisodesWhere(customWhere: String, arguments: [Any]?) -> [Episode] {
-        return []
+        return episodesToReturn
+    }
+}
+
+class EpisodeBuilder {
+    var episode = Episode()
+
+    func with(playedUpTo: Double) -> Self {
+        episode.playedUpTo = playedUpTo
+        return self
+    }
+
+    func with(lastPlaybackInteractionDate: Date) -> Self {
+        episode.lastPlaybackInteractionDate = lastPlaybackInteractionDate
+        return self
+    }
+
+    func build() -> Episode {
+        episode
     }
 }
