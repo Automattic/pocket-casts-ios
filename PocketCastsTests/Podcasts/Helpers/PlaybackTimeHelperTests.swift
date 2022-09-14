46c7d46bdcfd4bb43bda79a313d764ed18722892
Leandro Alonso <contato@leandroalonso.com>
2022-09-14 15:08:13 -0300

    Add a test with multiple episodes

--- PocketCastsTests/Podcasts/Helpers/PlaybackTimeHelperTests.swift
+++ PocketCastsTests/Podcasts/Helpers/PlaybackTimeHelperTests.swift
@@ -28,6 +28,34 @@ class PlaybackTimeHelperTests: XCTestCase {
 
         XCTAssertEqual(playbackTime, 2)
     }
+
+    func testLastSevenDaysPlaytimeWhenPlayedMultipleEpisodes() {
+        let dataManagerMock = DataManagerMock()
+        let playbackTimeHelper = PlaybackTimeHelper(dataManager: dataManagerMock)
+        dataManagerMock.episodesToReturn = [
+            // These episodes were played in the last 7 days
+            EpisodeBuilder()
+                .with(playedUpTo: 120)
+                .with(lastPlaybackInteractionDate: Date())
+                .build(),
+
+            EpisodeBuilder()
+                .with(playedUpTo: 1200)
+                .with(lastPlaybackInteractionDate: Date().addingTimeInterval(-5000))
+                .build(),
+
+            // This was played a month ago
+
+            EpisodeBuilder()
+                .with(playedUpTo: 1200)
+                .with(lastPlaybackInteractionDate: Calendar.current.date(byAdding: .month, value: -1, to: Date())!)
+                .build()
+        ]
+
+        let playbackTime = playbackTimeHelper.playtimeLastSevenDaysInMinutes()
+
+        XCTAssertEqual(playbackTime, 22)
+    }
 }
 
 class DataManagerMock: DataManager {
