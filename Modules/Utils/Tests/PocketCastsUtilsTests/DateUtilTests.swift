3bfc39ee72064f48584c7b8084951aaa81ef91d7
Leandro Alonso <contato@leandroalonso.com>
2022-09-14 13:29:59 -0300

    Small improvements on an existent test

--- Modules/Utils/Tests/PocketCastsUtilsTests/DateUtilTests.swift
+++ Modules/Utils/Tests/PocketCastsUtilsTests/DateUtilTests.swift
@@ -3,16 +3,18 @@ import PocketCastsUtils
 import XCTest
 
 final class DateUtilTests: XCTestCase {
-    func test_if_enough_time_has_passed() throws {
+    func testIfEnoughTimeHasPassed() throws {
         let sevenDaysAgo = Date(timeIntervalSinceNow: -7.days)
         let twelveHoursAgo = Date(timeIntervalSinceNow: -12.hours)
         let fiveMinutesAgo = Date(timeIntervalSinceNow: -5.minutes)
         let thirtySecondsAgo = Date(timeIntervalSinceNow: -30.seconds)
+
         // Test if enough time has passed:
         XCTAssertTrue(DateUtil.hasEnoughTimePassed(since: sevenDaysAgo, time: 6.days))
         XCTAssertTrue(DateUtil.hasEnoughTimePassed(since: twelveHoursAgo, time: 11.hours))
         XCTAssertTrue(DateUtil.hasEnoughTimePassed(since: fiveMinutesAgo, time: 4.minutes))
         XCTAssertTrue(DateUtil.hasEnoughTimePassed(since: thirtySecondsAgo, time: 15.seconds))
+
         // Test if not enough time has passed:
         XCTAssertFalse(DateUtil.hasEnoughTimePassed(since: sevenDaysAgo, time: 8.days))
         XCTAssertFalse(DateUtil.hasEnoughTimePassed(since: twelveHoursAgo, time: 13.hours))
