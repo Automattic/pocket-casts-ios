5bf7421b9d300322c5cfbc05df64c3ffbecb5e9b
Leandro Alonso <contato@leandroalonso.com>
2022-09-14 14:51:18 -0300

    Refactor DataManagerMock to return episodes that are provided

--- PocketCastsTests/Podcasts/Helpers/PlaybackTimeHelperTests.swift
+++ PocketCastsTests/Podcasts/Helpers/PlaybackTimeHelperTests.swift
@@ -7,6 +7,7 @@ class PlaybackTimeHelperTests: XCTestCase {
     func testLastSevenDaysPlaytimeWhenNothingIsReturned() {
         let dataManagerMock = DataManagerMock()
         let playbackTimeHelper = PlaybackTimeHelper(dataManager: dataManagerMock)
+        dataManagerMock.episodesToReturn = []
 
         let playbackTime = playbackTimeHelper.playtimeLastSevenDaysInSeconds()
 
@@ -15,6 +16,8 @@ class PlaybackTimeHelperTests: XCTestCase {
 }
 
 class DataManagerMock: DataManager {
+    var episodesToReturn: [Episode] = []
+
     override func findEpisodesWhere(customWhere: String, arguments: [Any]?) -> [Episode] {
         return []
     }
