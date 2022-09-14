14b10255ebb77091eba8cbb08395d89f08adacdd
Leandro Alonso <contato@leandroalonso.com>
2022-09-14 17:09:13 -0300

    Track review requested

--- podcasts/UIViewController+requestReview.swift
+++ podcasts/UIViewController+requestReview.swift
@@ -8,13 +8,15 @@ extension UIViewController {
         guard Settings.reviewRequestDates().count == 0 else { return }
 
         Task { @MainActor [weak self] in
+            guard let self else { return }
             // Delay for two seconds to avoid interrupting the person using the app.
             // Use the equation n * 10^9 to convert seconds to nanoseconds.
             try? await Task.sleep(nanoseconds: UInt64(2e9))
-            if let windowScene = self?.view.window?.windowScene,
-               self?.navigationController?.topViewController == self {
+            if let windowScene = self.view.window?.windowScene,
+               self.navigationController?.topViewController == self {
                 SKStoreReviewController.requestReview(in: windowScene)
                 Settings.addReviewRequested()
+                Analytics.track(.appStoreReviewRequested, properties: ["from": NSStringFromClass(self.classForCoder)])
            }
         }
     }
