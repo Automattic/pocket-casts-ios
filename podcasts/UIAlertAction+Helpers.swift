4b2f8771fc09b7fb55b1b9c3d8a476443501b751
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2022-09-21 10:21:26 -0400

    Remove the UIAlertAction testing code

--- podcasts/UIAlertAction+Helpers.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Extension that allows the UIAlertAction to be tested
-extension UIAlertAction {
-    @objc class func make(title: String?, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertAction {
-        return UIAlertAction(title: title, style: style, handler: handler)
-    }
-}
