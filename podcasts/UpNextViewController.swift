2426477865f5ebc353e4db127efd3cbd5401310d
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2022-09-23 18:27:40 -0400

    Add a way to track where the up next is being displayed from

--- podcasts/UpNextViewController.swift
+++ podcasts/UpNextViewController.swift
@@ -87,7 +87,19 @@ class UpNextViewController: UIViewController, UIGestureRecognizerDelegate {
         
         return longPressRecognizer
     }()
-    
+
+    let source: String
+
+    init(source: String) {
+        self.source = source
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = L10n.upNext
