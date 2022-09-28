89d76b5c653e4e0f763831f1e4cecb8e58d13f5d
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2022-09-28 14:39:08 -0400

    Update the episode helper to use the coordinator

--- podcasts/Analytics/Helpers/AnalyticsEpisodeHelper.swift
+++ podcasts/Analytics/Helpers/AnalyticsEpisodeHelper.swift
@@ -2,201 +2,154 @@ import Foundation
 import PocketCastsDataModel
 import PocketCastsServer
 
-class AnalyticsEpisodeHelper {
+class AnalyticsEpisodeHelper: AnalyticsCoordinator {
     static var shared = AnalyticsEpisodeHelper()
-    
-    /// Sometimes the playback source can't be inferred, just inform it here
-    var currentSource: String?
 
     // Internally track the episode UUIDs that the user is downloading or uploadiung
     private var episodeDownloadQueue: Set<String> = []
     private var episodeUploadQueue: Set<String> = []
 
-    init() {
+    override init() {
+        super.init()
         addNotificationObservers()
     }
 
-    #if !os(watchOS)
+    // MARK: - Star
 
-        // MARK: - Star
+    func star(episode: BaseEpisode) {
+        episodeEvent(.episodeStarred, episode: episode)
+    }
 
-        func star(episode: BaseEpisode) {
-            episodeEvent(.episodeStarred, episode: episode)
-        }
+    func bulkStar(count: Int) {
+        bulkEvent(.episodeBulkStarred, count: count)
+    }
 
-        func bulkStar(count: Int) {
-            bulkEvent(.episodeBulkStarred, count: count)
-        }
+    func unstar(episode: BaseEpisode) {
+        episodeEvent(.episodeUnstarred, episode: episode)
+    }
 
-        func unstar(episode: BaseEpisode) {
-            episodeEvent(.episodeUnstarred, episode: episode)
-        }
+    func bulkUnstar(count: Int) {
+        bulkEvent(.episodeBulkUnstarred, count: count)
+    }
 
-        func bulkUnstar(count: Int) {
-            bulkEvent(.episodeBulkUnstarred, count: count)
-        }
+    // MARK: - Download
 
-        // MARK: - Download
+    func downloadCancelled(episodeUUID: String) {
+        episodeEvent(.episodeDownloadCancelled, uuid: episodeUUID)
+    }
 
-        func downloadCancelled(episodeUUID: String) {
-            episodeEvent(.episodeDownloadCancelled, uuid: episodeUUID)
-        }
+    func downloaded(episodeUUID: String) {
+        episodeDownloadQueue.insert(episodeUUID)
+        episodeEvent(.episodeDownloadQueued, uuid: episodeUUID)
+    }
 
-        func downloaded(episodeUUID: String) {
-            episodeDownloadQueue.insert(episodeUUID)
-            episodeEvent(.episodeDownloadQueued, uuid: episodeUUID)
-        }
+    func downloadFinished(episodeUUID: String) {
+        episodeEvent(.episodeDownloadFinished, uuid: episodeUUID)
+    }
 
-        func downloadFinished(episodeUUID: String) {
-            episodeEvent(.episodeDownloadFinished, uuid: episodeUUID)
-        }
-
-        func bulkDownloadEpisodes(episodes: [BaseEpisode]) {
-            let uuids = episodes.map { $0.uuid }
-            episodeDownloadQueue.formUnion(uuids)
-            bulkEvent(.episodeBulkDownloadQueued, count: episodes.count)
-        }
+    func bulkDownloadEpisodes(episodes: [BaseEpisode]) {
+        let uuids = episodes.map { $0.uuid }
+        episodeDownloadQueue.formUnion(uuids)
+        bulkEvent(.episodeBulkDownloadQueued, count: episodes.count)
+    }
     
-        func downloadDeleted(episode: BaseEpisode) {
-            episodeEvent(.episodeDownloadDeleted, episode: episode)
-        }
+    func downloadDeleted(episode: BaseEpisode) {
+        episodeEvent(.episodeDownloadDeleted, episode: episode)
+    }
 
-        func bulkDeleteDownloadedEpisodes(count: Int) {
-            bulkEvent(.episodeBulkDownloadDeleted, count: count)
-        }
+    func bulkDeleteDownloadedEpisodes(count: Int) {
+        bulkEvent(.episodeBulkDownloadDeleted, count: count)
+    }
 
-        // MARK: - Played
+    // MARK: - Played
 
-        func markAsPlayed(episode: BaseEpisode) {
-            episodeEvent(.episodeMarkedAsPlayed, episode: episode)
-        }
+    func markAsPlayed(episode: BaseEpisode) {
+        episodeEvent(.episodeMarkedAsPlayed, episode: episode)
+    }
 
-        func bulkMarkAsPlayed(count: Int) {
-            bulkEvent(.episodeBulkMarkedAsPlayed, count: count)
-        }
+    func bulkMarkAsPlayed(count: Int) {
+        bulkEvent(.episodeBulkMarkedAsPlayed, count: count)
+    }
 
-        func markAsUnplayed(episode: BaseEpisode) {
-            episodeEvent(.episodeMarkedAsUnplayed, episode: episode)
-        }
+    func markAsUnplayed(episode: BaseEpisode) {
+        episodeEvent(.episodeMarkedAsUnplayed, episode: episode)
+    }
 
-        func bulkMarkAsUnplayed(count: Int) {
-            bulkEvent(.episodeBulkMarkedAsUnplayed, count: count)
-        }
+    func bulkMarkAsUnplayed(count: Int) {
+        bulkEvent(.episodeBulkMarkedAsUnplayed, count: count)
+    }
 
-        // MARK: - Archive
+    // MARK: - Archive
 
-        func archiveEpisode(_ episode: BaseEpisode) {
-            episodeEvent(.episodeArchived, episode: episode)
-        }
+    func archiveEpisode(_ episode: BaseEpisode) {
+        episodeEvent(.episodeArchived, episode: episode)
+    }
 
-        func bulkArchiveEpisodes(count: Int) {
-            bulkEvent(.episodeBulkArchived, count: count)
-        }
+    func bulkArchiveEpisodes(count: Int) {
+        bulkEvent(.episodeBulkArchived, count: count)
+    }
 
-        func unarchiveEpisode(_ episode: BaseEpisode) {
-            episodeEvent(.episodeUnarchived, episode: episode)
-        }
+    func unarchiveEpisode(_ episode: BaseEpisode) {
+        episodeEvent(.episodeUnarchived, episode: episode)
+    }
 
-        func bulkUnarchiveEpisodes(count: Int) {
-            bulkEvent(.episodeBulkUnarchived, count: count)
-        }
+    func bulkUnarchiveEpisodes(count: Int) {
+        bulkEvent(.episodeBulkUnarchived, count: count)
+    }
 
-        // MARK: - Uploads
+    // MARK: - Uploads
 
-        func episodeUploaded(episodeUUID: String) {
-            episodeUploadQueue.insert(episodeUUID)
-            episodeEvent(.episodeUploadQueued, uuid: episodeUUID)
-        }
+    func episodeUploaded(episodeUUID: String) {
+        episodeUploadQueue.insert(episodeUUID)
+        episodeEvent(.episodeUploadQueued, uuid: episodeUUID)
+    }
 
-        func episodeUploadCancelled(episodeUUID: String) {
-            episodeEvent(.episodeUploadCancelled, uuid: episodeUUID)
-        }
+    func episodeUploadCancelled(episodeUUID: String) {
+        episodeEvent(.episodeUploadCancelled, uuid: episodeUUID)
+    }
 
-        func episodeDeletedFromCloud(episode: BaseEpisode) {
-            episodeEvent(.episodeDeletedFromCloud, episode: episode)
-        }
+    func episodeDeletedFromCloud(episode: BaseEpisode) {
+        episodeEvent(.episodeDeletedFromCloud, episode: episode)
+    }
 
-        func episodeUploadFinished(episodeUUID: String) {
-            episodeEvent(.episodeUploadFinished, uuid: episodeUUID)
-        }
+    func episodeUploadFinished(episodeUUID: String) {
+        episodeEvent(.episodeUploadFinished, uuid: episodeUUID)
+    }
 
-        // MARK: - Up Next
+    // MARK: - Up Next
 
-        func episodeAddedToUpNext(episode: BaseEpisode, toTop: Bool) {
-            track(.episodeAddedToUpNext, properties: ["episode_uuid": episode.uuid, "to_top": toTop])
-        }
+    func episodeAddedToUpNext(episode: BaseEpisode, toTop: Bool) {
+        track(.episodeAddedToUpNext, properties: ["episode_uuid": episode.uuid, "to_top": toTop])
+    }
 
-        func bulkAddToUpNext(count: Int, toTop: Bool) {
-            track(.episodeBulkAddToUpNext, properties: ["episode_count": count, "to_top": toTop])
-        }
+    func bulkAddToUpNext(count: Int, toTop: Bool) {
+        track(.episodeBulkAddToUpNext, properties: ["episode_count": count, "to_top": toTop])
+    }
 
-        func episodeRemovedFromUpNext(episode: BaseEpisode) {
-            episodeEvent(.episodeRemovedFromUpNext, episode: episode)
-        }
-
-        // MARK: - Private
-
-        private var currentPlaybackSource: String {
-            if let currentSource = currentSource {
-                self.currentSource = nil
-                return currentSource
-            }
-
-            return (getTopViewController() as? PlaybackSource)?.playbackSource ?? "unknown"
-        }
-    #endif
+    func episodeRemovedFromUpNext(episode: BaseEpisode) {
+        episodeEvent(.episodeRemovedFromUpNext, episode: episode)
+    }
 }
 
 private extension AnalyticsEpisodeHelper {
-    #if !os(watchOS)
-        func episodeEvent(_ event: AnalyticsEvent, episode: BaseEpisode? = nil, uuid: String? = nil) {
-            let episodeUUID: String
-            if let episode {
-                episodeUUID = episode.uuid
-            }
-            else if let uuid {
-                episodeUUID = uuid
-            }
-            else {
-                episodeUUID = "unknown"
-            }
-
-            track(event, properties: ["episode_uuid": episodeUUID])
+    func episodeEvent(_ event: AnalyticsEvent, episode: BaseEpisode? = nil, uuid: String? = nil) {
+        let episodeUUID: String
+        if let episode {
+            episodeUUID = episode.uuid
+        }
+        else if let uuid {
+            episodeUUID = uuid
+        }
+        else {
+            episodeUUID = "unknown"
         }
 
-        func bulkEvent(_ event: AnalyticsEvent, count: Int) {
-            track(event, properties: ["episode_count": count])
-        }
+        track(event, properties: ["episode_uuid": episodeUUID])
+    }
 
-        func track(_ event: AnalyticsEvent, properties: [String: Any]? = nil) {
-            DispatchQueue.main.async { [weak self] in
-                guard let self = self else {
-                    return
-                }
-            
-                let defaultProperties: [String: Any] = ["source": self.currentPlaybackSource]
-                let mergedProperties = defaultProperties.merging(properties ?? [:]) { current, _ in current }
-                Analytics.track(event, properties: mergedProperties)
-            }
-        }
-    
-        func getTopViewController(base: UIViewController? = UIApplication.shared.windows.first { $0.isKeyWindow }?.rootViewController) -> UIViewController? {
-            guard UIApplication.shared.applicationState == .active else {
-                return nil
-            }
-        
-            if let nav = base as? UINavigationController {
-                return getTopViewController(base: nav.visibleViewController)
-            }
-            else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
-                return getTopViewController(base: selected)
-            }
-            else if let presented = base?.presentedViewController {
-                return getTopViewController(base: presented)
-            }
-            return base
-        }
-    #endif
+    func bulkEvent(_ event: AnalyticsEvent, count: Int) {
+        track(event, properties: ["episode_count": count])
+    }
 }
 
 private extension AnalyticsEpisodeHelper {
