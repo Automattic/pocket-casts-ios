c1c1f16c3e578f4b1128505f5bdf29b5384a4b30
Leandro Alonso <contato@leandroalonso.com>
2022-10-10 13:38:59 -0300

    Add end of year modal

---
+++ podcasts/End of Year/EndOfYearModal.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct EndOfYearModal: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct EndOfYearModal_Previews: PreviewProvider {
+    static var previews: some View {
+        EndOfYearModal()
+    }
+}
