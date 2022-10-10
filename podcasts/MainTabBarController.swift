1e0d2a6a86158c4377070fb624562f44b1cdc716
Leandro Alonso <contato@leandroalonso.com>
2022-10-10 16:30:16 -0300

    Extract EndOfYear to its own var and add method to show stories

--- podcasts/MainTabBarController.swift
+++ podcasts/MainTabBarController.swift
@@ -10,6 +10,8 @@ class MainTabBarController: UITabBarController, NavigationProtocol {
 
     let playPauseCommand = UIKeyCommand(title: L10n.keycommandPlayPause, action: #selector(handlePlayPauseKey), input: " ", modifierFlags: [])
 
+    private lazy var endOfYear = EndOfYear()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -52,7 +54,7 @@ class MainTabBarController: UITabBarController, NavigationProtocol {
         checkPromotionFinishedAcknowledged()
         checkWhatsNewAcknowledged()
 
-        EndOfYear().showPrompt(in: self)
+        endOfYear.showPrompt(in: self)
     }
 
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
@@ -336,6 +338,10 @@ class MainTabBarController: UITabBarController, NavigationProtocol {
             navController.pushViewController(AccountViewController(), animated: false)
             navController.pushViewController(supporterVC, animated: true)
         }
+    }
+
+    func showEndOfYearStories() {
+        endOfYear.showStories(in: self)
     }
 
     private func topController() -> UIViewController {
