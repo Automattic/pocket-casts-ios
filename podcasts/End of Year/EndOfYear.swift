af1c98fe841cd8a068a8fa6395ff2f0726f917fb
Leandro Alonso <contato@leandroalonso.com>
2022-10-10 16:29:42 -0300

    Fix file name and add method to show stories

--- podcasts/End of Year/EndOfYear.swift
+++ podcasts/End of Year/EndOfYear.swift
@@ -11,4 +11,15 @@ struct EndOfYear {
         let bottomSheet = MDCBottomSheetController(contentViewController: endfOfYearModalViewController)
         viewController.present(bottomSheet, animated: true, completion: nil)
     }
+
+    func showStories(in viewController: UIViewController) {
+        guard FeatureFlag.endOfYear else {
+            return
+        }
+
+        let storiesViewController = UIViewController()
+        storiesViewController.view.backgroundColor = .systemBackground
+        storiesViewController.modalPresentationStyle = .fullScreen
+        viewController.present(storiesViewController, animated: true, completion: nil)
+    }
 }
