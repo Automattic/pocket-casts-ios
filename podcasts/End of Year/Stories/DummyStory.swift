f134cd20919dd8a31919fcd5d035bbb91514588d
Leandro Alonso <contato@leandroalonso.com>
2022-10-14 11:44:00 -0300

    Add a dummy story

---
+++ podcasts/End of Year/Stories/DummyStory.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct DummyStory: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct DummyStory_Previews: PreviewProvider {
+    static var previews: some View {
+        DummyStory()
+    }
+}
