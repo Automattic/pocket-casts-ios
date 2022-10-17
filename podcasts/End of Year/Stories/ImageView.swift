e2b1aed5358d93ab8f1a383b95f9ce61e9f7ee7a
Leandro Alonso <contato@leandroalonso.com>
2022-10-17 20:38:34 -0300

    Add a SwiftUI wrapper for UIImageView

---
+++ podcasts/End of Year/Stories/ImageView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+/// A SwiftUI representation of UIImageView
+///
+/// This is necessary because `Image` does not render correctly
+/// when taking screenshots of it — the image doesn't appear.
+struct ImageView: UIViewRepresentable {
+    let url: URL
+
+    init(_ url: URL) {
+        self.url = url
+    }
+
+    func makeUIView(context: Context) -> UIImageView {
+        let v = ImageViewWithFixedIntrinsicContentSize()
+
+        return v
+    }
+
+    func updateUIView(_ uiImage: UIImageView, context: Context) {
+        uiImage.kf.setImage(with: url)
+    }
+}
+
+private class ImageViewWithFixedIntrinsicContentSize: UIImageView {
+    override var intrinsicContentSize: CGSize {
+        return .init(width: 76, height: 76)
+    }
+}
