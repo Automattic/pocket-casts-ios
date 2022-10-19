03786dd5e5e31a8798f4a279fdd34b9a27a1dfa5
Leandro Alonso <contato@leandroalonso.com>
2022-10-19 11:08:24 -0300

    Add a method to return listening time in 2022

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EndOfYearDataManager.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EndOfYearDataManager.swift
@@ -4,4 +4,26 @@ import PocketCastsUtils
 /// Calculates user End of Year stats
 class EndOfYearDataManager {
 
+    /// Returns 5 random podcasts from the DB
+    /// This is here for development purposes.
+    func listeningTime(dbQueue: FMDatabaseQueue) -> Double? {
+        var listeningTime: Double?
+
+        dbQueue.inDatabase { db in
+            do {
+                let query = "SELECT SUM(playedUpTo) as totalPlayedTime from SJEpisode WHERE lastPlaybackInteractionDate IS NOT NULL AND lastPlaybackInteractionDate BETWEEN strftime('%s', date('now','start of year')) and strftime('%s', 'now')"
+                let resultSet = try db.executeQuery(query, values: nil)
+                defer { resultSet.close() }
+
+                while resultSet.next() {
+                    listeningTime = resultSet.double(forColumn: "totalPlayedTime")
+                }
+            } catch {
+                FileLog.shared.addMessage("PodcastDataManager.listeningTime error: \(error)")
+            }
+        }
+
+        return listeningTime
+    }
+
 }
