f153d64d5341b5b5e53686ecf9c85a46775fcb93
Leandro Alonso <contato@leandroalonso.com>
2022-10-19 13:59:24 -0300

    Add a query to return all listened categories and the number of podcasts inside each

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EndOfYearDataManager.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EndOfYearDataManager.swift
@@ -26,4 +26,46 @@ class EndOfYearDataManager {
         return listeningTime
     }
 
+    /// Returns all the categories the user has listened to podcasts
+    ///
+    /// The returned array is ordered from the most listened to the least
+    func listenedCategories(dbQueue: FMDatabaseQueue) -> [ListenedCategory] {
+        var listenedCategories: [ListenedCategory] = []
+
+        dbQueue.inDatabase { db in
+            do {
+                let query = """
+                            SELECT COUNT(DISTINCT podcastUuid) as numberOfPodcasts,
+                                SUM(playedUpTo) as totalPlayedTime,
+                                replace(IFNULL( nullif(substr(\(DataManager.podcastTableName).podcastCategory, 0, INSTR(\(DataManager.podcastTableName).podcastCategory, char(10))), '') , \(DataManager.podcastTableName).podcastCategory), CHAR(10), '') as category
+                            FROM \(DataManager.episodeTableName), \(DataManager.podcastTableName)
+                            WHERE \(DataManager.podcastTableName).uuid = \(DataManager.episodeTableName).podcastUuid and
+                                lastPlaybackInteractionDate IS NOT NULL AND
+                                lastPlaybackInteractionDate BETWEEN strftime('%s', '2022-01-01') and strftime('%s', '\(endPeriod)')
+                            GROUP BY category
+                            ORDER BY totalPlayedTime DESC
+"""
+
+                let resultSet = try db.executeQuery(query, values: nil)
+                defer { resultSet.close() }
+
+                while resultSet.next() {
+                    let numberOfPodcasts = Int(resultSet.int(forColumn: "numberOfPodcasts"))
+                    if let categoryTitle = resultSet.string(forColumn: "category") {
+                        listenedCategories.append(ListenedCategory(numberOfPodcasts: numberOfPodcasts, categoryTitle: categoryTitle))
+                    }
+                }
+            } catch {
+                FileLog.shared.addMessage("PodcastDataManager.listenedCategories error: \(error)")
+            }
+        }
+
+        return listenedCategories
+    }
+
+}
+
+public struct ListenedCategory {
+    let numberOfPodcasts: Int
+    let categoryTitle: String
 }
