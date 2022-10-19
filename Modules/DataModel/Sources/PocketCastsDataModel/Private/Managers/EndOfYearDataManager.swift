e3a1be339e0cbadc517867304f6918f0373beedd
Leandro Alonso <contato@leandroalonso.com>
2022-10-19 13:07:11 -0300

    Make the period calculation static

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EndOfYearDataManager.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EndOfYearDataManager.swift
@@ -3,6 +3,7 @@ import PocketCastsUtils
 
 /// Calculates user End of Year stats
 class EndOfYearDataManager {
+    private let endPeriod = "2022-12-01"
 
     /// Returns the aproximately listening time for the current year
     func listeningTime(dbQueue: FMDatabaseQueue) -> Double? {
@@ -10,7 +11,7 @@ class EndOfYearDataManager {
 
         dbQueue.inDatabase { db in
             do {
-                let query = "SELECT SUM(playedUpTo) as totalPlayedTime from \(DataManager.episodeTableName) WHERE lastPlaybackInteractionDate IS NOT NULL AND lastPlaybackInteractionDate BETWEEN strftime('%s', date('now','start of year')) and strftime('%s', 'now')"
+                let query = "SELECT SUM(playedUpTo) as totalPlayedTime from \(DataManager.episodeTableName) WHERE lastPlaybackInteractionDate IS NOT NULL AND lastPlaybackInteractionDate BETWEEN strftime('%s', '2022-01-01') and strftime('%s', '\(endPeriod)')"
                 let resultSet = try db.executeQuery(query, values: nil)
                 defer { resultSet.close() }
 
