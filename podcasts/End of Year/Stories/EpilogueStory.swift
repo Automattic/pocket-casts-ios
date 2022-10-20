2c3d2b4b0c379dac093a8750137a67b36c1e6b7e
Leandro Alonso <contato@leandroalonso.com>
2022-10-20 13:20:23 -0300

    Update Epilogue to replay when Replay button is tapped

--- podcasts/End of Year/Stories/EpilogueStory.swift
+++ podcasts/End of Year/Stories/EpilogueStory.swift
@@ -6,12 +6,25 @@ struct EpilogueStory: StoryView {
     var body: some View {
         ZStack {
             Color.orange
+                .allowsHitTesting(false)
 
             VStack {
-                Text("Thank you for letting Pocket Casts be apart of your listening experience in 2022")
+                Text("Thank you for letting Pocket Casts be a part of your listening experience in 2022")
                     .foregroundColor(.white)
                 Text("Don't forget to share with your friends and give a shout out to your favorite podcasts creators")
                     .foregroundColor(.white)
+                Button(action: {
+                    StoriesController.shared.replay()
+                }) {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "arrow.clockwise")
+                            .foregroundColor(.white)
+                        Text("Replay")
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                }
             }
             .padding()
         }
