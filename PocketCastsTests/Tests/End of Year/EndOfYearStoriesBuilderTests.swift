3d9161df6067066e42b86d455106cd5935d4ad5c
Leandro Alonso <contato@leandroalonso.com>
2022-10-21 09:36:21 -0300

    Create test suite for stories builder

---
+++ PocketCastsTests/Tests/End of Year/EndOfYearStoriesBuilderTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+
+@testable import podcasts
+
+class EndOfYearStoriesBuilderTests: XCTestCase {
+    func testSomething() {
+        let builder = EndOfYearStoriesBuilder(dataManager: DataManagerMock())
+    }
+}
--- podcasts/End of Year/EndOfYearStoriesBuilder.swift
+++ podcasts/End of Year/EndOfYearStoriesBuilder.swift
@@ -1,6 +1,16 @@
 import Foundation
+import PocketCastsDataModel
 
 /// Build the list of stories for End of Year alongside the data
 class EndOfYearStoriesBuilder {
-    
+    private let dataManager: DataManager
+
+    init(dataManager: DataManager = DataManager.sharedManager) {
+        self.dataManager = dataManager
+    }
+
+    // Call this method to build the list of stories and the data provider
+    func build() async {
+
+    }
 }
