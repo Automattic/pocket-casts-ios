e608a8ce839137edc9351bc43bad9905fc8f5956
Leandro Alonso <contato@leandroalonso.com>
2022-10-24 16:36:45 -0300

    Refactor EndOfYearModal for better legibility

--- podcasts/End of Year/Views/EndOfYearModal.swift
+++ podcasts/End of Year/Views/EndOfYearModal.swift
@@ -6,71 +6,99 @@ struct EndOfYearModal: View {
     @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-        VStack(alignment: .center, spacing: 20) {
+        VStack(alignment: .center, spacing: Constants.verticalSpacing) {
             Text(L10n.eoyTitle)
                 .font(.title2)
                 .fontWeight(.semibold)
 
-            ZStack {
-                Image("modal_background")
-                    .resizable()
-                ZStack {
-                    VStack {
-                        Image("2022_small")
-                        Text(L10n.eoySmallTitle)
-                            .foregroundColor(.white)
-                            .font(.system(size: 14))
-                            .fontWeight(.semibold)
-                            .padding(.top, -30)
-                            .padding(.trailing, 10)
-                            .padding(.leading, 10)
-                            .multilineTextAlignment(.center)
-                            .minimumScaleFactor(0.01)
-                    }
-                    .frame(width: 145, height: 145)
-                    .background(Color.black)
-                    .cornerRadius(8)
-                    .shadow(radius: 3, x: 0, y: 1)
-                }
-                .padding()
-            }
-            .frame(maxWidth: .infinity)
-            .frame(height: 180)
-            .background(UIColor(hex: "#FF6262").color)
-            .cornerRadius(16)
+            cover
 
             Text(L10n.eoyDescription)
                 .font(.body)
                 .multilineTextAlignment(.center)
                 .allowsTightening(false)
 
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-                NavigationManager.sharedManager.navigateTo(NavigationManager.endOfYearStories, data: nil)
-            }) {
-                HStack {
-                    Spacer()
-                    Text(L10n.eoyViewYear)
-                    Spacer()
-                }
-            }
-            .textStyle(RoundedDarkButton())
-            .contentShape(Rectangle())
+            showStoriesButton
 
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                HStack {
-                    Spacer()
-                    Text(L10n.eoyNotNow)
-                    Spacer()
-                }
-            }
-            .textStyle(StrokeButton())
-            .contentShape(Rectangle())
+            dismissButton
         }
         .padding()
         .applyDefaultThemeOptions()
+    }
+
+    var cover: some View {
+        ZStack {
+            Image("modal_background")
+                .resizable()
+            ZStack {
+                VStack {
+                    Image("2022_small")
+                    Text(L10n.eoySmallTitle)
+                        .foregroundColor(.white)
+                        .font(.system(size: Constants.smallTitleFontSize))
+                        .fontWeight(.semibold)
+                        .padding(.top, Constants.smallTitleTopPadding)
+                        .padding(.trailing, Constants.smallTitleHorizontalPadding)
+                        .padding(.leading, Constants.smallTitleHorizontalPadding)
+                        .multilineTextAlignment(.center)
+                        .minimumScaleFactor(Constants.smallTitleMinimumScaleFactor)
+                }
+                .frame(width: Constants.enfOfYearCoverSize, height: Constants.enfOfYearCoverSize)
+                .background(Color.black)
+                .cornerRadius(Constants.coverCornerRadius)
+                .shadow(radius: Constants.coverShadowRadius, x: 0, y: Constants.coverShadowY)
+            }
+            .padding()
+        }
+        .frame(maxWidth: .infinity)
+        .frame(height: Constants.coverWrapperHeight)
+        .cornerRadius(Constants.coverWrapperCornerRadius)
+    }
+
+    var showStoriesButton: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+            NavigationManager.sharedManager.navigateTo(NavigationManager.endOfYearStories, data: nil)
+        }) {
+            HStack {
+                Spacer()
+                Text(L10n.eoyViewYear)
+                Spacer()
+            }
+        }
+        .textStyle(RoundedDarkButton())
+        .contentShape(Rectangle())
+    }
+
+    var dismissButton: some View {
+        Button(action: {
+            presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+                Spacer()
+                Text(L10n.eoyNotNow)
+                Spacer()
+            }
+        }
+        .textStyle(StrokeButton())
+        .contentShape(Rectangle())
+    }
+
+    private enum Constants {
+        static let verticalSpacing: CGFloat = 20
+
+        static let smallTitleFontSize: CGFloat = 14
+        static let smallTitleTopPadding: CGFloat = -30
+        static let smallTitleHorizontalPadding: CGFloat = 10
+        static let smallTitleMinimumScaleFactor: CGFloat = 0.01
+
+        static let enfOfYearCoverSize: CGFloat = 145
+        static let coverCornerRadius: CGFloat = 8
+        static let coverShadowRadius: CGFloat = 3
+        static let coverShadowY: CGFloat = 1
+
+        static let coverWrapperHeight: CGFloat = 180
+        static let coverWrapperCornerRadius: CGFloat = 16
     }
 }
 
