4e4299f85d47ae32af74d9dbb7b16a29baa6e81d
Leandro Alonso <contato@leandroalonso.com>
2022-10-25 11:48:32 -0300

    Add design into the first story
    To provide a somewhat similar experience in different devices, we use percentage-driven sizes to present some elements

--- podcasts/End of Year/Stories/IntroStory.swift
+++ podcasts/End of Year/Stories/IntroStory.swift
@@ -4,8 +4,42 @@ struct IntroStory: StoryView {
     var duration: TimeInterval = 5.seconds
 
     var body: some View {
-        Text("Let's celebrate your year of listening...")
-            .foregroundColor(.white)
+        GeometryReader { geometry in
+            ZStack {
+                VStack {
+                    Image("2022_big")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: geometry.size.height * Constants.imageHeightInPercentage)
+                        .padding(.top, Constants.imageVerticalPadding)
+                        .padding(.bottom, Constants.imageVerticalPadding)
+                    Text(L10n.eoyStoryIntroTitle)
+                        .font(.system(size: Constants.fontSize, weight: .bold))
+                        .foregroundColor(.white)
+                        .padding(.leading, Constants.textHorizontalPadding)
+                        .padding(.trailing, Constants.textHorizontalPadding)
+                        .multilineTextAlignment(.center)
+                        .frame(maxHeight: geometry.size.height * Constants.textMaxHeightInPercentage)
+                        .minimumScaleFactor(Constants.textMinimumScaleFactor)
+                    Spacer()
+                    Image("logo_white")
+                        .padding(.bottom, Constants.logoBottomPadding)
+                }
+            }
+            .background(UIColor(hex: "#1A1A1A").color)
+        }
+    }
+
+    private struct Constants {
+        static let imageVerticalPadding: CGFloat = 60
+        static let imageHeightInPercentage: CGFloat = 0.54
+
+        static let fontSize: CGFloat = 40
+        static let textHorizontalPadding: CGFloat = 35
+        static let textMaxHeightInPercentage: CGFloat = 0.07
+        static let textMinimumScaleFactor: CGFloat = 0.01
+
+        static let logoBottomPadding: CGFloat = 40
     }
 }
 
