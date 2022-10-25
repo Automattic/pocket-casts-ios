cdab5955cb7c7c5c27529fdcf8457e97717529cf
Leandro Alonso <contato@leandroalonso.com>
2022-10-25 16:50:15 -0300

    Change visual of listening time story

--- podcasts/End of Year/Stories/ListeningTimeStory.swift
+++ podcasts/End of Year/Stories/ListeningTimeStory.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+import PocketCastsServer
+import PocketCastsDataModel
 
 struct ListeningTimeStory: StoryView {
     var duration: TimeInterval = 5.seconds
@@ -6,17 +8,83 @@ struct ListeningTimeStory: StoryView {
     let listeningTime: Double
 
     var body: some View {
-        ZStack {
-            Color.black
+        GeometryReader { geometry in
+            ZStack {
+
+                VStack {
+                    Text("In 2022, you spent \(listeningTime.localizedTimeDescription ?? "") listening to podcasts")
+                        .foregroundColor(.white)
+                        .font(.system(size: 25, weight: .heavy))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(maxHeight: geometry.size.height * 0.12)
+                        .minimumScaleFactor(0.01)
+
+                    Text(FunnyTimeConverter.timeSecsToFunnyText(listeningTime))
+                        .font(.system(size: 15, weight: .bold))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(maxHeight: geometry.size.height * 0.07)
+                        .minimumScaleFactor(0.01)
+                        .opacity(0.8)
+                    Spacer()
+                }
+                .padding(.top, geometry.size.height * 0.15)
+                .padding(.trailing, 40)
+                .padding(.leading, 40)
+
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        ImageView(ServerHelper.imageUrl(podcastUuid: Podcast.previewPodcast().uuid, size: 280))
+                            .frame(width: 140, height: 140)
+                            .aspectRatio(1, contentMode: .fit)
+                            .cornerRadius(4)
+                            .shadow(radius: 2, x: 0, y: 1)
+                            .accessibilityHidden(true)
+
+                        ImageView(ServerHelper.imageUrl(podcastUuid: Podcast.previewPodcast().uuid, size: 280))
+                            .frame(width: 140, height: 140)
+                            .aspectRatio(1, contentMode: .fit)
+                            .cornerRadius(4)
+                            .shadow(radius: 2, x: 0, y: 1)
+                            .accessibilityHidden(true)
+
+                        ImageView(ServerHelper.imageUrl(podcastUuid: Podcast.previewPodcast().uuid, size: 280))
+                            .frame(width: 140, height: 140)
+                            .aspectRatio(1, contentMode: .fit)
+                            .cornerRadius(4)
+                            .shadow(radius: 2, x: 0, y: 1)
+                            .accessibilityHidden(true)
+                    }
+                    .transformEffect(androidValuesToIOSTransform())
+                    .rotationEffect(.init(degrees: -30))
+                    .position(x: 170, y: geometry.size.height - 230)
+                }
+            }
 
             VStack {
-                Text("In 2022, you spent \(listeningTime.localizedTimeDescription ?? "") listening to podcasts")
-                    .foregroundColor(.white)
-                Text(FunnyTimeConverter.timeSecsToFunnyText(listeningTime))
-                    .foregroundColor(.white)
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("logo_white")
+                        .padding(.bottom, 40)
+                    Spacer()
+                }
             }
-            .padding()
         }
+        .background(UIColor(hex: "#DB5E59").color)
+    }
+
+    func androidValuesToIOSTransform() -> CGAffineTransform {
+
+        let androidValues: [CGFloat] = [0.89, 0, 0.58, 1, 0, 0]
+        return CGAffineTransform(
+            a: androidValues[0], b: androidValues[1],
+            c: androidValues[2], d: androidValues[3],
+            tx: 0, ty: 0
+        )
     }
 }
 
