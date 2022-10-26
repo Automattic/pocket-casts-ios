eeb82adf4445ecd278d15c6e69c62751ebd0e5e0
Leandro Alonso <contato@leandroalonso.com>
2022-10-26 11:45:17 -0300

    Style categories number story

--- podcasts/End of Year/Stories/EndOfYearStoriesDataSource.swift
+++ podcasts/End of Year/Stories/EndOfYearStoriesDataSource.swift
@@ -17,7 +17,7 @@ class EndOfYearStoriesDataSource: StoriesDataSource {
         case .listeningTime:
             return ListeningTimeStory(listeningTime: data.listeningTime, podcasts: data.randomPodcasts)
         case .listenedCategories:
-            return ListenedCategoriesStory(listenedCategories: data.listenedCategories)
+            return ListenedCategoriesStory(listenedCategories: data.listenedCategories, podcasts: data.randomPodcasts)
         case .topFiveCategories:
             return TopListenedCategories(listenedCategories: data.listenedCategories)
         case .listenedNumbers:
--- podcasts/End of Year/Stories/ListenedCategoriesStory.swift
+++ podcasts/End of Year/Stories/ListenedCategoriesStory.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import PocketCastsServer
 import PocketCastsDataModel
 
 struct ListenedCategoriesStory: StoryView {
@@ -6,12 +7,67 @@ struct ListenedCategoriesStory: StoryView {
 
     let listenedCategories: [ListenedCategory]
 
+    let podcasts: [Podcast]
+
     var body: some View {
-        ZStack {
-            Color.purple
+        GeometryReader { geometry in
+            ZStack {
+                DynamicBackgroundView(podcast: podcasts[0])
+
+                VStack {
+                    ZStack {
+                        ImageView(ServerHelper.imageUrl(podcastUuid: podcasts[2].uuid, size: 280))
+                            .modifier(PodcastCover())
+                            .frame(width: 200, height: 200)
+                            .modifier(PodcastCoverPerspective())
+                            .padding(.leading, -60)
+                            .padding(.top, 140)
+
+                    ImageView(ServerHelper.imageUrl(podcastUuid: podcasts[1].uuid, size: 280))
+                        .modifier(PodcastCover())
+                        .frame(width: 200, height: 200)
+                        .modifier(PodcastCoverPerspective())
+                        .padding(.leading, -60)
+                        .padding(.top, 70)
+
+                        ImageView(ServerHelper.imageUrl(podcastUuid: podcasts[0].uuid, size: 280))
+                            .modifier(PodcastCover())
+                            .frame(width: 200, height: 200)
+                            .modifier(PodcastCoverPerspective())
+                            .padding(.leading, -60)
+                    }
+
+                    VStack {
+                        Text("You listened to \(listenedCategories.count) different categories this year")
+                            .foregroundColor(.white)
+                            .font(.system(size: 25, weight: .heavy))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .frame(maxHeight: geometry.size.height * 0.12)
+                            .minimumScaleFactor(0.01)
+                        Text("Let's take a look at some of your favourites...")
+                            .font(.system(size: 15, weight: .bold))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+                            .frame(maxHeight: geometry.size.height * 0.07)
+                            .minimumScaleFactor(0.01)
+                            .opacity(0.8)
+                    }
+                    .padding(.top, 25)
+                    .padding(.trailing, 40)
+                    .padding(.leading, 40)
+                }
+                .padding(.top, -30)
+            }
+
             VStack {
-                Text("You listened to \(listenedCategories.count) different categories this year")
-                Text("Let's take a look at some of your favourites..")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image("logo_white")
+                        .padding(.bottom, 40)
+                    Spacer()
+                }
             }
         }
     }
@@ -19,6 +75,6 @@ struct ListenedCategoriesStory: StoryView {
 
 struct ListenedCategoriesStory_Previews: PreviewProvider {
     static var previews: some View {
-        ListenedCategoriesStory(listenedCategories: [])
+        ListenedCategoriesStory(listenedCategories: [], podcasts: [Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast()])
     }
 }
