f0b4b79050f246381f92efc56f0ffac43170f77d
Leandro Alonso <contato@leandroalonso.com>
2022-10-26 18:30:44 -0300

    Style the list of top 5 podcasts

--- podcasts/End of Year/Stories/TopFivePodcastsStory.swift
+++ podcasts/End of Year/Stories/TopFivePodcastsStory.swift
@@ -16,40 +16,53 @@ struct TopFivePodcastsStory: StoryView {
     }
 
     var body: some View {
-        ZStack {
-            backgroundColor.ignoresSafeArea()
+        GeometryReader { geometry in
+            ZStack {
+                DynamicBackgroundView(podcast: podcasts[0])
 
-            VStack {
-                Text(L10n.eoyStoryTopPodcasts)
-                    .font(.system(size: 22, weight: .bold))
-                    .foregroundColor(tintColor)
-                    .padding(.bottom)
                 VStack {
-                    ForEach(0 ..< podcasts.count, id: \.self) { x in
-                        HStack(spacing: 16) {
-                            Text("\(x + 1).")
-                                .font(.system(size: 32, weight: .bold))
-                                .foregroundColor(tintColor)
-                            ImageView(ServerHelper.imageUrl(podcastUuid: podcasts[x].uuid, size: 280))
-                                .frame(width: 76, height: 76)
-                                .aspectRatio(1, contentMode: .fit)
-                                .cornerRadius(4)
-                                .shadow(radius: 2, x: 0, y: 1)
-                                .accessibilityHidden(true)
-                            VStack(alignment: .leading) {
-                                Text(podcasts[x].title ?? "")
-                                    .lineLimit(2)
-                                    .font(.system(size: 22, weight: .bold))
+                    Text(L10n.eoyStoryTopPodcasts)
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(maxHeight: geometry.size.height * 0.07)
+                        .minimumScaleFactor(0.01)
+                        .opacity(0.8)
+                        .foregroundColor(tintColor)
+                        .padding(.bottom)
+                    VStack() {
+                        ForEach(0 ..< podcasts.count, id: \.self) { x in
+                            HStack(spacing: 16) {
+                                Text("\(x + 1).")
+                                    .frame(width: 30)
+                                    .font(.system(size: 32, weight: .bold))
                                     .foregroundColor(tintColor)
-                                Text(podcasts[x].author ?? "").font(.system(size: 12, weight: .semibold))
-                                    .foregroundColor(tintColor)
+                                ImageView(ServerHelper.imageUrl(podcastUuid: podcasts[x].uuid, size: 280))
+                                    .frame(width: 76, height: 76)
+                                    .aspectRatio(1, contentMode: .fit)
+                                    .cornerRadius(4)
+                                    .shadow(radius: 2, x: 0, y: 1)
+                                    .accessibilityHidden(true)
+                                VStack(alignment: .leading) {
+                                    Text(podcasts[x].title ?? "")
+                                        .lineLimit(2)
+                                        .font(.system(size: 22, weight: .heavy))
+                                        .foregroundColor(.white)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                        .minimumScaleFactor(0.01)
+                                    Text(podcasts[x].author ?? "").font(.system(size: 12, weight: .semibold))
+                                        .lineLimit(1)
+                                        .font(.system(size: geometry.size.height * 0.07, weight: .bold))
+                                        .foregroundColor(.white)
+                                        .opacity(0.8)
+                                }
+                                Spacer()
                             }
-                            Spacer()
                         }
                     }
+                    .padding(.leading, 40)
+                    .padding(.trailing, 40)
                 }
-                .padding(.leading, 40)
-                .padding(.trailing, 40)
             }
         }
     }
@@ -57,6 +70,6 @@ struct TopFivePodcastsStory: StoryView {
 
 struct DummyStory_Previews: PreviewProvider {
     static var previews: some View {
-        TopFivePodcastsStory(podcasts: [Podcast.previewPodcast()])
+        TopFivePodcastsStory(podcasts: [Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast()])
     }
 }
