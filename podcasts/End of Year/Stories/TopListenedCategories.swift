3dc730720aa0c547029aa5bf26a317e3708a811b
Leandro Alonso <contato@leandroalonso.com>
2022-10-27 15:43:07 -0300

    Style top listened categories design

--- podcasts/End of Year/Stories/TopListenedCategories.swift
+++ podcasts/End of Year/Stories/TopListenedCategories.swift
@@ -21,7 +21,7 @@ struct TopListenedCategories: StoryView {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                DynamicBackgroundView(podcast: listenedCategories[0].mostListenedPodcast)
+                DynamicBackgroundView(podcast: podcastForBackground)
 
                 VStack {
                     Text(L10n.eoyStoryTopCategories)
@@ -34,13 +34,22 @@ struct TopListenedCategories: StoryView {
                         .padding(.bottom)
                         .padding(.top, geometry.size.height * 0.05)
 
-                    HStack(alignment: .bottom) {
+                    HStack(alignment: .bottom, spacing: 25) {
                         ForEach([1, 0, 2], id: \.self) {
                             pillar($0)
                         }
                     }
-                    .padding(.leading, 40)
-                    .padding(.trailing, 40)
+                }
+                .padding(.bottom, 100)
+
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Image("logo_white")
+                            .padding(.bottom, 40)
+                        Spacer()
+                    }
                 }
             }
         }
@@ -49,7 +58,8 @@ struct TopListenedCategories: StoryView {
     @ViewBuilder
     func pillar(_ index: Int) -> some View {
         if let listenedCategory = listenedCategories[safe: index] {
-            CategoryPillar(color: tintColor, textColor: darkTintColor, text: "\(index + 1)", title: listenedCategory.categoryTitle.localized, subtitle: listenedCategory.totalPlayedTime.localizedTimeDescription ?? "", height: CGFloat(200 - (index * 80)))
+            CategoryPillar(color: tintColor, textColor: darkTintColor, text: "\(index + 1)", title: listenedCategory.categoryTitle.localized, subtitle: listenedCategory.totalPlayedTime.localizedTimeDescription ?? "", height: CGFloat(200 - (index * 55)))
+                .padding(.bottom, index == 0 ? 70 : 0)
         } else {
             CategoryPillar(color: tintColor, textColor: darkTintColor, text: "", title: "", subtitle: "", height: 200)
                 .opacity(0)
@@ -67,44 +77,58 @@ struct CategoryPillar: View {
 
     var body: some View {
         VStack {
-            VStack {
-                Text(title)
-                    .multilineTextAlignment(.center)
-                    .font(.system(size: 14, weight: .heavy))
-                    .foregroundColor(.white)
-                    .frame(width: 90)
-                Text(subtitle)
-                    .font(.system(size: 12, weight: .bold))
-                    .foregroundColor(.white)
-                    .opacity(0.8)
-                    .frame(width: 90)
+            ZStack {
                 ZStack {
                     VStack {
-                        Image("square_perspective")
-                            .renderingMode(.template)
-                            .foregroundColor(color)
+                        Text(title)
+                            .multilineTextAlignment(.center)
+                            .font(.system(size: 14, weight: .heavy))
+                            .foregroundColor(.white)
+                            .frame(width: 90)
+                            .fixedSize()
+                        Text(subtitle)
+                            .font(.system(size: 12, weight: .bold))
+                            .foregroundColor(.white)
+                            .opacity(0.8)
+                            .frame(width: 90)
+                            .padding(.bottom)
+
+                        ZStack(alignment: .top) {
+                            ZStack {
+                                Image("square_perspective")
+                                    .resizable()
+                                    .renderingMode(.template)
+                                    .frame(width: 90, height: 52)
+                                    .foregroundColor(color)
+
+                                let values: [CGFloat] = [1, 0, 0.50, 1, 0, 0]
+                                VStack {
+                                    Text("\(text) ")
+                                        .multilineTextAlignment(.center)
+                                        .foregroundColor(textColor)
+                                        .padding(.leading, -8)
+                                }
+                                .transformEffect(CGAffineTransform(
+                                    a: values[0], b: values[1],
+                                    c: values[2], d: values[3],
+                                    tx: 0, ty: 0
+                                ))
+                                .rotationEffect(.init(degrees: -30))
+                            }
+                            .zIndex(1)
+
+                            Rectangle()
+                                .fill(LinearGradient(gradient: Gradient(colors: [color, .black.opacity(0)]), startPoint: .top, endPoint: .bottom))
+                                .frame(width: 90, height: height)
+                                .padding(.top, 26)
+
+
+                        }
                     }
 
-                    let values: [CGFloat] = [1, 0, 0.50, 1, 0, 0]
-                    VStack {
-                        Text(text)
-                            .foregroundColor(textColor)
-                            .padding(.leading, -8)
-                    }
-                    .transformEffect(CGAffineTransform(
-                        a: values[0], b: values[1],
-                        c: values[2], d: values[3],
-                        tx: 0, ty: 0
-                    ))
-                    .rotationEffect(.init(degrees: -30))
+                    Spacer()
                 }
             }
-            .zIndex(1)
-
-            Rectangle()
-                .fill(LinearGradient(gradient: Gradient(colors: [color, .black.opacity(0)]), startPoint: .top, endPoint: .bottom))
-                .padding(.top, -39)
-                .frame(height: height)
         }
         .fixedSize(horizontal: true, vertical: false)
     }
@@ -113,7 +137,11 @@ struct CategoryPillar: View {
 #if DEBUG
 struct TopListenedCategories_Previews: PreviewProvider {
     static var previews: some View {
-        TopListenedCategories(listenedCategories: [ListenedCategory(numberOfPodcasts: 5, categoryTitle: "Test category big title", mostListenedPodcast: Podcast.previewPodcast(), totalPlayedTime: 300)])
+        TopListenedCategories(listenedCategories: [
+            ListenedCategory(numberOfPodcasts: 5, categoryTitle: "Test category big title", mostListenedPodcast: Podcast.previewPodcast(), totalPlayedTime: 300),
+            ListenedCategory(numberOfPodcasts: 5, categoryTitle: "Small title", mostListenedPodcast: Podcast.previewPodcast(), totalPlayedTime: 300),
+            ListenedCategory(numberOfPodcasts: 5, categoryTitle: "Category", mostListenedPodcast: Podcast.previewPodcast(), totalPlayedTime: 300)
+        ])
     }
 }
 #endif
