291062fcf78220d117759b30cf17f6cee1c3b1a1
Leandro Alonso <contato@leandroalonso.com>
2022-10-27 13:41:02 -0300

    Display pillars

--- podcasts/End of Year/Stories/TopListenedCategories.swift
+++ podcasts/End of Year/Stories/TopListenedCategories.swift
@@ -6,48 +6,39 @@ struct TopListenedCategories: StoryView {
 
     let listenedCategories: [ListenedCategory]
 
+    private var podcastForBackground: Podcast {
+        listenedCategories.reversed()[0].mostListenedPodcast
+    }
+
+    var tintColor: Color {
+        ColorManager.darkThemeTintForPodcast(podcastForBackground).color
+    }
+
+    var darkTintColor: Color {
+        ColorManager.lightThemeTintForPodcast(podcastForBackground).color
+    }
+
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Color.purple
+                DynamicBackgroundView(podcast: listenedCategories[0].mostListenedPodcast)
 
                 VStack {
-                    Text("Your Top Categories")
-                        .font(.system(size: 22, weight: .bold))
+                    Text(L10n.eoyStoryTopCategories)
+                        .font(.system(size: 18, weight: .bold))
                         .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .frame(maxHeight: geometry.size.height * 0.07)
+                        .minimumScaleFactor(0.01)
+                        .opacity(0.8)
                         .padding(.bottom)
+                        .padding(.top, geometry.size.height * 0.05)
 
-                    VStack {
-                        VStack {
-                            ZStack {
-                                VStack {
-                                    Image("square_perspective")
-                                        .renderingMode(.template)
-                                        .foregroundColor(.red)
-                                }
-
-                                let values: [CGFloat] = [1, 0, 0.50, 1, 0, 0]
-                                VStack {
-                                    Text("8")
-                                        .foregroundColor(.white)
-                                        .padding(.leading, -8)
-                                }
-                                .transformEffect(CGAffineTransform(
-                                    a: values[0], b: values[1],
-                                    c: values[2], d: values[3],
-                                    tx: 0, ty: 0
-                                ))
-                                .rotationEffect(.init(degrees: -30))
-                            }
-                        }
-                        .zIndex(1)
-
-                        Rectangle()
-                            .fill(LinearGradient(gradient: Gradient(colors: [.red, .black.opacity(0)]), startPoint: .top, endPoint: .bottom))
-                            .padding(.top, -39)
-                            .frame(height: 200)
+                    HStack {
+                        CategoryPillar(color: tintColor, textColor: darkTintColor, text: "1", title: "Title", subtitle: "Subtitle")
+                        CategoryPillar(color: tintColor, textColor: darkTintColor, text: "1", title: "Title", subtitle: "Subtitle")
+                        CategoryPillar(color: tintColor, textColor: darkTintColor, text: "1", title: "Title", subtitle: "Subtitle")
                     }
-                    .fixedSize(horizontal: true, vertical: false)
 
                     VStack {
                         ForEach(0 ..< min(listenedCategories.count, 5), id: \.self) { x in
@@ -83,8 +74,65 @@ struct TopListenedCategories: StoryView {
     }
 }
 
-struct TopListenedCategories_Previews: PreviewProvider {
-    static var previews: some View {
-        TopListenedCategories(listenedCategories: [])
+struct CategoryPillar: View {
+    let color: Color
+
+    let textColor: Color
+
+    let text: String
+
+    let title: String
+
+    let subtitle: String
+
+    var body: some View {
+        VStack {
+            VStack {
+                Text(title)
+                    .lineLimit(1)
+                    .font(.system(size: 14, weight: .heavy))
+                    .foregroundColor(.white)
+                Text(subtitle)
+                    .lineLimit(1)
+                    .font(.system(size: 12, weight: .bold))
+                    .foregroundColor(.white)
+                    .opacity(0.8)
+                ZStack {
+                    VStack {
+                        Image("square_perspective")
+                            .renderingMode(.template)
+                            .foregroundColor(color)
+                    }
+
+                    let values: [CGFloat] = [1, 0, 0.50, 1, 0, 0]
+                    VStack {
+                        Text(text)
+                            .foregroundColor(textColor)
+                            .padding(.leading, -8)
+                    }
+                    .transformEffect(CGAffineTransform(
+                        a: values[0], b: values[1],
+                        c: values[2], d: values[3],
+                        tx: 0, ty: 0
+                    ))
+                    .rotationEffect(.init(degrees: -30))
+                }
+            }
+            .zIndex(1)
+
+            Rectangle()
+                .fill(LinearGradient(gradient: Gradient(colors: [color, .black.opacity(0)]), startPoint: .top, endPoint: .bottom))
+                .padding(.top, -39)
+                .frame(height: 200)
+        }
+        .fixedSize(horizontal: true, vertical: false)
     }
 }
+
+#if DEBUG
+struct TopListenedCategories_Previews: PreviewProvider {
+    static var previews: some View {
+        TopListenedCategories(listenedCategories: [ListenedCategory(numberOfPodcasts: 5, categoryTitle: "Test", mostListenedPodcast: Podcast.previewPodcast(), totalPlayedTime: 300)])
+    }
+}
+#endif
