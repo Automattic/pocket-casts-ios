7b8a377285009a16e8982ad7d2ec7d22170b9b70
Adin Cebic <cebic.ad@gmail.com>
2022-10-29 10:33:57 +0200

    Remove cellSwitch and add it as accessory view

--- podcasts/SwitchCell.swift
+++ podcasts/SwitchCell.swift
@@ -1,14 +1,12 @@
 import UIKit
 
 class SwitchCell: ThemeableCell {
-    @IBOutlet var cellSwitch: ThemeableSwitch!
+    let cellSwitch: ThemeableSwitch = {
+        let cellSwitch = ThemeableSwitch()
+        cellSwitch.isAccessibilityElement = false
+        return cellSwitch
+    }()
     @IBOutlet var cellLabel: UILabel!
-    @IBOutlet var cellSecondaryLabel: ThemeableLabel! {
-        didSet {
-            cellSecondaryLabel.style = .primaryText02
-        }
-    }
-
     @IBOutlet var cellImage: UIImageView!
     @IBOutlet var cellTextToImageConstraint: NSLayoutConstraint!
 
@@ -23,7 +21,7 @@ class SwitchCell: ThemeableCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-
+        accessoryView = cellSwitch
         setNoImage()
     }
 
@@ -47,28 +45,7 @@ class SwitchCell: ThemeableCell {
     override func setSelected(_ selected: Bool, animated: Bool) {}
     override func setHighlighted(_ highlighted: Bool, animated: Bool) {}
 
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        setupAccessibility()
-    }
-
     override func accessibilityActivate() -> Bool {
-        cellSwitch.setOn(!cellSwitch.isOn, animated: true)
-        cellSwitch.sendActions(for: .valueChanged)
-        matchAccessibilityValueWithSwitchControl()
-        return true
-    }
-
-    private func setupAccessibility() {
-        isAccessibilityElement = true
-        let labelText = cellLabel.text ?? ""
-        let secondaryLabelText = cellSecondaryLabel.text ?? ""
-        accessibilityLabel = "\(labelText), \(secondaryLabelText)"
-        accessibilityTraits = cellSwitch.accessibilityTraits
-        matchAccessibilityValueWithSwitchControl()
-    }
-
-    private func matchAccessibilityValueWithSwitchControl() {
-        accessibilityValue = cellSwitch.accessibilityValue
+        return isLocked
     }
 }
