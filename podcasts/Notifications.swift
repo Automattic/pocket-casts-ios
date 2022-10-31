8e7d63652a6bba193d88b50a84114fca60100052
Leandro Alonso <contato@leandroalonso.com>
2022-10-31 18:05:09 -0300

    Add a notification for when registration is not required for EOY

--- podcasts/Notifications.swift
+++ podcasts/Notifications.swift
@@ -3,4 +3,7 @@ import Foundation
 extension NSNotification.Name {
     /// When a user has signed in, signed out, or been signed in during account creation
     static let userLoginDidChange = NSNotification.Name("User.LoginChanged")
+
+    /// When the requirement for having an account or not to see End Of Year Stats changes
+    static let eoyRegistrationNotRequired = NSNotification.Name("EOY.RegistrationNotRequired")
 }
