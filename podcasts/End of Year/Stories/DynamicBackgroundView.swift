d15c37ebd6a41f222778e81e0b48933db931f567
Leandro Alonso <contato@leandroalonso.com>
2022-10-31 09:31:12 -0300

    Change DynamicBackground to be able to download the colors and update itself

--- podcasts/End of Year/Stories/DynamicBackgroundView.swift
+++ podcasts/End of Year/Stories/DynamicBackgroundView.swift
@@ -2,23 +2,19 @@ import SwiftUI
 import PocketCastsDataModel
 
 struct DynamicBackgroundView: View {
-    let backgroundColor: Color
-
-    let foregroundColor: Color
+    @ObservedObject private var model: DynamicBackgroundProvider
 
     init(podcast: Podcast) {
-        backgroundColor = podcast.bgColor().color
-        foregroundColor = ColorManager.lightThemeTintForPodcast(podcast).color
+        model = DynamicBackgroundProvider(podcast: podcast)
     }
 
     init(backgroundColor: Color, foregroundColor: Color) {
-        self.backgroundColor = backgroundColor
-        self.foregroundColor = foregroundColor
+        model = DynamicBackgroundProvider(backgroundColor: backgroundColor, foregroundColor: foregroundColor)
     }
 
     var body: some View {
         ZStack {
-            backgroundColor
+            model.backgroundColor
 
             Color.black.opacity(0.2)
 
@@ -34,9 +30,43 @@ struct DynamicBackgroundView: View {
                 Image("bottom_blob")
                     .resizable()
                     .renderingMode(.template)
-                    .foregroundColor(foregroundColor)
+                    .foregroundColor(model.foregroundColor)
             }
 
+        }
+    }
+}
+
+@MainActor
+class DynamicBackgroundProvider: ObservableObject {
+    @Published var backgroundColor: Color
+    @Published var foregroundColor: Color
+
+    private let podcastUuid: String?
+
+    init(podcast: Podcast) {
+        backgroundColor = podcast.bgColor().color
+        foregroundColor = ColorManager.lightThemeTintForPodcast(podcast).color
+        podcastUuid = podcast.uuid
+
+        if !ColorManager.podcastHasBackgroundColor(podcast) {
+            _ = ColorManager.backgroundColorForPodcast(podcast)
+            NotificationCenter.default.addObserver(self, selector: #selector(podcastColorsLoaded(_:)), name: Constants.Notifications.podcastColorsDownloaded, object: nil)
+        }
+    }
+
+    init(backgroundColor: Color, foregroundColor: Color) {
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+        self.podcastUuid = nil
+    }
+
+    @objc private func podcastColorsLoaded(_ notification: Notification) {
+        guard let uuidLoaded = notification.object as? String else { return }
+
+        if uuidLoaded == podcastUuid, let podcast = DataManager.sharedManager.findPodcast(uuid: uuidLoaded, includeUnsubscribed: true) {
+            backgroundColor = podcast.bgColor().color
+            foregroundColor = ColorManager.lightThemeTintForPodcast(podcast).color
         }
     }
 }
