99cd43c8472d2064547cf36f1afdf992ba6f9f35
Leandro Alonso <contato@leandroalonso.com>
2022-11-02 12:41:21 -0300

    Add Pocket Casts URL

--- podcasts/End of Year/StoryShareableText.swift
+++ podcasts/End of Year/StoryShareableText.swift
@@ -5,8 +5,10 @@ class StoryShareableText: UIActivityItemProvider {
 
     private let hashtags = "#pocketcasts #endofyear2022"
 
+    private let pocketCastsUrl = "https://pca.st/"
+
     init(_ text: String) {
-        self.text = "\(text) \(hashtags)"
+        self.text = "\(text) \(hashtags) \(pocketCastsUrl)"
         super.init(placeholderItem: self.text)
     }
 
