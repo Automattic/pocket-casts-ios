d3aca3941d4ce16c616fec1546d58973c2e9df09
Leandro Alonso <contato@leandroalonso.com>
2022-11-02 14:04:43 -0300

    Remove tracks based on enum

--- podcasts/End of Year/Analytics+story.swift
+++ podcasts/End of Year/Analytics+story.swift
@@ -1,10 +1,6 @@
 import Foundation
 
 extension Analytics {
-    static func track(_ event: AnalyticsEvent, story: EndOfYearStory) {
-        Analytics.track(event, properties: ["story": story.rawValue])
-    }
-
     static func track(_ event: AnalyticsEvent, story: String) {
         Analytics.track(event, properties: ["story": story])
     }
