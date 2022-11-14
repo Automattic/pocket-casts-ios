617d43ac0ec6d4e186afe5d9d9eac77b09b97fdf
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2022-11-14 11:03:31 -0500

    Remove ObservableObject

--- podcasts/Onboarding/Plus/PlusCoordinator.swift
+++ podcasts/Onboarding/Plus/PlusCoordinator.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SwiftUI
 
-class PlusCoordinator: ObservableObject {
+class PlusCoordinator {
     var navigationController: UINavigationController? = nil
 
     func unlockTapped() {
