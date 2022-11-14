a4df5db0d2b55016b97f016e466322321fc7ea0a
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2022-11-14 15:39:14 -0500

    Clean up the welcome view code

--- podcasts/Onboarding/Welcome/WelcomeView.swift
+++ podcasts/Onboarding/Welcome/WelcomeView.swift
@@ -8,7 +8,7 @@ struct WelcomeView: View {
         case newAccount
     }
 
-    let displayState: DisplayState = .plus
+    let displayState: DisplayState = .newAccount
 
     var titleText: String {
         switch displayState {
@@ -19,133 +19,62 @@ struct WelcomeView: View {
         }
     }
 
-    var iconName: String {
-        switch displayState {
-        case .plus:
-            return "welcome-icon"
-        case .newAccount:
-            return "welcome-icon"
-        }
-    }
-
-    var iconColor: Color {
-        switch displayState {
-        case .plus:
-            return Color.plusGradientColor1
-        case .newAccount:
-            return AppTheme.color(for: .primaryIcon01, theme: theme)
-        }
-    }
     var body: some View {
         ZStack(alignment: .top) {
-            GeometryReader { proxy in
-                switch displayState {
-                case .plus:
-                    PlusIconConfetti(frame: proxy.frame(in: .global))
-
-                case .newAccount:
-                    WelcomeConfetti(frame: proxy.frame(in: .global))
-
-                }
-            }
-            .allowsHitTesting(false)
-            .accessibilityHidden(true)
-            .ignoresSafeArea().zIndex(100)
+            WelcomeConfetti(type: (displayState == .plus) ? .plus : .normal)
 
             ScrollViewIfNeeded {
                 VStack(alignment: .leading) {
-                    HStack {
-                        ZStack {
-                            Image(iconName)
-                                .foregroundColor(iconColor)
-                                .gradientOverlay(displayState == .plus ? Color.plusGradient : nil)
-                            Image("welcome-icon-check")
+                    HeaderIcon(isPlus: displayState == .plus)
+
+                    Label(titleText, for: .title)
+
+                    ForEach(sections) { section in
+                        WelcomeSectionView(model: section) {
+                            print("Tappy")
                         }
-                        Spacer()
                     }
-
-                    Text(titleText)
-                        .foregroundColor(AppTheme.color(for: .primaryText01, theme: theme))
-                        .font(size: 31, style: .title, weight: .bold)
-
-                    VStack(alignment: .leading) {
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack {
-                                VStack(alignment: .leading, spacing: 4) {
-                                    Text("Import your podcasts")
-                                        .foregroundColor(AppTheme.color(for: .primaryText01, theme: theme))
-                                        .font(size: 18, style: .body, weight: .semibold)
-                                    Text("Coming from another app? Bring your podcasts with you.")
-                                        .foregroundColor(AppTheme.color(for: .primaryText02, theme: theme))
-                                        .font(size: 13, style: .footnote)
-                                }
-                                Spacer()
-                                Image("welcome-import")
-                                    .foregroundColor(AppTheme.color(for: .primaryIcon01, theme: theme))
-
-                            }.padding(24)
-
-                            Divider().background(AppTheme.color(for: .primaryUi05, theme: theme))
-
-                            Button("Import Podcasts") {
-
-                            }
-                            .foregroundColor(AppTheme.color(for: .primaryInteractive01, theme: theme))
-                            .font(size: 15, style: .callout, weight: .medium)
-                            .padding([.top, .bottom], 16)
-                            .padding([.leading, .trailing], 24)
-                        }
-
-                    }.overlay(
-                        RoundedRectangle(cornerRadius: 12).stroke(AppTheme.color(for: .primaryUi05, theme: theme), lineWidth: 1)
-                    )
-
-                    VStack(alignment: .leading) {
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack {
-                                VStack(alignment: .leading, spacing: 4) {
-                                    Text("Discover something new")
-                                        .foregroundColor(AppTheme.color(for: .primaryText01, theme: theme))
-                                        .font(size: 18, style: .body, weight: .semibold)
-                                    Text("Find under-the-radar and trending podcasts in our hand-curated Discover page.")
-                                        .foregroundColor(AppTheme.color(for: .primaryText02, theme: theme))
-                                        .font(size: 13, style: .footnote)
-                                }
-                                Spacer()
-                                Image("welcome-discover")
-                                    .foregroundColor(AppTheme.color(for: .primaryIcon01, theme: theme))
-                            }.padding(24)
-
-                            Divider().background(AppTheme.color(for: .primaryUi05, theme: theme))
-
-                            Button("Find My Next Podcast") {
-
-                            }
-                            .foregroundColor(AppTheme.color(for: .primaryInteractive01, theme: theme))
-                            .font(size: 15, style: .callout, weight: .medium)
-                            .padding([.top, .bottom], 16)
-                            .padding([.leading, .trailing], 24)
-                        }
-
-                    }.overlay(
-                        RoundedRectangle(cornerRadius: 12).stroke(AppTheme.color(for: .primaryUi05, theme: theme), lineWidth: 1)
-                    ).padding(.top, 16)
 
                     Spacer()
 
                     Button("Done") {
 
-                    }.buttonStyle(RoundedButtonStyle())
+                    }.buttonStyle(RoundedButtonStyle(theme: theme))
                 }
+                .padding(.top, Config.padding.top)
+                .padding([.leading, .trailing], Config.padding.horizontal)
+                .padding(.bottom)
             }
-            .padding(.top, 70)
-            .padding([.leading, .trailing], 24)
-            .padding(.bottom)
-            .background(AppTheme.color(for: .primaryUi01, theme: theme).ignoresSafeArea())
+            .background(AppTheme.color(for: .background, theme: theme).ignoresSafeArea())
         }
     }
+
+    private let sections: [WelcomeSection] = [
+        WelcomeSection(title: "Import your podcasts", subtitle: "Coming from another app? Bring your podcasts with you.", imageName: "welcome-import", buttonTitle: "Import Podcasts"),
+        WelcomeSection(title: "Discover something new", subtitle: "Find under-the-radar and trending podcasts in our hand-curated Discover page.", imageName: "welcome-discover", buttonTitle: "Find My Next Podcast")
+    ]
 }
 
+private extension ThemeStyle {
+    static let background = Self.primaryUi01
+    static let text = Self.primaryText01
+    static let sectionDescription = Self.primaryText02
+    static let icon = Self.primaryIcon01
+    static let sectionStroke = Self.primaryUi05
+    static let sectionButtonTitle = Self.primaryInteractive01
+}
+
+private enum Config {
+    enum padding {
+        static let top = 40.0
+        static let horizontal = 24.0
+        static let sectionButtonVertical = 16.0
+    }
+
+    static let sectionCornerRadius = 12.0
+}
+
+// MARK: - Preview
 struct WelcomeView_Previews: PreviewProvider {
     static var previews: some View {
         WelcomeView()
@@ -153,16 +82,179 @@ struct WelcomeView_Previews: PreviewProvider {
     }
 }
 
+// MARK: - View Components
+private struct WelcomeConfetti: View {
+    let type: WelcomeConfettiEmitter.ConfettiType
+
+    var body: some View {
+        GeometryReader { proxy in
+            WelcomeConfettiEmitter(type: type,
+                             frame: proxy.frame(in: .local)).ignoresSafeArea()
+        }
+        .allowsHitTesting(false)
+        .accessibilityHidden(true)
+        .ignoresSafeArea().zIndex(1000)
+    }
+}
+
+/// A view that displays the header icon
+/// We display this using 2 images to allow us to apply the color overlay to the
+/// icon without effecting the check mark
+private struct HeaderIcon: View {
+    @EnvironmentObject var theme: Theme
+    let isPlus: Bool
+
+    var body: some View {
+        HStack {
+            ZStack {
+                Image("welcome-icon")
+                    .foregroundColor(isPlus ? Color.plusGradientColor1 : AppTheme.color(for: .icon, theme: theme))
+                    .gradientOverlay(isPlus ? Color.plusGradient : nil)
+                Image("welcome-icon-check")
+            }
+            Spacer()
+        }
+    }
+}
+
+private struct Label: View {
+    @EnvironmentObject var theme: Theme
+
+    enum LabelStyle {
+        case title
+        case sectionTitle
+        case sectionDescription
+    }
+
+    let text: String
+    let labelStyle: LabelStyle
+
+    init(_ text: String, for style: LabelStyle) {
+        self.text = text
+        self.labelStyle = style
+    }
+
+    var body: some View {
+        Text(text)
+            .foregroundColor(textColor)
+            .fixedSize(horizontal: false, vertical: true)
+            .modifier(LabelFont(labelStyle: labelStyle))
+    }
+
+    private var textColor: Color {
+        switch labelStyle {
+
+        case .title:
+            return AppTheme.color(for: .text, theme: theme)
+        case .sectionTitle:
+            return AppTheme.color(for: .text, theme: theme)
+        case .sectionDescription:
+            return AppTheme.color(for: .sectionDescription, theme: theme)
+        }
+    }
+
+    private struct LabelFont: ViewModifier {
+        let labelStyle: LabelStyle
+
+        func body(content: Content) -> some View {
+            switch labelStyle {
+            case .title:
+                return content.font(size: 31, style: .title, weight: .bold)
+            case .sectionTitle:
+                return content.font(size: 18, style: .body, weight: .semibold)
+            case .sectionDescription:
+                return content.font(size: 13, style: .caption, maxSizeCategory: .extraExtraLarge)
+            }
+        }
+    }
+}
+
+private struct WelcomeSection: Identifiable {
+    let title: String
+    let subtitle: String
+    let imageName: String
+    let buttonTitle: String
+
+    var id: String { title }
+}
+
+private struct WelcomeSectionView: View {
+    @EnvironmentObject var theme: Theme
+
+    let model: WelcomeSection
+    let action: () -> Void
+
+    var body: some View {
+        // The container view that displays the stroke
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0) {
+
+                // The labels + icon view
+                HStack {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Label(model.title, for: .sectionTitle)
+                        Label(model.subtitle, for: .sectionDescription)
+                    }
+                    Spacer()
+                    Image(model.imageName)
+                        .foregroundColor(AppTheme.color(for: .icon, theme: theme))
+                }.padding(Config.padding.horizontal)
+
+                divider
+                button
+            }
+        }.overlay(roundedCorners)
+    }
+
+    private var roundedCorners: some View {
+        RoundedRectangle(cornerRadius: Config.sectionCornerRadius)
+            .stroke(AppTheme.color(for: .sectionStroke, theme: theme), lineWidth: 1)
+    }
+
+    private var divider: some View {
+        Divider().background(
+            AppTheme.color(for: .sectionStroke, theme: theme)
+        )
+    }
+
+    private var button: some View {
+            Button(model.buttonTitle) {
+                action()
+            }
+            .foregroundColor(AppTheme.color(for: .sectionButtonTitle, theme: theme))
+            .font(size: 15, style: .callout, weight: .medium)
+            .padding([.top, .bottom], Config.padding.sectionButtonVertical)
+            .padding([.leading, .trailing], Config.padding.horizontal)
+    }
+}
 // MARK: - Confetti 🎉
 
-struct PlusIconConfetti: UIViewRepresentable {
+private struct WelcomeConfettiEmitter: UIViewRepresentable {
+    let type: ConfettiType
     let frame: CGRect
+    let afterDelay: TimeInterval
+
+    enum ConfettiType {
+        case normal
+        case plus
+    }
+
+    init(type: ConfettiType, frame: CGRect, afterDelay: TimeInterval = 0.5) {
+        self.type = type
+        self.frame = frame
+        self.afterDelay = afterDelay
+    }
 
     func makeUIView(context: Context) -> UIView {
         let hostView = UIView(frame: frame)
 
-        PlusConfettiView.cleanupAndAnimate(on: hostView, frame: frame) { confettiView in
-            confettiView.removeFromSuperview()
+        DispatchQueue.main.asyncAfter(deadline: .now() + afterDelay) {
+            let completion: ConfettiView.AnimationCompletion = { confettiView in
+                confettiView.removeFromSuperview()
+            }
+
+            let confetti = (type == .plus) ? PlusConfettiView.self : NormalConfettiView.self
+            confetti.cleanupAndAnimate(on: hostView, frame: frame, onAnimationCompletion: completion)
         }
 
         return hostView
@@ -184,26 +276,10 @@ struct PlusIconConfetti: UIViewRepresentable {
 
             var config = PlusConfettiView.EmitterConfig()
             config.scaleRange = 1.2
-    //        config.spinRange = 0
 
             self.emit(with: particles, config: config)
         }
     }
-}
-
-struct WelcomeConfetti: UIViewRepresentable {
-    let frame: CGRect
-
-    func makeUIView(context: Context) -> UIView {
-        let hostView = UIView(frame: frame)
-        NormalConfettiView.cleanupAndAnimate(on: hostView, frame: frame) { confettiView in
-            confettiView.removeFromSuperview()
-        }
-
-        return hostView
-    }
-
-    func updateUIView(_ uiView: UIView, context: Context) { }
 
     private class NormalConfettiView: ConfettiView {
         override func emitConfetti() {
