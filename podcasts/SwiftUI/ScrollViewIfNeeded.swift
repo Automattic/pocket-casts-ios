a22dae374e1f5ce546e6194b20eb8699c029c414
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2022-11-16 18:38:17 -0500

    Update the scroll view if needed to allow it to be recalculated

--- podcasts/SwiftUI/ScrollViewIfNeeded.swift
+++ podcasts/SwiftUI/ScrollViewIfNeeded.swift
@@ -10,17 +10,15 @@ public struct ScrollViewIfNeeded<Content: View>: View {
     }
 
     @State private var willOverflow: Bool? = nil
-
+    
     public var body: some View {
         // If the content size if going to overflow outside of the view bounds, then wrap the content in a scrollview
         if willOverflow == true {
-            ScrollView { content() }
+            ScrollView {
+                content()
+            }
         }
         // If we don't need to wrap in a scroll view, then just return the original content
-        else if willOverflow == false {
-            content()
-        }
-        // If the content size hasn't been calculated yet, then do that..
         else {
             // Create an initial geometry reader to compare against
             GeometryReader { geometry in
