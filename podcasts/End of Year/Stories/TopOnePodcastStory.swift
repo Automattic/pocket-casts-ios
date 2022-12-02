8320f2c900e435725bf1a2dd18bda60fc5ffe029
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2022-12-02 10:18:01 -0500

    Update the top podcast story to use the new common views

--- podcasts/End of Year/Stories/TopOnePodcastStory.swift
+++ podcasts/End of Year/Stories/TopOnePodcastStory.swift
@@ -18,47 +18,21 @@ struct TopOnePodcastStory: ShareableStory {
     }
 
     var body: some View {
+        let podcast = topPodcast.podcast
         GeometryReader { geometry in
-            ZStack {
-                DynamicBackgroundView(podcast: topPodcast.podcast)
+            VStack {
+                PodcastStackView(podcasts: [podcast], geometry: geometry)
 
-                VStack {
-                    VStack {
-                        ZStack {
-                            let size = geometry.size.width * 0.60
-                            Rectangle().frame(width: size, height: size)
-                                .foregroundColor(ColorManager.darkThemeTintForPodcast(topPodcast.podcast).color)
-                                .modifier(BigCoverShadow())
-                                .modifier(PodcastCoverPerspective())
-                                .padding(.top, (size * 0.6))
-
-                            Rectangle().frame(width: size, height: size)
-                                .foregroundColor(ColorManager.lightThemeTintForPodcast(topPodcast.podcast).color)
-                                .modifier(BigCoverShadow())
-                                .modifier(PodcastCoverPerspective())
-                                .padding(.top, (size * 0.30))
-
-                            PodcastCover(podcastUuid: topPodcast.podcast.uuid, big: true)
-                                .frame(width: size, height: size)
-                                .modifier(PodcastCoverPerspective())
-                        }
-
-                        let title = topPodcast.podcast.title ?? ""
-                        let author = topPodcast.podcast.author ?? ""
-                        StoryLabel(L10n.eoyStoryTopPodcast("\n" + title, author), highlighting: [title, author], for: .title)
-                            .frame(maxHeight: geometry.size.height * 0.12)
-                            .minimumScaleFactor(0.01)
-                        StoryLabel(L10n.eoyStoryTopPodcastSubtitle(topPodcast.numberOfPlayedEpisodes, topPodcast.totalPlayedTime.storyTimeDescription), for: .subtitle)
-                            .frame(maxHeight: geometry.size.height * 0.07)
-                            .minimumScaleFactor(0.01)
-                            .opacity(0.8)
-                    }
-                    .padding(.leading, 40)
-                    .padding(.trailing, 40)
+                StoryLabelContainer {
+                    let title = podcast.title ?? ""
+                    let author = podcast.author ?? ""
+                    StoryLabel(L10n.eoyStoryTopPodcast("\n" + title, author), highlighting: [title, author], for: .title)
+                    StoryLabel(L10n.eoyStoryTopPodcastSubtitle(topPodcast.numberOfPlayedEpisodes, topPodcast.totalPlayedTime.storyTimeDescription), for: .subtitle)
+                        .opacity(0.8)
                 }
-            }
-            .padding(.top, -(geometry.size.height * 0.15))
-        }
+                Spacer()
+            }.frame(width: geometry.size.width).padding(.top, (geometry.size.height * 0.10))
+        }.background(DynamicBackgroundView(podcast: podcast))
     }
 
     func onAppear() {
