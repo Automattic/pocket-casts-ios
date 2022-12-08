f687d5ec252f936a546a92b3694fdb0e9ca57335
Leandro Alonso <contato@leandroalonso.com>
2022-12-08 09:47:50 -0300

    Add beta features menu and screen

---
+++ podcasts/Beta/BetaMenu.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct BetaMenu: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct BetaMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        BetaMenu()
+    }
+}
