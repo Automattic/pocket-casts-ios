bace4f0e5bafc13a488cf69c353bf716bffb6969
Leandro Alonso <contato@leandroalonso.com>
2023-01-18 13:50:08 -0300

    getToken should be async and throws

--- podcasts/Login/SocialLogin.swift
+++ podcasts/Login/SocialLogin.swift
@@ -5,7 +5,7 @@ protocol SocialLogin {
     init(viewController: UIViewController)
 
     /// This is the step in which an identification token is retrieved from the provider
-    func getToken()
+    func getToken() async throws
 
     /// Once the identification token is retrieved, this step is responsible to sending this to
     /// the server so the user identity may be verified
