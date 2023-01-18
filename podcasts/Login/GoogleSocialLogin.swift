856e9c0a5dfa7ce0cb0a284237474933d965038a
Leandro Alonso <contato@leandroalonso.com>
2023-01-18 13:53:36 -0300

    Make Google social login to conform to SociaLogin

--- podcasts/Login/GoogleSocialLogin.swift
+++ podcasts/Login/GoogleSocialLogin.swift
@@ -6,11 +6,17 @@ enum GoogleSocialLoginError: Error {
     case emptyIdToken
 }
 
-class GoogleSocialLogin {
+class GoogleSocialLogin: SocialLogin {
+    private weak var viewController: UIViewController?
+
     private var idToken = ""
 
-    func getToken(from viewController: UIViewController) async throws {
-        idToken = try await idToken(from: viewController)
+    required init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+
+    func getToken() async throws {
+        idToken = try await idToken(from: viewController!)
     }
 
     func login() async throws -> AuthenticationResponse {
