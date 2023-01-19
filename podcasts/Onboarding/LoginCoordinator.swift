b80f2a00105c23f619ba665effd8610c074636cd
Leandro Alonso <contato@leandroalonso.com>
2023-01-19 16:21:36 -0300

    In case the flow is canceled, an alert showing a message won't appear

--- podcasts/Onboarding/LoginCoordinator.swift
+++ podcasts/Onboarding/LoginCoordinator.swift
@@ -171,7 +171,10 @@ extension LoginCoordinator: SyncSigninDelegate, CreateAccountDelegate {
     }
 
     func showError(_ error: Error) {
-        navigationController?.presentedViewController?.dismiss(animated: true)
+        guard (error as? SocialLoginError) != .canceled else {
+            return
+        }
+
         SJUIUtils.showAlert(title: L10n.accountSsoFailed, message: nil, from: navigationController)
     }
 
