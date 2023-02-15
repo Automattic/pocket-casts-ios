741259ecca3f1c507ae23cc75deab775ef790fb1
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-02-14 21:51:58 -0500

    Add a clip length property

--- podcasts/BookmarkManager.swift
+++ podcasts/BookmarkManager.swift
@@ -4,4 +4,7 @@ import PocketCastsUtils
 
 struct BookmarkManager {
     private let dataManager = DataManager.sharedManager.bookmarks
+    /// How long a bookmark clip is
+    /// TODO: Make configurable
+    private let clipLength = 1.minute
     func bookmarks(for podcast: Podcast) -> [Bookmark] {
