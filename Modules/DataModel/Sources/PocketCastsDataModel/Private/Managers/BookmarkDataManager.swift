6cfcbefebcb58f018a752022bc123676467aa276
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-02-14 20:59:31 -0500

    Add the Bookmark columns constants

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/BookmarkDataManager.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/BookmarkDataManager.swift
@@ -8,4 +8,15 @@ public struct BookmarkDataManager {
     init(dbQueue: FMDatabaseQueue) {
         self.dbQueue = dbQueue
     }
+// MARK: - DB Column Constants
+
+private extension String {
+    static let uuid = "uuid"
+    static let createdDate = "date_added"
+    static let timestampStart = "timestamp_start"
+    static let timestampEnd = "timestamp_end"
+    static let episode = "episode_uuid"
+    static let podcast = "podcast_uuid"
+    static let transcription = "transcription"
+}
 }
