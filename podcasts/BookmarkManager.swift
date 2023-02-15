1a2c331e79680b069800a6aca4728b2605c3bde9
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-02-14 21:52:51 -0500

    Add a method to add a bookmark to an episode

--- podcasts/BookmarkManager.swift
+++ podcasts/BookmarkManager.swift
@@ -7,4 +7,19 @@ struct BookmarkManager {
     /// How long a bookmark clip is
     /// TODO: Make configurable
     private let clipLength = 1.minute
+
+    /// Adds a new bookmark for an episode at the given time
+    func add(to episode: BaseEpisode, at time: TimeInterval) {
+        // If the episode has a podcast attached, also save that
+        let podcastUuid: String? = (episode as? Episode)?.podcastUuid
+
+        // If someone is bookmarking a point in time, they probably want to remember the info leading up to the bookmark
+        // time, so calculate the start of the clip as X seconds before the bookmark time
+        let startTime = max(0, time - clipLength)
+
+        dataManager.add(episodeUuid: episode.uuid, podcastUuid: podcastUuid, start: startTime, end: time)
+
+
+        FileLog.shared.addMessage("[Bookmarks] Added bookmark for \(episode.displayableTitle()) from \(startTime) to \(time)")
+    }
     func bookmarks(for podcast: Podcast) -> [Bookmark] {
