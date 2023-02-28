25bb7e05f148d3a701acecbad4592b38805c550b
Leandro Alonso <contato@leandroalonso.com>
2023-02-28 11:02:09 -0300

    Avoid declaring the whole padding axes
    Co-authored-by: Emily Laguna <793774+emilylaguna@users.noreply.github.com>

--- podcasts/New Search/Views/Results/PodcastCarouselView.swift
+++ podcasts/New Search/Views/Results/PodcastCarouselView.swift
@@ -24,7 +24,7 @@ struct PodcastsCarouselView: View {
                 .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
             }
             ThemeableSeparatorView()
-                .padding(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 0))
+                .padding(.leading, 16)
         }
         .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
         .listRowSeparator(.hidden)
