220c3363f9484c94df7a61404b33a06615815b3d
Leandro Alonso <contato@leandroalonso.com>
2023-03-20 17:16:15 -0700

    Move MiniPlayerPadding to Styles

--- podcasts/New Search/Views/SearchView.swift
+++ podcasts/New Search/Views/SearchView.swift
@@ -32,22 +32,3 @@ struct SearchView_Previews: PreviewProvider {
         SearchView()
     }
 }
-
-/// Apply a bottom padding whenever the mini player is visible
-struct MiniPlayerPadding: ViewModifier {
-    @State var isMiniPlayerVisible: Bool = false
-
-    func body(content: Content) -> some View {
-        content
-            .padding(.bottom, isMiniPlayerVisible ? Constants.Values.miniPlayerOffset - 2 : 0).onAppear {
-                isMiniPlayerVisible = (PlaybackManager.shared.currentEpisode() != nil)
-            }
-            .ignoresSafeArea(.keyboard)
-            .onReceive(NotificationCenter.default.publisher(for: Constants.Notifications.miniPlayerDidAppear), perform: { _ in
-                isMiniPlayerVisible = true
-            })
-            .onReceive(NotificationCenter.default.publisher(for: Constants.Notifications.miniPlayerDidDisappear), perform: { _ in
-                isMiniPlayerVisible = false
-            })
-    }
-}
