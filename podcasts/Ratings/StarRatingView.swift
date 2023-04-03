39458ec96c692884ed49595defcd255e5a7ccd46
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-04-03 19:23:33 -0400

    Update the star rating view to take the view model

--- podcasts/Ratings/StarRatingView.swift
+++ podcasts/Ratings/StarRatingView.swift
@@ -1,44 +1,61 @@
 import SwiftUI
 import PocketCastsUtils
+import PocketCastsServer
 
 struct StarRatingView: View {
     @EnvironmentObject var theme: Theme
+    @ObservedObject var viewModel: PodcastRatingViewModel
 
-    /// The rating value between 0.0 and 5.0
-    let rating: Double
+    /// Keeps track of when we appear to determine if we should animate
+    private var startDate: Date = .now
 
-    /// The total number of ratings to display, or nil if this should be hidden
-    let total: Int?
+    /// Only animate in if there's a rating and enough time has passed since appearing
+    private var shouldAnimate: Bool {
+        viewModel.rating != nil && Date().timeIntervalSince(startDate) > Constants.minTimeToAnimate
+    }
+
+    init(viewModel: PodcastRatingViewModel) {
+        self.viewModel = viewModel
+    }
 
     var body: some View {
         HStack(alignment: .center) {
-            stars.foregroundColor(AppTheme.color(for: .filter03, theme: theme))
-
-            if let total {
-                Text(total.abbreviated)
-                    .foregroundColor(AppTheme.color(for: .primaryText01, theme: theme))
-                    .font(size: 14, style: .footnote)
-                    .padding(.top, 1)
-                    .monospacedDigit()
-            }
+            ratingView(rating: viewModel.rating)
+                .animation(.easeIn, value: shouldAnimate)
 
             Spacer()
         }
     }
 
-    private var stars: some View {
+    @ViewBuilder
+    private func ratingView(rating: PodcastRating?) -> some View {
+        starsView(rating: rating?.average ?? 0)
+        labelView(total: rating?.total)
+    }
+
+    @ViewBuilder
+    private func starsView(rating: Double) -> some View {
         // truncate the floating points off without rounding
         let stars = Int(rating)
         // Get the float value
         let half = rating.truncatingRemainder(dividingBy: 1)
 
-        return HStack(spacing: 0) {
+        HStack(spacing: 0) {
             ForEach(0..<Constants.maxStars, id: \.self) { index in
                 image(for: index, stars: stars, half: half)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
             }
-        }
+        }.foregroundColor(AppTheme.color(for: .filter03, theme: theme))
+    }
+
+    @ViewBuilder
+    private func labelView(total: Int?) -> some View {
+        Text(total?.abbreviated ?? "")
+            .foregroundColor(AppTheme.color(for: .primaryText01, theme: theme))
+            .font(size: 14, style: .footnote)
+            .padding(.top, 1)
+            .monospacedDigit()
     }
 
     private func image(for index: Int, stars: Int, half: Double) -> Image {
@@ -61,5 +78,7 @@ struct StarRatingView: View {
         static let filled = Image(systemName: "star.fill")
         static let empty = Image(systemName: "star")
         static let half = Image(systemName: "star.fill.left")
+
+        static let minTimeToAnimate: TimeInterval = 0.2
     }
 }
