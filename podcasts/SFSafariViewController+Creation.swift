15be2fc8f404f620be368d8ade403c2a73eaae05
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-04-05 10:08:28 -0400

    Update podcasts/SFSafariViewController+Creation.swift
    Co-authored-by: Leandro Alonso <contato@leandroalonso.com>

--- podcasts/SFSafariViewController+Creation.swift
+++ podcasts/SFSafariViewController+Creation.swift
@@ -3,11 +3,9 @@ import SafariServices
 extension SFSafariViewController {
 
     /// Creates a SFSafariViewController with consistent config to be used in the app
-    static func controller(with url: URL, config: SFSafariViewController.Configuration = .appDefault) -> SFSafariViewController {
-        let controller = SFSafariViewController(url: url, configuration: config)
-        controller.dismissButtonStyle = .close
-
-        return controller
+    convenience init(with url: URL, config: SFSafariViewController.Configuration = .appDefault) {
+        self.init(url: url, configuration: config)
+        dismissButtonStyle = .close
     }
 }
 
