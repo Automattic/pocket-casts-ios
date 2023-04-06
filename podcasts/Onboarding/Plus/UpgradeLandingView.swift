7511d224953eb64f8de0ab72ba2dd6c434f3a8a9
Leandro Alonso <contato@leandroalonso.com>
2023-04-06 17:16:00 -0300

    Extract card

--- podcasts/Onboarding/Plus/UpgradeLandingView.swift
+++ podcasts/Onboarding/Plus/UpgradeLandingView.swift
@@ -16,122 +16,39 @@ struct UpgradeLandingView: View {
                     UpgradeRoundedSegmentedControl()
                         .padding(.bottom, 24)
 
-                    VStack() {
-                        VStack(alignment: .leading, spacing: 0) {
-                            HStack(spacing: 4) {
-                                Image("plusGold")
-                                    .padding(.leading, 8)
-                                Text("Plus")
-                                    .foregroundColor(.white)
-                                    .font(style: .subheadline, weight: .medium)
-                                    .padding(.trailing, 8)
-                                    .padding(.top, 2)
-                                    .padding(.bottom, 2)
-                            }
-                            .background(.black)
-                            .cornerRadius(800)
-                            .padding(.bottom, 10)
-
-                            HStack() {
-                                Text("$39.99")
-                                    .font(style: .largeTitle, weight: .bold)
-                                Text("/\(L10n.year)")
-                                    .font(style: .headline, weight: .bold)
-                                    .opacity(0.6)
-                                    .padding(.top, 6)
-                            }
-                            .padding(.bottom, 8)
-
-                            Text(L10n.accountDetailsPlusTitle)
-                                .font(style: .caption2, weight: .semibold)
-                                .opacity(0.64)
-                                .padding(.bottom, 16)
-
-                            VStack(alignment: .leading, spacing: 12) {
-                                HStack(spacing: 16) {
-                                    Image("plus-feature-desktop")
-                                        .renderingMode(.template)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(.black)
-                                        .frame(width: 16, height: 16)
-                                    Text(L10n.plusMarketingDesktopAppsTitle)
-                                        .font(size: 14, style: .subheadline, weight: .medium)
-                                }
-                                HStack(spacing: 16) {
-                                    Image("plus-feature-folders")
-                                        .renderingMode(.template)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(.black)
-                                        .frame(width: 16, height: 16)
-                                    Text(L10n.folders)
-                                        .font(size: 14, style: .subheadline, weight: .medium)
-                                }
-                                HStack(spacing: 16) {
-                                    Image("plus-feature-cloud")
-                                        .renderingMode(.template)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(.black)
-                                        .frame(width: 16, height: 16)
-                                    Text(L10n.plusCloudStorageLimitFormat(10))
-                                        .font(size: 14, style: .subheadline, weight: .medium)
-                                }
-                                HStack(spacing: 16) {
-                                    Image("plus-feature-watch")
-                                        .renderingMode(.template)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(.black)
-                                        .frame(width: 16, height: 16)
-                                    Text(L10n.plusMarketingWatchPlaybackTitle)
-                                        .font(size: 14, style: .subheadline, weight: .medium)
-                                }
-                                HStack(spacing: 16) {
-                                    Image("plus-feature-extra")
-                                        .renderingMode(.template)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(.black)
-                                        .frame(width: 16, height: 16)
-                                    Text(L10n.plusFeatureThemesIcons)
-                                        .font(size: 14, style: .subheadline, weight: .medium)
-                                }
-                                HStack(alignment: .top, spacing: 16) {
-                                    Image("plus-feature-love")
-                                        .renderingMode(.template)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .foregroundColor(.black)
-                                        .frame(width: 16, height: 16)
-                                    Text(L10n.plusFeatureGratitude)
-                                        .font(size: 14, style: .subheadline, weight: .medium)
-                                }
-                            }
-                            .padding(.bottom, 24)
-
-                            Button("Subscribe to Plus") {
-
-                            }
-                            .buttonStyle(PlusGradientFilledButtonStyle(isLoading: false, background: Color(hex: "FFD846")))
-                        }
-                        .padding(.all, 24)
-
-                    }
-                    .background(.white)
-                    .cornerRadius(24)
-                    .padding(.leading, 40)
-                    .padding(.trailing, 40)
-                    .shadow(color: .black.opacity(0.01), radius: 10, x: 0, y: 24)
-                    .shadow(color: .black.opacity(0.05), radius: 8, x: 0, y: 14)
-                    .shadow(color: .black.opacity(0.09), radius: 6, x: 0, y: 6)
-                    .shadow(color: .black.opacity(0.1), radius: 3, x: 0, y: 2)
-                    .shadow(color: .black.opacity(0.1), radius: 0, x: 0, y: 0)
+                    UpgradeCard(tier: PlusTier())
                 }
             }
         }
     }
+}
+
+protocol UpgradeTier {
+    var iconName: String { get }
+    var title: String { get }
+    var price: String { get }
+    var description: String { get }
+    var features: [TierFeature] { get }
+}
+
+struct TierFeature: Hashable {
+    let iconName: String
+    let title: String
+}
+
+struct PlusTier: UpgradeTier {
+    let iconName = "plus_gold"
+    let title = "Plus"
+    let price = "$39.99"
+    let description = L10n.accountDetailsPlusTitle
+    let features = [
+        TierFeature(iconName: "plus-feature-desktop", title: L10n.plusMarketingDesktopAppsTitle),
+        TierFeature(iconName: "plus-feature-folders", title: L10n.folders),
+        TierFeature(iconName: "plus-feature-cloud", title: L10n.plusCloudStorageLimitFormat(10)),
+        TierFeature(iconName: "plus-feature-watch", title: L10n.plusMarketingWatchPlaybackTitle),
+        TierFeature(iconName: "plus-feature-extra", title: L10n.plusFeatureThemesIcons),
+        TierFeature(iconName: "plus-feature-love", title: L10n.plusFeatureGratitude)
+    ]
 }
 
 struct UpgradeRoundedSegmentedControl: View {
@@ -159,6 +76,77 @@ struct UpgradeRoundedSegmentedControl: View {
         }
         .background(.white.opacity(0.16))
         .cornerRadius(24)
+    }
+}
+
+struct UpgradeCard: View {
+    let tier: UpgradeTier
+
+    var body: some View {
+        VStack() {
+            VStack(alignment: .leading, spacing: 0) {
+                HStack(spacing: 4) {
+                    Image(tier.iconName)
+                        .padding(.leading, 8)
+                    Text(tier.title)
+                        .foregroundColor(.white)
+                        .font(style: .subheadline, weight: .medium)
+                        .padding(.trailing, 8)
+                        .padding(.top, 2)
+                        .padding(.bottom, 2)
+                }
+                .background(.black)
+                .cornerRadius(800)
+                .padding(.bottom, 10)
+
+                HStack() {
+                    Text(tier.price)
+                        .font(style: .largeTitle, weight: .bold)
+                    Text("/\(L10n.year)")
+                        .font(style: .headline, weight: .bold)
+                        .opacity(0.6)
+                        .padding(.top, 6)
+                }
+                .padding(.bottom, 8)
+
+                Text(tier.description)
+                    .font(style: .caption2, weight: .semibold)
+                    .opacity(0.64)
+                    .padding(.bottom, 16)
+
+                VStack(alignment: .leading, spacing: 12) {
+                    ForEach(tier.features, id: \.self) { feature in
+                        HStack(spacing: 16) {
+                            Image(feature.iconName)
+                                .renderingMode(.template)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(.black)
+                                .frame(width: 16, height: 16)
+                            Text(feature.title)
+                                .font(size: 14, style: .subheadline, weight: .medium)
+                        }
+                    }
+                }
+                .padding(.bottom, 24)
+
+                Button("Subscribe to Plus") {
+
+                }
+                .buttonStyle(PlusGradientFilledButtonStyle(isLoading: false, background: Color(hex: "FFD846")))
+            }
+            .padding(.all, 24)
+
+        }
+        .background(.white)
+        .cornerRadius(24)
+        .padding(.leading, 40)
+        .padding(.trailing, 40)
+        .shadow(color: .black.opacity(0.01), radius: 10, x: 0, y: 24)
+        .shadow(color: .black.opacity(0.05), radius: 8, x: 0, y: 14)
+        .shadow(color: .black.opacity(0.09), radius: 6, x: 0, y: 6)
+        .shadow(color: .black.opacity(0.1), radius: 3, x: 0, y: 2)
+        .shadow(color: .black.opacity(0.1), radius: 0, x: 0, y: 0)
     }
 }
 
