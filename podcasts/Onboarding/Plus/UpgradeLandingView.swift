7802e39bf12438a2f0cf1fd818393f85f47113c8
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-04-07 11:06:43 -0400

    Make the feature cards be the equal heights

--- podcasts/Onboarding/Plus/UpgradeLandingView.swift
+++ podcasts/Onboarding/Plus/UpgradeLandingView.swift
@@ -17,14 +17,50 @@ struct UpgradeLandingView: View {
                     UpgradeRoundedSegmentedControl()
                         .padding(.bottom, 24)
 
-                    HorizontalCarousel(items: tiers) {
-                        UpgradeCard(tier: $0)
-                    }
-                    .carouselPeekAmount(.constant(20))
-                    .carouselItemSpacing(30)
+                    FeaturesCarousel(tiers: tiers)
                 }
             }
         }
+    }
+}
+
+struct FeaturesCarousel: View {
+    let tiers: [UpgradeTier]
+
+    @State var calculatedCardHeight: CGFloat?
+
+    var body: some View {
+        // Store the calculated card heights
+        var cardHeights: [CGFloat] = []
+
+        HorizontalCarousel(items: tiers) {
+            UpgradeCard(tier: $0)
+                .overlay(
+                    // Calculate the height of the card after it's been laid out
+                    GeometryReader { proxy in
+                        Action {
+                            // Add the calculated height to the array
+                            cardHeights.append(proxy.size.height)
+
+                            // Determine the max height only once we've calculated all the heights
+                            if cardHeights.count == tiers.count {
+                                calculatedCardHeight = cardHeights.max()
+
+                                // Reset the card heights so any view changes won't use old data
+                                cardHeights = []
+                            }
+                        }
+                    }
+                )
+        }
+        .carouselPeekAmount(.constant(Constants.peekAmount))
+        .carouselItemSpacing(Constants.spacing)
+        .frame(height: calculatedCardHeight)
+    }
+
+    private enum Constants {
+        static let peekAmount: Double = 20
+        static let spacing: Double = 30
     }
 }
 
