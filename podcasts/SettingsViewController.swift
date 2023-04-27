61fb4ac510904fe80cc69a2d2adfd055fdaaf89f
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-04-27 16:49:51 -0400

    Display the full upsell view when tapping on the plus settings prompt

--- podcasts/SettingsViewController.swift
+++ podcasts/SettingsViewController.swift
@@ -143,7 +143,7 @@ class SettingsViewController: PCViewController, UITableViewDataSource, UITableVi
         case .watch:
             navigationController?.pushViewController(WatchSettingsViewController(), animated: true)
         case .pocketCastsPlus:
-            navigationController?.pushViewController(PlusDetailsViewController(), animated: true)
+            navigationController?.present(OnboardingFlow.shared.begin(flow: .plusUpsell, source: "settings"), animated: true)
         case .privacy:
             navigationController?.pushViewController(PrivacySettingsViewController(), animated: true)
         case .developer:
