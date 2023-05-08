fa3c0b257b0e887758c522b320a136a8c289d275
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-05-08 14:54:09 -0400

    Add some theme colors for Plus

---
+++ podcasts/Onboarding/Plus/Theme+Plus.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Theme {
+    var plusPrimaryColor: Color {
+        Color(hex: "FEB525")
+    }
+
+    var patronPrimaryColor: Color {
+        Color(hex: "6046F5")
+    }
+}
