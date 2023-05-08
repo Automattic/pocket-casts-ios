bd17ffb865eedca7f48114dbd15729056648f8c3
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-05-08 17:23:54 -0400

    Remove the old header code

--- podcasts/AccountViewController.swift
+++ podcasts/AccountViewController.swift
@@ -22,125 +22,28 @@ class AccountViewController: UIViewController, ChangeEmailDelegate {
         }
     }
 
-    @IBOutlet var headerView: UIStackView! {
+    var upgradePromptViewSize: CGSize? = nil {
         didSet {
-            headerView.translatesAutoresizingMaskIntoConstraints = false
+            tableView.reloadData()
         }
     }
 
-    @IBOutlet var headerBackgroundView: ThemeableView! {
-        didSet {
-            headerBackgroundView.style = .primaryUi02
-        }
-    }
-
-    @IBOutlet var profileView: ProfileProgressCircleView! {
-        didSet {
-            profileView.style = .primaryUi02
-        }
-    }
-
-    @IBOutlet var profileImageView: UIImageView!
-    @IBOutlet var emailLabel: ThemeableLabel! {
-        didSet {
-            emailLabel.style = .primaryText01
-        }
-    }
-
-    @IBOutlet var accountTypeLabel: ThemeableLabel! {
-        didSet {
-            accountTypeLabel.style = .primaryText02
-        }
-    }
-
-    @IBOutlet var accountDetailsLabel: ThemeableLabel! {
-        didSet {
-            accountDetailsLabel.style = .primaryText02
-        }
-    }
-
-    @IBOutlet var paymentExpiryLabel: ThemeableLabel! {
-        didSet {
-            paymentExpiryLabel.style = .primaryText02
-        }
-    }
-
-    @IBOutlet var seperatorView: ThemeableView! {
-        didSet {
-            seperatorView.style = .primaryUi05
-        }
-    }
-
-    @IBOutlet var oldUpgradeView: ThemeableView!
     lazy var headerViewModel: AccountHeaderViewModel = {
         let viewModel = AccountHeaderViewModel()
 
-    @IBOutlet var upgradeSeperatorView: ThemeableView! {
-        didSet {
-            upgradeSeperatorView.style = .primaryUi05
-        }
-    }
-
-    @IBOutlet var noInternetView: ThemeableView! {
-        didSet {
-            noInternetView.style = .primaryUi05
-            noInternetView.layer.cornerRadius = 8
-            noInternetView.layer.borderColor = AppTheme.colorForStyle(.primaryUi06).cgColor
-            noInternetView.layer.borderWidth = 1
         viewModel.viewContentSizeChanged = { [weak self] in
             self?.tableView.reloadData()
         }
-    }
 
-    @IBOutlet var noInternetLabel: ThemeableLabel! {
-        didSet {
-            noInternetLabel.style = .primaryText02
-        }
-    }
         return viewModel
     }()
 
-    @IBOutlet var plusLogo: ThemeableImageView! {
-        didSet {
-            plusLogo.imageNameFunc = AppTheme.pcPlusLogoHorizontalImageName
-        }
-    }
     lazy var updatedHeaderContentView: UIView = {
         let headerView = AccountHeaderView(viewModel: headerViewModel)
 
-    @IBOutlet var priceLabel: ThemeableLabel! {
-        didSet {
-            priceLabel.style = .primaryText01
-        }
-    }
         let view = headerView.themedUIView
         view.backgroundColor = .clear
 
-    @IBOutlet var upgradeButton: ThemeableRoundedButton!
-
-    @IBOutlet var learnMoreButton: ThemeableRoundedButton! {
-        didSet {
-            learnMoreButton.textStyle = .primaryInteractive01
-            learnMoreButton.buttonStyle = .primaryUi01
-        }
-    }
-
-    @IBOutlet var trialDetailLabel: ThemeableLabel! {
-        didSet {
-            trialDetailLabel.style = .primaryText02
-        }
-    }
-
-    @IBOutlet var pricingCenterConstraint: NSLayoutConstraint!
-
-    private var upgradeView: UIView? {
-        return oldUpgradeView
-    }
-
-    private func hideOldUpgradeViewIfNeeded() {
-        guard FeatureFlag.onboardingUpdates.enabled else { return }
-        oldUpgradeView.removeFromSuperview()
-    }
         return view
     }()
 
@@ -158,7 +61,6 @@ class AccountViewController: UIViewController, ChangeEmailDelegate {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        hideOldUpgradeViewIfNeeded()
         updateDisplayedData()
         title = L10n.accountTitle
     }
@@ -185,9 +87,6 @@ class AccountViewController: UIViewController, ChangeEmailDelegate {
     }
 
     private func updateDisplayedData() {
-        if let email = ServerSettings.syncingEmail() {
-            emailLabel.text = email
-        }
         headerViewModel.update()
 
         // Only accounts created with username/password can change email/password
@@ -198,164 +97,37 @@ class AccountViewController: UIViewController, ChangeEmailDelegate {
             accountOptions = [.newsletter]
         }
 
-        var upgradeHidden = false
-
         if SubscriptionHelper.hasActiveSubscription() {
-            accountTypeLabel.text = L10n.pocketCastsPlus
-
-            profileView.isSubscribed = true
-
-            let expiryDate = SubscriptionHelper.subscriptionRenewalDate()
-            var hideExpiryDate = true
-
-            if SubscriptionHelper.hasRenewingSubscription() {
-                if SubscriptionHelper.subscriptionType() == .plus {
-                    let nextPaymentDate = DateFormatHelper.sharedHelper.longLocalizedFormat(expiryDate)
-                    accountDetailsLabel.text = L10n.nextPaymentFormat(nextPaymentDate)
-                    paymentExpiryLabel.text = SubscriptionHelper.subscriptionFrequency()
-                    upgradeHidden = true
-                    upgradeView?.isHidden = true
-                } else if SubscriptionHelper.subscriptionType() == .supporter {
-                    accountDetailsLabel.style = .support02
-                    accountDetailsLabel.text = L10n.supporter
-                    paymentExpiryLabel.text = L10n.supporterContributionsSubtitle
-                    upgradeHidden = true
-                    upgradeView?.isHidden = true
-                } else {
-                    // This handles a state where the user has plus, but their subscription type is none
-                    // IE: If the receipt request fails the first time
-                    let nextPaymentDate = DateFormatHelper.sharedHelper.longLocalizedFormat(expiryDate)
-                    accountDetailsLabel.text = L10n.nextPaymentFormat(nextPaymentDate)
-                    paymentExpiryLabel.text = SubscriptionHelper.subscriptionFrequency()
-                    upgradeHidden = true
-                    upgradeView?.isHidden = true
-                }
-            } else { // Gifted account
-                if SubscriptionHelper.subscriptionPlatform() == .gift {
-                    if SubscriptionHelper.hasLifetimeGift() {
-                        hideExpiryDate = true
-                        upgradeHidden = true
-                        upgradeView?.isHidden = true
-                        accountDetailsLabel.text = L10n.subscriptionsThankYou
-                        paymentExpiryLabel.text = L10n.plusLifetimeMembership
-                        paymentExpiryLabel.font = UIFont.systemFont(ofSize: 14.0, weight: .medium)
-                        paymentExpiryLabel.style = .support02
-                    } else {
-                        let freeTime = Double(SubscriptionHelper.subscriptionGiftDays()).days
-                        let freeTimeStr = DateFormatHelper.sharedHelper.shortTimeRemaining(freeTime).capitalized
-                        accountDetailsLabel.text = L10n.plusFreeMembershipFormat(freeTimeStr)
-                        hideExpiryDate = false
-                    }
-                } else {
-                    if SubscriptionHelper.subscriptionType() == .plus {
-                        accountDetailsLabel.text = L10n.plusPaymentCanceled
-                        hideExpiryDate = false
-                    } else if SubscriptionHelper.subscriptionType() == .supporter {
-                        accountDetailsLabel.style = .support05
-                        accountDetailsLabel.text = L10n.supporterPaymentCanceled
-                        upgradeHidden = true
-                        upgradeView?.isHidden = true
-                    }
-                }
-            }
-
-            if let expiryTime = SubscriptionHelper.timeToSubscriptionExpiry(), let expiryDate = expiryDate {
-                if !hideExpiryDate {
-                    let time = DateFormatHelper.sharedHelper.longLocalizedFormat(expiryDate)
-                    paymentExpiryLabel.text = L10n.plusExpirationFormat(time)
-
-                    if expiryTime < 15.days {
-                        paymentExpiryLabel.style = .support05
-                        upgradeHidden = SubscriptionHelper.hasRenewingSubscription()
-                        upgradeView?.isHidden = SubscriptionHelper.hasRenewingSubscription()
-                    } else if expiryTime < 30.days {
-                        paymentExpiryLabel.style = .support08
-                        upgradeHidden = SubscriptionHelper.hasRenewingSubscription()
-                        upgradeView?.isHidden = SubscriptionHelper.hasRenewingSubscription()
-                    } else {
-                        paymentExpiryLabel.style = .primaryText02
-                        upgradeHidden = true
-                        upgradeView?.isHidden = true
-                    }
-                }
-                profileView.secondsTillExpiry = expiryTime
-            }
-
             var newTableRows: [[TableRow]] = [accountOptions, [.privacyPolicy, .termsOfUse], [.logout], [.deleteAccount]]
 
-            if SubscriptionHelper.numActiveSubscriptionBundles() > 0 {
-                newTableRows[0].insert(.supporterContributions, at: 0)
-            }
-            if SubscriptionHelper.hasRenewingSubscription(), SubscriptionHelper.subscriptionType() == .plus {
+            if SubscriptionHelper.hasRenewingSubscription(), SubscriptionHelper.subscriptionType() != .none {
                 newTableRows[0].append(.cancelSubscription)
             }
 
-            if FeatureFlag.onboardingUpdates.enabled, !upgradeHidden {
+            if !SubscriptionHelper.hasRenewingSubscription() && !SubscriptionHelper.hasLifetimeGift() {
                 newTableRows[0].insert(.upgradeView, at: 0)
             }
 
             updateTableRows(newRows: newTableRows)
 
         } else {
-            // Free Account
-            accountTypeLabel.text = L10n.pocketCasts
-
-            let totalListeningTime = StatsManager.shared.totalListeningTimeInclusive()
-            if totalListeningTime > 0, let totalTime = totalListeningTime.localizedTimeDescription {
-                accountDetailsLabel.text = L10n.accountDetailsFreeAccount
-                paymentExpiryLabel.text = L10n.accountDetailsListenedFor(totalTime)
-            } else {
-                accountDetailsLabel.text = nil
-                paymentExpiryLabel.text = nil
-            }
-
-            upgradeHidden = false
-            upgradeView?.isHidden = false
-            profileView.isSubscribed = false
-
             var newTableRows: [[TableRow]] = [accountOptions, [.privacyPolicy, .termsOfUse], [.logout], [.deleteAccount]]
+
             if let subscriptionPodcasts = SubscriptionHelper.subscriptionPodcasts(), subscriptionPodcasts.count > 0 {
                 newTableRows[0].insert(.supporterContributions, at: 0)
             }
 
-            if FeatureFlag.onboardingUpdates.enabled {
-                newTableRows[0].insert(.upgradeView, at: 0)
-            }
+            newTableRows[0].insert(.upgradeView, at: 0)
 
             updateTableRows(newRows: newTableRows)
-        }
-
-        // Hide this by default
-        noInternetView.isHidden = true
-
-        // If we're not hiding the upgrade view, then refresh the pricing info
-        // If the IAP information hasn't been pulled in yet, this method will trigger a refresh and the view will
-        // be updated via `iapProductsUpdated`
-        if !FeatureFlag.onboardingUpdates.enabled, upgradeHidden == false {
-            updatePricingLabels()
-        }
-
-        // recalculate header height if subscription status changed
-        let size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
-        if headerView.frame.size.height != size.height {
-            headerView.frame.size.height = size.height
-            tableView.tableHeaderView = headerView
         }
     }
 
     @objc func iapProductsUpdated() {
-        upgradeButton.isHidden = false
-        noInternetView.isHidden = true
         updateDisplayedData()
     }
 
     @objc func iapProductsFailed() {
-        #if !targetEnvironment(simulator)
-            priceLabel.text = ""
-            trialDetailLabel.isHidden = true
-            upgradeButton.isHidden = true
-            noInternetView.isHidden = false
-        #endif
         updateDisplayedData()
     }
 
@@ -397,40 +169,7 @@ class AccountViewController: UIViewController, ChangeEmailDelegate {
 
     func emailChanged() {
         DispatchQueue.main.async {
-            if let email = ServerSettings.syncingEmail() {
-                self.emailLabel.text = email
-            }
             self.headerViewModel.update()
         }
-    }
-}
-
-private extension AccountViewController {
-    func updatePricingLabels() {
-        guard let trialDetails = IapHelper.shared.getFirstFreeTrialDetails() else {
-            configurePricingLabels()
-            return
-        }
-
-        upgradeButton.setTitle(L10n.freeTrialStartButton, for: .normal)
-        priceLabel.text = L10n.freeTrialDurationFree(trialDetails.duration).localizedLowercase
-        trialDetailLabel.text = L10n.pricingTermsAfterTrial(trialDetails.pricing)
-        trialDetailLabel.isHidden = false
-        pricingCenterConstraint.constant = 1
-        noInternetView.isHidden = true
-    }
-
-    func configurePricingLabels() {
-        trialDetailLabel.isHidden = true
-
-        guard let price = IapHelper.shared.pricingStringWithFrequency(for: .monthly) else {
-            priceLabel.isHidden = true
-            return
-        }
-
-        priceLabel.isHidden = false
-        noInternetView.isHidden = true
-        priceLabel.text = price
-        upgradeButton.setTitle(L10n.plusMarketingUpgradeButton, for: .normal)
     }
 }
