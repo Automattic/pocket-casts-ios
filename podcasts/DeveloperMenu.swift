ae6dccffde932f3f7ea555610d720dbc98c8567d
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-05-08 15:35:10 -0400

    Add some more debug options

--- podcasts/DeveloperMenu.swift
+++ podcasts/DeveloperMenu.swift
@@ -24,7 +24,7 @@ struct DeveloperMenu: View {
             }
 
             Section {
-                Button("Set to No Plus") {
+                Button("Set to No Subscription") {
                     SubscriptionHelper.setSubscriptionPaid(Int(0))
                     SubscriptionHelper.setSubscriptionPlatform(Int(0))
                     SubscriptionHelper.setSubscriptionExpiryDate(Date(timeIntervalSinceNow: 30.days).timeIntervalSince1970)
@@ -37,7 +37,7 @@ struct DeveloperMenu: View {
                     HapticsHelper.triggerSubscribedHaptic()
                 }
 
-                Button("Set to Paid") {
+                Button("Set to Paid Plus") {
                     SubscriptionHelper.setSubscriptionPaid(Int(1))
                     SubscriptionHelper.setSubscriptionPlatform(Int(1))
                     SubscriptionHelper.setSubscriptionExpiryDate(Date(timeIntervalSinceNow: 30.days).timeIntervalSince1970)
@@ -50,8 +50,21 @@ struct DeveloperMenu: View {
                     HapticsHelper.triggerSubscribedHaptic()
                 }
 
+                Button("Set to Paid Patron") {
+                    SubscriptionHelper.setSubscriptionPaid(Int(1))
+                    SubscriptionHelper.setSubscriptionPlatform(Int(1))
+                    SubscriptionHelper.setSubscriptionExpiryDate(Date(timeIntervalSinceNow: 30.days).timeIntervalSince1970)
+                    SubscriptionHelper.setSubscriptionAutoRenewing(true)
+                    SubscriptionHelper.setSubscriptionGiftDays(Int(0))
+                    SubscriptionHelper.setSubscriptionFrequency(Int(2))
+                    SubscriptionHelper.setSubscriptionType(Int(3))
+
+                    NotificationCenter.postOnMainThread(notification: ServerNotifications.subscriptionStatusChanged)
+                    HapticsHelper.triggerSubscribedHaptic()
+                }
+
                 VStack(alignment: .leading, spacing: 5) {
-                    Button("Set to Active but Cancelled") {
+                    Button("Set to Active but Cancelled: Plus") {
                         SubscriptionHelper.setSubscriptionPaid(Int(1))
                         SubscriptionHelper.setSubscriptionPlatform(Int(1))
                         SubscriptionHelper.setSubscriptionExpiryDate(Date(timeIntervalSinceNow: 3.days).timeIntervalSince1970)
@@ -63,12 +76,25 @@ struct DeveloperMenu: View {
                         NotificationCenter.postOnMainThread(notification: ServerNotifications.subscriptionStatusChanged)
                         HapticsHelper.triggerSubscribedHaptic()
                     }
+
+                    Button("Set to Active but Cancelled: Patron") {
+                        SubscriptionHelper.setSubscriptionPaid(Int(1))
+                        SubscriptionHelper.setSubscriptionPlatform(Int(1))
+                        SubscriptionHelper.setSubscriptionExpiryDate(Date(timeIntervalSinceNow: 3.days).timeIntervalSince1970)
+                        SubscriptionHelper.setSubscriptionAutoRenewing(false)
+                        SubscriptionHelper.setSubscriptionGiftDays(Int(0))
+                        SubscriptionHelper.setSubscriptionFrequency(Int(0))
+                        SubscriptionHelper.setSubscriptionType(Int(3))
+
+                        NotificationCenter.postOnMainThread(notification: ServerNotifications.subscriptionStatusChanged)
+                        HapticsHelper.triggerSubscribedHaptic()
+                    }
                     Text("Expiring in 2 days")
                         .font(Font.footnote)
                 }
 
                 VStack(alignment: .leading, spacing: 5) {
-                    Button("Set Cancelled and Expired") {
+                    Button("Set to Cancelled and Expired: Plus") {
                         SubscriptionHelper.setSubscriptionPaid(Int(0))
                         SubscriptionHelper.setSubscriptionPlatform(Int(1))
                         SubscriptionHelper.setSubscriptionExpiryDate(Date(timeIntervalSinceNow: (1.days * -1)).timeIntervalSince1970)
@@ -76,6 +102,18 @@ struct DeveloperMenu: View {
                         SubscriptionHelper.setSubscriptionGiftDays(Int(0))
                         SubscriptionHelper.setSubscriptionFrequency(Int(0))
                         SubscriptionHelper.setSubscriptionType(Int(0))
+
+                        NotificationCenter.postOnMainThread(notification: ServerNotifications.subscriptionStatusChanged)
+                        HapticsHelper.triggerSubscribedHaptic()
+                    }
+                    Button("Set to Cancelled and Expired: Patron") {
+                        SubscriptionHelper.setSubscriptionPaid(Int(0))
+                        SubscriptionHelper.setSubscriptionPlatform(Int(1))
+                        SubscriptionHelper.setSubscriptionExpiryDate(Date(timeIntervalSinceNow: (1.days * -1)).timeIntervalSince1970)
+                        SubscriptionHelper.setSubscriptionAutoRenewing(false)
+                        SubscriptionHelper.setSubscriptionGiftDays(Int(0))
+                        SubscriptionHelper.setSubscriptionFrequency(Int(0))
+                        SubscriptionHelper.setSubscriptionType(Int(3))
 
                         NotificationCenter.postOnMainThread(notification: ServerNotifications.subscriptionStatusChanged)
                         HapticsHelper.triggerSubscribedHaptic()
