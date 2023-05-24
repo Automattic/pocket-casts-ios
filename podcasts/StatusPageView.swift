daea18e687779f46d6dfaaceb67a959a521f28b0
Leandro Alonso <contato@leandroalonso.com>
2023-05-24 15:14:51 -0300

    Add status page icon and view

---
+++ podcasts/StatusPageView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct StatusPageView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct StatusPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatusPageView()
+    }
+}
