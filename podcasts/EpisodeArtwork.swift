b74093826f2f3201e34aef4f148c2365a8422d9c
Leandro Alonso <contato@leandroalonso.com>
2023-06-13 16:06:44 -0300

    Refactor EpisodeArtwork for better readability

--- podcasts/EpisodeArtwork.swift
+++ podcasts/EpisodeArtwork.swift
@@ -18,40 +18,51 @@ class EpisodeArtwork {
     ///   - asset: an AVAsset
     ///   - podcastUuid: the UUID of the current playing podcast
     ///   - episodeUuid: the UUID of the current playing episode
-    func loadEmbeddedImage(asset: AVAsset, podcastUuid: String, episodeUuid: String) {
+    func loadEmbeddedImage(asset: AVAsset?, podcastUuid: String, episodeUuid: String) {
         guard Settings.loadEmbeddedImages, !isCached(episodeUuid: episodeUuid) else {
             return
         }
 
-        let artworkItems = AVMetadataItem.metadataItems(from: asset.commonMetadata, filteredByIdentifier: .commonIdentifierArtwork)
-        let image = artworkItems.compactMap { $0.dataValue.flatMap { UIImage(data: $0) } }.first
-
-        if let image {
-            set(for: episodeUuid, image: image) {
-                NotificationCenter.postOnMainThread(notification: .episodeEmbeddedArtworkLoaded)
-            }
-        } else {
-            CacheServerHandler.shared.loadEpisodeArtworkUrl(podcastUuid: podcastUuid, episodeUuid: episodeUuid) { [weak self] imageUrl in
-                if let imageUrl, let url = URL(string: imageUrl) {
-                    KingfisherManager.shared.retrieveImage(with: url, options: nil) { result in
-                        if let image = try? result.get().image {
-                            self?.set(for: episodeUuid, image: image) {
-                                NotificationCenter.postOnMainThread(notification: .episodeEmbeddedArtworkLoaded)
-                            }
-                        }
-                    }
-                }
-            }
+        if let assetEpisodeArtwork = loadEpisodeArtwork(from: asset) {
+            save(assetEpisodeArtwork, for: episodeUuid)
+            return
         }
+
+        loadEpisodeArtworkFromUrl(podcastUuid: podcastUuid, episodeUuid: episodeUuid)
     }
 
     func isCached(episodeUuid: String) -> Bool {
         imageManager.subscribedPodcastsCache.isCached(forKey: episodeUuid)
     }
 
-    private func set(for episodeUuid: String, image: UIImage, completion: (() -> Void)?) {
+    private func loadEpisodeArtwork(from asset: AVAsset?) -> UIImage? {
+        guard let asset else {
+            return nil
+        }
+
+        let artworkItems = AVMetadataItem.metadataItems(from: asset.commonMetadata, filteredByIdentifier: .commonIdentifierArtwork)
+        return artworkItems.compactMap { $0.dataValue.flatMap { UIImage(data: $0) } }.first
+    }
+
+    private func loadEpisodeArtworkFromUrl(podcastUuid: String, episodeUuid: String) {
+        CacheServerHandler.shared.loadEpisodeArtworkUrl(podcastUuid: podcastUuid, episodeUuid: episodeUuid) { [weak self] imageUrl in
+            guard let self else {
+                return
+            }
+
+            if let imageUrl, let url = URL(string: imageUrl) {
+                KingfisherManager.shared.retrieveImage(with: url, options: [.targetCache(self.imageManager.subscribedPodcastsCache)]) { result in
+                    if let image = try? result.get().image {
+                        self.save(image, for: episodeUuid)
+                    }
+                }
+            }
+        }
+    }
+
+    private func save(_ image: UIImage, for episodeUuid: String) {
         imageManager.subscribedPodcastsCache.store(image, forKey: episodeUuid) { _ in
-            completion?()
+            NotificationCenter.postOnMainThread(notification: .episodeEmbeddedArtworkLoaded)
         }
     }
 }
