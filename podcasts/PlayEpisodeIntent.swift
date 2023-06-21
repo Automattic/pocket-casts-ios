a16c64660e1ce01d8cd1a326c22e8076053ff85f
John Caruso <johncaruso@hey.com>
2023-06-21 17:36:07 -0400

    using query to get all episode ids instead of hardcoding -- query isnt right for all widgets, but Ok for current testing

--- podcasts/PlayEpisodeIntent.swift
+++ podcasts/PlayEpisodeIntent.swift
@@ -56,11 +56,7 @@ struct EpisodeEntity: AppEntity, Identifiable {
 
 struct EpisodeQuery: EntityQuery {
     func entities(for identifiers: [UUID]) async throws -> [EpisodeEntity] {
-//        try await MusicCatalog.shared.albums(for: identifiers)
-//            .map { AlbumEntity(id: $0.id, albumName: $0.name) }
-        
         // TODO: return episode entities that are needed, or all episodes since this could be an UpNext or a playlist?
-        return [EpisodeEntity(id: UUID(uuidString:"779ffa58-88e1-4418-b222-fdcee52dd9d3")!), EpisodeEntity(id: UUID(uuidString:"bd82e019-42a8-4f2f-ad82-1d6acfcdd89f")!)]
-        
+        DataManager.sharedManager.allUpNextEpisodes().map( { EpisodeEntity(id: UUID(uuidString: $0.uuid )!) } )
     }
 }
