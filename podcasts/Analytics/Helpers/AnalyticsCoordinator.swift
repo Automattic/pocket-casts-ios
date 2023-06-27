bce8b2fd872daead7cbc2aefdfdf119ec0de057c
Leandro Alonso <contato@leandroalonso.com>
2023-06-27 17:46:14 -0300

    Revert "Extract getTopViewController"
    This reverts commit 10f5ffec54a152164d624f92c06a538e76a78129.

--- podcasts/Analytics/Helpers/AnalyticsCoordinator.swift
+++ podcasts/Analytics/Helpers/AnalyticsCoordinator.swift
@@ -59,7 +59,7 @@ class AnalyticsCoordinator {
                 return currentSource
             }
 
-            return (UIApplication.shared.getTopViewController() as? AnalyticsSourceProvider)?.analyticsSource ?? .unknown
+            return (getTopViewController() as? AnalyticsSourceProvider)?.analyticsSource ?? .unknown
         }
 
         func track(_ event: AnalyticsEvent, properties: [String: Any]? = nil) {
@@ -74,6 +74,21 @@ class AnalyticsCoordinator {
             let defaultProperties: [String: Any] = ["source": currentAnalyticsSource]
             let mergedProperties = defaultProperties.merging(properties ?? [:]) { current, _ in current }
             Analytics.track(event, properties: mergedProperties)
+        }
+
+    func getTopViewController(base: UIViewController? = SceneHelper.rootViewController()) -> UIViewController? {
+            guard UIApplication.shared.applicationState == .active else {
+                return nil
+            }
+
+            if let nav = base as? UINavigationController {
+                return getTopViewController(base: nav.visibleViewController)
+            } else if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+                return getTopViewController(base: selected)
+            } else if let presented = base?.presentedViewController {
+                return getTopViewController(base: presented)
+            }
+            return base
         }
     #else
         /// NOOP track event to preventing needing to wrap all the events in #if checks
