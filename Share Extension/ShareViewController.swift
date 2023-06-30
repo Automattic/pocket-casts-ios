530ebbaad4f30ba580abf05fa286134d7f33b10e
Yael Rubinstein <yaelirub@gmail.com>
2023-06-30 15:15:45 -0700

    Fixed trailing whitespace

--- Share Extension/ShareViewController.swift
+++ Share Extension/ShareViewController.swift
@@ -18,7 +18,6 @@ class ShareViewController: SLComposeServiceViewController {
 
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
     }
