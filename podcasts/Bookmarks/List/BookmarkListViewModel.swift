82c1bdbad8a28475d236beab2276b4b2e559ec77
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-07-10 16:50:30 -0400

    Add the BookmarkListViewModel

---
+++ podcasts/Bookmarks/List/BookmarkListViewModel.swift
@@ -0,0 +1,49 @@
+import Combine
+import PocketCastsDataModel
+
+class BookmarkListViewModel: ObservableObject {
+    let bookmarkManager: BookmarkManager
+
+    @Published var bookmarkCount: Int = 0
+    @Published var bookmarks: [BookmarkManager.Bookmark] = []
+
+    private var cancellables = Set<AnyCancellable>()
+
+    weak var episode: BaseEpisode? = nil {
+        didSet {
+            reload()
+        }
+    }
+
+    init(bookmarkManager: BookmarkManager) {
+        self.bookmarkManager = bookmarkManager
+
+        listenForAddedBookmarks()
+    }
+
+    func reload() {
+        let bookmarks = episode.map { bookmarkManager.bookmarks(for: $0) } ?? []
+
+        bookmarkCount = bookmarks.count
+        self.bookmarks = bookmarks
+    }
+
+    private func listenForAddedBookmarks() {
+        bookmarkManager.onBookmarkCreated
+            .filter { [weak self] (episode, _) in
+                self?.episode?.uuid == episode.uuid
+            }
+            .sink { [weak self] (_, _) in
+                self?.reload()
+            }
+            .store(in: &cancellables)
+    }
+
+    func bookmarkTapped(_ bookmark: BookmarkManager.Bookmark) {
+        print("Tapped")
+    }
+
+    func bookmarkPlayTapped(_ bookmark: BookmarkManager.Bookmark) {
+        PlaybackManager.shared.seekTo(time: bookmark.time, startPlaybackAfterSeek: true)
+    }
+}
