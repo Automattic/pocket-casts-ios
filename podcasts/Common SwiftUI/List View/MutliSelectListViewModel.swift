b13b915993813b90b59137505ad30539a36050cf
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-07-12 20:12:18 -0400

    Add the MutliSelectListViewModel class

---
+++ podcasts/Common SwiftUI/List View/MutliSelectListViewModel.swift
@@ -0,0 +1,121 @@
+import Foundation
+
+/// A generic list view model that allows the user to enter a mode that allows them to select multiple items from the list and perform actions on those items
+///
+/// Usage:
+///
+///     class MyListViewModel: MutliSelectListViewModel<MyCustomModel> {
+///         ...
+///     }
+class MutliSelectListViewModel<Model: Hashable>: ListViewModel<Model> {
+    /// Whether the list is currently in the multi selection mode
+    @Published private(set) var isMultiSelecting = false
+
+    /// The total number of items that are currently selected
+    @Published private(set) var numberOfSelectedItems = 0
+
+    /// Whether all the items in the list have been selected
+    @Published private(set) var hasSelectedAll = false
+
+    /// An internal set that keeps track of the items that are currently selected
+    private lazy var selectedItems: Set<Model> = []
+
+    // MARK: - Entering / Exiting Multi Select
+    func toggleMultiSelection() {
+        deselectAll()
+        isMultiSelecting.toggle()
+    }
+
+    // MARK: - Item Selection
+
+    func itemIsSelected(_ item: Model) -> Bool {
+        selectedItems.contains(where: { $0 == item })
+    }
+
+    func selectItem(_ item: Model) {
+        selectedItems.insert(item)
+        updateCounts()
+    }
+
+    func deselectItem(_ item: Model) {
+        selectedItems.remove(item)
+        updateCounts()
+    }
+
+    func toggleItemSelected(_ item: Model) {
+        itemIsSelected(item) ? deselectItem(item) : selectItem(item)
+    }
+
+    // MARK: - Select All / Deselect All
+
+    func toggleSelectAll() {
+        hasSelectedAll ? deselectAll() : selectAll()
+    }
+
+    func selectAll() {
+        selectedItems = Set(items)
+        updateCounts()
+    }
+
+    func deselectAll() {
+        selectedItems.removeAll()
+        updateCounts()
+    }
+
+    func selectAllBefore(_ item: Model) {
+        guard let index = items.firstIndex(of: item) else { return }
+
+        selectedItems.formUnion(items[...index])
+        updateCounts()
+    }
+
+    func selectAllAfter(_ item: Model) {
+        guard let index = items.firstIndex(of: item) else { return }
+
+        selectedItems.formUnion(items[index...])
+        updateCounts()
+    }
+
+    // MARK: - Long Press
+
+    /// Handles when an item is long pressed:
+    /// - If we're not currently in the multi selection mode, then we'll enter and select the pressed item
+    /// - Otherwise we'll show the Select All Above/Below options picker
+    func longPressed(_ item: Model) {
+        // If we're not multiselecting, then enter and select the long pressed item
+        guard isMultiSelecting else {
+            isMultiSelecting = true
+            selectItem(item)
+            return
+        }
+
+        // Show the select all above/below options
+        showOptionsPicker(item)
+    }
+}
+
+// MARK: - Private Methods
+
+private extension MutliSelectListViewModel {
+    func updateCounts() {
+        let selected = selectedItems.count
+        numberOfSelectedItems = selected
+        hasSelectedAll = selected == items.count
+    }
+
+    /// Shows the option picker to allow for Select All Above/Below
+    func showOptionsPicker(_ item: Model) {
+        let optionPicker = OptionsPicker(title: nil)
+
+        optionPicker.addActions([
+            .init(label: L10n.selectAllAbove, icon: "selectall-up") { [weak self] in
+                self?.selectAllBefore(item)
+            },
+            .init(label: L10n.selectAllBelow, icon: "selectall-down") { [weak self] in
+                self?.selectAllAfter(item)
+            }
+        ])
+
+        optionPicker.show(statusBarStyle: AppTheme.defaultStatusBarStyle())
+    }
+}
