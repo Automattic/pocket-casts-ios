94ec1d6eca503a817a837bd5724b92cde4c5ac96
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-07-12 22:33:34 -0400

    Add the edit/delete actions to the view model

--- podcasts/Bookmarks/List/BookmarkListViewModel.swift
+++ podcasts/Bookmarks/List/BookmarkListViewModel.swift
@@ -42,11 +42,53 @@ class BookmarkListViewModel: MutliSelectListViewModel<Bookmark> {
             .store(in: &cancellables)
     }
 
+    // MARK: - View Methods
+
     func bookmarkTapped(_ bookmark: Bookmark) {
-        print("Tapped")
+        guard isMultiSelecting else {
+            return
+        }
+
+        toggleItemSelected(bookmark)
     }
 
     func bookmarkPlayTapped(_ bookmark: Bookmark) {
         router?.bookmarkPlay(bookmark)
+    }
+
+    func editSelectedBookmarks() {
+        guard let bookmark = selectedItems.first else { return }
+
+        print("TODO \(bookmark)")
+    }
+
+    func deleteSelectedBookmarks() {
+        guard numberOfSelectedItems > 0 else { return }
+
+        let items = Array(selectedItems)
+
+        confirmDeletion { [weak self] in
+            self?.actuallyDelete(items)
+        }
+    }
+}
+
+private extension BookmarkListViewModel {
+    func confirmDeletion(_ delete: @escaping () -> Void) {
+        guard let controller = SceneHelper.rootViewController() else { return }
+
+        let alert = UIAlertController(title: L10n.bookmarkDeleteWarningTitle,
+                                      message: L10n.bookmarkDeleteWarningBody,
+                                      preferredStyle: .alert)
+
+        alert.addAction(.init(title: L10n.cancel, style: .cancel))
+        alert.addAction(.init(title: L10n.delete, style: .destructive, handler: { _ in
+            delete()
+        }))
+
+        controller.present(alert, animated: true, completion: nil)
+    }
+
+    func actuallyDelete(_ items: [Bookmark]) {
     }
 }
