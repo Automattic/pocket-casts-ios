d808432e45c9fcaa2836006aa7e3e80c383e8092
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-07-14 10:36:08 -0400

    Improve tests

--- Modules/DataModel/Tests/PocketCastsDataModelTests/BookmarkDataManagerTests.swift
+++ Modules/DataModel/Tests/PocketCastsDataModelTests/BookmarkDataManagerTests.swift
@@ -29,6 +29,7 @@ final class BookmarkDataManagerTests: XCTestCase {
         dbQueue.close()
     }
 
+    // MARK: - Adding
     func testAddBookmarkSucceeds() {
         XCTAssertNotNil(dataManager.add(episodeUuid: "episode-uuid", podcastUuid: "podcast-uuid", time: 1))
     }
@@ -42,24 +43,21 @@ final class BookmarkDataManagerTests: XCTestCase {
     }
 
     func testAddingExistingBookmarkIsNotAdded() {
-        let episode = "episode-uuid"
-        let podcast = "podcast-uuid"
-        let time = 1.0
-
-        let firstUuid = dataManager.add(episodeUuid: episode, podcastUuid: podcast, time: time)
-        let secondUuid = dataManager.add(episodeUuid: episode, podcastUuid: podcast, time: time)
+        let firstUuid = addBookmark()
+        let secondUuid = addBookmark()
 
         XCTAssertEqual(firstUuid, secondUuid)
     }
 
+    // MARK: - Retrieving
+
     func testGettingAllBookmarksForPodcast() {
         let podcast = "podcast-uuid"
 
-        dataManager.add(episodeUuid: "episode-1", podcastUuid: podcast, time: 1)
-        dataManager.add(episodeUuid: "episode-1", podcastUuid: podcast, time: 3)
-
-        dataManager.add(episodeUuid: "episode-2", podcastUuid: podcast, time: 1)
-        dataManager.add(episodeUuid: "episode-2", podcastUuid: podcast, time: 3)
+        ["episode-1", "episode-2"].forEach {
+            addBookmark(episodeUuid: $0, podcastUuid: podcast, time: 1)
+            addBookmark(episodeUuid: $0, podcastUuid: podcast, time: 3)
+        }
 
         let bookmarks = dataManager.bookmarks(forPodcast: podcast)
         XCTAssertEqual(bookmarks.count, 4)
@@ -68,42 +66,48 @@ final class BookmarkDataManagerTests: XCTestCase {
     func testGettingAllBookmarksForPodcastAndEpisode() {
         let podcast = "podcast-uuid"
 
-        dataManager.add(episodeUuid: "episode-1", podcastUuid: podcast, time: 1)
-        dataManager.add(episodeUuid: "episode-1", podcastUuid: podcast, time: 3)
-
-        dataManager.add(episodeUuid: "episode-2", podcastUuid: podcast, time: 1)
-        dataManager.add(episodeUuid: "episode-2", podcastUuid: podcast, time: 3)
+        ["episode-1", "episode-2"].forEach {
+            addBookmark(episodeUuid: $0, podcastUuid: podcast, time: 1)
+            addBookmark(episodeUuid: $0, podcastUuid: podcast, time: 3)
+        }
 
         let bookmarks = dataManager.bookmarks(forPodcast: podcast, episodeUuid: "episode-2")
         XCTAssertEqual(bookmarks.count, 2)
     }
 
+    // MARK: - Data Validation
+
+    func testBookmarkReturnsCorrectValues() {
+        let episode = "episode-uuid"
+        let podcast = "podcast-uuid"
+        let time: TimeInterval = 12345
+        let created = Date(timeIntervalSince1970: 0)
+
+        let bookmark = addBookmark(episodeUuid: episode, podcastUuid: podcast, time: time, created: created)
+
+        XCTAssertEqual(bookmark.episodeUuid, episode)
+        XCTAssertEqual(bookmark.podcastUuid, podcast)
+        XCTAssertEqual(bookmark.time, time)
+        XCTAssertEqual(bookmark.createdDate, created)
+    }
+
     // MARK: - Deletion
 
     func testRemovingBookmarksSucceeds() async {
-        let bookmark = dataManager.add(episodeUuid: "episode-1", podcastUuid: "podcast-uuid", time: 1).flatMap {
-            dataManager.bookmark(for: $0)
-        }!
-
+        let bookmark = addBookmark()
         let success = await dataManager.remove(bookmarks: [bookmark])
         XCTAssertTrue(success)
     }
 
     func testRemovedBookmarksArentReturned() async {
-        let bookmark = dataManager.add(episodeUuid: "episode-1", podcastUuid: "podcast-uuid", time: 1).flatMap {
-            dataManager.bookmark(for: $0)
-        }!
-
+        let bookmark = addBookmark()
         _ = await dataManager.remove(bookmarks: [bookmark])
 
         XCTAssertNil(dataManager.bookmark(for: bookmark.uuid))
     }
 
     func testAllBookmarksReturnsDeletedItems() async {
-        let bookmark = dataManager.add(episodeUuid: "episode-1", podcastUuid: "podcast-uuid", time: 1).flatMap {
-            dataManager.bookmark(for: $0)
-        }!
-
+        let bookmark = addBookmark()
         _ = await dataManager.remove(bookmarks: [bookmark])
         let allBookmarks = dataManager.allBookmarks(includeDeleted: true)
 
@@ -111,13 +115,19 @@ final class BookmarkDataManagerTests: XCTestCase {
     }
 
     func testBookmarkIsPermanentlyRemoved() async {
-        let bookmark = dataManager.add(episodeUuid: "episode-1", podcastUuid: "podcast-uuid", time: 1).flatMap {
-            dataManager.bookmark(for: $0)
-        }!
-
+        let bookmark = addBookmark()
         let success = await dataManager.permanentlyDelete(bookmarks: [bookmark])
         XCTAssertTrue(success)
 
         XCTAssertTrue(dataManager.allBookmarks(includeDeleted: true).isEmpty)
+    }
+}
+
+private extension BookmarkDataManagerTests {
+    @discardableResult
+    func addBookmark(episodeUuid: String = "episode-1", podcastUuid: String = "podcast-uuid", time: TimeInterval = 1, created: Date = .now) -> Bookmark {
+        dataManager.add(episodeUuid: episodeUuid, podcastUuid: podcastUuid, time: time, dateCreated: created).flatMap {
+            dataManager.bookmark(for: $0)
+        }!
     }
 }
