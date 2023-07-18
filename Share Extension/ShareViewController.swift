2efb943a7bbe436d3e9fcc9b0fdf8b5968ca39eb
Leandro Alonso <contato@leandroalonso.com>
2023-07-18 12:40:26 -0300

    Copy the file to the shared group container

--- Share Extension/ShareViewController.swift
+++ Share Extension/ShareViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import UniformTypeIdentifiers
 import Social
 
 class ShareViewController: UIViewController {
@@ -6,16 +7,43 @@ class ShareViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        let content = extensionContext?.inputItems.first as? NSExtensionItem
+        guard let attachment = content?.attachments?.first as? NSItemProvider else {
+            close()
+            return
+        }
 
-        redirectToHostApp()
+        if attachment.hasItemConformingToTypeIdentifier(UTType.data.identifier) {
+
+            attachment.loadItem(forTypeIdentifier: UTType.data.identifier, options: nil) { [weak self] data, error in
+                guard let url = data as? URL else {
+                    return
+                }
+
+                let fileManager = FileManager.default
+                guard let container = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.au.com.shiftyjelly.pocketcasts") else {
+                    return
+                }
+
+                let destURL = container.appendingPathComponent("opml.opml")
+
+                do { try FileManager.default.copyItem(at: url, to: destURL) } catch { }
+
+                self?.close()
+
+                self?.redirectToHostApp(destURL.absoluteString)
+            }
+        }
     }
 
-    func redirectToHostApp() {
-        let url = URL(string: "pktc://item?id=20036169")
+    func redirectToHostApp(_ url: String) {
+        guard let url = URL(string: "pktc://import-opml/\(url)") else {
+            return
+        }
+
         let selectorOpenURL = sel_registerName("openURL:")
         let context = NSExtensionContext()
-        context.open(url! as URL, completionHandler: nil)
+        context.open(url as URL, completionHandler: nil)
 
         var responder = self as UIResponder?
 
@@ -23,8 +51,12 @@ class ShareViewController: UIViewController {
             if responder?.responds(to: selectorOpenURL) == true {
                 responder?.perform(selectorOpenURL, with: url)
             }
-            responder = responder!.next
+            responder = responder?.next
         }
+    }
+
+    private func close() {
+        self.extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
     }
 
 }
