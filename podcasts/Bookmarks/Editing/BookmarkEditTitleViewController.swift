cee3e06bbc828018f9c04c98ab66a8be962b6d48
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-07-18 18:53:18 -0400

    Add a closure when the edit title view is dismissed

--- podcasts/Bookmarks/Editing/BookmarkEditTitleViewController.swift
+++ podcasts/Bookmarks/Editing/BookmarkEditTitleViewController.swift
@@ -3,10 +3,17 @@ import PocketCastsDataModel
 
 class BookmarkEditTitleViewController: ThemedHostingController<BookmarkEditTitleView> {
     private let viewModel: BookmarkEditViewModel
+    let onDismiss: (String) -> Void
+    let bookmark: Bookmark
 
-    init(manager: BookmarkManager, bookmark: Bookmark, state: BookmarkEditViewModel.EditState) {
+    init(manager: BookmarkManager,
+         bookmark: Bookmark,
+         state: BookmarkEditViewModel.EditState,
+         onDismiss: @escaping (String) -> Void) {
         let viewModel = BookmarkEditViewModel(manager: manager, bookmark: bookmark, state: state)
         self.viewModel = viewModel
+        self.bookmark = bookmark
+        self.onDismiss = onDismiss
 
         super.init(rootView: .init(viewModel: viewModel))
 
@@ -27,10 +34,11 @@ class BookmarkEditTitleViewController: ThemedHostingController<BookmarkEditTitle
 extension BookmarkEditTitleViewController: BookmarkEditRouter {
     func dismiss() {
         dismiss(animated: true)
+        onDismiss(bookmark.title)
     }
 
     func titleUpdated(title: String) {
-        print("Title updated!")
-        dismiss()
+        dismiss(animated: true)
+        onDismiss(title)
     }
 }
