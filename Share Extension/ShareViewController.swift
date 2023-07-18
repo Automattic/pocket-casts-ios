6b272e417642647c3f8929973d845ea28fc8ed07
Leandro Alonso <contato@leandroalonso.com>
2023-07-18 11:06:18 -0300

    Open Pocket Casts when sharing an OPML file

--- Share Extension/ShareViewController.swift
+++ Share Extension/ShareViewController.swift
@@ -1,22 +1,30 @@
 import UIKit
 import Social
 
-class ShareViewController: SLComposeServiceViewController {
+class ShareViewController: UIViewController {
 
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        return true
-    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+
+        redirectToHostApp()
     }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
+    func redirectToHostApp() {
+        let url = URL(string: "pktc://item?id=20036169")
+        let selectorOpenURL = sel_registerName("openURL:")
+        let context = NSExtensionContext()
+        context.open(url! as URL, completionHandler: nil)
+
+        var responder = self as UIResponder?
+
+        while responder != nil {
+            if responder?.responds(to: selectorOpenURL) == true {
+                responder?.perform(selectorOpenURL, with: url)
+            }
+            responder = responder!.next
+        }
     }
 
 }
