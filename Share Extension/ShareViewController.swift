35f0086952a10eae7064cde5b02a1dcfcf388f62
Leandro Alonso <contato@leandroalonso.com>
2023-07-19 10:22:56 -0300

    Add documentation of what is going on

--- Share Extension/ShareViewController.swift
+++ Share Extension/ShareViewController.swift
@@ -7,20 +7,26 @@ class ShareViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
+        // Get the attachment
         let content = extensionContext?.inputItems.first as? NSExtensionItem
         guard let attachment = content?.attachments?.first as? NSItemProvider else {
             close()
             return
         }
 
+        // We only accept OPML files, and they need to conform to data identifier
+        // If we ever want to accept other files we need to change the Share Extension
+        // Info.plist
         if attachment.hasItemConformingToTypeIdentifier(UTType.data.identifier) {
 
+            // Request the OPML file URL
             attachment.loadItem(forTypeIdentifier: UTType.data.identifier, options: nil) { [weak self] data, error in
                 guard let url = data as? URL else {
                     self?.close()
                     return
                 }
 
+                // Convert the file to Data
                 guard let opmlData = try? Data(contentsOf: url) else {
                     self?.close()
                     return
@@ -28,6 +34,7 @@ class ShareViewController: UIViewController {
 
                 self?.close()
 
+                // Redirect to Pocket Casts sharing the OPML data encoded in Base64
                 self?.redirectToHostApp(opmlData.base64EncodedString())
             }
         }
