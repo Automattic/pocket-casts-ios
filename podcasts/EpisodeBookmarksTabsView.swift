61b072019382babfc96a07abf8166a2e8ce07832
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-07-20 16:02:47 -0400

    Fix background of tab view

--- podcasts/EpisodeBookmarksTabsView.swift
+++ podcasts/EpisodeBookmarksTabsView.swift
@@ -3,13 +3,14 @@ import SwiftUI
 /// Displays a fake set of tabs that allows the user to open the bookmarks view from the podcast list
 struct EpisodeBookmarksTabsView: View {
     @EnvironmentObject var theme: Theme
-    
+
     weak var delegate: PodcastActionsDelegate?
-    
+
     var body: some View {
         HStack(spacing: 12) {
-            Text(L10n.episodes).applyStyle(theme: theme)
-            
+            Text(L10n.episodes)
+                .applyStyle(theme: theme, highlighted: true)
+
             Text(L10n.bookmarks)
                 .buttonize {
                     delegate?.showBookmarks()
@@ -18,27 +19,32 @@ struct EpisodeBookmarksTabsView: View {
                         .applyStyle(theme: theme)
                         .applyButtonEffect(isPressed: config.isPressed)
                 }
-            
+
             Spacer()
         }
         .font(style: .subheadline, weight: .medium)
     }
 }
 
-struct EpisodeBookmarksTabsView_Previews: PreviewProvider {
-    static var previews: some View {
-        EpisodeBookmarksTabsView()
-            .setupDefaultEnvironment()
-    }
-}
+// MARK: - View Extension
 
 private extension View {
-    func applyStyle(theme: Theme) -> some View {
+    func applyStyle(theme: Theme, highlighted: Bool = false) -> some View {
         self
             .contentShape(Rectangle())
             .padding(.vertical, 8)
             .padding(.horizontal, 12)
-            .foregroundColor(theme.primaryText02)
+            .foregroundColor(highlighted ? theme.primaryUi01 : theme.primaryText02)
+            .background(highlighted ? theme.primaryText01 : nil)
             .cornerRadius(8)
+    }
+}
+
+// MARK: - Previews
+
+struct EpisodeBookmarksTabsView_Previews: PreviewProvider {
+    static var previews: some View {
+        EpisodeBookmarksTabsView()
+            .setupDefaultEnvironment()
     }
 }
