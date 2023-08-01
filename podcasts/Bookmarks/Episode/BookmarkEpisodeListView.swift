776bc26b79bd1a0fece43c9b14a30e20388960c5
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-08-01 01:04:54 -0400

    Hide the more button and add top padding

--- podcasts/Bookmarks/Episode/BookmarkEpisodeListView.swift
+++ podcasts/Bookmarks/Episode/BookmarkEpisodeListView.swift
@@ -4,6 +4,7 @@ struct BookmarkEpisodeListView: View {
     @ObservedObject var viewModel: BookmarkEpisodeListViewModel
 
     var body: some View {
-        BookmarksListView(viewModel: viewModel, style: ThemedBookmarksStyle())
+        BookmarksListView(viewModel: viewModel, style: ThemedBookmarksStyle(), showMoreInHeader: false)
+            .padding(.top, 16)
     }
 }
