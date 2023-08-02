065cc4ad5e2e487a267bc2e3f3acc24ccddc490c
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-08-01 20:56:43 -0400

    Wrap the episode detail tabs in a scroll view if we're on a small screen

--- podcasts/Bookmarks/Episode/EpisodeDetailTabView.swift
+++ podcasts/Bookmarks/Episode/EpisodeDetailTabView.swift
@@ -41,34 +41,54 @@ struct EpisodeDetailTabView: View {
     @EnvironmentObject var theme: Theme
     @ObservedObject var viewModel: EpisodeTabsViewModel
 
-    var body: some View {
-        HStack(spacing: 12) {
-            ForEach(viewModel.tabs) { tab in
-                Text(tab.title)
-                    .buttonize {
-                        viewModel.select(tab: tab)
-                    } customize: { config in
-                        config.label
-                            .applyStyle(theme: theme, highlighted: viewModel.selectedTab == tab)
-                            .applyButtonEffect(isPressed: config.isPressed)
-                    }
-            }
+    private var isSmallScreen: Bool {
+        UIScreen.main.bounds.height <= 667
+    }
 
-            Spacer()
+    var body: some View {
+        wrapperView {
+            HStack(spacing: 12) {
+                ForEach(viewModel.tabs) { tab in
+                    Text(tab.title)
+                        .buttonize {
+                            viewModel.select(tab: tab)
+                        } customize: { config in
+                            config.label
+                                .fixedSize()
+                                .applyStyle(theme: theme,
+                                            highlighted: viewModel.selectedTab == tab,
+                                            isSmallScreen: isSmallScreen)
+                                .applyButtonEffect(isPressed: config.isPressed)
+                        }
+                }
+
+                Spacer()
+            }
+            .font(style: .subheadline, weight: .medium)
         }
-        .font(style: .subheadline, weight: .medium)
-        .padding(.leading, 10)
+        .padding(.leading, isSmallScreen ? 0 : 10)
+    }
+
+    @ViewBuilder
+    func wrapperView<Content: View>(_ content: () -> Content) -> some View {
+        if isSmallScreen {
+            ScrollView(.horizontal, showsIndicators: false) {
+                content()
+            }
+        } else {
+            content()
+        }
     }
 }
 
 // MARK: - View Extension
 
 private extension View {
-    func applyStyle(theme: Theme, highlighted: Bool = false) -> some View {
+    func applyStyle(theme: Theme, highlighted: Bool = false, isSmallScreen: Bool) -> some View {
         self
             .contentShape(Rectangle())
             .padding(.vertical, 8)
-            .padding(.horizontal, 12)
+            .padding(.horizontal, isSmallScreen ? 6 : 12)
             .foregroundColor(highlighted ? theme.primaryUi01 : theme.primaryText02)
             .background(highlighted ? theme.primaryText01 : nil)
             .animation(.linear(duration: 0.1), value: highlighted)
