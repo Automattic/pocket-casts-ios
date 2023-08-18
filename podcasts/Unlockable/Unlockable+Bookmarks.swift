6be8eb305aa9b4a1790105ced71831acc211ef91
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-08-18 19:33:13 -0400

    Move the bookmarks unlockables into their own file

---
+++ podcasts/Unlockable/Unlockable+Bookmarks.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// MARK: - HeadphoneControlAction
+
+extension HeadphoneControlAction: Unlockable {
+    var paidFeature: PaidFeature? {
+        switch self {
+        case .addBookmark:
+            return .bookmarks
+        default:
+            return nil
+        }
+    }
+}
+
+// MARK: - PlayerAction
+
+extension PlayerAction: Unlockable {
+    var paidFeature: PaidFeature? {
+        switch self {
+        case .addBookmark:
+            return .bookmarks
+        default:
+            return nil
+        }
+    }
+}
