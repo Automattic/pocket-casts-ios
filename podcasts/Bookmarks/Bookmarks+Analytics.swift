7a68d93ad07f6a99f992ea081089c2b06fbaf292
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-08-18 22:42:51 -0400

    Add bookmark analytics helper file

---
+++ podcasts/Bookmarks/Bookmarks+Analytics.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+// MARK: - Source
+
+enum BookmarkAnalyticsSource: String, AnalyticsDescribable {
+    case podcasts = "podcast_screen"
+    case episodes = "episode_details"
+    case player
+    case files
+    case headphones
+
+    case unknown
+
+    var analyticsDescription: String {
+        rawValue
+    }
+}
+
+extension BookmarkSortOption: AnalyticsDescribable {
+    var analyticsDescription: String {
+        switch self {
+        case .newestToOldest:
+            return "date_added_newest_to_oldest"
+        case .oldestToNewest:
+            return "date_added_oldest_to_newest"
+        case .timestamp:
+            return "timestamp"
+        case .episode:
+            return "episode"
+        }
+    }
+}
