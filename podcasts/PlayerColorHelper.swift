40b1200312f913f52a4c7e78b3758c936f4bcedb
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-08-25 14:27:52 -0400

    Allow injecting of the episode into the PlayerColorHelper

--- podcasts/PlayerColorHelper.swift
+++ podcasts/PlayerColorHelper.swift
@@ -2,74 +2,83 @@ import Foundation
 import PocketCastsDataModel
 
 struct PlayerColorHelper {
-    static func playerBackgroundColor01() -> UIColor {
-        guard let podcastBackgroundColor = backgroundColorForPlayingEpisode() else { return UIColor.black }
+    static func playerBackgroundColor01(for theme: Theme.ThemeType = Theme.sharedTheme.activeTheme,
+                                        episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastBackgroundColor = backgroundColor(for: episode) else { return UIColor.black }
 
-        return ThemeColor.playerBackground01(podcastColor: podcastBackgroundColor)
+        return ThemeColor.playerBackground01(podcastColor: podcastBackgroundColor, for: theme)
     }
 
-    static func playerBackgroundColor02() -> UIColor {
-        guard let podcastBackgroundColor = backgroundColorForPlayingEpisode() else { return UIColor.black }
+    static func playerBackgroundColor02(for theme: Theme.ThemeType = Theme.sharedTheme.activeTheme,
+                                        episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastBackgroundColor = backgroundColor(for: episode) else { return UIColor.black }
 
-        return ThemeColor.playerBackground02(podcastColor: podcastBackgroundColor)
+        return ThemeColor.playerBackground02(podcastColor: podcastBackgroundColor, for: theme)
     }
 
-    static func playerHighlightColor01(for theme: Theme.ThemeType) -> UIColor {
-        guard let podcastColor = themeTintForPlayingEpisode(for: theme) else { return UIColor.white }
+    static func playerHighlightColor01(for theme: Theme.ThemeType,
+                                       episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastColor = tint(for: episode, with: theme) else { return UIColor.white }
 
         return ThemeColor.playerHighlight01(podcastColor: podcastColor)
     }
 
-    static func playerHighlightColor02(for theme: Theme.ThemeType) -> UIColor {
-        guard let podcastColor = themeTintForPlayingEpisode(for: theme) else { return UIColor.white }
+    static func playerHighlightColor02(for theme: Theme.ThemeType,
+                                       episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastColor = tint(for: episode, with: theme) else { return UIColor.white }
 
         return ThemeColor.playerHighlight02(podcastColor: podcastColor)
     }
 
-    static func playerHighlightColor06(for theme: Theme.ThemeType) -> UIColor {
-        guard let podcastColor = themeTintForPlayingEpisode(for: theme) else { return UIColor.white }
+    static func playerHighlightColor06(for theme: Theme.ThemeType,
+                                       episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastColor = tint(for: episode, with: theme) else { return UIColor.white }
 
         return ThemeColor.playerHighlight06(podcastColor: podcastColor)
     }
 
-    static func playerHighlightColor07(for theme: Theme.ThemeType) -> UIColor {
-        guard let podcastColor = themeTintForPlayingEpisode(for: theme) else { return UIColor.white }
+    static func playerHighlightColor07(for theme: Theme.ThemeType,
+                                       episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastColor = tint(for: episode, with: theme) else { return UIColor.white }
 
         return ThemeColor.playerHighlight07(podcastColor: podcastColor)
     }
 
-    static func podcastInteractive03(for theme: Theme.ThemeType) -> UIColor {
-        guard let podcastColor = themeTintForPlayingEpisode(for: theme) else { return UIColor.white }
+    static func podcastInteractive03(for theme: Theme.ThemeType,
+                                     episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastColor = tint(for: episode, with: theme) else { return UIColor.white }
 
         return ThemeColor.podcastInteractive03(podcastColor: podcastColor)
     }
 
-    static func podcastInteractive04(for theme: Theme.ThemeType) -> UIColor {
-        guard let podcastColor = themeTintForPlayingEpisode(for: theme) else { return UIColor.white }
+    static func podcastInteractive04(for theme: Theme.ThemeType,
+                                     episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastColor = tint(for: episode, with: theme) else { return UIColor.white }
 
         return ThemeColor.podcastInteractive04(podcastColor: podcastColor)
     }
 
-    static func podcastInteractive05(for theme: Theme.ThemeType) -> UIColor {
-        guard let podcastColor = themeTintForPlayingEpisode(for: theme) else { return UIColor.white }
+    static func podcastInteractive05(for theme: Theme.ThemeType,
+                                     episode: BaseEpisode? = PlaybackManager.shared.currentEpisode()) -> UIColor {
+        guard let podcastColor = tint(for: episode, with: theme) else { return UIColor.white }
 
         return ThemeColor.podcastInteractive05(podcastColor: podcastColor)
     }
 
-    private static func themeTintForPlayingEpisode(for theme: Theme.ThemeType) -> UIColor? {
-        if let episode = PlaybackManager.shared.currentEpisode() as? UserEpisode {
-            return episode.imageColor > 0 ? AppTheme.userEpisodeColor(number: Int(episode.imageColor)) : AppTheme.userEpisodeNoArtworkColor()
+    private static func tint(for episode: BaseEpisode?, with theme: Theme.ThemeType) -> UIColor? {
+        if let userEpisode = episode as? UserEpisode {
+            return userEpisode.imageColor > 0 ? AppTheme.userEpisodeColor(number: Int(userEpisode.imageColor)) : AppTheme.userEpisodeNoArtworkColor()
         }
 
-        guard let episode = PlaybackManager.shared.currentEpisode() as? Episode, let parentPodcast = episode.parentPodcast() else {
+        guard let parentPodcast = (episode as? Episode)?.parentPodcast() else {
             return nil
         }
 
         return theme.isDark ? ColorManager.darkThemeTintForPodcast(parentPodcast) : ColorManager.lightThemeTintForPodcast(parentPodcast)
     }
 
-    private static func backgroundColorForPlayingEpisode() -> UIColor? {
-        guard let episode = PlaybackManager.shared.currentEpisode() as? Episode, let parentPodcast = episode.parentPodcast() else {
+    private static func backgroundColor(for episode: BaseEpisode?) -> UIColor? {
+        guard let parentPodcast = (episode as? Episode)?.parentPodcast() else {
             return nil
         }
 
