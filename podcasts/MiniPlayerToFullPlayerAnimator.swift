045a0218883edabfa0f282d487c8aad02542e9f4
Leandro Alonso <contato@leandroalonso.com>
2023-09-06 14:07:26 -0300

    Add a basic transition logic without any animation/effects

--- podcasts/MiniPlayerToFullPlayerAnimator.swift
+++ podcasts/MiniPlayerToFullPlayerAnimator.swift
@@ -1,11 +1,38 @@
 import UIKit
 
 class MiniPlayerToFullPlayerAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    private let fromViewController: UIViewController
+    private let toViewController: UIViewController
+    private let transition: Transition
+
+    init?(fromViewController: UIViewController, toViewController: UIViewController, transition: Transition) {
+        self.fromViewController = fromViewController
+        self.toViewController = toViewController
+        self.transition = transition
+    }
+
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         3.5
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+
+        guard let toView = toViewController.view else {
+            transitionContext.completeTransition(false)
+            return
+        }
+
+        containerView.addSubview(toView)
+        toView.frame = containerView.frame
+        toViewController.view.setNeedsLayout()
+        toViewController.view.layoutIfNeeded()
+
         transitionContext.completeTransition(true)
+    }
+
+    enum Transition {
+        case presenting
+        case dismissing
     }
 }
--- podcasts/MiniPlayerViewController+TransitionDelegate.swift
+++ podcasts/MiniPlayerViewController+TransitionDelegate.swift
@@ -2,10 +2,10 @@ import Foundation
 
 extension MiniPlayerViewController: UIViewControllerTransitioningDelegate {
     func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return MiniPlayerToFullPlayerAnimator()
+        return MiniPlayerToFullPlayerAnimator(fromViewController: self, toViewController: dismissed, transition: .dismissing)
     }
 
     func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return MiniPlayerToFullPlayerAnimator()
+        return MiniPlayerToFullPlayerAnimator(fromViewController: self, toViewController: presented, transition: .presenting)
     }
 }
