e691810912473a0781d5af3dc40f562ecc890aa1
Leandro Alonso <contato@leandroalonso.com>
2023-09-06 16:24:46 -0300

    Add the artwork and perform the transition

--- podcasts/MiniPlayerToFullPlayerAnimator.swift
+++ podcasts/MiniPlayerToFullPlayerAnimator.swift
@@ -14,6 +14,10 @@ class MiniPlayerToFullPlayerAnimator: NSObject, UIViewControllerAnimatedTransiti
     private var containerView = UIView()
     private var toView = UIView()
 
+    private var isPresenting: Bool {
+        transition == .presenting
+    }
+
     init?(fromViewController: UIViewController, toViewController: UIViewController, transition: Transition, miniPlayerArtwork: PodcastImageView, fullPlayerArtwork: UIImageView) {
         self.fromViewController = fromViewController
         self.toViewController = toViewController
@@ -68,7 +72,42 @@ class MiniPlayerToFullPlayerAnimator: NSObject, UIViewControllerAnimatedTransiti
         toViewController.view.setNeedsLayout()
         toViewController.view.layoutIfNeeded()
 
+        // Calculate initial and final frame for the artwork
+        var fullPlayerArtworkFrame: CGRect = {
+            var fullPlayerArtworkFrame = fullPlayerArtwork.superview?.convert(fullPlayerArtwork.frame, to: nil) ?? .zero
+            if !isPresenting {
+                fullPlayerArtworkFrame.origin = .init(x: fullPlayerArtworkFrame.origin.x, y: fullPlayerArtworkFrame.origin.y + fromFrame.origin.y)
+            }
+            return fullPlayerArtworkFrame
+        }()
+
+        var miniPlayerArtworkFrame = miniPlayerArtwork.superview?.convert(miniPlayerArtwork.frame, to: nil) ?? .zero
+
         toView.frame = fromFrame
+
+        let artwork = UIImageView()
+        artwork.image = fullPlayerArtwork.image
+
+        containerView.addSubview(artwork)
+        artwork.frame = isPresenting ? miniPlayerArtworkFrame : fullPlayerArtworkFrame
+        artwork.layer.cornerRadius = isPresenting ? miniPlayerArtwork.imageView!.layer.cornerRadius : fullPlayerArtwork.layer.cornerRadius
+        artwork.layer.masksToBounds = true
+
+        // has image?
+        if artwork.image != nil {
+            fullPlayerArtwork.layer.opacity = 0
+            miniPlayerArtwork.layer.opacity = 0
+        }
+
+        UIView.animate(withDuration: isPresenting ? 0.25 : 0.35, delay: isPresenting ? 0.1 : 0, options: .curveEaseInOut) { [self] in
+            artwork.frame = self.isPresenting ? fullPlayerArtworkFrame : miniPlayerArtworkFrame
+            artwork.layer.cornerRadius = self.isPresenting ? fullPlayerArtwork.layer.cornerRadius : miniPlayerArtwork.imageView!.layer.cornerRadius
+        } completion: { completed in
+            artwork.removeFromSuperview()
+
+            self.fullPlayerArtwork.layer.opacity = 1
+            self.miniPlayerArtwork.layer.opacity = 1
+        }
 
         UIView.animate(withDuration: duration, delay: 0, options: .curveEaseInOut) {
             toView.frame = toFrame
