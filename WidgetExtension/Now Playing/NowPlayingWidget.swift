cae4eb088aacb3037578119b7ea060480adfb1b1
Leandro Alonso <contato@leandroalonso.com>
2023-09-12 15:09:46 -0300

    Disable margins for now playing widget

--- WidgetExtension/Now Playing/NowPlayingWidget.swift
+++ WidgetExtension/Now Playing/NowPlayingWidget.swift
@@ -6,6 +6,7 @@ struct NowPlayingWidget: Widget {
         StaticConfiguration(kind: "Now_Playing_Widget", provider: NowPlayingProvider()) { entry in
             NowPlayingWidgetEntryView(entry: entry)
         }
+        .contentMarginsDisabled()
         .configurationDisplayName(L10n.nowPlaying)
         .description(L10n.widgetsNowPlayingDesc)
         .supportedFamilies([.systemSmall])
