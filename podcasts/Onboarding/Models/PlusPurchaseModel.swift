11652b6fc37a9ebc3a5715d76583fd9102407982
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-09-14 17:52:37 -0400

    Display an alert if the purchases are disabled

--- podcasts/Onboarding/Models/PlusPurchaseModel.swift
+++ podcasts/Onboarding/Models/PlusPurchaseModel.swift
@@ -39,6 +39,11 @@ class PlusPurchaseModel: PlusPricingInfoModel, OnboardingModel {
 
     // MARK: - Triggers the purchase process
     func purchase(product: Constants.IapProducts) {
+        guard purchaseHandler.canMakePurchases else {
+            showPurchaseDisabledAlert(product: product)
+            return
+        }
+
         guard purchaseHandler.buyProduct(identifier: product.rawValue) else {
             handlePurchaseFailed(error: nil)
             return
@@ -48,6 +53,22 @@ class PlusPurchaseModel: PlusPricingInfoModel, OnboardingModel {
 
         purchasedProduct = product
         state = .purchasing
+    }
+
+    func showPurchaseDisabledAlert(product: Constants.IapProducts) {
+        guard let presentingViewController = parentController ?? SceneHelper.rootViewController() else {
+            return
+        }
+
+        let displayName = product.subscriptionTier.displayName
+
+        let alert = UIAlertController(title: L10n.betaThankYou,
+                                      message: L10n.betaPurchaseDisabled(displayName),
+                                      preferredStyle: .alert)
+
+        alert.addAction(.init(title: L10n.ok, style: .cancel))
+
+        presentingViewController.present(alert, animated: true, completion: nil)
     }
 
     // Our internal state
