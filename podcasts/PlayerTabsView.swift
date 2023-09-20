87ebeb8f2de9485e2e79394e43748bc719f13f97
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-09-20 17:52:08 -0400

    Fix lint

--- podcasts/PlayerTabsView.swift
+++ podcasts/PlayerTabsView.swift
@@ -132,7 +132,7 @@ class PlayerTabsView: UIScrollView {
             button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
             tabsStackView.addArrangedSubview(button)
         }
-        
+
         // Add an empty view to make sure the sizes are calculated correctly when there is a longer first item
         let empty = UIView()
         empty.isUserInteractionEnabled = false
