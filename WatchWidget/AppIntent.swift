7d04bfd0e264001091d1fbddd6a8b032d4addabf
Leandro Alonso <contato@leandroalonso.com>
2023-09-22 13:49:52 -0300

    Update configuration app intent

--- WatchWidget/AppIntent.swift
+++ WatchWidget/AppIntent.swift
@@ -1,19 +1,7 @@
-//
-//  AppIntent.swift
-//  WatchWidget
-//
-//  Created by Leandro Alonso on 20/09/23.
-//  Copyright © 2023 Shifty Jelly. All rights reserved.
-//
-
 import WidgetKit
 import AppIntents
 
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
-    static var title: LocalizedStringResource = "Configuration"
-    static var description = IntentDescription("This is an example widget.")
-
-    // An example configurable parameter.
-    @Parameter(title: "Favorite Emoji", default: "😃")
-    var favoriteEmoji: String
+    static var title: LocalizedStringResource = "Up Next"
+    static var description = "Check your Up Next episode"
 }
