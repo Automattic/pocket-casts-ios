d0abdb05004b5d377f1f9c47bc1d1c5f383f5f7d
Leandro Alonso <contato@leandroalonso.com>
2023-09-26 16:34:16 -0300

    Add logic into the view model to allow (or not) the user to rate a show

--- podcasts/Ratings/RatePodcastViewModel.swift
+++ podcasts/Ratings/RatePodcastViewModel.swift
@@ -3,10 +3,24 @@ import SwiftUI
 class RatePodcastViewModel: ObservableObject {
     @Binding var presented: Bool
 
+    @Published var userCanRate: UserCanRate = .checking
+
     var podcastUuid: String
 
     init(presented: Binding<Bool>, podcastUuid: String) {
         self._presented = presented
         self.podcastUuid = podcastUuid
+        checkIfUserCanRate()
+    }
+
+    private func checkIfUserCanRate() {
+        // Check through an API if the user can rate this podcast
+        userCanRate = .disallowed
+    }
+
+    enum UserCanRate {
+        case checking
+        case allowed
+        case disallowed
     }
 }
