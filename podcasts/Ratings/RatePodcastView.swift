92beba9b0c2d025bc858c62308d88eb2b84243ac
Leandro Alonso <contato@leandroalonso.com>
2023-09-26 14:20:20 -0300

    Apply the current theme

--- podcasts/Ratings/RatePodcastView.swift
+++ podcasts/Ratings/RatePodcastView.swift
@@ -4,7 +4,13 @@ struct RatePodcastView: View {
     @Binding var presented: Bool
 
     var body: some View {
-        Button("dismiss") { self.presented = false }
+        Group {
+            Spacer()
+            Button("dismiss") { self.presented = false }
+            Spacer()
+        }
+        .frame(maxWidth: .infinity)
+        .applyDefaultThemeOptions()
     }
 }
 
