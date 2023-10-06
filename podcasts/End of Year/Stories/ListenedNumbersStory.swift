27a55de54c372a5579264cb78eb527e96341b229
Leandro Alonso <contato@leandroalonso.com>
2023-10-06 11:21:46 -0300

    Add the background gradient used in stories

--- podcasts/End of Year/Stories/ListenedNumbersStory.swift
+++ podcasts/End of Year/Stories/ListenedNumbersStory.swift
@@ -64,7 +64,14 @@ struct ListenedNumbersStory: ShareableStory {
                 .rotationEffect(Angle(degrees: -15))
                 .padding(.top, geometry.size.height * 0.1)
             }
-            .background(DynamicBackgroundView(podcast: podcasts[safe: 3] ?? podcasts[0]))
+            .background(
+                ZStack(alignment: .bottom) {
+                    Color.black
+
+                    StoryGradient()
+                    .offset(x: geometry.size.width * 0.4, y: geometry.size.height * 0.25)
+                }
+            )
         }
     }
 
@@ -87,6 +94,28 @@ struct ListenedNumbersStory: ShareableStory {
             StoryShareableProvider.new(AnyView(self)),
             StoryShareableText(L10n.eoyStoryListenedToNumbersShareText(listenedNumbers.numberOfPodcasts, listenedNumbers.numberOfEpisodes))
         ]
+    }
+}
+
+struct StoryGradient: View {
+    var body: some View {
+        Rectangle()
+        .foregroundColor(.clear)
+        .frame(width: 510, height: 510)
+        .background(
+            LinearGradient(
+                stops: [
+                    Gradient.Stop(color: Color(red: 0.25, green: 0.11, blue: 0.92), location: 0.00),
+                    Gradient.Stop(color: Color(red: 0.68, green: 0.89, blue: 0.86), location: 0.61),
+                    Gradient.Stop(color: Color(red: 0.87, green: 0.91, blue: 0.53), location: 1.00),
+                ],
+                startPoint: UnitPoint(x: 0.49, y: 0.11),
+                endPoint: UnitPoint(x: 0.49, y: 0.98)
+            )
+        )
+        .cornerRadius(510)
+        .blur(radius: 107)
+        .opacity(0.6)
     }
 }
 
