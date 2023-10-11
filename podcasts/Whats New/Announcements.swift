442ef98e99488e871f848666b9013f1d85e04051
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-10-11 15:53:49 -0400

    Update the bookmarks announcements to use the isEnabled flag

--- podcasts/Whats New/Announcements.swift
+++ podcasts/Whats New/Announcements.swift
@@ -8,6 +8,7 @@ struct Announcements {
     // In the case a user migrates to, let's say, 7.10 to 7.15 and
     // there were two announcements, the last one will be picked.
     var announcements: [WhatsNew.Announcement] = [
+        // Autoplay
         .init(
             version: "7.43",
             header: {
@@ -21,84 +22,47 @@ struct Announcements {
 
                 NavigationManager.sharedManager.navigateTo(NavigationManager.settingsProfileKey, data: nil)
             }
+        ),
+
+        // Bookmarks Early Access: Beta
+        // Show only in TestFlight, for Plus and Patron
+        .init(
+            version: "99.99",
+            header: { AnyView(EmptyView()) },
+            title: "Early Access Beta Title",
+            message: "Message",
+            buttonTitle: "Button",
+            action: {},
+            isEnabled: PaidFeature.bookmarks.inEarlyAccess && BuildEnvironment.current == .testFlight && SubscriptionHelper.activeTier > .none
+        ),
+
+        // Bookmarks Early Access: Release
+        // Show when not in beta, for Patron only
+        .init(
+            version: "99.99",
+            header: { AnyView(EmptyView().onAppear {
+                // Record when someone sees the full announcement while in early access so we don't show it again to them when we move to full release.
+                UserDefaults.standard.setValue(true, forKey: "WhatsNew.Bookmarks.EarlyAccess.Seen")
+            }) },
+            title: "Early Access Normal Title",
+            message: "Message",
+            buttonTitle: "Button",
+            action: {},
+            isEnabled: PaidFeature.bookmarks.inEarlyAccess && BuildEnvironment.current != .testFlight && SubscriptionHelper.activeTier == .patron
+        ),
+
+        // Bookmarks: Full Release
+        // Show for everyone, except those who saw the `Early Access: Release` announcement
+        .init(
+            version: "99.99",
+            header: { AnyView(EmptyView()) },
+            title: "Release Title",
+            message: "Message",
+            buttonTitle: "Button",
+            action: {},
+            isEnabled: !PaidFeature.bookmarks.inEarlyAccess && !UserDefaults.standard.bool(forKey: "WhatsNew.Bookmarks.EarlyAccess.Seen")
         )
     ]
-
-    init() {
-        addBookmarksEarlyAccessAnnouncement()
-    }
-
-    /// Add the bookmarks announcement when in Early Access.
-    ///
-    /// Since the beta and App Store binaries are the same we'll dynamically determine:
-    /// - If we're in beta we'll show a "join beta testing" message for Plus and Patron
-    /// - If we're in production we'll show the regular announcement for Patron
-    ///
-    /// If not we don't show the what's new at all.
-    mutating func addBookmarksEarlyAccessAnnouncement() {
-        guard FeatureFlag.bookmarks.enabled, PaidFeature.bookmarks.inEarlyAccess else { return }
-
-        let activeTier = SubscriptionHelper.activeTier
-        let environment = BuildEnvironment.current
-        let isBeta = environment == .testFlight
-
-        // Check to determine if we are showing the announcement
-        let shouldShowInBeta = isBeta && activeTier > .none
-        let shouldShowInRelease = !isBeta && activeTier == .patron
-
-        // Don't add the announcement if we're not showing it
-        guard shouldShowInBeta || shouldShowInRelease else { return }
-
-        let title = isBeta ? "Beta Title" : "Normal Title"
-
-        // TODO: Update with the real content
-        let announcement = WhatsNew.Announcement(
-            version: "99.99",
-            header: {
-                AnyView(AutoplayWhatsNewHeader().onAppear {
-                    // Record when someone sees the full announcement while in early access so we don't show it again
-                    // to them when we move to full release.
-                    if shouldShowInRelease {
-                        UserDefaults.standard.setValue(true, forKey: "WhatsNew.Bookmarks.EarlyAccess.Seen")
-                    }
-                })
-            },
-            title: title,
-            message: "Bookmarks Message",
-            buttonTitle: "Action Button",
-            action: { }
-        )
-
-        announcements.append(announcement)
-    }
-
-    /// When bookmarks is out of early access we'll show the announcement to everyone
-    /// but don't show it to people who already saw the full announcement when it was in early access
-    /// This is a placeholder for now.
-    /**
-    mutating func addBookmarksReleaseAnnouncement() {
-        guard
-            FeatureFlag.bookmarks.enabled, !PaidFeature.bookmarks.inEarlyAccess,
-            !UserDefaults.standard.bool(forKey: "WhatsNew.Bookmarks.EarlyAccess.Seen")
-        else {
-            return
-        }
-
-        // TODO: Update with the real content
-        let announcement = WhatsNew.Announcement(
-            version: "99.99",
-            header: {
-                AnyView(AutoplayWhatsNewHeader())
-            },
-            title: "Normal Title",
-            message: "Bookmarks Message",
-            buttonTitle: "Action Button",
-            action: { }
-        )
-
-        announcements.append(announcement)
-    }
-     */
 }
 
 class AnnouncementFlow {
