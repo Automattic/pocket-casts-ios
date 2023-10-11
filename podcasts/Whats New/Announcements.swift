55d498088bd947ae72f6b29cf89e8a8d0c4d8046
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-10-10 22:37:40 -0400

    Add the logic to show the what's new in early access

--- podcasts/Whats New/Announcements.swift
+++ podcasts/Whats New/Announcements.swift
@@ -1,5 +1,7 @@
 import Foundation
 import SwiftUI
+import PocketCastsServer
+import PocketCastsUtils
 
 struct Announcements {
     // Order is important.
@@ -21,6 +23,54 @@ struct Announcements {
             }
         )
     ]
+
+    init() {
+        addBookmarksEarlyAccessAnnouncement()
+    }
+
+    /// Add the bookmarks announcement when in Early Access.
+    ///
+    /// Since the beta and App Store binaries are the same we'll dynamically determine:
+    /// - If we're in beta we'll show a "join beta testing" message for Plus and Patron
+    /// - If we're in production we'll show the regular announcement for Patron
+    ///
+    /// If not we don't show the what's new at all.
+    mutating func addBookmarksEarlyAccessAnnouncement() {
+        guard FeatureFlag.bookmarks.enabled, PaidFeature.bookmarks.inEarlyAccess else { return }
+
+        let activeTier = SubscriptionHelper.activeTier
+        let environment = BuildEnvironment.current
+        let isBeta = environment == .testFlight
+
+        // Check to determine if we are showing the announcement
+        let shouldShowInBeta = isBeta && activeTier > .none
+        let shouldShowInRelease = !isBeta && activeTier == .patron
+
+        // Don't add the announcement if we're not showing it
+        guard shouldShowInBeta || shouldShowInRelease else { return }
+
+        let title = isBeta ? "Beta Title" : "Normal Title"
+
+        // TODO: Update with the real content
+        let announcement = WhatsNew.Announcement(
+            version: "7.50",
+            header: {
+                AnyView(AutoplayWhatsNewHeader().onAppear {
+                    // Record when someone sees the full announcement while in early access so we don't show it again
+                    // to them when we move to full release.
+                    if shouldShowInRelease {
+                        UserDefaults.standard.setValue(true, forKey: "WhatsNew.Bookmarks.EarlyAccess.Seen")
+                    }
+                })
+            },
+            title: title,
+            message: "Bookmarks Message",
+            buttonTitle: "Action Button",
+            action: { }
+        )
+
+        announcements.append(announcement)
+    }
 }
 
 class AnnouncementFlow {
