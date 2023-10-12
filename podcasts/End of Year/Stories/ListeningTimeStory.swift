1a8332e9e12822de808ab15978906ef3c8485a57
Leandro Alonso <contato@leandroalonso.com>
2023-10-12 16:00:40 -0300

    Extract the listening circle days to a view

--- podcasts/End of Year/Stories/ListeningTimeStory.swift
+++ podcasts/End of Year/Stories/ListeningTimeStory.swift
@@ -11,16 +11,6 @@ struct ListeningTimeStory: ShareableStory {
 
     let podcasts: [Podcast]
 
-    var numberOfBalls: Int {
-        Int(listeningTime.firstNumber) ?? 0 > 100 ? 10 : 7
-    }
-
-    var numberOfLines: Int {
-        var linesSlashBalls = (Double(listeningTime.firstNumber) ?? 1) / Double(numberOfBalls)
-        linesSlashBalls.round(.up)
-        return Int(linesSlashBalls)
-    }
-
     var body: some View {
         GeometryReader { geometry in
             PodcastCoverContainer(geometry: geometry) {
@@ -47,66 +37,7 @@ struct ListeningTimeStory: ShareableStory {
 
                 Spacer()
 
-                let maxArea = CGSize(width: geometry.size.width, height: geometry.size.height * 0.3)
-                let number = Double(listeningTime / 86400).rounded(.up)
-                let eachBallSquareArea = Double(maxArea.width * maxArea.height) / number
-                let numberOfBallsPerLine = max(7, (maxArea.width / eachBallSquareArea.squareRoot()).rounded(.up))
-                let numberOfLines = min((number / numberOfBallsPerLine).rounded(.up), max(1, (maxArea.height / eachBallSquareArea.squareRoot()).rounded(.up)))
-                let ballCalculatedWidth = maxArea.width / numberOfBallsPerLine
-                let ballCalculatedHeight = maxArea.height / numberOfLines
-                let ballPadding = min(ballCalculatedWidth, ballCalculatedHeight) * 0.05
-                let ballFinalWidth = min(ballCalculatedWidth, ballCalculatedHeight) - 4 * ballPadding
-                // The width of the days displayed as balls that the user didn't listened to podcasts
-                let missingDaysWidth = ((((numberOfLines * numberOfBallsPerLine) * 86400) - listeningTime) / 86400) * (ballFinalWidth + (2 * ballPadding))
-                let ballTotalArea = min(ballCalculatedWidth, ballCalculatedHeight)
-
-                VStack(spacing: 0) {
-                    ForEach(0..<Int(numberOfLines), id: \.self) { _ in
-                        HStack(spacing: 0) {
-                            ForEach(0..<Int(numberOfBallsPerLine), id: \.self) { _ in
-                                Circle()
-                                    .foregroundStyle(.white)
-                                    .frame(width: ballFinalWidth, height: ballFinalWidth)
-                                    .padding(.all, ballPadding)
-                            }
-                        }
-                    }
-                    .opacity(0)
-                }
-                .background(
-                    ZStack(alignment: .bottomTrailing) {
-                        LinearGradient(
-                            stops: [
-                                Gradient.Stop(color: Color(red: 0.25, green: 0.11, blue: 0.92), location: 0.00),
-                                Gradient.Stop(color: Color(red: 0.68, green: 0.89, blue: 0.86), location: 0.24),
-                                Gradient.Stop(color: Color(red: 0.87, green: 0.91, blue: 0.53), location: 0.50),
-                                Gradient.Stop(color: Color(red: 0.91, green: 0.35, blue: 0.26), location: 0.76),
-                                Gradient.Stop(color: Color(red: 0.1, green: 0.1, blue: 0.1), location: 1.00),
-                            ],
-                            startPoint: UnitPoint(x: 0, y: 0),
-                            endPoint: UnitPoint(x: 1.22, y: 1.25)
-                        )
-
-                        Rectangle()
-                            .foregroundStyle(Color(hex: "8F97A4"))
-                            .frame(width: missingDaysWidth, height: ballFinalWidth + 2 * ballPadding)
-                    }
-                    .mask (
-                        VStack(spacing: 0) {
-                            ForEach(0..<Int(numberOfLines), id: \.self) { _ in
-                                HStack(spacing: 0) {
-                                    ForEach(0..<Int(numberOfBallsPerLine), id: \.self) { _ in
-                                        Circle()
-                                            .foregroundStyle(.white)
-                                            .frame(width: ballFinalWidth, height: ballFinalWidth)
-                                            .padding(.all, ballPadding)
-                                    }
-                                }
-                            }
-                        }
-                    )
-
-                )
+                CircleDays(proxy: geometry, listeningTime: listeningTime)
 
                 Spacer()
 
@@ -139,17 +70,84 @@ struct ListeningTimeStory: ShareableStory {
             StoryShareableText(L10n.eoyStoryListenedToShareText(listeningTime.storyTimeDescriptionForSharing))
         ]
     }
+}
 
-    private enum Constants {
-        /// The podcasts that are displayed on the view, the middle is your top 10 podcast
-        static let displayedPodcasts = [1, 0, 2]
-        static let coverSize = 180.0
+/// Given a GeometryProxy and a Double listening time it
+/// returns a view filled with circles representing days of listening.
+///
+/// The minimum amount of circles is 7.
+///
+/// The algorithm takes a `maxArea` and calculates how many balls it
+/// can fill there. However the returned `View` might be bigger or smaller,
+/// the area just give it some constraints to calculate values.
+struct CircleDays: View {
+    let proxy: GeometryProxy
+    let listeningTime: Double
 
-        static let spaceBetweenLabels = 22.0
-        static let labelHorizontalPadding = 35.0
+    var body: some View {
+        let maxArea = CGSize(width: proxy.size.width, height: proxy.size.height * 0.3)
+        let number = Double(listeningTime / 86400).rounded(.up)
+        let eachBallSquareArea = Double(maxArea.width * maxArea.height) / number
+        let numberOfBallsPerLine = max(7, (maxArea.width / eachBallSquareArea.squareRoot()).rounded(.up))
+        let numberOfLines = min((number / numberOfBallsPerLine).rounded(.up), max(1, (maxArea.height / eachBallSquareArea.squareRoot()).rounded(.up)))
+        let ballCalculatedWidth = maxArea.width / numberOfBallsPerLine
+        let ballCalculatedHeight = maxArea.height / numberOfLines
+        let ballPadding = min(ballCalculatedWidth, ballCalculatedHeight) * 0.05
+        let ballFinalWidth = min(ballCalculatedWidth, ballCalculatedHeight) - 4 * ballPadding
 
-        /// Top padding is a percent calculated using the height of the view
-        static let topPadding = 0.158
+        // The width of the days displayed as balls that the user didn't listened to podcasts
+        let missingDaysWidth = ((((numberOfLines * numberOfBallsPerLine) * 86400) - listeningTime) / 86400) * (ballFinalWidth + (2 * ballPadding))
+        let ballTotalArea = min(ballCalculatedWidth, ballCalculatedHeight)
+
+        // The content is repeated on the background so the gradient
+        // can have the exact same size as the circles.
+        VStack(spacing: 0) {
+            ForEach(0..<Int(numberOfLines), id: \.self) { _ in
+                HStack(spacing: 0) {
+                    ForEach(0..<Int(numberOfBallsPerLine), id: \.self) { _ in
+                        Circle()
+                            .foregroundStyle(.white)
+                            .frame(width: ballFinalWidth, height: ballFinalWidth)
+                            .padding(.all, ballPadding)
+                    }
+                }
+            }
+            .opacity(0)
+        }
+        .background(
+            ZStack(alignment: .bottomTrailing) {
+                LinearGradient(
+                    stops: [
+                        Gradient.Stop(color: Color(red: 0.25, green: 0.11, blue: 0.92), location: 0.00),
+                        Gradient.Stop(color: Color(red: 0.68, green: 0.89, blue: 0.86), location: 0.24),
+                        Gradient.Stop(color: Color(red: 0.87, green: 0.91, blue: 0.53), location: 0.50),
+                        Gradient.Stop(color: Color(red: 0.91, green: 0.35, blue: 0.26), location: 0.76),
+                        Gradient.Stop(color: Color(red: 0.1, green: 0.1, blue: 0.1), location: 1.00),
+                    ],
+                    startPoint: UnitPoint(x: 0, y: 0),
+                    endPoint: UnitPoint(x: 1.22, y: 1.25)
+                )
+
+                Rectangle()
+                    .foregroundStyle(Color(hex: "8F97A4"))
+                    .frame(width: missingDaysWidth, height: ballFinalWidth + 2 * ballPadding)
+            }
+            .mask (
+                VStack(spacing: 0) {
+                    ForEach(0..<Int(numberOfLines), id: \.self) { _ in
+                        HStack(spacing: 0) {
+                            ForEach(0..<Int(numberOfBallsPerLine), id: \.self) { _ in
+                                Circle()
+                                    .foregroundStyle(.white)
+                                    .frame(width: ballFinalWidth, height: ballFinalWidth)
+                                    .padding(.all, ballPadding)
+                            }
+                        }
+                    }
+                }
+            )
+
+        )
     }
 }
 
@@ -165,6 +163,6 @@ private extension Double {
 
 struct ListeningTimeStory_Previews: PreviewProvider {
     static var previews: some View {
-        ListeningTimeStory(listeningTime: 5000000, podcasts: [Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast()])
+        ListeningTimeStory(listeningTime: 8600000, podcasts: [Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast()])
     }
 }
