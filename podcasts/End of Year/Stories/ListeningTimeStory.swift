73ac3d35c6c166bc3006de5e60e15284c6a4abbc
Leandro Alonso <contato@leandroalonso.com>
2023-10-12 14:30:45 -0300

    Draft of the new view with the generated days as balls

--- podcasts/End of Year/Stories/ListeningTimeStory.swift
+++ podcasts/End of Year/Stories/ListeningTimeStory.swift
@@ -11,39 +11,147 @@ struct ListeningTimeStory: ShareableStory {
 
     let podcasts: [Podcast]
 
+    var numberOfBalls: Int {
+        Int(listeningTime.firstNumber) ?? 0 > 100 ? 10 : 7
+    }
+
+    var numberOfLines: Int {
+        var linesSlashBalls = (Double(listeningTime.firstNumber) ?? 1) / Double(numberOfBalls)
+        linesSlashBalls.round(.up)
+        return Int(linesSlashBalls)
+    }
+
     var body: some View {
         GeometryReader { geometry in
-            VStack(spacing: 0) {
-                StoryLabelContainer(topPadding: geometry.size.height * Constants.topPadding, geometry: geometry) {
-                    let time = listeningTime.storyTimeDescription
-                    if NSLocale.isCurrentLanguageEnglish {
-                        StoryLabel(L10n.eoyStoryListenedToUpdated("\n\(time)\n"), highlighting: [time], for: .title)
-                    } else {
-                        StoryLabel(L10n.eoyStoryListenedTo("\n\(time)\n"), highlighting: [time], for: .title)
-                    }
-                    StoryLabel(FunMessage.timeSecsToFunnyText(listeningTime), for: .subtitle)
-                        .opacity(0.8)
+            PodcastCoverContainer(geometry: geometry) {
+                StoryLabelContainer(geometry: geometry) {
+                    StoryLabel(L10n.eoyStoryListenedToTitle, for: .title, geometry: geometry)
+                    StoryLabel(L10n.eoyStoryListenedToSubtitle, for: .subtitle, color: Color(hex: "8F97A4"), geometry: geometry)
                 }
 
                 // Podcast images angled to fill the width of the view
                 let size = 0.30 * geometry.size.height
 
-                HStack(spacing: 20) {
-                    ForEach(Constants.displayedPodcasts, id: \.self) {
-                        podcastCover($0, size: size)
-                    }
+                ContentSizeGeometryReader { listeningTimeReader in
+                    Text(listeningTime.firstNumber)
+                        .font(.custom("DM Sans", size: geometry.size.height * 0.4))
+                        .fontWeight(.light)
+                        .frame(width: geometry.size.width - 70)
+                        .scaledToFill()
+                        .minimumScaleFactor(0.5)
+                        .lineLimit(1)
+                        .foregroundColor(.white)
+                        .padding([.leading, .trailing], 35)
+                        .padding(.bottom, -listeningTimeReader.size.height * 0.2)
                 }
-                .applyPodcastCoverPerspective()
-                .padding(.top)
-            }.frame(width: geometry.size.width)
-        }.background(DynamicBackgroundView(podcast: podcasts[0]))
-    }
 
-    @ViewBuilder
-    func podcastCover(_ index: Int, size: Double) -> some View {
-        let podcast = podcasts[safe: index] ?? podcasts[0]
-        PodcastCover(podcastUuid: podcast.uuid)
-            .frame(width: size, height: size)
+                StoryLabel(listeningTime.subtitle, for: .subtitle, color: Color(hex: "8F97A4"), geometry: geometry)
+                    .padding(.bottom, geometry.size.height * 0.025)
+
+
+//                VStack(alignment: .leading) {
+//                    ForEach(0..<numberOfLines, id: \.self) { _ in
+//                        LinearGradient(
+//                                                    stops: [
+//                                                    Gradient.Stop(color: Color(red: 0.25, green: 0.11, blue: 0.92), location: 0.00),
+//                                                    Gradient.Stop(color: Color(red: 0.68, green: 0.89, blue: 0.86), location: 0.24),
+//                                                    Gradient.Stop(color: Color(red: 0.87, green: 0.91, blue: 0.53), location: 0.50),
+//                                                    Gradient.Stop(color: Color(red: 0.91, green: 0.35, blue: 0.26), location: 0.76),
+//                                                    Gradient.Stop(color: Color(red: 0.1, green: 0.1, blue: 0.1), location: 1.00),
+//                                                    ],
+//                                                    startPoint: UnitPoint(x: 0, y: 0),
+//                                                    endPoint: UnitPoint(x: 1.22, y: 1.25)
+//                                                    )
+//                        .mask(
+//                            HStack(alignment: .top) {
+//                                Group {
+//                                    Circle()
+//                                    Circle()
+//                                    Circle()
+//                                    Circle()
+//                                    Circle()
+//                                    Circle()
+//                                    Circle()
+//                                }
+//                                .frame(width: geometry.size.height * 0.055)
+//                            }
+//                        )
+//                    }
+//                }
+//                .padding([.leading, .trailing], 18)
+
+                let maxArea = CGSize(width: geometry.size.width, height: geometry.size.height * 0.3)
+                let number = Double(listeningTime.firstNumber) ?? 0
+                let eachBallSquareArea = Double(maxArea.width * maxArea.height) / number
+                let numberOfBallsPerLine = max(7, (maxArea.width / eachBallSquareArea.squareRoot()).rounded(.up))
+                let numberOfLines = min((number / numberOfBallsPerLine).rounded(.up), max(1, (maxArea.height / eachBallSquareArea.squareRoot()).rounded(.up)))
+                let ballCalculatedWidth = maxArea.width / numberOfBallsPerLine
+                let ballCalculatedHeight = maxArea.height / numberOfLines
+                let ballPadding = min(ballCalculatedWidth, ballCalculatedHeight) * 0.05
+                let ballFinalWidth = min(ballCalculatedWidth, ballCalculatedHeight) - 4 * ballPadding
+                // The width of the days displayed as balls that the user didn't listened to podcasts
+                let missingDaysWidth = ((((numberOfLines * numberOfBallsPerLine) * 86400) - listeningTime) / 86400) * (ballFinalWidth + (2 * ballPadding))
+                let ballTotalArea = min(ballCalculatedWidth, ballCalculatedHeight)
+                let daysArea = CGSize(width: numberOfBallsPerLine * ballTotalArea, height: numberOfLines * ballTotalArea)
+
+                VStack(spacing: 0) {
+                    ForEach(0..<Int(numberOfLines), id: \.self) { _ in
+                        HStack(spacing: 0) {
+                            ForEach(0..<Int(numberOfBallsPerLine), id: \.self) { _ in
+                                Circle()
+                                    .foregroundStyle(.white)
+                                    .frame(width: ballFinalWidth, height: ballFinalWidth)
+                                    .padding(.all, ballPadding)
+                            }
+                        }
+                    }
+                    .opacity(0)
+                }
+                .background(
+                    ZStack(alignment: .bottomTrailing) {
+                        LinearGradient(
+                            stops: [
+                                Gradient.Stop(color: Color(red: 0.25, green: 0.11, blue: 0.92), location: 0.00),
+                                Gradient.Stop(color: Color(red: 0.68, green: 0.89, blue: 0.86), location: 0.24),
+                                Gradient.Stop(color: Color(red: 0.87, green: 0.91, blue: 0.53), location: 0.50),
+                                Gradient.Stop(color: Color(red: 0.91, green: 0.35, blue: 0.26), location: 0.76),
+                                Gradient.Stop(color: Color(red: 0.1, green: 0.1, blue: 0.1), location: 1.00),
+                            ],
+                            startPoint: UnitPoint(x: 0, y: 0),
+                            endPoint: UnitPoint(x: 1.22, y: 1.25)
+                        )
+
+                        Rectangle()
+                            .foregroundStyle(Color(hex: "8F97A4"))
+                            .frame(width: missingDaysWidth, height: ballFinalWidth + 2 * ballPadding)
+                    }
+                    .mask (
+                        VStack(spacing: 0) {
+                            ForEach(0..<Int(numberOfLines), id: \.self) { _ in
+                                HStack(spacing: 0) {
+                                    ForEach(0..<Int(numberOfBallsPerLine), id: \.self) { _ in
+                                        Circle()
+                                            .foregroundStyle(.white)
+                                            .frame(width: ballFinalWidth, height: ballFinalWidth)
+                                            .padding(.all, ballPadding)
+                                    }
+                                }
+                            }
+                        }
+                    )
+
+                )
+
+            }
+            .background(
+                ZStack(alignment: .bottom) {
+                    Color.black
+
+                    StoryGradient()
+                    .offset(x: -geometry.size.width * 0.8, y: geometry.size.height * 0.25)
+                }
+            )
+        }
     }
 
     func onAppear() {
@@ -74,22 +182,18 @@ struct ListeningTimeStory: ShareableStory {
     }
 }
 
-/// Always return the same funny message
-struct FunMessage {
-    static var message: String?
+private extension Double {
+    var firstNumber: String {
+        storyTimeDescription.components(separatedBy: "\u{00a0}").first ?? ""
+    }
 
-    static func timeSecsToFunnyText(_ timeInSeconds: Double) -> String {
-        guard let message = Self.message else {
-            Self.message = FunnyTimeConverter.timeSecsToFunnyText(timeInSeconds)
-            return Self.message ?? ""
-        }
-
-        return message
+    var subtitle: String {
+        storyTimeDescription.components(separatedBy: "\u{00a0}").dropFirst().joined(separator: "\u{00a0}")
     }
 }
 
 struct ListeningTimeStory_Previews: PreviewProvider {
     static var previews: some View {
-        ListeningTimeStory(listeningTime: 100, podcasts: [Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast()])
+        ListeningTimeStory(listeningTime: 22000000, podcasts: [Podcast.previewPodcast(), Podcast.previewPodcast(), Podcast.previewPodcast()])
     }
 }
