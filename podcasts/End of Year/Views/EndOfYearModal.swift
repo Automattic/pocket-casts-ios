11e1a58c968d1bc45cf69834d355d812d91482dc
Leandro Alonso <contato@leandroalonso.com>
2023-10-16 13:22:14 -0300

    Update EOY modal

--- podcasts/End of Year/Views/EndOfYearModal.swift
+++ podcasts/End of Year/Views/EndOfYearModal.swift
@@ -5,25 +5,24 @@ struct EndOfYearModal: View {
 
     var body: some View {
         VStack(spacing: 0) {
-            ModalTopPill()
+            ModalTopPill(fillColor: theme.activeTheme.isDark ? .white : .gray)
+                .padding(.bottom, 28)
 
             VStack(alignment: .center, spacing: Constants.verticalSpacing) {
 
-                Text(L10n.eoyTitle)
-                    .font(style: .title2, weight: .semibold, maxSizeCategory: .extraExtraExtraLarge)
-                    .fixedSize(horizontal: false, vertical: true)
-
-                cover
+                Image("modal_cover")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
 
                 Text(L10n.eoyDescription)
-                    .font(style: .body, maxSizeCategory: .accessibilityMedium)
+                    .font(style: .body, weight: .medium, maxSizeCategory: .accessibilityMedium)
                     .multilineTextAlignment(.center)
                     .fixedSize(horizontal: false, vertical: true)
                     .allowsTightening(false)
+                    .padding(.bottom, Constants.verticalSpacing)
 
                 showStoriesButton
-
-                dismissButton
             }
             .padding()
         }
@@ -35,48 +34,11 @@ struct EndOfYearModal: View {
         }
     }
 
-    var cover: some View {
-        ZStack {
-            Image("modal_background")
-                .resizable()
-            ZStack {
-                VStack {
-                    Image("2022_small")
-                    Text(L10n.eoySmallTitle)
-                        .foregroundColor(.white)
-                        .font(.system(size: Constants.smallTitleFontSize))
-                        .fontWeight(.semibold)
-                        .padding(.top, Constants.smallTitleTopPadding)
-                        .padding(.trailing, Constants.smallTitleHorizontalPadding)
-                        .padding(.leading, Constants.smallTitleHorizontalPadding)
-                        .multilineTextAlignment(.center)
-                        .minimumScaleFactor(Constants.smallTitleMinimumScaleFactor)
-                }
-                .frame(width: Constants.enfOfYearCoverSize, height: Constants.enfOfYearCoverSize)
-                .background(Color.black)
-                .cornerRadius(Constants.coverCornerRadius)
-                .shadow(radius: Constants.coverShadowRadius, x: 0, y: Constants.coverShadowY)
-            }
-            .padding()
-        }
-        .frame(maxWidth: .infinity)
-        .frame(height: Constants.coverWrapperHeight)
-        .cornerRadius(Constants.coverWrapperCornerRadius)
-    }
-
     var showStoriesButton: some View {
         Button(L10n.eoyViewYear) {
             NavigationManager.sharedManager.navigateTo(NavigationManager.endOfYearStories, data: nil)
         }
         .buttonStyle(RoundedDarkButton(theme: theme))
-        .frame(height: 44)
-    }
-
-    var dismissButton: some View {
-        Button(L10n.eoyNotNow) {
-            NavigationManager.sharedManager.dismissPresentedViewController()
-        }
-        .buttonStyle(StrokeButton(theme: theme))
         .frame(height: 44)
     }
 
@@ -94,9 +56,6 @@ struct EndOfYearModal: View {
         static let coverCornerRadius: CGFloat = 8
         static let coverShadowRadius: CGFloat = 3
         static let coverShadowY: CGFloat = 1
-
-        static let coverWrapperHeight: CGFloat = 180
-        static let coverWrapperCornerRadius: CGFloat = 16
     }
 }
 
