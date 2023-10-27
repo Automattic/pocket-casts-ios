57e59c5c31b9bd01d791f8437ad7408fe9875d2b
Leandro Alonso <contato@leandroalonso.com>
2023-10-27 11:11:00 +0200

    Add the rating view from the delegate

--- podcasts/PodcastHeadingTableCell.swift
+++ podcasts/PodcastHeadingTableCell.swift
@@ -270,22 +270,12 @@ class PodcastHeadingTableCell: ThemeableCell, SubscribeButtonDelegate, Expandabl
     }
 
     private lazy var ratingView: UIView? = {
-        guard let viewModel = self.delegate?.podcastRatingViewModel else {
-            return nil
-        }
-
-        let view = StarRatingView(viewModel: viewModel)
-            .padding(.top, 10)
-            .themedUIView
-
-        view.backgroundColor = .clear
-        return view
+        delegate?.ratingView
     }()
 
     private func addRatingIfNeeded() {
-        // Only add the rating if it hasn't been added already
         guard
-            let ratingView, ratingView.superview == nil,
+            let ratingView = ratingView,
             let index = podcastCategory.flatMap({
                 extraContentStackView.arrangedSubviews.firstIndex(of: $0)
             })
