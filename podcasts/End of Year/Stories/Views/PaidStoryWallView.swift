b0beef8c66940f3b8948b952040e1f03ff4c3c3c
Leandro Alonso <contato@leandroalonso.com>
2023-10-30 08:51:56 -0300

    Add paid story wall

---
+++ podcasts/End of Year/Stories/Views/PaidStoryWallView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct PaidStoryWallView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            PodcastCoverContainer(geometry: geometry) {
+                StoryLabelContainer(geometry: geometry) {
+                    SubscriptionBadge(tier: .plus, displayMode: .gradient, foregroundColor: .black)
+                    StoryLabel(L10n.eoyTheresMore, for: .title, geometry: geometry)
+                    StoryLabel(L10n.eoySubscribeToPlus, for: .subtitle, color: Color(hex: "8F97A4"), geometry: geometry)
+                }
+                .padding(.bottom, geometry.size.height * 0.06)
+
+                Button(L10n.eoyStartYourFreeTrial) {
+                    NavigationManager.sharedManager.showUpsellView(from: SceneHelper.rootViewController()!, source: .endOfYear)
+
+                }
+                .buttonStyle(ReplayButtonStyle(color: .black))
+            }
+        }
+        .background(
+            ZStack {
+                LinearGradient(
+                stops: [
+                Gradient.Stop(color: .black, location: 0.00),
+                Gradient.Stop(color: .black.opacity(0), location: 1.00),
+                ],
+                startPoint: UnitPoint(x: 0.5, y: 0.24),
+                endPoint: UnitPoint(x: 0.5, y: 1.04)
+                )
+
+                VisualEffectView(effect: UIBlurEffect(style: .systemThinMaterialDark))
+            }
+                .allowsHitTesting(false)
+        )
+    }
+}
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) { uiView.effect = effect }
+}
+
+#Preview {
+    PaidStoryWallView()
+}
