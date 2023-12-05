28b9fab62ff9da81c2aca8c77b9a6435d5daf741
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2023-12-04 21:32:35 -0500

    Fix lint

--- podcasts/DatabaseExport.swift
+++ podcasts/DatabaseExport.swift
@@ -5,10 +5,10 @@ import PocketCastsUtils
 class DatabaseExport {
     private let fileManager = FileManager.default
     private var loadingAlert: ShiftyLoadingAlert?
-
+    
     /// The resulting file name of the zip file
     private let exportName = "Pocket Casts Export"
-
+    
     /// ZIPs the users SQLite database and shows a share dialog for them to share it with us
     func exportDatabase(from controller: UIViewController, completion: @escaping () -> Void) {
         loadingAlert = ShiftyLoadingAlert(title: L10n.exportingDatabase)
@@ -18,29 +18,29 @@ class DatabaseExport {
             }
         })
     }
-
+    
     /// Open the resulting zip file in the share sheet, or show an error
     private func share(url: URL?, from controller: UIViewController, completion: @escaping () -> Void) {
         loadingAlert?.hideAlert(false)
         loadingAlert = nil
-
+        
         guard let url else {
             SJUIUtils.showAlert(title: L10n.settingsExportError, message: nil, from: controller)
             return
         }
-
+        
         // Share the file
         let shareSheet = UIActivityViewController(activityItems: [url], applicationActivities: nil)
         shareSheet.completionWithItemsHandler = { [weak self] _, _, _, _ in
             // Attempt to cleanup the temporary file
             self?.cleanup(url: url)
-
+            
             completion()
         }
-
+        
         controller.present(shareSheet, animated: true, completion: nil)
     }
-
+    
     /// Attempt to remove the temporary export directory and files
     private func cleanup(url: URL) {
         do {
@@ -49,7 +49,7 @@ class DatabaseExport {
             FileLog.shared.addMessage("[Export] Could not cleanup file: \(error)")
         }
     }
-
+    
     /// Create a zip of the database and prefrences
     private func export(_ handler: @escaping (URL?) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async {
@@ -57,9 +57,9 @@ class DatabaseExport {
                 handler(nil)
                 return
             }
-
+            
             let coordinator = NSFileCoordinator()
-
+            
             // The file coordinate will zip the export folder for us
             coordinator.coordinate(readingItemAt: exportFolder, options: .forUploading, error: nil) { zipURL in
                 do {
@@ -67,7 +67,7 @@ class DatabaseExport {
                     // so we need to move it to a more permanent location
                     let tempURL = exportFolder.appendingPathComponent("\(self.exportName).zip")
                     try self.fileManager.moveItem(at: zipURL, to: tempURL)
-
+                    
                     DispatchQueue.main.async {
                         handler(tempURL)
                     }
@@ -78,22 +78,22 @@ class DatabaseExport {
             }
         }
     }
-
+    
     /// Copies the database and preferences to a temporary directory
     private func prepareFiles() -> URL? {
         do {
             let databaseURL = URL(fileURLWithPath: DataManager.pathToDb())
-
+            
             // Create a temporary directory to store the files in
             let temporaryDirectory = try fileManager.url(for: .itemReplacementDirectory,
                                                          in: .userDomainMask,
                                                          appropriateFor: databaseURL,
                                                          create: true)
-
+            
             let exportDirectory = temporaryDirectory.appendingPathComponent(exportName, isDirectory: true)
-
+            
             try fileManager.createDirectory(at: exportDirectory, withIntermediateDirectories: true)
-
+            
             // Write the preferences to the export folder
             let preferencesFile = exportDirectory.appendingPathComponent("preferences.plist", isDirectory: false)
             try writePreferences(to: preferencesFile)
@@ -101,14 +101,14 @@ class DatabaseExport {
             // Copy the database file into the export folder
             let databaseFile = exportDirectory.appendingPathComponent("database.sqlite", isDirectory: false)
             try fileManager.copyItem(at: databaseURL, to: databaseFile)
-
+            
             return exportDirectory
         } catch {
             FileLog.shared.addMessage("[Export] Prepare failed with error: \(error)")
             return nil
         }
     }
-
+    
     /// Save the preferences to the url
     private func writePreferences(to url: URL) throws {
         guard
@@ -117,7 +117,7 @@ class DatabaseExport {
         else {
             return
         }
-
+        
         let preferencesFile = library.appendingPathComponent("Preferences/\(bundle).plist")
         try fileManager.copyItem(at: preferencesFile, to: url)
     }
