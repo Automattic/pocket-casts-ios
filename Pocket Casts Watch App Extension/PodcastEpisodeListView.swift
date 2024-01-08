0a80cb614d48c0e1b33132c5e7b4b23c56c21008
Sergio Estevao <sergioestevao@gmail.com>
2024-01-08 10:25:07 +0000

    Update PodcastEpisodeListView to use List instead of Scrollview/LazyStack combo
    Needed to update the header view to display correctly in a List Header

--- Pocket Casts Watch App Extension/PodcastEpisodeListView.swift
+++ Pocket Casts Watch App Extension/PodcastEpisodeListView.swift
@@ -8,11 +8,14 @@ struct PodcastEpisodeListView: View {
     @StateObject var viewModel: PodcastEpisodeListViewModel
 
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                podcastInfo
-                EpisodeListView(title: L10n.podcastsPlural.prefixSourceUnicode, showArtwork: false, episodes: $viewModel.episodes, playlist: .podcast(uuid: viewModel.podcast.uuid))
-            }
+        List {
+            Section(
+                content: {
+                    EpisodeListView(title: L10n.podcastsPlural.prefixSourceUnicode, showArtwork: false, episodes: $viewModel.episodes, playlist: .podcast(uuid: viewModel.podcast.uuid))
+                },
+                header: {
+                    podcastInfo.textCase(.none)
+                })
         }
         .navigationTitle(L10n.podcastsPlural.prefixSourceUnicode)
         .withOrderPickerToolbar(selectedOption: viewModel.sortOption, title: L10n.sortEpisodes, hasHorizontalPadding: true) { option in
@@ -21,18 +24,24 @@ struct PodcastEpisodeListView: View {
     }
 
     var podcastInfo: some View {
-        Group {
-            CachedImage(url: viewModel.podcast.artworkURL)
-                .frame(width: headerImageSize, height: headerImageSize, alignment: .center)
-            Text(viewModel.podcast.title ?? "")
-            Text(viewModel.podcast.author ?? "")
-                .font(.dynamic(size: 12))
-                .foregroundColor(.subheadlineText)
-                .padding(.bottom, 3)
-            if viewModel.episodes.isEmpty {
-                Text(L10n.watchNoEpisodes)
-                    .font(.subheadline)
+        HStack {
+            Spacer()
+            VStack(alignment: .center) {
+                CachedImage(url: viewModel.podcast.artworkURL)
+                    .frame(width: headerImageSize, height: headerImageSize, alignment: .center)
+                Text(viewModel.podcast.title ?? "")
+                    .font(.dynamic(size: 14))
+                    .foregroundColor(.white)
+                Text(viewModel.podcast.author ?? "")
+                    .font(.dynamic(size: 12))
+                    .foregroundColor(.subheadlineText)
+                    .padding(.bottom, 3)
+                if viewModel.episodes.isEmpty {
+                    Text(L10n.watchNoEpisodes)
+                        .font(.subheadline)
+                }
             }
+            Spacer()
         }
         .multilineTextAlignment(.center)
     }
