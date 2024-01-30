703fa8224265eb1ff702f4de50f8952e3a340c52
Leandro Alonso <contato@leandroalonso.com>
2024-01-30 18:09:07 -0300

    Simplify how fixture is loaded
    Co-authored-by: Emily Laguna <793774+emilylaguna@users.noreply.github.com>

--- Modules/Server/Tests/PocketCastsServerTests/ServerHelperTests.swift
+++ Modules/Server/Tests/PocketCastsServerTests/ServerHelperTests.swift
@@ -58,16 +58,12 @@ final class ServerHelperTests: XCTestCase {
 }
 
 public class JSONHelper {
-    public static func loadJSON(
-        withFile fileName: String,
-        inBundleWithName bundle: String = "PocketCastsServer",
-        subdirectory: String = "Fixtures") -> Data? {
-        let bundle = Bundle.currentModule(name: bundle)
+    public static func loadJSON(withFile fileName: String, subdirectory: String = "Fixtures") -> Data? {
+        let bundle = Bundle.module
 
         if let url = bundle.url(forResource: fileName, withExtension: "json", subdirectory: subdirectory) {
             do {
-                let data = try Data(contentsOf: url)
-                return data
+                return try Data(contentsOf: url)
             } catch {
                 debugPrint(error)
             }
@@ -75,25 +71,5 @@ public class JSONHelper {
             debugPrint("Could not find the json file: \(fileName) in bundle: \(bundle)")
         }
         return nil
-    }
-}
-
-extension Foundation.Bundle {
-    static func currentModule(name: String) -> Bundle {
-        var thisModuleName = name + "_" + name
-        var url = Bundle.main.bundleURL
-
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            url = bundle.bundleURL.deletingLastPathComponent()
-            thisModuleName = thisModuleName.appending("Tests")
-        }
-
-        url = url.appendingPathComponent("\(thisModuleName).bundle")
-
-        guard let bundle = Bundle(url: url) else {
-            fatalError("Foundation.Bundle.module could not load resource bundle: \(url.path)")
-        }
-
-        return bundle
     }
 }
