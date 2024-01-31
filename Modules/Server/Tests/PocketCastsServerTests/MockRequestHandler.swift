badf2cea3d6b5cd830014134e261b003fbc9d16f
Brandon Titus <b@titus.io>
2024-01-30 17:13:45 -0700

    Add generic handler for requests in APIBaseTask
    Unifies request + response handling for APIBaseTask instead of having individual methods for get, post, and delete.

---
+++ Modules/Server/Tests/PocketCastsServerTests/MockRequestHandler.swift
@@ -0,0 +1,31 @@
+@testable import PocketCastsServer
+import XCTest
+
+struct MockRequestHandler {
+    typealias Handler = ((URLRequest) throws -> (Data?, URLResponse?))
+
+    let handler: Handler
+
+    init(handler: @escaping ((URLRequest) throws -> (Data?, URLResponse?))) {
+        self.handler = handler
+    }
+}
+
+extension MockRequestHandler: RequestHandler {
+    func send(request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        do {
+            let (data, response) = try handler(request)
+            completion(data, response, nil)
+        } catch let error {
+            completion(nil, nil, error)
+        }
+    }
+}
+
+extension URLConnection {
+    /// A convenient initializer to pass a block which returns data, response, and error for a given URLRequest.
+    /// - Parameter mockHandler: The handler block (URLRequest) throws -> (Data, URLResponse?)
+    convenience init(mockHandler: @escaping MockRequestHandler.Handler) {
+        self.init(handler: MockRequestHandler(handler: mockHandler))
+    }
+}
