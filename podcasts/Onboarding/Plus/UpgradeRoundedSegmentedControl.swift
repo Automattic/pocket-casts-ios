3902d8f902657066a0ba76fd115c59c3332f580b
Sergio Estevao <sergioestevao@gmail.com>
2024-02-01 12:00:45 +0000

    Update variable name

---
+++ podcasts/Onboarding/Plus/UpgradeRoundedSegmentedControl.swift
@@ -0,0 +1,9 @@
+//
+//  UpgradeRoundedSegmentedControl.swift
+//  podcasts
+//
+//  Created by Sérgio Estêvão on 01/02/2024.
+//  Copyright © 2024 Shifty Jelly. All rights reserved.
+//
+
+import Foundation
