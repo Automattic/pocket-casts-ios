d6503e87723f5fb8162ed925f649c400e3eb3755
Emily Laguna <793774+emilylaguna@users.noreply.github.com>
2024-01-31 21:43:03 -0500

    Remove all of the files that are in the podcasts folder but aren't being used

--- podcasts/UIButton+EasyActions.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UIButton {
-    func addAction(_ action: @escaping () -> Void, for event: UIControl.Event = .touchUpInside) {
-        addAction(UIAction(handler: { _ in
-            action()
-        }), for: event)
-    }
-}
