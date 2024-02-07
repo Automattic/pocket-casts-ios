13091d1e6bfbe8ceec1e38308d57af15e0bfbd60
Brandon Titus <b@titus.io>
2024-02-06 21:07:07 -0700

    Change AppSettings defaults setup
    Add the defaults directly to properties so that they are at the declaration site

--- Modules/Server/Sources/PocketCastsServer/Public/AppSettings.swift
+++ Modules/Server/Sources/PocketCastsServer/Public/AppSettings.swift
@@ -2,15 +2,14 @@ import PocketCastsUtils
 
 /// Model type for synced & stored App Settings
 public struct AppSettings: JSONCodable {
-    @ModifiedDate public var openLinks: Bool
+    @ModifiedDate public var openLinks: Bool = false
 
-    @ModifiedDate public var rowAction: PrimaryRowAction
+    @ModifiedDate public var rowAction: PrimaryRowAction = .download
 
-    static var defaults: AppSettings {
-        return AppSettings(openLinks: true, rowAction: .download)
+    public init() {
     }
 }
 
 extension SettingsStore<AppSettings> {
-    public static let appSettings = SettingsStore(key: "app_settings", value: AppSettings.defaults)
+    public static let appSettings = SettingsStore(key: "app_settings", value: AppSettings())
 }
