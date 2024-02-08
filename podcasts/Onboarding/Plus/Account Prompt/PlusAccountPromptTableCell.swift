413e47f824098ff431be940a02275dbd4ac2594d
Sergio Estevao <sergioestevao@gmail.com>
2024-02-08 09:33:53 +0000

    Update source value to profile to match Android

--- podcasts/Onboarding/Plus/Account Prompt/PlusAccountPromptTableCell.swift
+++ podcasts/Onboarding/Plus/Account Prompt/PlusAccountPromptTableCell.swift
@@ -35,7 +35,7 @@ class PlusAccountPromptTableCell: ThemeableCell {
     // Update the model's parent so we can present the modal
     func updateParent(_ controller: UIViewController) {
         model.parentController = controller
-        model.source = .accountDetails
+        model.source = .profile
     }
 
     required init?(coder: NSCoder) {
