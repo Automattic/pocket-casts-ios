86da950bb6570bfb1f7690533fe56f7b3f266a63
Brandon Titus <b@titus.io>
2024-02-08 15:49:07 -0700

    Add AppSettings+SyncUserDefaults testing

---
+++ PocketCastsTests/Tests/Utilities/AppSettings+ImportUserDefaultsTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+import Foundation
+import PocketCastsUtils
+@testable import PocketCastsServer
+@testable import podcasts
+
+class AppSettingsImportUserDefaultsTests: XCTestCase {
+    private let userDefaultsSuiteName = "PocketCastsServer-AppSettingsSyncUserDefaultsTests"
+    private let defaultsKey = "app_settings"
+
+    // Initial values for testing
+    private let initialOpenLinks = false
+    private let newOpenLinks = true
+    private let initialRowAction = PrimaryRowAction.stream
+    private let newRowAction = PrimaryRowAction.download
+
+    override func setUp() {
+        super.setUp()
+        UserDefaults.standard.removePersistentDomain(forName: userDefaultsSuiteName)
+    }
+
+    /// Set up UserDefaults instance with initial preset values.
+    /// - Returns: An unwrapped `UserDefaults` instance with initial values set
+    private func setupDefaults() throws -> UserDefaults {
+        let defaults = try XCTUnwrap(UserDefaults(suiteName: userDefaultsSuiteName), "User Defaults suite should load")
+
+        // Set up initial values to update from
+        defaults.set(newOpenLinks, forKey: Constants.UserDefaults.openLinksInExternalBrowser)
+        defaults.set(newRowAction.rawValue, forKey: "SJRowAction")
+
+        return defaults
+    }
+
+    /// Tests migrating from values stored in `NSUserDefaults` to `SettingsStore<AppSettings>`
+    func testValueMigration() throws {
+        let defaults = try setupDefaults()
+        let store = SettingsStore(userDefaults: defaults, key: defaultsKey, value: AppSettings.defaults)
+
+        store.importUserDefaults(defaults)
+
+        XCTAssertEqual(store.openLinks, newOpenLinks, "Value of openLinks should change after update")
+        XCTAssertEqual(store.rowAction, newRowAction, "Value of rowAction should change after update")
+    }
+}
