444fb04b0960c7f91a914903bf85558627222fb9
Leandro Alonso <contato@leandroalonso.com>
2024-02-14 12:48:00 -0300

    Extract each view to its own var

--- podcasts/ChaptersHeader.swift
+++ podcasts/ChaptersHeader.swift
@@ -3,6 +3,32 @@ import UIKit
 class ChaptersHeader: UIView {
     weak var delegate: ChaptersHeaderDelegate?
 
+    private lazy var container: UIStackView = {
+        let container = UIStackView()
+        container.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        container.isLayoutMarginsRelativeArrangement = true
+        container.axis = .horizontal
+        container.backgroundColor = .black
+        return container
+    }()
+
+    private lazy var chaptersLabel: UILabel = {
+        let label = UILabel()
+        label.text = L10n.numberOfChapters(PlaybackManager.shared.chapterCount(onlyPlayable: true))
+        label.textColor = .white
+        label.font = .preferredFont(forTextStyle: .footnote)
+        return label
+    }()
+
+    private lazy var toggleButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Skip chapters", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .footnote)
+        button.addTarget(self, action: #selector(toggleChapterSelection), for: .touchUpInside)
+        return button
+    }()
+
     // MARK: - Config
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -17,24 +43,10 @@ class ChaptersHeader: UIView {
     }
 
     private func configure() {
-        let header = UIStackView()
-        header.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
-        header.isLayoutMarginsRelativeArrangement = true
-        header.axis = .horizontal
-        header.backgroundColor = .black
-        let label = UILabel()
-        label.text = L10n.numberOfChapters(PlaybackManager.shared.chapterCount(onlyPlayable: true))
-        label.textColor = .white
-        label.font = .preferredFont(forTextStyle: .footnote)
-        let button = UIButton()
-        button.setTitle("Skip chapters", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = .preferredFont(forTextStyle: .footnote)
-        button.addTarget(self, action: #selector(toggleChapterSelection), for: .touchUpInside)
-        header.addArrangedSubview(label)
-        header.addArrangedSubview(button)
-        addSubview(header)
-        header.anchorToAllSidesOf(view: self)
+        container.addArrangedSubview(chaptersLabel)
+        container.addArrangedSubview(toggleButton)
+        addSubview(container)
+        container.anchorToAllSidesOf(view: self)
     }
 
     @objc func toggleChapterSelection() {
