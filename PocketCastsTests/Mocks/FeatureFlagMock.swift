b2385e9f41915dd59a0e11ac43e15edac23614e8
Leandro Alonso <contato@leandroalonso.com>
2024-02-14 15:50:32 -0300

    Add a helper to set feature flag values without changing the main app value

---
+++ PocketCastsTests/Mocks/FeatureFlagMock.swift
@@ -0,0 +1,21 @@
+@testable import podcasts
+
+class FeatureFlagMock {
+    private var previousValues: [FeatureFlag: Bool] = [:]
+
+    func set(_ featureFlag: FeatureFlag, value: Bool) {
+        do {
+            previousValues[featureFlag] = featureFlag.enabled
+
+            try FeatureFlagOverrideStore().override(featureFlag, withValue: value)
+        } catch { }
+    }
+
+    func reset() {
+        previousValues.forEach { flag in
+            set(flag.key, value: flag.value)
+        }
+
+        previousValues = [:]
+    }
+}
