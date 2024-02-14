84910c2e85dbf3002600c9ae8a2c38a86ddf5a3f
Leandro Alonso <contato@leandroalonso.com>
2024-02-14 09:58:50 -0300

    Add header with button and text

--- podcasts/ChaptersViewController+Table.swift
+++ podcasts/ChaptersViewController+Table.swift
@@ -56,7 +56,34 @@ extension ChaptersViewController: UITableViewDataSource, UITableViewDelegate, UI
         }
     }
 
+    func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        38
+    }
+
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        CGFloat.leastNonzeroMagnitude
+        UITableView.automaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        let header = UIStackView()
+        header.layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        header.isLayoutMarginsRelativeArrangement = true
+        header.axis = .horizontal
+        header.backgroundColor = .black
+        let label = UILabel()
+        label.text = "X chapters"
+        label.textColor = .white
+        label.font = .preferredFont(forTextStyle: .footnote)
+        let button = UIButton()
+        button.setTitle("Skip chapters", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .footnote)
+        header.addArrangedSubview(label)
+        header.addArrangedSubview(button)
+        view.addSubview(header)
+        view.anchorToAllSidesOf(view: header)
+
+        return header
     }
 }
