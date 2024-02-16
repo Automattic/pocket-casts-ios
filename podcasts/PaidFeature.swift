d4ee34c053a4ca183f3e741b0ad2fe4d38d50ac6
Leandro Alonso <contato@leandroalonso.com>
2024-02-16 14:32:06 -0300

    Make deselect chapters a patron feature

--- podcasts/PaidFeature.swift
+++ podcasts/PaidFeature.swift
@@ -7,6 +7,7 @@ import SwiftUI
 
 extension PaidFeature {
     static var bookmarks: PaidFeature = .plusFeature
+    static var deselectChapters: PaidFeature = .patronFeature
 }
 
 /// A `PaidFeature` represents a feature that is unlocked with a subscription tier, and is considered to be unlocked if the tier
