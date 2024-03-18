a8d30f72e5af3001c4b7c3f7e9c6f4ca9a43c907
Brandon Titus <b@titus.io>
2024-03-18 14:35:45 -0600

    Add Int -> Bool handling for SQLite
    I discovered that json_extract will output Ints for Bool values. This needs to be converted to allow our Codable types to work

--- Modules/Utils/Sources/PocketCastsUtils/General/ModifiedDate.swift
+++ Modules/Utils/Sources/PocketCastsUtils/General/ModifiedDate.swift
@@ -26,3 +26,41 @@ public struct ModifiedDate<Value: Codable & Equatable>: Equatable, Codable {
         set { self = newValue }
     }
 }
+
+extension ModifiedDate {
+
+    /// This is a property wrapper which handles converting Integer value.
+    /// "the SQL datatype of the result is NULL for a JSON null, INTEGER or REAL for a JSON numeric value, an INTEGER zero for a JSON false value, an INTEGER one for a JSON true value"
+    /// See more details on the `json_extract` output here: https://www.sqlite.org/json1.html#jex
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Self.CodingKeys.self)
+
+        do {
+            value = try container.decode(Value.self, forKey: .value)
+        } catch let error {
+            // Add additional decoding for Int -> Bool due to SQLite type issues
+            // "the SQL datatype of the result is NULL for a JSON null, INTEGER or REAL for a JSON numeric value, an INTEGER zero for a JSON false value, an INTEGER one for a JSON true value"
+            // See more details on the `json_extract` output here: https://www.sqlite.org/json1.html#jex
+            if let intValue = try? container.decode(Int.self, forKey: .value), let val = Bool(int: intValue) as? Value {
+                value = val
+            } else {
+                throw error
+            }
+        }
+
+        modifiedAt = try container.decodeIfPresent(Date.self, forKey: .modifiedAt)
+    }
+}
+
+fileprivate extension Bool {
+    init?(int: Int) {
+        switch int {
+        case 0:
+            self = false
+        case 1:
+            self = true
+        default:
+            return nil
+        }
+    }
+}
