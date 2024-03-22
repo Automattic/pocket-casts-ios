fe782bfb7404f4ed0080200d2806d1e1da6b2830
Sérgio Estêvão <sergioestevao@gmail.com>
2024-03-22 15:29:24 +0000

    Remove not changed variable warning

--- podcasts/SwiftUI/UnderlineLinkTextView.swift
+++ podcasts/SwiftUI/UnderlineLinkTextView.swift
@@ -13,7 +13,7 @@ struct UnderlineLinkTextView: View {
         attributedString.runs.filter({ $0.link != nil }).forEach({ run in
             attributedString[run.range].underlineStyle = .init(pattern: .solid)
         })
-        var textView = Text(attributedString)
+        let textView = Text(attributedString)
 
         // Open the link inside the app
         return textView.environment(\.openURL, OpenURLAction { url in
