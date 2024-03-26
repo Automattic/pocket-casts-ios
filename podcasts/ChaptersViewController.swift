eb9fa2f3bbe221ac56df33570d52943cbff52247
Leandro Alonso <contato@leandroalonso.com>
2024-03-26 14:14:44 -0300

    Fix crash when tapping a chapter

--- podcasts/ChaptersViewController.swift
+++ podcasts/ChaptersViewController.swift
@@ -42,8 +42,12 @@ class ChaptersViewController: PlayerItemViewController {
     func scrollToCurrentlyPlayingChapter(animated: Bool) {
         let currentChapter = PlaybackManager.shared.currentChapters()
 
+        guard let index = playbackManager.index(for: currentChapter) else {
+            return
+        }
+
         // scroll far enough to at least see the current chapter + a few more
-        chaptersTable.scrollToRow(at: IndexPath(item: currentChapter.index, section: 0), at: .middle, animated: animated)
+        chaptersTable.scrollToRow(at: IndexPath(item: index, section: 0), at: .middle, animated: animated)
     }
 
     private func addObservers() {
