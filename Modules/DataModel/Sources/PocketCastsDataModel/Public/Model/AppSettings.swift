fa62c38946bd40d20df24526dc6abe88d776908a
Brandon Titus <b@titus.io>
2024-03-28 16:52:11 -0600

    Add Default values for JSON decoding AppSettings

--- Modules/DataModel/Sources/PocketCastsDataModel/Public/Model/AppSettings.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Public/Model/AppSettings.swift
@@ -1,112 +1,143 @@
 import PocketCastsUtils
-import PocketCastsDataModel
+import MetaCodable
 
 /// Model type for synced & stored App Settings
+@Codable
+@MemberInit
 public struct AppSettings: JSONCodable {
 
     // MARK: - General
+    @Default(false)
     @ModifiedDate public var openLinks: Bool
 
+    @Default(PrimaryRowAction.stream)
     @ModifiedDate public var rowAction: PrimaryRowAction
 
+    @Default(PodcastGrouping.none)
     @ModifiedDate public var episodeGrouping: PodcastGrouping
+    @Default(false)
     @ModifiedDate public var showArchived: Bool
+    @Default(PrimaryUpNextSwipeAction.playNext)
     @ModifiedDate public var upNextSwipe: PrimaryUpNextSwipeAction
 
+    @Default(45)
     @ModifiedDate public var skipForward: Int32
+    @Default(10)
     @ModifiedDate public var skipBack: Int32
 
+    @Default(false)
     @ModifiedDate public var keepScreenAwake: Bool
+    @Default(false)
     @ModifiedDate public var openPlayer: Bool
+    @Default(true)
     @ModifiedDate public var intelligentResumption: Bool
 
+    @Default(false)
     @ModifiedDate public var playUpNextOnTap: Bool
+    @Default(false)
     @ModifiedDate public var playbackActions: Bool
+    @Default(false)
     @ModifiedDate public var legacyBluetooth: Bool
+    @Default(true)
     @ModifiedDate public var multiSelectGesture: Bool
+    @Default(true)
     @ModifiedDate public var chapterTitles: Bool
+    @Default(true)
     @ModifiedDate public var autoPlayEnabled: Bool
 
-    @ModifiedDate public var notifications: Bool = false
+    @Default(false)
+    @ModifiedDate public var notifications: Bool
 
-    @ModifiedDate public var appBadge: AppBadge = .off
-    @ModifiedDate public var appBadgeFilter: String = ""
+    @Default(AppBadge.off)
+    @ModifiedDate public var appBadge: AppBadge
+    @Default("")
+    @ModifiedDate public var appBadgeFilter: String
 
-    @ModifiedDate public var autoArchivePlayed: AutoArchiveAfterPlayed = .afterPlaying
-    @ModifiedDate public var autoArchiveInactive: AutoArchiveAfterInactive = .never
-    @ModifiedDate public var autoArchiveIncludesStarred: Bool = false
+    @Default(AutoArchiveAfterPlayed.afterPlaying)
+    @ModifiedDate public var autoArchivePlayed: AutoArchiveAfterPlayed
+    @Default(AutoArchiveAfterInactive.never)
+    @ModifiedDate public var autoArchiveInactive: AutoArchiveAfterInactive
+    @Default(false)
+    @ModifiedDate public var autoArchiveIncludesStarred: Bool
 
     // MARK: Playback Effects
 
+    @Default(false)
     @ModifiedDate public var volumeBoost: Bool
+    @Default(TrimSilence.off)
     @ModifiedDate public var trimSilence: TrimSilence
+    @Default(1)
     @ModifiedDate public var playbackSpeed: Double
 
-    @ModifiedDate public var playerBookmarksSortType: BookmarksSort = .newestToOldest
-    @ModifiedDate public var episodeBookmarksSortType: BookmarksSort = .newestToOldest
-    @ModifiedDate public var podcastBookmarksSortType: BookmarksSort = .newestToOldest
-    @ModifiedDate public var profileBookmarksSortType: BookmarksSort = .newestToOldest
+    @Default(BookmarksSort.newestToOldest)
+    @ModifiedDate public var playerBookmarksSortType: BookmarksSort
+    @Default(BookmarksSort.newestToOldest)
+    @ModifiedDate public var episodeBookmarksSortType: BookmarksSort
+    @Default(BookmarksSort.newestToOldest)
+    @ModifiedDate public var podcastBookmarksSortType: BookmarksSort
+    @Default(BookmarksSort.newestToOldest)
+    @ModifiedDate public var profileBookmarksSortType: BookmarksSort
 
-    @ModifiedDate public var filesAutoUpNext: Bool = false
-    @ModifiedDate public var filesAfterPlayingDeleteLocal: Bool = false
-    @ModifiedDate public var filesAfterPlayingDeleteCloud: Bool = false
+    @Default(false)
+    @ModifiedDate public var filesAutoUpNext: Bool
+    @Default(false)
+    @ModifiedDate public var filesAfterPlayingDeleteLocal: Bool
+    @Default(false)
+    @ModifiedDate public var filesAfterPlayingDeleteCloud: Bool
 
-    @ModifiedDate public var warnDataUsage: Bool = false
+    @Default(false)
+    @ModifiedDate public var warnDataUsage: Bool
 
-    @ModifiedDate public var autoUpNextLimit: Int32 = 100
-    @ModifiedDate public var autoUpNextLimitReached: AutoAddLimitReachedAction = .stopAdding
+    @Default(100)
+    @ModifiedDate public var autoUpNextLimit: Int32
+    @Default(AutoAddLimitReachedAction.stopAdding)
+    @ModifiedDate public var autoUpNextLimitReached: AutoAddLimitReachedAction
 
-    @ModifiedDate public var headphoneControlsNextAction: HeadphoneControl = .skipForward
-    @ModifiedDate public var headphoneControlsPreviousAction: HeadphoneControl = .skipBack
+    @Default(HeadphoneControl.skipForward)
+    @ModifiedDate public var headphoneControlsNextAction: HeadphoneControl
+    @Default(HeadphoneControl.skipBack)
+    @ModifiedDate public var headphoneControlsPreviousAction: HeadphoneControl
 
-    @ModifiedDate public var privacyAnalytics: Bool = true
-    @ModifiedDate public var marketingOptIn: Bool = false
-    @ModifiedDate public var freeGiftAcknowledgement: Bool = false
+    @Default(true)
+    @ModifiedDate public var privacyAnalytics: Bool
+    @Default(false)
+    @ModifiedDate public var marketingOptIn: Bool
+    @Default(false)
+    @ModifiedDate public var freeGiftAcknowledgement: Bool
 
-    @ModifiedDate public var gridOrder: LibrarySort = .dateAddedNewestToOldest
+    @Default(LibrarySort.dateAddedNewestToOldest)
+    @ModifiedDate public var gridOrder: LibrarySort
+    @Default(LibraryType.threeByThree)
     @ModifiedDate public var gridLayout: LibraryType = .threeByThree
-    @ModifiedDate public var badges: BadgeType = .off
+    @Default(BadgeType.off)
+    @ModifiedDate public var badges: BadgeType
 
-    @ModifiedDate public var filesSortOrder: UploadedSort = .newestToOldest
+    @Default(UploadedSort.newestToOldest)
+    @ModifiedDate public var filesSortOrder: UploadedSort
 
-    @ModifiedDate public var playerShelf: [ActionOption] = []
+    @Default([ActionOption]())
+    @ModifiedDate public var playerShelf: [ActionOption]
 
     // MARK: - Appearance
 
-    @ModifiedDate public var useSystemTheme: Bool = true
-    @ModifiedDate public var theme: ThemeType = .light
-    @ModifiedDate public var lightThemePreference: ThemeType = .light
-    @ModifiedDate public var darkThemePreference: ThemeType = .dark
+    @Default(true)
+    @ModifiedDate public var useSystemTheme: Bool
+    @Default(ThemeType.light)
+    @ModifiedDate public var theme: ThemeType
+    @Default(ThemeType.light)
+    @ModifiedDate public var lightThemePreference: ThemeType
+    @Default(ThemeType.dark)
+    @ModifiedDate public var darkThemePreference: ThemeType
 
-    @ModifiedDate public var useEmbeddedArtwork: Bool = false
+    @Default(false)
+    @ModifiedDate public var useEmbeddedArtwork: Bool
 
-    @ModifiedDate public var useDarkUpNextTheme: Bool = true
-    @ModifiedDate public var autoPlayLastListUuid: AutoPlaySource = .uuid("")
-
-    static var defaults: AppSettings {
-        return AppSettings(openLinks: false,
-                           rowAction: .stream,
-                           episodeGrouping: .none,
-                           showArchived: false,
-                           upNextSwipe: .playNext,
-                           skipForward: 45,
-                           skipBack: 10,
-                           keepScreenAwake: false,
-                           openPlayer: false,
-                           intelligentResumption: true,
-                           playUpNextOnTap: false,
-                           playbackActions: false,
-                           legacyBluetooth: false,
-                           multiSelectGesture: true,
-                           chapterTitles: true,
-                           autoPlayEnabled: true,
-                           volumeBoost: false,
-                           trimSilence: .off,
-                           playbackSpeed: 0
-        )
-    }
+    @Default(true)
+    @ModifiedDate public var useDarkUpNextTheme: Bool
+    @Default(AutoPlaySource.uuid(""))
+    @ModifiedDate public var autoPlayLastListUuid: AutoPlaySource
 }
 
 extension SettingsStore<AppSettings> {
-    public static internal(set) var appSettings = SettingsStore(key: "app_settings", value: AppSettings.defaults)
+    public static internal(set) var appSettings = SettingsStore(key: "app_settings", value: AppSettings())
 }
