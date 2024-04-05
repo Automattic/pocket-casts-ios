f857774abec90c3d69b71c931d50ac6667e9f4ff
Brandon Titus <b@titus.io>
2024-04-04 18:04:54 -0600

    Add Discover handling for new categories style

--- podcasts/Categories/CategoriesSelectorViewController.swift
+++ podcasts/Categories/CategoriesSelectorViewController.swift
@@ -1,12 +1,30 @@
 import SwiftUI
 import PocketCastsServer
 
-class CategoriesSelectorViewController: UIHostingController<CategoriesSelectorView> {
-    init(item: DiscoverItem) {
-        super.init(rootView: CategoriesSelectorView(item: item))
+class CategoriesSelectorViewController: UIHostingController<CategoriesSelectorView>, DiscoverSummaryProtocol {
+
+    class Observable: ObservableObject {
+        @Published public var item: DiscoverItem?
+    }
+
+    @ObservedObject fileprivate var observable: Observable
+
+    func registerDiscoverDelegate(_ delegate: any DiscoverDelegate) {}
+
+    func populateFrom(item: PocketCastsServer.DiscoverItem) {
+        observable.item = item
+        view.setNeedsLayout()
+    }
+
+    init() {
+        let observable = Observable()
+        self.observable = observable
+
+        super.init(rootView: CategoriesSelectorView(observable: observable))
         if #available(iOS 16.0, *) {
             sizingOptions =  [.intrinsicContentSize]
         }
+        view.backgroundColor = nil
     }
 
     required init?(coder aDecoder: NSCoder) {
