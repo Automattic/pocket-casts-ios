b4f3d7a1380e5d2cf7c921f2a27bfc3c6c0df9a3
Leandro Alonso <contato@leandroalonso.com>
2024-04-10 17:02:48 -0300

    Add calls to sleep timer manager to handle automatically restarting it

--- podcasts/PlaybackManager.swift
+++ podcasts/PlaybackManager.swift
@@ -23,6 +23,7 @@ class PlaybackManager: ServerPlaybackDelegate {
         didSet {
             if sleepOnEpisodeEnd {
                 sleepTimeRemaining = -1
+                sleepTimerManager.recordSleepTimerDuration(duration: nil, onEpisodeEnd: true)
             }
             NotificationCenter.postOnMainThread(notification: Constants.Notifications.sleepTimerChanged)
         }
@@ -59,6 +60,8 @@ class PlaybackManager: ServerPlaybackDelegate {
     lazy var bookmarkManager: BookmarkManager = {
         BookmarkManager(playbackManager: self)
     }()
+
+    private lazy var sleepTimerManager = SleepTimerManager()
 
     /// The player we should fallback to
     private var fallbackToPlayer: PlaybackProtocol.Type? = nil
@@ -236,6 +239,8 @@ class PlaybackManager: ServerPlaybackDelegate {
             }
 
             self.updateIdleTimer()
+
+            self.sleepTimerManager.restartSleepTimerIfNeeded()
         })
     }
 
@@ -945,7 +950,7 @@ class PlaybackManager: ServerPlaybackDelegate {
 
     func playerDidFinishPlayingEpisode() {
         if sleepOnEpisodeEnd {
-            pause()
+            pauseAndRecordSleepTimerFinished()
             cancelSleepTimer()
             return
         }
@@ -1366,13 +1371,18 @@ class PlaybackManager: ServerPlaybackDelegate {
             sleepTimeRemaining = sleepTimeRemaining - updateTimerInterval
 
             if sleepTimeRemaining < 0 {
-                pause()
+                pauseAndRecordSleepTimerFinished()
             }
         }
 
         if player.buffering() == false {
             updateChapterInfo()
         }
+    }
+
+    private func pauseAndRecordSleepTimerFinished() {
+        sleepTimerManager.recordSleepTimerFinished()
+        pause()
     }
 
     private func fireProgressNotification() {
@@ -1473,6 +1483,7 @@ class PlaybackManager: ServerPlaybackDelegate {
     }
 
     func setSleepTimerInterval(_ stopIn: TimeInterval) {
+        sleepTimerManager.recordSleepTimerDuration(duration: stopIn, onEpisodeEnd: nil)
         sleepTimeRemaining = stopIn
         NotificationCenter.postOnMainThread(notification: Constants.Notifications.sleepTimerChanged)
         Analytics.track(.playerSleepTimerEnabled, properties: ["time": Int(stopIn)])
