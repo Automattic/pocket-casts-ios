c97a0eb3e551d2b3a7e7f6a4081abcdda3bded34
Sérgio Estêvão <sergioestevao@gmail.com>
2024-04-18 16:35:30 +0100

    Ensure circle view is redraw if color are changed.

--- podcasts/CircleView.swift
+++ podcasts/CircleView.swift
@@ -3,8 +3,17 @@ import UIKit
 class CircleView: UIView {
 
     var borderWidth: CGFloat = 2
-    var borderColor = UIColor.white.withAlphaComponent(0.3)
-    var centerColor = UIColor.white.withAlphaComponent(0.3)
+    var borderColor = UIColor.white.withAlphaComponent(0.3) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    var centerColor = UIColor.white.withAlphaComponent(0.3) {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
 
     override func draw(_ rect: CGRect) {
         let radius = (rect.width / 2) - 1
