40811f7f2c4d8fc0d1b351cb0f3e55aa781e650d
Sérgio Estêvão <sergioestevao@gmail.com>
2024-04-18 19:31:27 +0100

    Update unplayed episodes badge to new design

--- podcasts/UnplayedSashOverlayView.swift
+++ podcasts/UnplayedSashOverlayView.swift
@@ -2,10 +2,9 @@ import PocketCastsDataModel
 import UIKit
 
 class UnplayedSashOverlayView: UIView {
-    private let bgImage = UIImageView()
     private let badgeLabel = UILabel()
 
-    private var bgImageHeightConstraint: NSLayoutConstraint!
+    private var labelWidthConstraint: NSLayoutConstraint!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -24,64 +23,37 @@ class UnplayedSashOverlayView: UIView {
     }
 
     func populateFrom(podcast: Podcast, badgeType: BadgeType, libraryType: LibraryType) {
-        updateForLibraryType(libraryType)
-
         updateBadge(count: podcast.cachedUnreadCount, badgeType: badgeType)
     }
 
     func populateFrom(folder: Folder, badgeType: BadgeType, libraryType: LibraryType) {
-        updateForLibraryType(libraryType)
-
         updateBadge(count: folder.cachedUnreadCount, badgeType: badgeType)
     }
 
     private func updateBadge(count: Int, badgeType: BadgeType) {
-        if badgeType == .latestEpisode || badgeType == .allUnplayed, count > 0 {
-            bgImage.isHidden = false
+        if count > 0 {
             badgeLabel.isHidden = false
-            if badgeType == .latestEpisode {
-                badgeLabel.text = "●"
+            badgeLabel.text = count < 99 ? "\(count)" : "99"
+            if count > 9 {
+                labelWidthConstraint.constant = 26
             } else {
-                badgeLabel.text = count < 99 ? "\(count)" : "99"
+                labelWidthConstraint.constant = 20
             }
         } else {
-            bgImage.isHidden = true
             badgeLabel.isHidden = true
         }
     }
 
-    private func updateForLibraryType(_ libraryType: LibraryType) {
-        if libraryType == .fourByFour {
-            bgImage.image = UIImage(named: "badge4x4")
-            bgImageHeightConstraint.constant = 33
-        } else {
-            bgImage.image = UIImage(named: "badge3x3")
-            bgImageHeightConstraint.constant = 42
-        }
-    }
-
     private func setup() {
-        translatesAutoresizingMaskIntoConstraints = false
-
-        bgImage.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(bgImage)
-        bgImageHeightConstraint = bgImage.heightAnchor.constraint(equalToConstant: 42)
-        NSLayoutConstraint.activate([
-            bgImageHeightConstraint,
-            bgImage.widthAnchor.constraint(equalTo: bgImage.heightAnchor),
-            bgImage.trailingAnchor.constraint(equalTo: trailingAnchor),
-            bgImage.topAnchor.constraint(equalTo: topAnchor)
-        ])
-
-        badgeLabel.font = UIFont.systemFont(ofSize: 12)
+        badgeLabel.font = UIFont.systemFont(ofSize: 11, weight: .semibold)
         badgeLabel.translatesAutoresizingMaskIntoConstraints = false
         badgeLabel.textAlignment = .center
         addSubview(badgeLabel)
+        labelWidthConstraint = badgeLabel.widthAnchor.constraint(equalToConstant: 20)
         NSLayoutConstraint.activate([
             badgeLabel.heightAnchor.constraint(equalToConstant: 20),
-            badgeLabel.widthAnchor.constraint(equalToConstant: 20),
-            badgeLabel.centerYAnchor.constraint(equalTo: bgImage.centerYAnchor, constant: -8),
-            badgeLabel.centerXAnchor.constraint(equalTo: bgImage.centerXAnchor, constant: 8)
+            labelWidthConstraint,
+            badgeLabel.trailingAnchor.constraint(equalTo: trailingAnchor)
         ])
 
         NotificationCenter.default.addObserver(self, selector: #selector(themeDidChange), name: Constants.Notifications.themeChanged, object: nil)
@@ -94,7 +66,12 @@ class UnplayedSashOverlayView: UIView {
     }
 
     private func updateBadgeColors() {
-        bgImage.tintColor = ThemeColor.support05()
-        badgeLabel.textColor = ThemeColor.contrast01()
+        badgeLabel.clipsToBounds = true
+        backgroundColor = .clear
+        badgeLabel.textColor = ThemeColor.primaryInteractive02()
+        badgeLabel.backgroundColor = ThemeColor.primaryInteractive01()
+        badgeLabel.layer.borderColor = ThemeColor.primaryUi02().cgColor
+        badgeLabel.layer.borderWidth = 2
+        badgeLabel.layer.cornerRadius = 10
     }
 }
