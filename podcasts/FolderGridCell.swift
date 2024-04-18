1c8100d9787c1f28a003f861af0da5a470d222ab
Sérgio Estêvão <sergioestevao@gmail.com>
2024-04-18 17:19:05 +0100

    Update badge for folders.

--- podcasts/FolderGridCell.swift
+++ podcasts/FolderGridCell.swift
@@ -2,6 +2,11 @@ import PocketCastsDataModel
 import UIKit
 
 class FolderGridCell: UICollectionViewCell {
+
+    @IBOutlet var containerView: UIView!
+
+    @IBOutlet var simpleBadgeView: CircleView!
+
     @IBOutlet var folderPreview: FolderPreviewView!
 
     @IBOutlet var unplayedSashView: UnplayedSashOverlayView!
@@ -9,9 +14,28 @@ class FolderGridCell: UICollectionViewCell {
     func populateFrom(folder: Folder, badgeType: BadgeType, libraryType: LibraryType) {
         folderPreview.populateFromAsync(folder: folder)
 
-        self.layer.cornerRadius = 4
-        self.layer.masksToBounds = true
+        containerView.layer.cornerRadius = 4
+        containerView.layer.masksToBounds = true
 
+        updateBadge(folder: folder, badgeType: badgeType, libraryType: libraryType)
         unplayedSashView.populateFrom(folder: folder, badgeType: badgeType, libraryType: libraryType)
+    }
+
+    private func updateBadge(folder: Folder, badgeType: BadgeType, libraryType: LibraryType) {
+        switch badgeType {
+        case .latestEpisode:
+            simpleBadgeView.isHidden = false
+            unplayedSashView.isHidden = true
+            simpleBadgeView.borderColor = ThemeColor.secondaryUi01()
+            simpleBadgeView.centerColor = ThemeColor.primaryInteractive01()
+            simpleBadgeView.backgroundColor = .clear
+        case .allUnplayed:
+            simpleBadgeView.isHidden = true
+            unplayedSashView.isHidden = false
+            unplayedSashView.populateFrom(folder: folder, badgeType: badgeType, libraryType: libraryType)
+        case .off:
+            simpleBadgeView.isHidden = true
+            unplayedSashView.isHidden = true
+        }
     }
 }
