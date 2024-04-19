31072894b0ed30506661fad77cb3d55db3d4adee
Sérgio Estêvão <sergioestevao@gmail.com>
2024-04-19 20:17:38 +0100

    fix background color for lists

--- podcasts/FolderViewController.swift
+++ podcasts/FolderViewController.swift
@@ -99,7 +99,7 @@ class FolderViewController: PCViewController, UIGestureRecognizerDelegate {
         let iconColor = ThemeColor.filterIcon01(filterColor: folderColor)
         let backgroundColor = ThemeColor.filterUi01(filterColor: folderColor)
 
-        if let themeableCollectionView = mainGrid as? ThemeableCollectionView {
+        if let themeableCollectionView = mainGrid as? ThemeableCollectionView, Settings.libraryType() != .list {
             themeableCollectionView.backgroundColor = backgroundColor
         }
         changeNavTint(titleColor: titleColor, iconsColor: iconColor, backgroundColor: backgroundColor)
