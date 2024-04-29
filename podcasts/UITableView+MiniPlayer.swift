640c634e1d432d605ae767e255aad2ca0807a454
Sérgio Estêvão <sergioestevao@gmail.com>
2024-04-29 16:45:07 +0100

    Add update scroll indicator position depending of the mini player

--- podcasts/UITableView+MiniPlayer.swift
+++ podcasts/UITableView+MiniPlayer.swift
@@ -4,11 +4,15 @@ extension UITableView {
     func applyInsetForMiniPlayer(additionalBottomInset: CGFloat = 0) {
         let existingInset = contentInset
         contentInset = UIEdgeInsets(top: existingInset.top, left: existingInset.left, bottom: existingInset.bottom + Constants.Values.miniPlayerOffset + additionalBottomInset, right: existingInset.right)
+
+        let existingScrollIndicatorInset = verticalScrollIndicatorInsets
+        scrollIndicatorInsets = UIEdgeInsets(top: existingScrollIndicatorInset.top, left: existingScrollIndicatorInset.left, bottom: existingScrollIndicatorInset.bottom + Constants.Values.miniPlayerOffset + additionalBottomInset, right: existingScrollIndicatorInset.right)
     }
 
     func updateContentInset(multiSelectEnabled: Bool) {
         let multiSelectFooterOffset: CGFloat = multiSelectEnabled ? 80 : 0
         let miniPlayerOffset: CGFloat = PlaybackManager.shared.currentEpisode() == nil ? 0 : Constants.Values.miniPlayerOffset
         contentInset = UIEdgeInsets(top: 0, left: 0, bottom: miniPlayerOffset + multiSelectFooterOffset, right: 0)
+        scrollIndicatorInsets = contentInset
     }
 }
