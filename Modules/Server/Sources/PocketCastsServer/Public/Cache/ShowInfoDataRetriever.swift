1f0929c7190159f74587464a2472e3fbf48fb967
Leandro Alonso <contato@leandroalonso.com>
2024-04-30 07:43:59 -0300

    Ensure we cache requests and check for if-modified

--- Modules/Server/Sources/PocketCastsServer/Public/Cache/ShowInfoDataRetriever.swift
+++ Modules/Server/Sources/PocketCastsServer/Public/Cache/ShowInfoDataRetriever.swift
@@ -4,7 +4,11 @@ import Foundation
 public actor ShowInfoDataRetriever {
     private var dataRequestMap: [String: Task<Data, Error>] = [:]
 
-    public init() { }
+    private let cache: URLCache
+
+    public init() {
+        cache = URLCache(memoryCapacity: 1.megabytes, diskCapacity: 10.megabytes, diskPath: "show_notes")
+    }
 
     public func loadShowInfoData(
         for podcastUuid: String
@@ -14,11 +18,30 @@ public actor ShowInfoDataRetriever {
         }
 
         let url = ServerHelper.asUrl(ServerConstants.Urls.cache() + "mobile/show_notes/full/\(podcastUuid)")
-        let request = URLRequest(url: url, cachePolicy: .reloadRevalidatingCacheData)
+        var request = URLRequest(url: url, cachePolicy: .reloadRevalidatingCacheData)
+
+        if let cachedResponse = cache.cachedResponse(for: request) {
+            if let etag = cachedResponse.response.etag {
+                request.setValue(etag, forHTTPHeaderField: ServerConstants.HttpHeaders.ifNoneMatch)
+            }
+
+            if let lastModified = cachedResponse.response.lastModified {
+                request.setValue(lastModified, forHTTPHeaderField: ServerConstants.HttpHeaders.ifModifiedSince)
+            }
+        }
 
         let task = Task<Data, Error> {
             do {
-                let (data, _) = try await URLSession.shared.data(for: request)
+                let (data, response) = try await URLSession.shared.data(for: request)
+
+                if response.extractStatusCode() == 200 {
+                    let responseToCache = CachedURLResponse(response: response, data: data)
+                    cache.storeCachedResponse(responseToCache, for: request)
+                } else if let data = cache.cachedResponse(for: request)?.data {
+                    dataRequestMap[podcastUuid] = nil
+                    return data
+                }
+
                 dataRequestMap[podcastUuid] = nil
                 return data
             } catch {
