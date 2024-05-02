4f88738f4af225872b550a81368e6783bd1ec65f
Leandro Alonso <contato@leandroalonso.com>
2024-05-02 17:33:19 -0300

    Close outside of the transaction

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/Util/DatabaseIndexHelper.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/Util/DatabaseIndexHelper.swift
@@ -20,9 +20,9 @@ public class DatabaseIndexHelper {
                 } catch {
                     rollback.pointee = true
                 }
-
-                self?.queue.close()
             }
+
+            self?.queue.close()
         }
     }
 }
