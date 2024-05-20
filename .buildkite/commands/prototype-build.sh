cd19aeedd9af9f34959d682d0ddea86d21913ce8
Gio Lodi <gio@mokacoding.com>
2024-05-20 21:27:58 +1000

    Add CI step for Prototype Build

---
+++ .buildkite/commands/prototype-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -eu
+
+# Sentry CLI needs to be up-to-date
+brew upgrade sentry-cli
+
+echo "--- :rubygems: Setting up Gems"
+install_gems
+
+echo "--- :cocoapods: Setting up Pods"
+install_cocoapods
+
+echo "--- :closed_lock_with_key: Installing Secrets"
+bundle exec fastlane run configure_apply
+
+echo "--- :swift: Setting up Swift Packages"
+install_swiftpm_dependencies
+
+echo "--- :hammer_and_wrench: Building"
+bundle exec fastlane build_and_upload_entreprise
