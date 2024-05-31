1a7aac2c93ede49a29c7e16eb859f2e92edf9c77
Sérgio Estêvão <sergioestevao@gmail.com>
2024-05-31 16:39:48 +0100

    Update file size after export successful

--- podcasts/DownloadManager.swift
+++ podcasts/DownloadManager.swift
@@ -255,7 +255,8 @@ class DownloadManager: NSObject, FilePathProtocol {
             FileLog.shared.addMessage("DownloadManager export session: start exporting \(episode.uuid)")
             let exportCompleted = await MediaExporter.exportMediaItem(playbackItem, to: outputURL)
             if exportCompleted {
-                DataManager.sharedManager.saveEpisode(downloadStatus: .downloadedForStreaming, downloadError: nil, downloadTaskId: nil, episode: episode)
+                let fileSize = FileManager.default.fileSize(of: outputURL) ?? 0
+                DataManager.sharedManager.saveEpisode(downloadStatus: .downloadedForStreaming, sizeInBytes: fileSize, downloadTaskId: nil, episode: episode)
                 NotificationCenter.postOnMainThread(notification: Constants.Notifications.episodeDownloaded, object: episode.uuid)
             } else {
                 DataManager.sharedManager.saveEpisode(downloadStatus: .notDownloaded, downloadError: nil, downloadTaskId: nil, episode: episode)
