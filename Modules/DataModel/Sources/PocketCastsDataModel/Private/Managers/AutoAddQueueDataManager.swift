085b881e746674bd5cc861ffc2d76e00c8e5b4c1
Leandro Alonso <contato@leandroalonso.com>
2024-06-05 16:55:59 -0300

    Update AutoAddQueueDataManager.swift

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/AutoAddQueueDataManager.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/AutoAddQueueDataManager.swift
@@ -1,45 +1,48 @@
 import FMDB
+import GRDB
 import PocketCastsUtils
 
 public struct AutoAddCandidatesDataManager {
     private let dbQueue: FMDatabaseQueue
+    private let dbPool: DatabasePool
 
-    init(dbQueue: FMDatabaseQueue) {
+    init(dbQueue: FMDatabaseQueue, dbPool: DatabasePool) {
         self.dbQueue = dbQueue
+        self.dbPool = dbPool
     }
 
     /// Adds a new auto add candidate to the database
     public func add(podcastUUID: String, episodeUUID: String) {
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("INSERT INTO \(Constants.tableName) (episode_uuid, podcast_uuid) VALUES (?, ?)", values: [episodeUUID, podcastUUID])
-            } catch {
-                FileLog.shared.addMessage("AutoAddCandidatesDataManager.add error: \(error)")
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: "INSERT INTO \(Constants.tableName) (episode_uuid, podcast_uuid) VALUES (?, ?)", arguments: [episodeUUID, podcastUUID])
             }
+        } catch {
+            FileLog.shared.addMessage("AutoAddCandidatesDataManager.add error: \(error)")
         }
     }
 
     /// Removes a single candidate from the DB
     public func remove(_ candidate: AutoAddCandidate) {
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("""
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: """
                 DELETE FROM \(Constants.tableName) WHERE id = ? LIMIT 1
-                """, values: [candidate.id])
-            } catch {
-                FileLog.shared.addMessage("AutoAddCandidatesDataManager.remove error: \(error)")
+                """, arguments: [candidate.id])
             }
+        } catch {
+            FileLog.shared.addMessage("AutoAddCandidatesDataManager.remove error: \(error)")
         }
     }
 
     /// Reset the the entire candidates table
     public func clearAll() {
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("DELETE FROM \(Constants.tableName)", values: nil)
-            } catch {
-                FileLog.shared.addMessage("AutoAddCandidatesDataManager.clearAll error: \(error)")
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: "DELETE FROM \(Constants.tableName)")
             }
+        } catch {
+            FileLog.shared.addMessage("AutoAddCandidatesDataManager.clearAll error: \(error)")
         }
     }
 
@@ -48,9 +51,9 @@ public struct AutoAddCandidatesDataManager {
     public func candidates() -> [AutoAddCandidate] {
         var results: [AutoAddCandidate] = []
 
-        dbQueue.inDatabase { db in
-            do {
+        do {
 
+            try dbPool.read { db in
                 let query: String
 
                 if FeatureFlag.newSettingsStorage.enabled {
@@ -85,17 +88,16 @@ public struct AutoAddCandidatesDataManager {
                     """
                 }
 
-                let resultSet = try db.executeQuery(query, values: nil)
+                let rows = try Row.fetchCursor(db, sql: query)
 
-                defer { resultSet.close() }
-                while resultSet.next() {
-                    if let result = AutoAddCandidate(from: resultSet) {
+                while let row = try rows.next() {
+                    if let result = AutoAddCandidate(from: row) {
                         results.append(result)
                     }
                 }
-            } catch {
-                FileLog.shared.addMessage("candidates error: \(error)")
             }
+        } catch {
+            FileLog.shared.addMessage("candidates error: \(error)")
         }
 
         return results
@@ -135,6 +137,37 @@ public struct AutoAddCandidatesDataManager {
             guard
                 let idObj = resultSet.object(forColumn: Constants.idColumnName) as? NSNumber,
                 let episodeUuid = resultSet.string(forColumn: Constants.episodeColumnName),
+                let autoAddSetting = AutoAddToUpNextSetting(rawValue: setting)
+            else {
+                return nil
+            }
+
+            self.id = idObj.intValue
+            self.autoAddToUpNextSetting = autoAddSetting
+            self.episodeUuid = episodeUuid
+        }
+
+        init?(from row: RowCursor.Element) {
+
+            let setting: Int32
+            if FeatureFlag.newSettingsStorage.enabled {
+                let value: Int32 = row[Constants.autoAddSettingColumnName]
+                let position = UpNextPosition(rawValue: value)
+                switch position {
+                case .top:
+                    setting = AutoAddToUpNextSetting.addFirst.rawValue
+                case .bottom:
+                    setting = AutoAddToUpNextSetting.addLast.rawValue
+                default:
+                    setting = AutoAddToUpNextSetting.off.rawValue
+                }
+            } else {
+                setting = row[Constants.autoAddSettingColumnName]
+            }
+
+            guard
+                let idObj: NSNumber = row[Constants.idColumnName],
+                let episodeUuid: String = row[Constants.episodeColumnName],
                 let autoAddSetting = AutoAddToUpNextSetting(rawValue: setting)
             else {
                 return nil
