239c7bc720a5d4d6ada9434f67df0fa69c54aa91
Leandro Alonso <contato@leandroalonso.com>
2024-06-05 16:55:43 -0300

    Update EpisodeFilterDataManager.swift

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EpisodeFilterDataManager.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/EpisodeFilterDataManager.swift
@@ -1,4 +1,5 @@
 import FMDB
+import GRDB
 import PocketCastsUtils
 
 class EpisodeFilterDataManager {
@@ -28,173 +29,170 @@ class EpisodeFilterDataManager {
         "shorterThan"
     ]
 
-    func count(includeDeleted: Bool, dbQueue: FMDatabaseQueue) -> Int {
+    func count(includeDeleted: Bool, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) -> Int {
         var count = 0
-        dbQueue.inDatabase { db in
-            do {
-                let query = includeDeleted ? "SELECT COUNT(*) from \(DataManager.filtersTableName) WHERE manual = 0" : "SELECT COUNT(*) from \(DataManager.filtersTableName) WHERE manual = 0 AND wasDeleted = 0"
-                let resultSet = try db.executeQuery(query, values: nil)
-                defer { resultSet.close() }
+        do {
+            try dbPool.read { db in
+                let query = includeDeleted ? "SELECT COUNT(*) as count from \(DataManager.filtersTableName) WHERE manual = 0" : "SELECT COUNT(*) as count from \(DataManager.filtersTableName) WHERE manual = 0 AND wasDeleted = 0"
+                let rows = try Row.fetchCursor(db, sql: query)
 
-                if resultSet.next() {
-                    count = resultSet.long(forColumnIndex: 0)
+                while let row = try rows.next() {
+                    count = row["count"]
                 }
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.count error: \(error)")
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.count error: \(error)")
         }
 
         return count
     }
 
-    func episodeCount(forFilter filter: EpisodeFilter, episodeUuidToAdd: String?, dbQueue: FMDatabaseQueue) -> Int {
+    func episodeCount(forFilter filter: EpisodeFilter, episodeUuidToAdd: String?, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) -> Int {
         var count = 0
-        dbQueue.inDatabase { db in
-            do {
+        do {
+            try dbPool.read { db in
                 let queryForFilter = PlaylistHelper.queryFor(filter: filter, episodeUuidToAdd: episodeUuidToAdd, limit: 0)
-                let resultSet = try db.executeQuery("SELECT COUNT(*) from SJEpisode WHERE \(queryForFilter)", values: nil)
-                defer { resultSet.close() }
 
-                if resultSet.next() {
-                    count = resultSet.long(forColumnIndex: 0)
+                let rows = try Row.fetchCursor(db, sql: "SELECT COUNT(*) as count from SJEpisode WHERE \(queryForFilter)")
+
+                while let row = try rows.next() {
+                    count = row["count"]
                 }
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.episodeCount error: \(error)")
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.episodeCount error: \(error)")
         }
 
         return count
     }
 
-    func allFilters(includeDeleted: Bool, dbQueue: FMDatabaseQueue) -> [EpisodeFilter] {
+    func allFilters(includeDeleted: Bool, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) -> [EpisodeFilter] {
         let query = includeDeleted ? "SELECT * from \(DataManager.filtersTableName) WHERE manual = 0 ORDER BY sortPosition ASC" : "SELECT * from \(DataManager.filtersTableName) WHERE manual = 0 AND wasDeleted = 0 ORDER BY sortPosition ASC"
 
-        return allFilters(query: query, values: nil, dbQueue: dbQueue)
+        return allFilters(query: query, values: nil, dbQueue: dbQueue, dbPool: dbPool)
     }
 
-    func findBy(uuid: String, dbQueue: FMDatabaseQueue) -> EpisodeFilter? {
+    func findBy(uuid: String, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) -> EpisodeFilter? {
         var filter: EpisodeFilter?
-        dbQueue.inDatabase { db in
-            do {
-                let resultSet = try db.executeQuery("SELECT * from \(DataManager.filtersTableName) WHERE uuid = ?", values: [uuid])
-                defer { resultSet.close() }
+        do {
+            try dbPool.read { db in
+                let rows = try Row.fetchCursor(db, sql: "SELECT * from \(DataManager.filtersTableName) WHERE uuid = ?", arguments: [uuid])
 
-                if resultSet.next() {
-                    filter = self.createFilterFrom(resultSet: resultSet)
+                if let row = try rows.next() {
+                    filter = self.createFilterFrom(row: row)
                 }
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.findBy error: \(error)")
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.findBy error: \(error)")
         }
 
         return filter
     }
 
-    func deleteDeletedFilters(dbQueue: FMDatabaseQueue) {
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("DELETE FROM \(DataManager.filtersTableName) WHERE wasDeleted = 1", values: nil)
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.deleteDeletedFilters error: \(error)")
+    func deleteDeletedFilters(dbQueue: FMDatabaseQueue, dbPool: DatabasePool) {
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: "DELETE FROM \(DataManager.filtersTableName) WHERE wasDeleted = 1")
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.deleteDeletedFilters error: \(error)")
         }
     }
 
-    func allUnsyncedFilters(dbQueue: FMDatabaseQueue) -> [EpisodeFilter] {
-        allFilters(query: "SELECT * from \(DataManager.filtersTableName) WHERE syncStatus = ? ORDER BY sortPosition ASC", values: [SyncStatus.notSynced.rawValue], dbQueue: dbQueue)
+    func allUnsyncedFilters(dbQueue: FMDatabaseQueue, dbPool: DatabasePool) -> [EpisodeFilter] {
+        allFilters(query: "SELECT * from \(DataManager.filtersTableName) WHERE syncStatus = ? ORDER BY sortPosition ASC", values: [SyncStatus.notSynced.rawValue], dbQueue: dbQueue, dbPool: dbPool)
     }
 
-    func updatePosition(filter: EpisodeFilter, newPosition: Int32, dbQueue: FMDatabaseQueue) {
+    func updatePosition(filter: EpisodeFilter, newPosition: Int32, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) {
         filter.sortPosition = newPosition
         filter.syncStatus = SyncStatus.notSynced.rawValue
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("UPDATE \(DataManager.filtersTableName) SET sortPosition = ?, syncStatus = ? WHERE uuid = ?", values: [filter.sortPosition, filter.syncStatus, filter.uuid])
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.updatePosition error: \(error)")
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: "UPDATE \(DataManager.filtersTableName) SET sortPosition = ?, syncStatus = ? WHERE uuid = ?", arguments: [filter.sortPosition, filter.syncStatus, filter.uuid])
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.updatePosition error: \(error)")
         }
     }
 
-    func save(filter: EpisodeFilter, dbQueue: FMDatabaseQueue) {
-        dbQueue.inDatabase { db in
-            do {
+    func save(filter: EpisodeFilter, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) {
+        do {
+            try dbPool.write { db in
                 if filter.id == 0 {
                     filter.id = DBUtils.generateUniqueId()
-                    try db.executeUpdate("INSERT INTO \(DataManager.filtersTableName) (\(self.columnNames.joined(separator: ","))) VALUES \(DBUtils.valuesQuestionMarks(amount: self.columnNames.count))", values: self.createValuesFrom(filter: filter))
+                    try db.execute(sql: "INSERT INTO \(DataManager.filtersTableName) (\(self.columnNames.joined(separator: ","))) VALUES \(DBUtils.valuesQuestionMarks(amount: self.columnNames.count))", arguments: StatementArguments(self.createValuesFrom(filter: filter))!)
                 } else {
                     let setStatement = "\(self.columnNames.joined(separator: " = ?, ")) = ?"
-                    try db.executeUpdate("UPDATE \(DataManager.filtersTableName) SET \(setStatement) WHERE uuid = ?", values: self.createValuesFrom(filter: filter, includeUuidForWhere: true))
+                    try db.execute(sql: "UPDATE \(DataManager.filtersTableName) SET \(setStatement) WHERE uuid = ?", arguments: StatementArguments(self.createValuesFrom(filter: filter, includeUuidForWhere: true))!)
                 }
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.save error: \(error)")
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.save error: \(error)")
         }
     }
 
-    func delete(filter: EpisodeFilter, dbQueue: FMDatabaseQueue) {
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("DELETE FROM \(DataManager.filtersTableName) WHERE uuid = ?", values: [filter.uuid])
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.delete error: \(error)")
+    func delete(filter: EpisodeFilter, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) {
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: "DELETE FROM \(DataManager.filtersTableName) WHERE uuid = ?", arguments: [filter.uuid])
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.delete error: \(error)")
         }
     }
 
-    func markAllSynced(dbQueue: FMDatabaseQueue) {
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("UPDATE \(DataManager.filtersTableName) SET syncStatus = ? WHERE syncStatus = ?", values: [SyncStatus.synced.rawValue, SyncStatus.notSynced.rawValue])
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.markAllSynced error: \(error)")
+    func markAllSynced(dbQueue: FMDatabaseQueue, dbPool: DatabasePool) {
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: "UPDATE \(DataManager.filtersTableName) SET syncStatus = ? WHERE syncStatus = ?", arguments: [SyncStatus.synced.rawValue, SyncStatus.notSynced.rawValue])
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.markAllSynced error: \(error)")
         }
     }
 
-    func markAllUnsynced(dbQueue: FMDatabaseQueue) {
-        dbQueue.inDatabase { db in
-            do {
-                try db.executeUpdate("UPDATE \(DataManager.filtersTableName) SET syncStatus = ? WHERE syncStatus = ?", values: [SyncStatus.notSynced.rawValue, SyncStatus.synced.rawValue])
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.markAllUnsynced error: \(error)")
+    func markAllUnsynced(dbQueue: FMDatabaseQueue, dbPool: DatabasePool) {
+        do {
+            try dbPool.write { db in
+                try db.execute(sql: "UPDATE \(DataManager.filtersTableName) SET syncStatus = ? WHERE syncStatus = ?", arguments: [SyncStatus.notSynced.rawValue, SyncStatus.synced.rawValue])
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.markAllUnsynced error: \(error)")
         }
     }
 
-    private func allFilters(query: String, values: [Any]?, dbQueue: FMDatabaseQueue) -> [EpisodeFilter] {
+    private func allFilters(query: String, values: [Any]?, dbQueue: FMDatabaseQueue, dbPool: DatabasePool) -> [EpisodeFilter] {
         var allFilters = [EpisodeFilter]()
-        dbQueue.inDatabase { db in
-            do {
-                let resultSet = try db.executeQuery(query, values: values)
-                defer { resultSet.close() }
+        do {
+            try dbPool.read { db in
+                let rows = try Row.fetchCursor(db, sql: query, arguments: StatementArguments(values ?? [])!)
 
-                while resultSet.next() {
-                    let filter = self.createFilterFrom(resultSet: resultSet)
+                while let row = try rows.next() {
+                    let filter = self.createFilterFrom(row: row)
                     allFilters.append(filter)
                 }
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.allFilters error: \(error)")
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.allFilters error: \(error)")
         }
 
         return allFilters
     }
 
-    func nextSortPositionForFilter(dbQueue: FMDatabaseQueue) -> Int {
+    func nextSortPositionForFilter(dbQueue: FMDatabaseQueue, dbPool: DatabasePool) -> Int {
         var highestPosition = 0
-        dbQueue.inDatabase { db in
-            do {
-                let query = "SELECT MAX(sortPosition) from \(DataManager.filtersTableName)"
-                let resultSet = try db.executeQuery(query, values: nil)
-                defer { resultSet.close() }
+        do {
+            try dbPool.read { db in
+                let query = "SELECT MAX(sortPosition) as highestPosition from \(DataManager.filtersTableName)"
 
-                if resultSet.next() {
-                    highestPosition = resultSet.long(forColumnIndex: 0)
+                let rows = try Row.fetchCursor(db, sql: query)
+
+                if let row = try rows.next() {
+                    highestPosition = row["highestPosition"]
                 }
-            } catch {
-                FileLog.shared.addMessage("EpisodeFilterDataManager.nextSortPositionForFilter error: \(error)")
             }
+        } catch {
+            FileLog.shared.addMessage("EpisodeFilterDataManager.nextSortPositionForFilter error: \(error)")
         }
 
         return highestPosition + 1
@@ -227,6 +225,35 @@ class EpisodeFilterDataManager {
         filter.filterDuration = rs.bool(forColumn: "filterDuration")
         filter.longerThan = rs.int(forColumn: "longerThan")
         filter.shorterThan = rs.int(forColumn: "shorterThan")
+
+        return filter
+    }
+
+    private func createFilterFrom(row: RowCursor.Element) -> EpisodeFilter {
+        let filter = EpisodeFilter()
+        filter.id = row["id"]
+        filter.autoDownloadEpisodes = row["autoDownloadEpisodes"]
+        filter.customIcon = row["customIcon"]
+        filter.filterAllPodcasts = row["filterAllPodcasts"]
+        filter.filterAudioVideoType = row["filterAudioVideoType"]
+        filter.filterDownloaded = row["filterDownloaded"]
+        filter.filterFinished = row["filterFinished"]
+        filter.filterNotDownloaded = row["filterNotDownloaded"]
+        filter.filterPartiallyPlayed = row["filterPartiallyPlayed"]
+        filter.filterStarred = row["filterStarred"]
+        filter.filterUnplayed = row["filterUnplayed"]
+        filter.filterHours = row["filterHours"]
+        filter.playlistName = row["playlistName"]
+        filter.sortPosition = row["sortPosition"]
+        filter.sortType = row["sortType"]
+        filter.uuid = row["uuid"]
+        filter.podcastUuids = row["podcastUuids"]
+        filter.autoDownloadLimit = row["autoDownloadLimit"]
+        filter.syncStatus = row["syncStatus"]
+        filter.wasDeleted = row["wasDeleted"]
+        filter.filterDuration = row["filterDuration"]
+        filter.longerThan = row["longerThan"]
+        filter.shorterThan = row["shorterThan"]
 
         return filter
     }
