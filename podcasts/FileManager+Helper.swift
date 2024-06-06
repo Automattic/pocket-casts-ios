828ee47bf6aa81e4540723f514b5ef9e95926b73
Sérgio Estêvão <sergioestevao@gmail.com>
2024-06-06 13:43:44 +0100

    Convert method to use URL methods for files

--- podcasts/FileManager+Helper.swift
+++ podcasts/FileManager+Helper.swift
@@ -3,10 +3,10 @@ import Foundation
 extension FileManager {
 
     func fileSize(of url: URL) -> Int64? {
-        guard let attrs = try? self.attributesOfItem(atPath: url.path),
-              let fileSize = attrs[.size] as? Int64 else {
+
+        guard let fileSize = try? url.resourceValues(forKeys: [.fileSizeKey]).fileSize else {
             return nil
         }
-        return fileSize
+        return Int64(fileSize)
     }
 }
