d7f6fd66ff9b4011617c0b74fb246ae5ce718df2
Brandon Titus <b@titus.io>
2024-06-21 14:45:46 -0600

    Update SwiftUI view snapshot for iOS 16

--- podcasts/End of Year/Views/View+Snapshot.swift
+++ podcasts/End of Year/Views/View+Snapshot.swift
@@ -2,19 +2,25 @@ import SwiftUI
 
 extension View {
     /// Returns a `UIImage` from a SwiftUI View
-    @MainActor 
+    @MainActor
     public func snapshot() -> UIImage {
-        let controller = UIHostingController(rootView: self.edgesIgnoringSafeArea(.top))
-        let view = controller.view
+        if #available(iOS 16.0, *) {
+            let renderer = ImageRenderer(content: self)
+            renderer.scale = 3
+            return renderer.uiImage!
+        } else {
+            let controller = UIHostingController(rootView: self.edgesIgnoringSafeArea(.top))
+            let view = controller.view
 
-        let targetSize = controller.view.intrinsicContentSize
-        view?.bounds = CGRect(origin: .zero, size: targetSize)
-        view?.backgroundColor = .clear
+            let targetSize = controller.view.intrinsicContentSize
+            view?.bounds = CGRect(origin: .zero, size: targetSize)
+            view?.backgroundColor = .clear
 
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
+            let renderer = UIGraphicsImageRenderer(size: targetSize)
 
-        return renderer.image { _ in
-            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+            return renderer.image { _ in
+                view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+            }
         }
     }
 }
