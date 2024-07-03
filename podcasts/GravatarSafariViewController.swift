af5e863b390d6289323081550e13c0dadd5b504f
Pinar Olguc <pinarolguc@gmail.com>
2024-07-03 17:47:39 +0300

    Add custom encoding for +

--- podcasts/GravatarSafariViewController.swift
+++ podcasts/GravatarSafariViewController.swift
@@ -10,11 +10,11 @@ final class GravatarSafariViewController: SFSafariViewController {
             switch self {
             case .avatarUpdate(let email):
                 guard var components = URLComponents(string: "https://gravatar.com/profile") else { return nil }
-                components.queryItems = [
-                    .init(name: "is_quick_editor", value: "true"),
-                    .init(name: "email", value: email),
-                    .init(name: "scope", value: "avatars")
-                ]
+                components.percentEncodedQueryItems = [
+                    URLQueryItem(name: "is_quick_editor", value: "true"),
+                    URLQueryItem(name: "email", value: email),
+                    URLQueryItem(name: "scope", value: "avatars")
+                ].map({ $0.percentEncoded() })
                 return components.url
             }
         }
@@ -28,5 +28,20 @@ final class GravatarSafariViewController: SFSafariViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         NotificationCenter.default.post(name: Constants.Notifications.avatarNeedsRefreshing, object: nil)
+    }
+}
+
+fileprivate extension URLQueryItem {
+    func percentEncoded() -> URLQueryItem {
+        /// addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) encode parameters following RFC 3986
+        /// According to RFC 3986, `+` is a valid character so it is not encoded.
+        /// We should encode "+" sign with its HTTP equivalent.
+
+        var newQueryItem = self
+        newQueryItem.value = value?
+            .addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)?
+            .replacingOccurrences(of: "+", with: "%2B")
+
+        return newQueryItem
     }
 }
