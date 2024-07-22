3b027e0b11a2dc49b01d0edb02199b016b717ebf
Sérgio Estêvão <sergioestevao@gmail.com>
2024-07-22 18:57:01 +0100

    Ensure that question mark and exclamation mark also break line

--- podcasts/TranscriptFilter.swift
+++ podcasts/TranscriptFilter.swift
@@ -49,9 +49,9 @@ extension RegexFilter {
     // Remove SRT tags, for example: "Speaker 1: " to ""
     static let speakerFilter = RegexFilter(pattern: "Speaker \\d?: *", replacement: "")
     // Ensure that any full stop starts a new line
-    static let fullStopNewLineFilter = RegexFilter(pattern: "[\\?\\.][ \\t]*", replacement: ".\n")
+    static let fullStopNewLineFilter = RegexFilter(pattern: "[\\!\\?\\.][ \\t]*", replacement: ".\n")
     // Ensure that end of cues have a space when appended to the next cue
-    static let notfullStopNewLineFilter = RegexFilter(pattern: "([^\\.])\\z", replacement: "$0 ")
+    static let notfullStopNewLineFilter = RegexFilter(pattern: "(^[\\!\\?\\.])\\z", replacement: "$0 ")
 }
 
 struct SuffixFilter: TranscriptFilter {
