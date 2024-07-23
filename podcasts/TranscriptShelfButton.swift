9968b17b181cbaed78a390e6ca1b0f899e624cc3
Leandro Alonso <contato@leandroalonso.com>
2024-07-23 14:46:18 -0300

    Move extension to its own file

--- podcasts/TranscriptShelfButton.swift
+++ podcasts/TranscriptShelfButton.swift
@@ -37,16 +37,3 @@ class TranscriptShelfButton: UIButton {
         currentEpisode?.checkTranscriptAvailability()
     }
 }
-
-extension Episode {
-    // MARK: - Transcripts
-
-    func checkTranscriptAvailability() {
-        Task.init {
-            if let transcripts = try? await ShowInfoCoordinator.shared.loadTranscripts(podcastUuid: parentIdentifier(), episodeUuid: uuid) {
-                let transcriptsAvailable = !transcripts.isEmpty
-                NotificationCenter.postOnMainThread(notification: Constants.Notifications.episodeTranscriptAvailabilityChanged, userInfo: ["episodeUuid": uuid, "isAvailable": transcriptsAvailable])
-            }
-        }
-    }
-}
