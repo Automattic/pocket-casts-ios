a9b36bc2f5ea53ac3303256aa706c3dccd38e2c3
Leandro Alonso <contato@leandroalonso.com>
2024-07-25 15:42:56 -0300

    Open review URL when tapping on the button

--- podcasts/Profile - SwiftUI/Account/ChampionView.swift
+++ podcasts/Profile - SwiftUI/Account/ChampionView.swift
@@ -1,24 +1,32 @@
 import SwiftUI
+import PocketCastsServer
 
 struct ChampionView: View {
     @EnvironmentObject var theme: Theme
 
+    @Environment(\.openURL) private var openURL
+
     var body: some View {
         VStack(spacing: 16) {
             Image("crown")
+                .renderingMode(.template)
+                .tint(theme.primaryUi05Selected)
             Text("You’re a true champion of Pocket Casts!")
                 .font(style: .title, weight: .semibold)
                 .multilineTextAlignment(.center)
             Text("Thanks for being with us since the beginning! If you enjoy using our app, we’d love to hear your feedback.")
-                .font(style: .body)
+                .font(style: .callout)
                 .multilineTextAlignment(.center)
+                .foregroundStyle(theme.primaryUi05Selected)
             Button(action: {
-                // action
+                openURL(URL(string: ServerConstants.Urls.appStoreReview)!)
             }, label: {
                 Text("Rate Pocket Casts")
             })
             .buttonStyle(BasicButtonStyle(textColor: theme.primaryInteractive02, backgroundColor: theme.primaryText01))
         }
+        .padding()
+        .applyDefaultThemeOptions()
     }
 }
 
