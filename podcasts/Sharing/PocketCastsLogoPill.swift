14aacb3a1f870c8a10b98d51e160c5a8dfca4ba8
Brandon Titus <b@titus.io>
2024-07-24 20:42:20 -0600

    Add rotating logo animation
    Just a sample for testing

--- podcasts/Sharing/PocketCastsLogoPill.swift
+++ podcasts/Sharing/PocketCastsLogoPill.swift
@@ -6,6 +6,7 @@ struct PocketCastsLogoPill: View {
             Image("splashlogo")
                 .resizable()
                 .frame(width: 18, height: 18)
+                .rotating()
             Text("Pocket Casts")
                 .padding(.trailing, 7)
                 .font(Font.system(size: 12, weight: .semibold))
@@ -14,6 +15,26 @@ struct PocketCastsLogoPill: View {
         .foregroundStyle(.white)
         .background(Theme(previewTheme: .classic).primaryIcon01)
         .clipShape(Capsule())
+    }
+}
+
+struct RotationAnimationModifier: ViewModifier {
+    @State private var angle: Double = 0
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(angle))
+            .onAppear {
+                withAnimation(Animation.linear(duration: 2).repeatForever(autoreverses: false)) {
+                    angle = 360
+                }
+            }
+    }
+}
+
+extension View {
+    func rotating() -> some View {
+        self.modifier(RotationAnimationModifier())
     }
 }
 
