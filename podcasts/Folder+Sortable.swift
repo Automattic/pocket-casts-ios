30cafce307c8c1ae72ba11eeeae0209b339c42d6
Daniele Bogo <me@bogodaniele.com>
2024-07-30 16:57:14 +0100

    Add new protocol and method to sort by name and uuid

---
+++ podcasts/Folder+Sortable.swift
@@ -0,0 +1,13 @@
+import Foundation
+import PocketCastsDataModel
+import PocketCastsUtils
+
+extension Folder: Sortable {
+    public var itemUUID: String {
+        uuid
+    }
+    
+    public var itemTitle: String? {
+        name
+    }
+}
