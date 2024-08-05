1999d3f868649eff31832c3dbf2805ef918a799a
Leandro Alonso <contato@leandroalonso.com>
2024-08-05 12:05:40 -0300

    Ensure no retain cycle happens

--- Modules/Utils/Sources/PocketCastsUtils/Extensions/UI/UITextView+scrollToRange.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Extensions/UI/UITextView+scrollToRange.swift
@@ -9,26 +9,28 @@ extension UITextView {
 
         let glyphRange = layoutManager.glyphRange(forCharacterRange: range, actualCharacterRange: nil)
 
-        layoutManager.enumerateEnclosingRects(forGlyphRange: glyphRange, withinSelectedGlyphRange: NSRange(location: NSNotFound, length: 0), in: textContainer) { rect, _ in
-            let finalRect = rect.offsetBy(dx: self.textContainerInset.left, dy: self.textContainerInset.top)
+        layoutManager.enumerateEnclosingRects(forGlyphRange: glyphRange, withinSelectedGlyphRange: NSRange(location: NSNotFound, length: 0), in: textContainer) { [weak self] rect, _ in
+            guard let self else { return }
+
+            let finalRect = rect.offsetBy(dx: textContainerInset.left, dy: textContainerInset.top)
 
             // Calculate the rectangle to scroll to such that the finalRect is centered
             var visibleRect = CGRect(
-                x: finalRect.origin.x - (self.bounds.width / 2) + (finalRect.width / 2),
-                y: finalRect.origin.y - (self.bounds.height / 2) + (finalRect.height / 2),
-                width: self.bounds.width,
-                height: self.bounds.height
-            ).inset(by: self.contentInset)
-            visibleRect = .init(x: visibleRect.origin.x, y: visibleRect.origin.y + (self.contentInset.bottom / 2), width: visibleRect.width, height: visibleRect.height)
+                x: finalRect.origin.x - (bounds.width / 2) + (finalRect.width / 2),
+                y: finalRect.origin.y - (bounds.height / 2) + (finalRect.height / 2),
+                width: bounds.width,
+                height: bounds.height
+            ).inset(by: contentInset)
+            visibleRect = .init(x: visibleRect.origin.x, y: visibleRect.origin.y + (contentInset.bottom / 2), width: visibleRect.width, height: visibleRect.height)
 
-            if visibleRect.origin.y + visibleRect.height > self.contentSize.height {
-                let location = self.text.count - 1
+            if visibleRect.origin.y + visibleRect.height > contentSize.height {
+                let location = text.count - 1
                 let bottom = NSMakeRange(location, 1)
-                self.scrollRangeToVisible(bottom)
+                scrollRangeToVisible(bottom)
                 return
             }
 
-            self.scrollRectToVisible(visibleRect, animated: true)
+            scrollRectToVisible(visibleRect, animated: true)
         }
     }
 }
