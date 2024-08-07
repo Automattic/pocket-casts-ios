0352fdc92d7f4c173eab538e71577b6229503f45
Leandro Alonso <contato@leandroalonso.com>
2024-08-07 12:50:52 -0300

    Update transcript icon for the shelf bottom sheet

--- podcasts/Enumerations.swift
+++ podcasts/Enumerations.swift
@@ -382,7 +382,7 @@ extension PlayerAction: AnalyticsDescribable {
         case .addBookmark:
             return "bookmarks-shelf-overflow-icon"
         case .transcript:
-            return "show_notes"
+            return "transcript"
         }
     }
 
