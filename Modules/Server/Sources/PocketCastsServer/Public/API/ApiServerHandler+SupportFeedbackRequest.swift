fe8873094d343c042b47d8c9e0be742d93688b8a
Daniele Bogo <me@bogodaniele.com>
2024-08-07 11:51:23 +0100

    Add generic error for feedback toast

--- Modules/Server/Sources/PocketCastsServer/Public/API/ApiServerHandler+SupportFeedbackRequest.swift
+++ Modules/Server/Sources/PocketCastsServer/Public/API/ApiServerHandler+SupportFeedbackRequest.swift
@@ -1,8 +1,13 @@
 import Foundation
 
 public extension ApiServerHandler {
-    func sendFeedback(message: String) {
-        let operation = SupportFeedbackTask(message: message)
-        apiQueue.addOperation(operation)
+    func sendFeedback(message: String) async -> Bool {
+        return await withCheckedContinuation { continuation in
+            let operation = SupportFeedbackTask(message: message)
+            operation.completion = { success in
+                continuation.resume(returning: success)
+            }
+            apiQueue.addOperation(operation)
+        }
     }
 }
