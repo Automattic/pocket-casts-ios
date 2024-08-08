05762f2e6a8b7c87f90c002ba6b6b3fa64aa9a1e
Daniele Bogo <me@bogodaniele.com>
2024-08-08 15:33:21 +0100

    Copy update

--- podcasts/Strings+Generated.swift
+++ podcasts/Strings+Generated.swift
@@ -2218,9 +2218,9 @@ internal enum L10n {
   }
   /// Got it
   internal static var ratingWhatsNewButtonTitle: String { return L10n.tr("Localizable", "rating_whats_new_button_title") }
-  /// You can start rating any show in Pocket Casts. Make sure you give it a few listens first and go support your favorite creators!
+  /// Rate your top podcasts and let creators know how much you appreciate their work. Plus, your ratings help others find new favorite shows!
   internal static var ratingWhatsNewMessage: String { return L10n.tr("Localizable", "rating_whats_new_message") }
-  /// Rating podcasts available now!
+  /// Now Available: Podcast Ratings 🎉
   internal static var ratingWhatsNewTitle: String { return L10n.tr("Localizable", "rating_whats_new_title") }
   /// FINDING NEW PODCAST EPISODES
   internal static var refreshControlFetchingEpisodes: String { return L10n.tr("Localizable", "refresh_control_fetching_episodes") }
