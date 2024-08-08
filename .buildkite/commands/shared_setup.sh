6ccaf034fcf2888defd04d5ff62014984427234f
Gio Lodi <gio@mokacoding.com>
2024-08-08 20:43:05 +1000

    Clarify reason for using `xcodebuild` when resolving SPMs
    Co-authored-by: Olivier Halligon <olivier@halligon.net>

--- .buildkite/commands/shared_setup.sh
+++ .buildkite/commands/shared_setup.sh
@@ -7,7 +7,9 @@ echo "--- :cocoapods: Setting up Pods"
 install_cocoapods
 
 echo "--- :swift: Installing Swift Package Manager Dependencies"
-# Disabled until the xcodeproj will be in a folder other than the project root
+# Temporarily disable `install_swiftpm_dependencies` and calling `xcodebuild -resolvePackageDependencies`
+# command directly instead until we move the `.xcodeproj` to a folder other than the project root to workaround
+# an issue with `xcodebuild` not using the right `Package.resolved` file (`*.xcworkspace/*` vs `*.xcodeproj/*`)
 #
 # install_swiftpm_dependencies
 xcodebuild -resolvePackageDependencies \
