cfaf6dbc38260c14028a7c9f88bbbc6418c5322a
Sérgio Estêvão <sergioestevao@gmail.com>
2024-08-12 14:45:08 +0100

    Try to load other transcript if our preferred option is not available.

--- podcasts/TranscriptManager.swift
+++ podcasts/TranscriptManager.swift
@@ -43,11 +43,23 @@ class TranscriptManager {
     public func loadTranscript() async throws -> TranscriptModel {
         guard
             let transcripts = try? await showCoordinator.loadTranscriptsMetadata(podcastUuid: podcastUUID, episodeUuid: episodeUUID, cacheTranscript: false),
-            let transcript = TranscriptFormat.bestTranscript(from: transcripts) else {
+            !transcripts.isEmpty else {
             throw TranscriptError.notAvailable
         }
-
-        return try await loadTranscript(transcript)
+        var transcriptsAvailable = transcripts
+        while let transcript = TranscriptFormat.bestTranscript(from: transcriptsAvailable) {
+            do {
+                let model = try await loadTranscript(transcript)
+                return model
+            } catch TranscriptError.empty, TranscriptError.failedToParse {
+                transcriptsAvailable.removeAll { other in
+                    other.transcriptFormat == transcript.transcriptFormat
+                }
+            } catch {
+                throw error
+            }
+        }
+        throw TranscriptError.failedToLoad
     }
 
     private func loadTranscript(_ transcript: Transcript) async throws -> TranscriptModel {
