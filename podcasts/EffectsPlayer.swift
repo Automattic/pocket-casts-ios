60a67436d0407ee9fdb93e61896d55e27505330b
Sérgio Estêvão <sergioestevao@gmail.com>
2024-08-13 22:00:51 +0100

    Pause when seeking

--- podcasts/EffectsPlayer.swift
+++ podcasts/EffectsPlayer.swift
@@ -208,7 +208,7 @@ class EffectsPlayer: PlaybackProtocol, Hashable {
 
     func seekTo(_ time: TimeInterval, completion: (() -> Void)?) {
         guard let readOperation = audioReadTask else { return }
-
+        playbackSpeed = 0
         lastSeekTime = max(0.1, time)
         seeking = true
         readOperation.seekTo(time, completion: { [weak self] seekedToEnd in
@@ -219,7 +219,7 @@ class EffectsPlayer: PlaybackProtocol, Hashable {
                 FileLog.shared.addMessage("EffectsPlayer seeked passed end of episode, calling finished playing")
                 PlaybackManager.shared.playerDidFinishPlayingEpisode()
             }
-
+            self?.playbackSpeed = 1
             self?.seeking = false
         })
     }
