fbfc0d09715ffa89138b41baff049ed010041201
Leandro Alonso <contato@leandroalonso.com>
2024-08-15 14:18:38 -0300

    Improve code

--- podcasts/Analytics/WidgetAnalytics.swift
+++ podcasts/Analytics/WidgetAnalytics.swift
@@ -2,8 +2,8 @@ import Foundation
 import WidgetKit
 
 class WidgetAnalytics {
-    let userDefaults: UserDefaults
-    let analytics: Analytics
+    private let userDefaults: UserDefaults
+    private let analytics: Analytics
 
     init(userDefaults: UserDefaults = UserDefaults.standard,
          analytics: Analytics = .shared) {
@@ -12,31 +12,38 @@ class WidgetAnalytics {
     }
 
     func track() {
-        var widgets = userDefaults.dictionary(forKey: "installed-widgets") as? [String: Bool] ?? [String: Bool]()
-        var presentWidgets: [String] = []
+        var previousWidgets = userDefaults.dictionary(forKey: "installed-widgets") as? [String: Bool] ?? [String: Bool]()
+        var currentWidgets: [String] = []
+
         WidgetCenter.shared.getCurrentConfigurations { [self] widgetInfos in
             guard case .success(let infos) = widgetInfos else { return }
+
+            // Track installed widgets
             infos.forEach { widget in
-                let installed = widgets["widget-\(widget.kind)-\(widget.family)"] ?? false
-                presentWidgets.append("widget-\(widget.kind)-\(widget.family)")
+                let installed = previousWidgets[widgetKey(widget)] ?? false
+                currentWidgets.append(widgetKey(widget))
                 if !installed {
                     // Installed
                     print("$$ \(widget.kind) \(widget.family) installed")
-                    widgets["widget-\(widget.kind)-\(widget.family)"] = true
+                    previousWidgets[widgetKey(widget)] = true
                 }
             }
 
-            var unninstalledWidgets = widgets.filter { !presentWidgets.contains($0.key) }
-
+            // Track unninstalled widgets
+            let unninstalledWidgets = previousWidgets.filter { !currentWidgets.contains($0.key) }
             unninstalledWidgets.forEach { widget in
-                let components = widget.key.split(separator: "-")
-                print("$$ \(components[1]) \(components[2]) unninstalled")
-                widgets.removeValue(forKey: widget.key)
+                let components = widget.key.split(separator: "$")
+                if let kind = components[safe: 1], let family = components[safe: 2] {
+                    print("$$ \(kind) \(family) unninstalled")
+                }
+                previousWidgets.removeValue(forKey: widget.key)
             }
 
-            userDefaults.set(widgets, forKey: "installed-widgets")
+            userDefaults.set(previousWidgets, forKey: "installed-widgets")
         }
+    }
 
-
+    private func widgetKey(_ widget: WidgetInfo) -> String {
+        return "widget$\(widget.kind)$\(widget.family)"
     }
 }
