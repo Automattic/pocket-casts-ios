d83e588f280fc93f4be3473ac0e12c0cc1c74da2
Brandon Titus <b@titus.io>
2024-08-19 00:04:31 -0600

    Move CGSize+FittingSize extension

--- podcasts/Sharing/AVAsset+Crop.swift
+++ podcasts/Sharing/AVAsset+Crop.swift
@@ -1,23 +1,5 @@
 import AVFoundation
 
-extension CGSize {
-    func fitting(aspectRatio: CGSize) -> CGSize {
-        let targetAspectRatio = aspectRatio.width / aspectRatio.height
-
-        if self.width / self.height > targetAspectRatio {
-            // Original is wider, constrain height
-            let newHeight = self.height
-            let newWidth = newHeight * targetAspectRatio
-            return CGSize(width: newWidth, height: newHeight)
-        } else {
-            // Original is taller, constrain width
-            let newWidth = self.width
-            let newHeight = newWidth / targetAspectRatio
-            return CGSize(width: newWidth, height: newHeight)
-        }
-    }
-}
-
 extension AVAsset {
     enum CropError: Error {
         case failedVideoTrackCreation
