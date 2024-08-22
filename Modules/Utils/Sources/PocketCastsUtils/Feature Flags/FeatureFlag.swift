7d6c9ecb724320733058ffa5a7e9cb3133e3fd25
rviljoen <25402814+rviljoen@users.noreply.github.com>
2024-08-22 22:30:36 +0200

    Add comment to feature flag with reference to issue being addressed

--- Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
@@ -88,7 +88,11 @@ public enum FeatureFlag: String, CaseIterable {
     /// Enable the new Upgrade Experiments
     case upgradeExperiment
 
-    ///
+    /// When enabled, we ignore audio interruptions with InterruptionReason set to routeDisconnected
+    /// (introduced in iOS 17 and watchOS 10) because these are not really interruptions as we have
+    /// implemented them previously. If the route is disconnected, audio stops indefinitely
+    /// until a new route connects (for which we'll received a different notification and handle accordingly)
+    /// See: https://github.com/Automattic/pocket-casts-ios/issues/2049
     case ignoreRouteDisconnectedInterruption
 
     public var enabled: Bool {
