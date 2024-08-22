c95ed1df733443ad7a3e9e4b96de7fd8e779ce84
Daniele Bogo <me@bogodaniele.com>
2024-08-22 18:51:54 +0100

    Create new view

---
+++ podcasts/Onboarding/Plus/UpgradeExperiment/PlusPaywallReviews.swift
@@ -0,0 +1,22 @@
+//
+//  PlusPaywallReviews.swift
+//  podcasts
+//
+//  Created by Daniele Bogo on 22/08/2024.
+//  Copyright © 2024 Shifty Jelly. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlusPaywallReviews: View {
+    let tier: UpgradeTier
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlusPaywallReviews(tier: .plus)
+        .background(.blue)
+}
