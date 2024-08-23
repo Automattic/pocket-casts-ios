4dd8fcbc585cbc5772c2fb268f75ba7383e050d1
Brandon Titus <b@titus.io>
2024-08-22 20:32:28 -0600

    Handle offsets when Trim Handles reach edges
    This required refactoring of the Trim Handle positioning

--- podcasts/Sharing/Clip/TrimSelectionView.swift
+++ podcasts/Sharing/Clip/TrimSelectionView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct TrimSelectionView: View {
     @Binding var leading: CGFloat
     @Binding var trailing: CGFloat
+    let handleWidth: CGFloat
     let indicatorWidth: CGFloat
 
     let changed: (CGFloat, TrimHandle.Side) -> Void
@@ -19,8 +20,8 @@ struct TrimSelectionView: View {
                 // Offset and frame are adjusted to hide this rectangle behind the Trim Handles
                 .frame(width: (trailing - leading + indicatorWidth) + (Constants.borderWidth * 2))
                 .offset(x: leading - Constants.borderWidth)
-            TrimHandle(position: $leading, side: .leading, indicatorWidth: indicatorWidth, onChanged: { changed($0, .leading) })
-            TrimHandle(position: $trailing, side: .trailing, indicatorWidth: indicatorWidth, onChanged: { changed( $0, .trailing) })
+            TrimHandle(position: $leading, side: .leading, width: handleWidth, onChanged: { changed($0, .leading) })
+            TrimHandle(position: $trailing, side: .trailing, width: handleWidth, onChanged: { changed( $0, .trailing) })
         }
     }
 }
