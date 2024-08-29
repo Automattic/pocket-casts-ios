03f4b9abc4a2326afb6cd4632190ef66a080afcf
Leandro Alonso <contato@leandroalonso.com>
2024-08-29 14:40:12 -0300

    Persist changes if it did change

--- Modules/Server/Sources/PocketCastsServer/Public/StatsManager.swift
+++ Modules/Server/Sources/PocketCastsServer/Public/StatsManager.swift
@@ -184,6 +184,10 @@ public class StatsManager {
                 }
             }
 
+            if didChange {
+                persistTimes()
+            }
+
             completion?(didChange)
         }
     }
