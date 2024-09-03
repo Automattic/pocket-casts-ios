c0af981dbf702b162b2443e8143197c44491a557
Sérgio Estêvão <sergioestevao@gmail.com>
2024-09-03 15:50:44 +0100

    Layout cards

--- podcasts/Referrals/ReferralSendPass.swift
+++ podcasts/Referrals/ReferralSendPass.swift
@@ -4,7 +4,6 @@ struct ReferralCard: View {
 
     var body: some View {
         Rectangle()
-            .frame(width: 315, height: 200)
             .cornerRadius(13)
             .foregroundColor(Color(red: 0.08, green: 0.03, blue: 0.3))
             .overlay(alignment: .bottomLeading) {
@@ -20,26 +19,38 @@ struct ReferralCard: View {
                     .foregroundColor(.white)
                     .padding()
             }
+            .overlay(
+                RoundedRectangle(cornerRadius: 13)
+                    .inset(by: -0.5)
+                    .stroke(Color(red: 0.23, green: 0.23, blue: 0.23), lineWidth: 1)
+            )
     }
 }
 
 struct ReferralSendPass: View {
+    let numberOfDaysOffered: Int
+    let numberOfPasses: Int
+
     var body: some View {
         VStack {
             VStack(spacing: 24) {
                 Button("Plus") {
 
                 }.buttonStyle(PlusGradientFilledButtonStyle(isLoading: false, plan: .plus))
-                Text(L10n.referralsTipMessage(30))
+                Text(L10n.referralsTipMessage(numberOfDaysOffered))
                     .font(size: 31, style: .title, weight: .bold)
                     .multilineTextAlignment(.center)
                     .foregroundColor(.white)
-                Text(L10n.referralsTipTitle(3))
+                Text(L10n.referralsTipTitle(numberOfPasses))
                     .font(size: 14, style: .body, weight: .medium)
                     .multilineTextAlignment(.center)
                     .foregroundColor(.white)
                 ZStack {
-                    ReferralCard()
+                    ForEach(0..<numberOfPasses, id: \.self) { i in
+                        ReferralCard()
+                            .frame(width: 315.0 - (Double(numberOfPasses-1-i) * 40.0), height: 200.0)
+                            .offset(CGSize(width: 0, height: Double(numberOfPasses * i) * 5.0))
+                    }
                 }
             }
             Spacer()
@@ -53,5 +64,5 @@ struct ReferralSendPass: View {
 }
 
 #Preview {
-    ReferralSendPass()
+    ReferralSendPass(numberOfDaysOffered: 30, numberOfPasses: 3)
 }
