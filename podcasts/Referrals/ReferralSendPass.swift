08cd9166b55256cb0fd4251d4bd084b1e9ec7e41
Sérgio Estêvão <sergioestevao@gmail.com>
2024-09-03 15:03:13 +0100

    Implement screen skeleton

--- podcasts/Referrals/ReferralSendPass.swift
+++ podcasts/Referrals/ReferralSendPass.swift
@@ -1,16 +1,54 @@
-//
-//  ReferralSendPass.swift
-//  podcasts
-//
-//  Created by Sérgio Estêvão on 03/09/2024.
-//  Copyright © 2024 Shifty Jelly. All rights reserved.
-//
-
 import SwiftUI
+
+struct ReferralCard: View {
+
+    var body: some View {
+        Rectangle()
+            .frame(width: 315, height: 200)
+            .cornerRadius(13)
+            .foregroundColor(Color(red: 0.08, green: 0.03, blue: 0.3))
+            .overlay(alignment: .bottomLeading) {
+                Text("30-Day Guest Pass")
+                    .font(size: 12, style: .body, weight: .semibold)
+                    .foregroundColor(.white)
+                    .padding()
+
+            }
+            .overlay(alignment: .topTrailing) {
+                Text("+")
+                    .font(size: 12, style: .body, weight: .semibold)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+    }
+}
 
 struct ReferralSendPass: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack(spacing: 24) {
+                Button("Plus") {
+
+                }.buttonStyle(PlusGradientFilledButtonStyle(isLoading: false, plan: .plus))
+                Text(L10n.referralsTipMessage(30))
+                    .font(size: 31, style: .title, weight: .bold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                Text(L10n.referralsTipTitle(3))
+                    .font(size: 14, style: .body, weight: .medium)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                ZStack {
+                    ReferralCard()
+                }
+            }
+            Spacer()
+            Button("Share Guest Pass") {
+
+            }.buttonStyle(PlusGradientFilledButtonStyle(isLoading: false, plan: .plus))
+        }
+        .padding()
+        .background(.black)
     }
 }
 
