f850b43209ab90d610262d68c35ab4509f06e9ce
Sérgio Estêvão <sergioestevao@gmail.com>
2024-09-03 14:32:00 +0100

    Add pass view to project

---
+++ podcasts/Referrals/ReferralSendPass.swift
@@ -0,0 +1,19 @@
+//
+//  ReferralSendPass.swift
+//  podcasts
+//
+//  Created by Sérgio Estêvão on 03/09/2024.
+//  Copyright © 2024 Shifty Jelly. All rights reserved.
+//
+
+import SwiftUI
+
+struct ReferralSendPass: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReferralSendPass()
+}
