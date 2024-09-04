cdb1e473ef1da081b119a60e33e957356f5b63b3
Brandon Titus <b@titus.io>
2024-09-04 09:40:05 -0600

    Fix linter issues

--- podcasts/Sharing/ShareButton.swift
+++ podcasts/Sharing/ShareButton.swift
@@ -60,7 +60,7 @@ struct ShareButton: View {
 struct FrameKey: PreferenceKey {
     static var defaultValue: [UUID: CGRect] { [:] }
 
-    static func reduce(value: inout [UUID : CGRect], nextValue: () -> [UUID : CGRect]) {
+    static func reduce(value: inout [UUID: CGRect], nextValue: () -> [UUID: CGRect]) {
         value.merge(nextValue(), uniquingKeysWith: { $1 })
     }
 }
