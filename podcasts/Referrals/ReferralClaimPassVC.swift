99e877b7c301da468044e9ecb727622ed263bd78
Sérgio Estêvão <sergioestevao@gmail.com>
2024-09-09 16:36:39 +0100

    Add Claim Pass VC

---
+++ podcasts/Referrals/ReferralClaimPassVC.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+class ReferralClaimPassVC: ThemedHostingController<ReferralClaimPassView> {
+
+    private let viewModel: ReferralClaimPassModel
+
+    init() {
+        self.viewModel = ReferralClaimPassModel()
+        let screen = ReferralClaimPassView(viewModel: viewModel)
+        super.init(rootView: screen)
+    }
+
+    @MainActor required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+        setupViewModel()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .clear
+    }
+
+    private func prepareToDismiss() {
+        self.dismiss(animated: true)
+    }
+
+    private func setupViewModel() {
+        viewModel.onCloseTap = { [weak self] in
+            self?.prepareToDismiss()
+        }
+    }
+}
