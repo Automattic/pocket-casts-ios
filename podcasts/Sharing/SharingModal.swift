4c84bee04a26b1cd8b2a3afe884102276d5c77f3
Brandon Titus <b@titus.io>
2024-09-12 11:15:10 -0600

    Move ActivityItemSourceItem to separate file

--- podcasts/Sharing/SharingModal.swift
+++ podcasts/Sharing/SharingModal.swift
@@ -2,27 +2,6 @@ import PocketCastsDataModel
 import SwiftUI
 import PocketCastsUtils
 
-class ActivityItemSourceItem: NSObject, UIActivityItemSource {
-    let item: Any
-    let disallowedActivityTypes: [UIActivity.ActivityType]?
-
-    init(item: Any, disallowedActivityTypes: [UIActivity.ActivityType]? = nil) {
-        self.item = item
-        self.disallowedActivityTypes = disallowedActivityTypes
-    }
-
-    func activityViewControllerPlaceholderItem(_ activityViewController: UIActivityViewController) -> Any {
-        return item
-    }
-
-    func activityViewController(_ activityViewController: UIActivityViewController, itemForActivityType activityType: UIActivity.ActivityType?) -> Any? {
-        guard disallowedActivityTypes?.contains(where: { $0 == activityType }) != true else {
-            return nil
-        }
-        return item
-    }
-}
-
 enum SharingModal {
 
     /// Share options including which type of content will be shared
