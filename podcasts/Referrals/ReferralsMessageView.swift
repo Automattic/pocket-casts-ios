4a9579f007e49d2af542e5079fd8bedc96b867e5
Sérgio Estêvão <sergioestevao@gmail.com>
2024-09-20 00:00:15 +0100

    Create screen to show offer not available

---
+++ podcasts/Referrals/ReferralsMessageView.swift
@@ -0,0 +1,40 @@
+import Foundation
+import SwiftUI
+
+struct ReferralMessageView: View {
+    let title: String
+    let detail: String
+    let onDismiss: (() -> ())?
+
+    var body: some View {
+        VStack {
+            VStack(spacing: Constants.verticalSpacing) {
+                SubscriptionBadge(tier: .plus, displayMode: .gradient, foregroundColor: .black)
+                Text(title)
+                    .font(size: 31, style: .title, weight: .bold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                Text(detail)
+                    .font(size: 14, style: .body, weight: .medium)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+            }
+            Spacer()
+            Button(L10n.gotIt) {
+                onDismiss?()
+            }.buttonStyle(PlusGradientFilledButtonStyle(isLoading: false, plan: .plus))
+        }
+        .padding()
+        .background(.black)
+    }
+
+    enum Constants {
+        static let verticalSpacing = CGFloat(24)
+    }
+}
+
+#Preview {
+    ReferralMessageView(title: L10n.referralsOfferNotAvailableTitle,
+                        detail: L10n.referralsOfferNotAvailableDetail,
+                        onDismiss: nil)
+}
