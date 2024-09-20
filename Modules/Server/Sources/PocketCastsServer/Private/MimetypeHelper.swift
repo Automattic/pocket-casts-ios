b930d86769086c3f411b34a89479e62fee83639f
Daniele Bogo <me@bogodaniele.com>
2024-09-20 17:25:22 +0100

    Read partial file

--- Modules/Server/Sources/PocketCastsServer/Private/MimetypeHelper.swift
+++ Modules/Server/Sources/PocketCastsServer/Private/MimetypeHelper.swift
@@ -4,8 +4,13 @@ import Foundation
 public struct MimetypeHelper {
     public static func contetType(for url: URL) -> String? {
         do {
-            let data = try Data(contentsOf: url)
+            guard let fileSize = try url.resourceValues(forKeys: [.fileSizeKey]).fileSize,
+                  let fileHandle = FileHandle(forReadingAtPath: url.relativePath),
+                  let data = try fileHandle.read(upToCount: min(fileSize, 4500)) else {
+                return nil
+            }
             let mimeType = Swime.mimeType(data: data)
+            fileHandle.closeFile()
             return mimeType?.mime ?? "application/octet-stream"
         } catch {
             return nil
