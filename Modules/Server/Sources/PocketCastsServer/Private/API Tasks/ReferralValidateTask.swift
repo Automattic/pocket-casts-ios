89597f03e417bf46440b23cbd94a976a2d70ea97
Sérgio Estêvão <sergioestevao@gmail.com>
2024-09-30 16:01:16 +0100

    Update validation task with new fields

--- Modules/Server/Sources/PocketCastsServer/Private/API Tasks/ReferralValidateTask.swift
+++ Modules/Server/Sources/PocketCastsServer/Private/API Tasks/ReferralValidateTask.swift
@@ -4,6 +4,8 @@ import SwiftProtobuf
 
 public struct ReferralValidate: Codable {
     public let offer: String
+    public let platform: Int
+    public let details: String
 }
 
 class ReferralValidateTask: ApiBaseTask, @unchecked Sendable {
@@ -17,7 +19,7 @@ class ReferralValidateTask: ApiBaseTask, @unchecked Sendable {
     }
 
     override func apiTokenAcquired(token: String) {
-        let urlString = "\(ServerConstants.Urls.api())referrals/validate?code=\(code)"
+        let urlString = "\(ServerConstants.Urls.api())referrals/validate?code=\(code)&platform=ios"
 
         do {
             let (data, httpResponse) = getToServer(url: urlString, token: token)
@@ -29,8 +31,8 @@ class ReferralValidateTask: ApiBaseTask, @unchecked Sendable {
                 completion?(nil)
                 return
             }
-            let apiCode = try Api_ReferralValidationResponse(serializedBytes: responseData)
-            completion?(ReferralValidate(offer: apiCode.offer))
+            let validationResponse = try Api_ReferralValidationResponse(serializedBytes: responseData)
+            completion?(ReferralValidate(offer: validationResponse.offer, platform: Int(validationResponse.platform), details: validationResponse.details))
         } catch {
             FileLog.shared.addMessage("Failed to parse  Api_ReferralValidationResponse \(error.localizedDescription)")
             completion?(nil)
