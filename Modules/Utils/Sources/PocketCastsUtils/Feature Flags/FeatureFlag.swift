6e7652f04aacca110df0d457ce2322d0da5ce00a
Sérgio Estêvão <sergioestevao@gmail.com>
2024-11-25 13:52:39 +0000

    Add feature flag to control use of html description

--- Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
@@ -155,6 +155,9 @@ public enum FeatureFlag: String, CaseIterable {
     /// Uses the episode IDs from the server's response rather than our local database IDs
     case useSyncResponseEpisodeIDs
 
+    ///Use html description for podcast details
+    case usePodcastHTMLDescription
+
     public var enabled: Bool {
         if let overriddenValue = FeatureFlagOverrideStore().overriddenValue(for: self) {
             return overriddenValue
@@ -259,6 +262,8 @@ public enum FeatureFlag: String, CaseIterable {
 			true
         case .useSyncResponseEpisodeIDs:
             true
+        case .usePodcastHTMLDescription:
+            false
         }
     }
 
