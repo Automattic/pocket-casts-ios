2b3e8eb390a8cfe429435ebc72290d9ad186d809
Sérgio Estêvão <sergioestevao@gmail.com>
2024-12-11 10:59:28 +0000

    Detect link taps.

--- podcasts/ExpandableLabel.swift
+++ podcasts/ExpandableLabel.swift
@@ -33,7 +33,6 @@ class ExpandableLabel: ThemeableLabel {
     }
 
     func setRichText(html: String) {
-        let fontSize = font.pointSize
         let styledHTML: String = """
         <html>
         <head>
@@ -58,7 +57,11 @@ class ExpandableLabel: ThemeableLabel {
         collapsed = linesRequired() > maxLines
     }
 
-    @objc private func labelTapped() {
+    @objc private func labelTapped(gesture: UITapGestureRecognizer) {
+        if let url = gesture.didTapLinkInLabel(label: self) {
+            UIApplication.shared.open(url)
+            return
+        }
         if collapsed {
             delegate?.willExpandLabel(self)
 
@@ -100,4 +103,44 @@ class ExpandableLabel: ThemeableLabel {
             return Int(ceil(CGFloat(labelSize.height) / (font.lineHeight * desiredLinedHeightMultiple)))
         }
     }
+}
+
+extension UITapGestureRecognizer {
+    func didTapLinkInLabel(label: UILabel) -> URL? {
+            guard let attributedText = label.attributedText else {
+                return nil
+            }
+            // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+            let layoutManager = NSLayoutManager()
+            let textContainer = NSTextContainer(size: CGSize.zero)
+            let textStorage = NSTextStorage(attributedString: attributedText)
+
+            // Configure layoutManager and textStorage
+            layoutManager.addTextContainer(textContainer)
+            textStorage.addLayoutManager(layoutManager)
+
+            // Configure textContainer
+            textContainer.lineFragmentPadding = 0.0
+            textContainer.lineBreakMode = label.lineBreakMode
+            textContainer.maximumNumberOfLines = label.numberOfLines
+            let labelSize = label.bounds.size
+            textContainer.size = labelSize
+
+            // Find the tapped character location and compare it to the specified range
+            let locationOfTouchInLabel = self.location(in: label)
+            let textBoundingBox = layoutManager.usedRect(for: textContainer)
+            let textContainerOffset = CGPoint(
+                x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,
+                y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y
+            )
+            let locationOfTouchInTextContainer = CGPoint(
+                x: locationOfTouchInLabel.x - textContainerOffset.x,
+                y: locationOfTouchInLabel.y - textContainerOffset.y
+            )
+            let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+
+            let attributes = attributedText.attributes(at: indexOfCharacter, effectiveRange: nil)
+            let link = attributes[NSAttributedString.Key.link] as? URL
+            return link
+        }
 }
