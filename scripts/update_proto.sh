b499eb20cedd0fa8055feabb971d964ed2cb5cfc
Leandro Alonso <contato@leandroalonso.com>
2024-12-12 10:48:04 -0300

    Ensure swift-protobuf is up-to-date

--- scripts/update_proto.sh
+++ scripts/update_proto.sh
@@ -14,5 +14,7 @@ then
 fi
 
 brew upgrade protobuf
+brew upgrade swift-protobuf
+
 protoc --swift_out=./Modules/Server/Sources/PocketCastsServer/Private/Protobuffer --proto_path=$API_BASE_FOLDER/ $API_BASE_FOLDER/api.proto
 protoc --swift_out=./Modules/Server/Sources/PocketCastsServer/Private/Protobuffer --proto_path=$API_BASE_FOLDER/ $API_BASE_FOLDER/files.proto
