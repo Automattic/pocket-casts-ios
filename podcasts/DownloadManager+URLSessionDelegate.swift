1c3193d69d73345d7800d8d435610affacb324c8
Brandon Titus <b@titus.io>
2024-12-16 09:05:26 -0700

    Move non-throwing code out of do catch block

--- podcasts/DownloadManager+URLSessionDelegate.swift
+++ podcasts/DownloadManager+URLSessionDelegate.swift
@@ -141,26 +141,26 @@ extension DownloadManager: URLSessionDelegate, URLSessionDownloadDelegate {
     }
 
     func processEpisode(_ episode: BaseEpisode, downloadedFile location: URL, reportedContentType: String?) {
+        var contentType = reportedContentType
+
+        if FeatureFlag.useMimetypePackage.enabled {
+            contentType = MimetypeHelper.contetType(for: location)
+            if let contentType, contentType != episode.contentType {
+                DataManager.sharedManager.saveEpisode(contentType: contentType, episode: episode)
+            }
+        }
+
+        let fileSize = FileManager.default.fileSize(of: location) ?? 0
+        guard isEpisodeFileValid(contentType: contentType, fileSize: fileSize) else {
+            markEpisode(episode, asFailedWithMessage: L10n.downloadErrorContactAuthorVersion2, reason: .suspiciousContent(fileSize))
+            return
+        }
+
+        let autoDownloadStatus = AutoDownloadStatus(rawValue: episode.autoDownloadStatus)!
+        let destinationPath = autoDownloadStatus == .playerDownloadedForStreaming ? streamingBufferPathForEpisode(episode) : pathForEpisode(episode)
+        let destinationUrl = URL(fileURLWithPath: destinationPath)
+        
         do {
-            var contentType = reportedContentType
-
-            if FeatureFlag.useMimetypePackage.enabled {
-                contentType = MimetypeHelper.contetType(for: location)
-                if let contentType, contentType != episode.contentType {
-                    DataManager.sharedManager.saveEpisode(contentType: contentType, episode: episode)
-                }
-            }
-
-            let fileSize = FileManager.default.fileSize(of: location) ?? 0
-            guard isEpisodeFileValid(contentType: contentType, fileSize: fileSize) else {
-                markEpisode(episode, asFailedWithMessage: L10n.downloadErrorContactAuthorVersion2, reason: .suspiciousContent(fileSize))
-                return
-            }
-
-            let autoDownloadStatus = AutoDownloadStatus(rawValue: episode.autoDownloadStatus)!
-            let destinationPath = autoDownloadStatus == .playerDownloadedForStreaming ? streamingBufferPathForEpisode(episode) : pathForEpisode(episode)
-            let destinationUrl = URL(fileURLWithPath: destinationPath)
-
             try StorageManager.copyItem(at: location, to: destinationUrl)
 
             let newDownloadStatus: DownloadStatus = autoDownloadStatus == .playerDownloadedForStreaming ? .downloadedForStreaming : .downloaded
