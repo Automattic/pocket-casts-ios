e35e819bf3d0033e58154ea70097d9f147a5aca4
Brandon Titus <b@titus.io>
2024-12-16 19:27:38 -0700

    Move UserActivity handler to separate function

--- Pocket Casts App Clip/ContentView.swift
+++ Pocket Casts App Clip/ContentView.swift
@@ -9,50 +9,54 @@ struct ContentView: View {
         }
         .background(Color(uiColor: PlayerColorHelper.playerBackgroundColor01()))
         .onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { userActivity in
-            guard
-                let incomingURL = userActivity.webpageURL,
-                let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true),
-                let path = components.path,
-                path != "/get",
-                path != "/get/"
-            else { return }
+            handle(userActivity: userActivity)
+        }
+    }
 
-            // NOTE: This doesn't handle the redeem URL. See `AppDelegate.handleContinue(_ userActivity: NSUserActivity)` for this logic
+    private func handle(userActivity: NSUserActivity) {
+        guard
+            let incomingURL = userActivity.webpageURL,
+            let components = NSURLComponents(url: incomingURL, resolvingAgainstBaseURL: true),
+            let path = components.path,
+            path != "/get",
+            path != "/get/"
+        else { return }
 
-            // Also pass any query params from the share URL to the server to allow support for episode position handling
-            // Ex: ?t=123
-            let query = components.query.map { "?\($0)" } ?? ""
-            let sharePath = "\(path)\(query)"
+        // NOTE: This doesn't handle the redeem URL. See `AppDelegate.handleContinue(_ userActivity: NSUserActivity)` for this logic
 
-            let importPath = "social/share/show\(sharePath)"
+        // Also pass any query params from the share URL to the server to allow support for episode position handling
+        // Ex: ?t=123
+        let query = components.query.map { "?\($0)" } ?? ""
+        let sharePath = "\(path)\(query)"
 
-            PodcastManager.shared.importSharedItemFromUrl(importPath) { shareItem in
-                guard let shareItem else {
-                    print("Share Item")
+        let importPath = "social/share/show\(sharePath)"
+
+        PodcastManager.shared.importSharedItemFromUrl(importPath) { shareItem in
+            guard let shareItem else {
+                print("Share Item")
+                return
+            }
+
+            guard let episodeUUID = shareItem.episodeHeader?.uuid else {
+                print("No episode found in share item")
+                return
+            }
+
+            guard let podcastUUID = shareItem.podcastHeader?.uuid else {
+                print("No podcast found in share item")
+                return
+            }
+
+
+            loadEpisode(episodeUuid: episodeUUID, podcastUuid: podcastUUID) {
+                guard let episode = DataManager.sharedManager.findEpisode(uuid: episodeUUID) else {
+                    print("Could not find Episode")
                     return
                 }
 
-                guard let episodeUUID = shareItem.episodeHeader?.uuid else {
-                    print("No episode found in share item")
-                    return
-                }
+                print("Loaded episode: \(episode.title)")
 
-                guard let podcastUUID = shareItem.podcastHeader?.uuid else {
-                    print("No podcast found in share item")
-                    return
-                }
-
-
-                loadEpisode(episodeUuid: episodeUUID, podcastUuid: podcastUUID) {
-                    guard let episode = DataManager.sharedManager.findEpisode(uuid: episodeUUID) else {
-                        print("Could not find Episode")
-                        return
-                    }
-
-                    print("Loaded episode: \(episode.title)")
-
-                    PlaybackManager.shared.load(episode: episode, autoPlay: true, overrideUpNext: false)
-                }
+                PlaybackManager.shared.load(episode: episode, autoPlay: true, overrideUpNext: false)
             }
         }
     }
