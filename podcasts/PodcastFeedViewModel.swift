cc949fcba9637a96a657293c21dab679668b120d
Daniele Bogo <me@bogodaniele.com>
2025-01-30 17:31:10 +0000

    Working on API implementation

--- podcasts/PodcastFeedViewModel.swift
+++ podcasts/PodcastFeedViewModel.swift
@@ -1,4 +1,7 @@
 import Foundation
+import PocketCastsDataModel
+import PocketCastsServer
+import PocketCastsUtils
 
 enum PodcastFeedReloadNotification {
     public static let loading = NSNotification.Name(rawValue: "PodcastFeedReloadNotificationLoading")
@@ -21,35 +24,36 @@ class PodcastFeedViewModel {
     }
 
     func checkIfNewEpisodesAreAvailable(from source: PodcastFeedReloadSource) async -> Bool {
-        // Test implementation to simulate the API
+        guard let uuid, let podcast = DataManager.sharedManager.findPodcast(uuid: uuid, includeUnsubscribed: true) else {
+            return false
+        }
         await MainActor.run {
             loadingState = .loading
-        }
-        _ = await shouldReloadPodcasts(source: source)
-
-        try? await Task.sleep(nanoseconds: 3_000_000_000)
-
-        await MainActor.run {
-            if source == .refreshControl {
-                NotificationCenter.default.post(name: PodcastFeedReloadNotification.noEpisodesFound, object: nil)
-            } else {
-                Toast.show(L10n.podcastFeedReloadNoEpisodesFound)
-            }
-            loadingState = .idle
-        }
-        return false
-    }
-
-    private func shouldReloadPodcasts(source: PodcastFeedReloadSource) async -> Int {
-        await MainActor.run {
             if source == .refreshControl {
                 NotificationCenter.default.post(name: PodcastFeedReloadNotification.loading, object: nil)
             } else {
                 Toast.show(L10n.podcastFeedReloadLoading)
             }
         }
-        return 202
-    }
 
-    private func pollUpdatePodcast() async { }
+        let success: Bool
+        do {
+            success = try await MainServerHandler.shared.updatePodcast(uuid: uuid, lastEpisodeUuid: podcast.latestEpisodeUuid)
+        } catch {
+            success = false
+            FileLog.shared.console("Failed update podcast \(uuid) - \(error.localizedDescription)")
+        }
+
+        await MainActor.run {
+            if source == .refreshControl {
+                let notification = success ? PodcastFeedReloadNotification.episodesFound : PodcastFeedReloadNotification.noEpisodesFound
+                NotificationCenter.default.post(name: notification, object: nil)
+            } else {
+                let message = success ? L10n.podcastFeedReloadNewEpisodesFound : L10n.podcastFeedReloadNoEpisodesFound
+                Toast.show(message)
+            }
+            loadingState = .idle
+        }
+        return success
+    }
 }
