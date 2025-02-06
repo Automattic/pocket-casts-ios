ee2b90575a3a2c3c920c37d2c3649a0c0036ef43
Daniele Bogo <me@bogodaniele.com>
2025-02-06 11:00:21 +0000

    Remove placeholders

--- podcasts/NowPlayingPlayerItemViewController.swift
+++ podcasts/NowPlayingPlayerItemViewController.swift
@@ -37,12 +37,18 @@ class NowPlayingPlayerItemViewController: PlayerItemViewController {
 
     @IBOutlet var episodeName: ThemeableLabel! {
         didSet {
+#if APPCLIP
+            episodeName.text = ""
+#endif
             episodeName.style = .playerContrast01
         }
     }
 
     @IBOutlet var podcastName: ThemeableLabel! {
         didSet {
+#if APPCLIP
+            podcastName.text = ""
+#endif
             podcastName.style = .playerContrast02
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(podcastNameTapped))
             podcastName.addGestureRecognizer(tapGesture)
@@ -54,6 +60,9 @@ class NowPlayingPlayerItemViewController: PlayerItemViewController {
 
     @IBOutlet var chapterName: ThemeableLabel! {
         didSet {
+#if APPCLIP
+            chapterName.text = ""
+#endif
             chapterName.style = .playerContrast01
 
             let tapGesture = UITapGestureRecognizer(target: self, action: #selector(chapterNameTapped))
