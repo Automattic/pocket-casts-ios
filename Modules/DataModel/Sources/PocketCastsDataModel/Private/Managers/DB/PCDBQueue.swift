9ab7c34ccd8089803171fcb44597ab14c5499afc
Leandro Alonso <contato@leandroalonso.com>
2025-02-21 16:02:59 -0300

    Add protocols for Database abstraction

---
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/Managers/DB/PCDBQueue.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+protocol PCDBQueue {
+    var changes: Int32 { get }
+
+    func executeQuery(_ sql: String, values: [Any]?) throws -> PCDBResultSet
+
+    func executeUpdate(_ sql: String, values: [Any]?) throws
+
+    func commit()
+
+    func beginTransaction()
+
+    func insert(into: String, columns: [String], values: [Any?]) throws
+
+    func lastErrorCode() -> Int32
+
+    func lastErrorMessage() -> String
+
+    func rollback()
+}
