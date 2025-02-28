3b3049410ee0d34466253da196c598bd3dd55478
Sérgio Estêvão <sergioestevao@gmail.com>
2025-02-28 14:36:15 +0000

    Abstract use of singletons to class properties

--- podcasts/FoldersCoordinator.swift
+++ podcasts/FoldersCoordinator.swift
@@ -6,7 +6,15 @@ import PocketCastsUtils
 
 class FoldersCoordinator: NSObject {
 
-    	func startFolderCreationFlow(from vc: UIViewController) {
+    private let navigationManager: NavigationManager
+    private let dataManager: DataManager
+
+    init(navigationManager: NavigationManager = .sharedManager, dataManager: DataManager = .sharedManager) {
+        self.navigationManager = navigationManager
+        self.dataManager = dataManager
+    }
+
+    func startFolderCreationFlow(from vc: UIViewController) {
         if FeatureFlag.suggestedFolders.enabled {
             newSuggestedFolderCreationFlow(from: vc)
         } else {
@@ -19,14 +27,14 @@ class FoldersCoordinator: NSObject {
 
     private func oldFolderCreationFlow(from vc: UIViewController) {
         if !SubscriptionHelper.hasActiveSubscription() {
-            NavigationManager.sharedManager.showUpsellView(from: vc, source: .folders)
+            navigationManager.showUpsellView(from: vc, source: .folders)
             return
         }
 
         let creatFolderView = CreateFolderView { [weak vc] folderUuid in
             if let folderUuid = folderUuid, let folder = DataManager.sharedManager.findFolder(uuid: folderUuid) {
-                vc?.dismiss(animated: true, completion: {
-                    NavigationManager.sharedManager.navigateTo(NavigationManager.folderPageKey, data: [NavigationManager.folderKey: folder])
+                vc?.dismiss(animated: true, completion: { [weak self] in
+                    self?.navigationManager.navigateTo(NavigationManager.folderPageKey, data: [NavigationManager.folderKey: folder])
                 })
             } else {
                 vc?.dismiss(animated: true, completion: nil)
@@ -40,26 +48,28 @@ class FoldersCoordinator: NSObject {
     private func newSuggestedFolderCreationFlow(from vc: UIViewController) {
         if !SubscriptionHelper.hasActiveSubscription() {
             if !SyncManager.isUserLoggedIn() {
-                NavigationManager.sharedManager.showUpsellView(from: vc, source: .folders)
+                navigationManager.showUpsellView(from: vc, source: .folders)
             } else {
                 showUpSellSuggestedFolder(from: vc)
             }
             return
         }
         let suggestedFoldersView = SuggestedFoldersView { [weak vc, weak self] result in
+            guard let self, let vc else { return }
+
             switch result {
             case .dismiss:
-                vc?.dismiss(animated: true, completion: nil)
+                vc.dismiss(animated: true, completion: nil)
             case .applySuggestedFolders(let folders):
-                vc?.dismiss(animated: true, completion: nil)
-                self?.applySuggestedFolders(folders)
+                vc.dismiss(animated: true, completion: nil)
+                applySuggestedFolders(folders)
             case .createdManualFolder(let folderUuid):
-                guard let folder = DataManager.sharedManager.findFolder(uuid: folderUuid) else {
-                    vc?.dismiss(animated: true, completion: nil)
+                guard let folder = dataManager.findFolder(uuid: folderUuid) else {
+                    vc.dismiss(animated: true, completion: nil)
                     return
                 }
-                vc?.dismiss(animated: true, completion: {
-                    NavigationManager.sharedManager.navigateTo(NavigationManager.folderPageKey, data: [NavigationManager.folderKey: folder])
+                vc.dismiss(animated: true, completion: { [weak self] in
+                    self?.navigationManager.navigateTo(NavigationManager.folderPageKey, data: [NavigationManager.folderKey: folder])
                 })
             }
         }
@@ -71,8 +81,8 @@ class FoldersCoordinator: NSObject {
     private func showUpSellSuggestedFolder(from vc: UIViewController) {
         let upsellSuggestedFoldersView = SuggestedFoldersUpSellView(model: SuggestedFoldersModel(failedToLoadAction: {[weak vc] in
             guard let vc else { return }
-            vc.dismiss(animated: false) {
-                NavigationManager.sharedManager.showUpsellView(from: vc, source: .folders)
+            vc.dismiss(animated: false) { [weak self] in
+                self?.navigationManager.showUpsellView(from: vc, source: .folders)
             }
         }))
         let hostingController = PCHostingController(rootView: upsellSuggestedFoldersView.environmentObject(Theme.sharedTheme))
@@ -96,7 +106,7 @@ class FoldersCoordinator: NSObject {
         DataManager.sharedManager.clearAllFolderInformation()
         for suggestedFolder in suggestedFolders {
             let folder = makeFolder(from: suggestedFolder)
-            DataManager.sharedManager.bulkSetFolderUuid(folderUuid: folder.uuid, podcastUuids: suggestedFolder.topPodcastUuids)
+            dataManager.bulkSetFolderUuid(folderUuid: folder.uuid, podcastUuids: suggestedFolder.topPodcastUuids)
         }
         NotificationCenter.postOnMainThread(notification: ServerNotifications.podcastsRefreshed, object: nil)
     }
@@ -111,7 +121,7 @@ class FoldersCoordinator: NSObject {
 
         // the sort type for newly created folders defaults to the same thing the home grid is set to
         folder.sortType = Int32(Settings.homeFolderSortOrder().old.rawValue)
-        DataManager.sharedManager.save(folder: folder)
+        dataManager.save(folder: folder)
         return folder
     }
 }
