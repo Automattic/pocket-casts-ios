4677f7a60fddf7c46be5fecf156e4fdc2c2583ae
Sérgio Estêvão <sergioestevao@gmail.com>
2025-03-06 12:36:47 +0000

    Fix size for removal screen.

--- podcasts/SuggestedFoldersView.swift
+++ podcasts/SuggestedFoldersView.swift
@@ -151,7 +151,7 @@ struct SuggestedFoldersView: View {
         })
         .modify {
             if #available(iOS 16.0, *) {
-                $0.presentationDetents([.fraction(0.35)])
+                $0.presentationDetents([.fraction(0.4)])
                     .presentationDragIndicator(.visible)
             } else {
                 $0
