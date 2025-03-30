fe5a5853f9c22e1a560deb95d814508e2c89fd2b
Sérgio Estêvão <sergioestevao@gmail.com>
2025-03-30 12:12:28 +0100

    remove custom animation from image

--- podcasts/PodcastHeaderView.swift
+++ podcasts/PodcastHeaderView.swift
@@ -37,7 +37,6 @@ struct PodcastHeaderView: View {
                 Spacer()
                 PodcastImageViewWrapper(podcastUUID: viewModel.podcast.uuid, size: .grid)
                     .frame(width: viewModel.isExpanded ? Constants.largeImageSize : Constants.smallImageSize, height: viewModel.isExpanded ? Constants.largeImageSize : Constants.smallImageSize)
-                    .animation(.bouncy, value: viewModel.isExpanded)
                     .onLongPressGesture {
                         viewModel.podcastArtworkTapped()
                     }
