e34c87e8433960bd20ba4169047675bc7b3034b4
Daniele Bogo <me@bogodaniele.com>
2025-04-01 16:41:45 +0200

    Enable Feature Flag

--- Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
@@ -254,7 +254,7 @@ public enum FeatureFlag: String, CaseIterable {
         case .podcastViewChanges:
             true
         case .podcastNewformAppsFlyer:
-            false
+            true
         }
     }
 
