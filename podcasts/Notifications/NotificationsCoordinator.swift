885b89b370c7211def86fa67048de1097dc2f116
Sérgio Estêvão <sergioestevao@gmail.com>
2025-04-07 23:03:20 +0100

    Refactor to use enum to identify notifications

--- podcasts/Notifications/NotificationsCoordinator.swift
+++ podcasts/Notifications/NotificationsCoordinator.swift
@@ -1,5 +1,38 @@
 import Foundation
 
+enum NotificationType: String {
+
+    case onboardingThemes
+
+    var title: String {
+        switch self {
+        case .onboardingThemes:
+            return "Time for a new look"
+        }
+    }
+
+    var body: String {
+        switch self {
+        case .onboardingThemes:
+            return "Browse our themes and find the one that suits your style."
+        }
+    }
+
+    var identifier: String {
+        switch self {
+        case .onboardingThemes:
+            return self.rawValue
+        }
+    }
+
+    var link: String {
+        switch self {
+        case .onboardingThemes:
+            return "pktc://settings/themes"
+        }
+    }
+}
+
 class NotificationsCoordinator {
 
     static var shared: NotificationsCoordinator = NotificationsCoordinator()
@@ -9,20 +42,19 @@ class NotificationsCoordinator {
     }
 
     func setupOnboardingNotifications() {
-        makeOnboardingThemeNotification()
+        scheduleNotification(.onboardingThemes)
     }
 
-    func makeOnboardingThemeNotification(timeInterval: TimeInterval = 5.seconds) {
+    func scheduleNotification(_ type: NotificationType, timeInterval: TimeInterval = 5.seconds) {
         let content = UNMutableNotificationContent()
-        content.title = "Time for a new look"
-        content.body = "Browse our themes and find the one that suits your style."
+        content.title = type.title
+        content.body = type.body
         content.categoryIdentifier = NotificationsHelper.NotificationsCategory.deepLink.rawValue
-        content.userInfo = ["destination_url": "pktc://settings/themes"]
+        content.userInfo = ["destination_url": type.link]
 
         let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
 
-        let uuidString = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: type.identifier, content: content, trigger: trigger)
 
         // Schedule the request with the system.
         Task {
