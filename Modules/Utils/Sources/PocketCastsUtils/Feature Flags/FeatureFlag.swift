c70d1ed9b41f605838d2eb3c45bb91a88ce8b8b8
Brandon Titus <b@titus.io>
2025-04-16 16:41:16 -0600

    Add Recommendations Feature Flag

--- Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
+++ Modules/Utils/Sources/PocketCastsUtils/Feature Flags/FeatureFlag.swift
@@ -174,6 +174,9 @@ public enum FeatureFlag: String, CaseIterable {
     /// Enable the new podcast sorting options
     case podcastsSortChanges
 
+    /// Recommendations including discover v3 support
+    case recommendations
+
     public var enabled: Bool {
         if let overriddenValue = FeatureFlagOverrideStore().overriddenValue(for: self) {
             return overriddenValue
@@ -289,6 +292,8 @@ public enum FeatureFlag: String, CaseIterable {
         case .avoidReplaceOnEpisodeSwap:
             true
         case .podcastsSortChanges:
+            false
+        case .recommendations:
             false
         }
     }
