f4cf1addab2192910a844b7e004d36277204f60b
Daniele Bogo <me@bogodaniele.com>
2025-04-17 14:51:59 +0100

    Replace with new model and view

--- podcasts/Onboarding/Encourage Account Creation/Banner/InformationalBannerViewModel.swift
+++ podcasts/Onboarding/Encourage Account Creation/Banner/InformationalBannerViewModel.swift
@@ -19,7 +19,7 @@ extension InformationalBannerPresenting {
     }
 }
 
-class InformationalBannerViewModel: ObservableObject, InformationalBannerPresenting {
+class InformationalBannerViewModel: BannerModel, InformationalBannerPresenting {
     let bannerType: InformationalBannerType
 
     var onCloseBannerTap: (() -> Void)? = nil
@@ -27,5 +27,13 @@ class InformationalBannerViewModel: ObservableObject, InformationalBannerPresent
 
     init(bannerType: InformationalBannerType) {
         self.bannerType = bannerType
+        super.init(
+            title: bannerType.title,
+            message: bannerType.description,
+            action: L10n.eacInformationalBannerCreateAccount,
+            iconName: bannerType.iconName,
+            invertedColor: bannerType == .profile,
+            onActionTap: onCloseBannerTap,
+            onCloseTap: onCreateFreeAccountTap)
     }
 }
