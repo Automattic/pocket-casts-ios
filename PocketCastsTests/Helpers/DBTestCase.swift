55c6bddade4ccdd1702b512c0299a921a6dfd9ca
Leandro Alonso <contato@leandroalonso.com>
2025-04-18 10:34:12 -0300

    Fix download-related tests

--- PocketCastsTests/Helpers/DBTestCase.swift
+++ PocketCastsTests/Helpers/DBTestCase.swift
@@ -4,7 +4,15 @@ import FMDB
 @testable import podcasts
 
 class DBTestCase: XCTestCase {
-    var dataManager: DataManager!
+    // We use a single DataManager instance for tests based on DBTestCase,
+    // since some tests interact with the download logic.
+    // Creating multiple DataManager instances cause the app delegate
+    // to reference an outdated one with a closed database.
+    // This issue is silently ignored when using FMDB, but GRDB surfaces an error.
+    static var dataManager: DataManager!
+    var dataManager: DataManager! {
+        Self.dataManager
+    }
     var downloadManager: DownloadManager!
     var podcast: Podcast!
     var episode: Episode!
@@ -19,7 +27,7 @@ class DBTestCase: XCTestCase {
     }
 
     private func setupData() throws {
-        let dataManager = try setupDatabase()
+        let dataManager = Self.dataManager == nil ? try setupDatabase() : Self.dataManager!
         let downloadManager = DownloadManager(dataManager: dataManager)
 
         let podcast = Podcast()
@@ -39,7 +47,7 @@ class DBTestCase: XCTestCase {
         episode.playingStatus = PlayingStatus.notPlayed.rawValue
 
         dataManager.save(episode: episode)
-        self.dataManager = dataManager
+        Self.dataManager = dataManager
         self.downloadManager = downloadManager
         self.episode = episode
         self.podcast = podcast
@@ -71,9 +79,5 @@ class DBTestCase: XCTestCase {
         XCTAssertEqual(task.state, URLSessionTask.State.running)
 
         return (podcastManager, task)
-    }
-
-    func cleanDownloadQueue() {
-        downloadManager.removeFromQueue(episode: episode, fireNotification: false, userInitiated: false)
     }
 }
