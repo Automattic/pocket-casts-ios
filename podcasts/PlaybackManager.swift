c2ab50a9b6d32e52a62a8368ce03ed877b6e7e58
Brandon Titus <b@titus.io>
2025-04-18 11:56:27 -0600

    Revert "Fix switch to effects player  (#2971)"
    This reverts commit 31bce2d48c203e964d25360e6b0b8ed9d3cae8de, reversing
    changes made to cc772c45ee219093d98f4e59c24bf8926df57f8c.

--- podcasts/PlaybackManager.swift
+++ podcasts/PlaybackManager.swift
@@ -894,7 +894,9 @@ class PlaybackManager: ServerPlaybackDelegate {
 
     func silenceRemovalAvailable() -> Bool {
         #if APPCLIP
-        return false
+        if let episode = currentEpisode() {
+            return !episode.videoPodcast()
+        }
         #elseif !os(watchOS)
             if let episode = currentEpisode() {
                 return !episode.videoPodcast() && !GoogleCastManager.sharedManager.connectedOrConnectingToDevice()
@@ -2036,14 +2038,10 @@ class PlaybackManager: ServerPlaybackDelegate {
             // the current episode we were playing has downloaded, switch to playing the downloaded version
             let currentlyPlaying = playing()
             recordPlaybackPosition(sendToServerImmediately: false, fireNotifications: true)
-            #if APPCLIP
+
             if refreshedEpisode.uuid != currentEpisode()?.uuid {
                 load(episode: refreshedEpisode, autoPlay: currentlyPlaying, overrideUpNext: false, saveCurrentEpisode: false)
             }
-            #else
-            load(episode: refreshedEpisode, autoPlay: currentlyPlaying, overrideUpNext: false, saveCurrentEpisode: false)
-            #endif
-
             if refreshedEpisode.videoPodcast() {
                 NotificationCenter.postOnMainThread(notification: Constants.Notifications.videoPlaybackEngineSwitched)
             }
