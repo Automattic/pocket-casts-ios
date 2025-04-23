670f4235e889489878f429e4eeff6980cc301ad2
Leandro Alonso <contato@leandroalonso.com>
2025-04-23 17:28:08 -0300

    Add read/write methods

--- Modules/DataModel/Sources/PocketCastsDataModel/Private/DB/PCDBQueue.swift
+++ Modules/DataModel/Sources/PocketCastsDataModel/Private/DB/PCDBQueue.swift
@@ -5,5 +5,9 @@ public protocol PCDBQueue {
 
     func inTransaction(_ block: (PCDatabase, UnsafeMutablePointer<ObjCBool>) -> Void)
 
+    func read(_ block: (PCDatabase) -> Void)
+
+    func write(_ block: (PCDatabase) -> Void)
+
     func close()
 }
