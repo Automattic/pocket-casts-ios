b4eeac62f1292a376dcd1862cc2e47106ef5cfce
Sérgio Estêvão <sergioestevao@gmail.com>
2025-05-12 11:18:41 +0100

    Open the AppStore page when tapping on the notification

---
+++ Pocket Casts App Clip/AppClipAppDelegate.swift
@@ -0,0 +1,36 @@
+import Foundation
+import SwiftUI
+import UserNotifications
+
+class AppClipAppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // This is where we register this device to recieve push notifications from Apple
+        // All this function does is register the device with APNs, it doesn't set up push notifications by itself
+        application.registerForRemoteNotifications()
+
+        // Setting the notification delegate
+        UNUserNotificationCenter.current().delegate = self
+
+        return true
+    }
+
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+
+    }
+}
+
+extension AppClipAppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+        guard let url = URL(string: "itms-apps://itunes.apple.com/app/apple-store/id414834813?mt=8") else {
+            return
+        }
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+
+    // This function allows us to view notifications in the app even with it in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        return [.badge, .banner, .list, .sound]
+    }
+}
