aeda5f60467dd7c13a76983fde6a3aece9749b43
Ian Maia <ian.maia@automattic.com>
2025-05-16 15:50:18 +0200

    Use constants for job types and use `dirname` for consistency when calling `should-skip-job.sh`

--- .buildkite/commands/should-skip-job.sh
+++ .buildkite/commands/should-skip-job.sh
@@ -28,9 +28,13 @@ LOCALIZATION_PATTERNS=(
   "**/*.stringsdict"
 )
 
+# Define constants for job types
+BUILD="build"
+LOCALIZATION="localization"
+
 # Check if arguments are valid
 if [ -z "${1:-}" ] || [ "$1" != "--job-type" ] || [ -z "${2:-}" ]; then
-  echo "Error: Must specify --job-type [build|localization]"
+  echo "Error: Must specify --job-type [$BUILD|$LOCALIZATION]"
   buildkite-agent step cancel
   exit 15
 fi
@@ -38,7 +42,7 @@ fi
 # Function to display skip message and create annotation
 show_skip_message() {
   local job_type=$1
-  local message="Skipping ${BUILDKITE_LABEL:-Job} - no relevant files changed"
+  local message="Skipped ${BUILDKITE_LABEL:-Job} - no relevant files changed"
   local context="skip-$(echo "${BUILDKITE_LABEL:-$job_type}" | sed -E -e 's/[^[:alnum:]]+/-/g' | tr A-Z a-z)"
   
   echo "$message" | buildkite-agent annotate --style "info" --context "$context"
@@ -47,7 +51,7 @@ show_skip_message() {
 
 job_type="$2"
 case "$job_type" in
-  "localization")
+  $LOCALIZATION)
     # Check if any localization files have changed
     # Return true (skip) if NO localization files have changed
     if ! pr_changed_files --any-match "${LOCALIZATION_PATTERNS[@]}"; then
@@ -56,7 +60,7 @@ case "$job_type" in
     fi
     exit 1
     ;;
-  "build")
+  $BUILD)
     # We should skip if changes are limited to documentation, tooling, and non-code files
     # We'll let the job run (won't skip) if PR includes changes in localization files though
     PATTERNS=("${COMMON_PATTERNS[@]}")
@@ -67,7 +71,7 @@ case "$job_type" in
     exit 1
     ;;
   *)
-    echo "Error: Job type must be either 'build' or 'localization'"
+    echo "Error: Job type must be either '$BUILD' or '$LOCALIZATION'"
     buildkite-agent step cancel
     exit 15
     ;;
