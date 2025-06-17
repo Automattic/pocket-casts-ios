d37672ae8b26527a45c46288ef6a28b39508c099
Brandon Titus <b@titus.io>
2025-06-17 10:56:26 -0600

    Add individual cell types for discover items

--- podcasts/DiscoverCollectionViewController.swift
+++ podcasts/DiscoverCollectionViewController.swift
@@ -133,7 +133,10 @@ class DiscoverCollectionViewController: PCViewController {
                             self.loadingTasks.removeValue(forKey: uuid)
                         }
                     } else {
-                        snapshot.appendItems([.item(.init(item: item, region: currentRegion, selectedCategory: selectedCategory))])
+                        let model = DiscoverCellModel(item: item, region: currentRegion, selectedCategory: selectedCategory)
+                        if let cellType = item.cellType() {
+                            snapshot.appendItems([.item(DiscoverCellType.ItemType(cellType: cellType, model: model))])
+                        }
                     }
                 }
             }
@@ -158,8 +161,11 @@ class DiscoverCollectionViewController: PCViewController {
         }) {
             // Replace the loading placeholder with either the authenticated item or empty
             if isAuthenticated {
-                let newItem = CellType.item(.init(item: item, region: region, selectedCategory: selectedCategory))
-                snapshot.insertItems([newItem], beforeItem: loadingItem)
+                let model = DiscoverCellModel(item: item, region: region, selectedCategory: selectedCategory)
+                if let cellType = item.cellType() {
+                    let newItem = CellType.item(DiscoverCellType.ItemType(cellType: cellType, model: model))
+                    snapshot.insertItems([newItem], beforeItem: loadingItem)
+                }
             } else {
                 snapshot.insertItems([.empty], beforeItem: loadingItem)
             }
@@ -257,8 +263,7 @@ extension DiscoverCollectionViewController {
             case .loading, .noResults, .noNetwork, .empty:
                 return collectionView.dequeueConfiguredReusableCell(using: nonItemRegistration, for: indexPath, item: item)
             case .item(let item):
-                guard let cellType = item.item.cellType() else { return UICollectionViewCell() }
-                let cellRegistration = registrations[cellType]!
+                let cellRegistration = registrations[item.cellType]!
                 return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
             }
         }
