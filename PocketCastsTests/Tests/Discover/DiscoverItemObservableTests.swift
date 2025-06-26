53e5918f90cfa611d1321464f4b66869ab6e0a4d
Brandon Titus <b@titus.io>
2025-06-26 13:27:45 -0600

    Remove recommended categories tests

--- PocketCastsTests/Tests/Discover/DiscoverItemObservableTests.swift
+++ PocketCastsTests/Tests/Discover/DiscoverItemObservableTests.swift
@@ -19,45 +19,13 @@ final class DiscoverItemObservableTests: XCTestCase {
                 DiscoverCategory(id: 3, name: "Science")
             ]
         }
-
-        func discoverRecommendedCategories(source: String, authenticated: Bool?) async -> [Int]? {
-            return [2, 3]
-        }
     }
 
-    func testRecommendedFiltering_appliesCorrectly() async {
-        FeatureFlagMock().set(.smartCategories, value: true)
-
-        // Given
-        let mockHandler = MockServerHandler()
-        let observable = CategoriesSelectorViewController.DiscoverItemObservable(serverHandler: mockHandler)
-        observable.item = DiscoverItem(
-            id: "item1",
-            title: "Test",
-            type: "categories",
-            source: "mockSource",
-            regions: [],
-            popular: [1, 2],
-            authenticated: true,
-            recommendations: DiscoverSource(source: "recSource", authenticated: true)
-        )
-
-        // When
-        let result = await observable.load()
-
-        // Then
-        XCTAssertEqual(result?.categories.count, 3)
-        XCTAssertEqual(result?.prioritized.map(\.id), [2, 3])
-    }
-
-    func testPopularFiltering_appliesWhenNoRecommendations() async {
-        class NoRecMock: MockServerHandler {
-            override func discoverRecommendedCategories(source: String, authenticated: Bool?) async -> [Int]? {
-                return nil
-            }
+    func testPopularFiltering() async {
+        class Mock: MockServerHandler {
         }
 
-        let observable = CategoriesSelectorViewController.DiscoverItemObservable(serverHandler: NoRecMock())
+        let observable = CategoriesSelectorViewController.DiscoverItemObservable(serverHandler: Mock())
         observable.item = DiscoverItem(
             id: "item2",
             title: "Test 2",
