58bb4c1aa489f2c121ebacbbce15e9a30e128e5e
Brandon Titus <b@titus.io>
2025-06-27 19:37:16 -0600

    Remove EpisodeEntity
    This is used for iOS 26

--- podcasts/Intents/EpisodeEntity+IndexedEntity.swift
+++
@@ -1,9 +0,0 @@
-import AppIntents
-import CoreSpotlight
-
-@available(iOS 18.2, *)
-extension EpisodeEntity: IndexedEntity {
-//    var attributeSet: CSSearchableItemAttributeSet {
-//        let attributes = CSSearchableItemAttributeSet()
-//    }
-}
