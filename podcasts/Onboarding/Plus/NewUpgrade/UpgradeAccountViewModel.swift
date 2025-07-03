fc72ae43dbe338498c3234050205588fee03ba27
Sérgio Estêvão <sergioestevao@gmail.com>
2025-07-03 18:59:24 +0100

    Refactor Upgrade Account View Model to separate file

---
+++ podcasts/Onboarding/Plus/NewUpgrade/UpgradeAccountViewModel.swift
@@ -0,0 +1,33 @@
+class UpgradeAccountViewModel: PlusPricingInfoModel {
+
+    @Published var upgradeTier: UpgradeTier = .plus
+    @Published var selectedProduct: IAPProductID = .yearly
+    @Published var products: [PlusProductPricingInfo] = []
+
+    init() {
+        super.init()
+        loadPrices() {
+            Task { @MainActor [weak self] in
+                guard let self else { return }
+                self.products = self.pricingInfo.products.filter {
+                    self.upgradeTier.plan.products.contains($0.identifier)
+                }
+            }
+        }
+    }
+
+    var features: [UpgradeTier.TierFeature] {
+        return upgradeTier.monthlyFeatures
+    }
+
+    var isFreeTrialAvailable: Bool {
+        guard let product = pricingInfo.products.first(where: {$0.identifier == selectedProduct}) else {
+            return false
+        }
+        guard let offer = product.offer else {
+            return false
+        }
+
+        return offer.type == .freeTrial
+    }
+}
