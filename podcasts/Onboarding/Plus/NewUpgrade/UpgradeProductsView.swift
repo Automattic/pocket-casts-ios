ed91078a7ad7dd6a2779fc25971f151ac573ae85
Sérgio Estêvão <sergioestevao@gmail.com>
2025-07-04 13:38:34 +0100

    Theme account products view

--- podcasts/Onboarding/Plus/NewUpgrade/UpgradeProductsView.swift
+++ podcasts/Onboarding/Plus/NewUpgrade/UpgradeProductsView.swift
@@ -2,27 +2,34 @@ import SwiftUI
 
 struct UpgradeProductsView: View {
 
+    @EnvironmentObject var theme: Theme
+
     @ObservedObject var model: UpgradeAccountViewModel
 
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             ForEach(model.products, id: \.self.id) { product in
                 HStack(alignment: .center) {
-                    Image(systemName: true ? "checkmark" : "circle")
+                    Image(true ? "rounded-selected" : "rounded-unselected")
+                        .renderingMode(.template)
+                        .resizable()
+                        .foregroundStyle(theme.primaryIcon01)
+                        .frame(width: 24, height: 24)
                     VStack(alignment: .leading) {
                         Text(product.price)
                             .font(.subheadline).fontWeight(.bold)
+                            .foregroundStyle(theme.primaryText01)
                         Text(product.price)
                             .font(.subheadline).fontWeight(.medium)
-                            .foregroundStyle(.gray)
+                            .foregroundStyle(theme.primaryText02)
                     }
                     Spacer()
                     Text(product.weeklyPrice)
                         .font(.subheadline).fontWeight(.medium)
-                        .foregroundStyle(.gray)
+                        .foregroundStyle(theme.primaryText02)
                 }
                 .padding(16)
-                .background(Color(red: 0.98, green: 0.98, blue: 0.98))
+                .background(theme.primaryUi03)
                 .cornerRadius(12)
                 .overlay {
                     if let offer = product.offer {
@@ -30,7 +37,8 @@ struct UpgradeProductsView: View {
                             RoundedRectangle(cornerRadius: 12)
                                 .inset(by: 1)
                                 .stroke(Color(red: 0.01, green: 0.66, blue: 0.96), lineWidth: 2)
-                            badge.offset(x: 0, y: -10)
+                            badge
+                                .offset(x: 0, y: -10)
                         }
                     } else {
                         EmptyView()
