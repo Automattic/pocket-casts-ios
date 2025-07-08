96a0112d08fe0279ba44ac6d04cc043f5e024769
Sérgio Estêvão <sergioestevao@gmail.com>
2025-07-08 15:49:17 +0100

    Fix font sizes

--- podcasts/Onboarding/Plus/NewUpgrade/UpgradeFeaturesView.swift
+++ podcasts/Onboarding/Plus/NewUpgrade/UpgradeFeaturesView.swift
@@ -12,12 +12,10 @@ struct UpgradeFeaturesView: View {
                     Image(feature.iconName)
                         .renderingMode(.template)
                         .resizable()
-                        .font(.title)
                         .frame(width: 16, height: 16)
                         .foregroundColor(theme.primaryText01)
                     Text(feature.title)
-                        .font(.subheadline)
-                        .fontWeight(.medium)
+                        .font(size: 15, style: .subheadline, weight: .medium)
                         .foregroundColor(theme.primaryText02)
                 }
             }
