94fcee62a4cd82e65d27df6538bfb5e0f4256851
Daniele Bogo <me@bogodaniele.com>
2025-07-25 14:16:06 +0100

    Update BG colors and add drag and drop to order

--- podcasts/PlaylistsViewController+Table.swift
+++ podcasts/PlaylistsViewController+Table.swift
@@ -174,3 +174,26 @@ extension PlaylistsViewController: UIPopoverPresentationControllerDelegate {
         dismissTipView()
     }
 }
+
+extension PlaylistsViewController: UITableViewDragDelegate, UITableViewDropDelegate {
+    func tableView(_ tableView: UITableView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        let movedObject = playlists[indexPath.row]
+        let itemProvider = NSItemProvider(object: "\(movedObject.id)" as NSString)
+        return [UIDragItem(itemProvider: itemProvider)]
+    }
+
+    func tableView(_ tableView: UITableView, performDropWith coordinator: UITableViewDropCoordinator) {
+        guard let destinationIndexPath = coordinator.destinationIndexPath else { return }
+
+        coordinator.items.forEach { item in
+            if let sourceIndexPath = item.sourceIndexPath {
+                tableView.performBatchUpdates {
+                    let movedItem = playlists.remove(at: sourceIndexPath.row)
+                    playlists.insert(movedItem, at: destinationIndexPath.row)
+                    tableView.moveRow(at: sourceIndexPath, to: destinationIndexPath)
+                }
+                coordinator.drop(item.dragItem, toRowAt: destinationIndexPath)
+            }
+        }
+    }
+}
--- podcasts/PlaylistsViewController.swift
+++ podcasts/PlaylistsViewController.swift
@@ -3,9 +3,18 @@ import PocketCastsUtils
 import UIKit
 
 class PlaylistsViewController: PCViewController, FilterCreatedDelegate {
-    @IBOutlet var filtersTable: UITableView! {
+    @IBOutlet var filtersTable: ThemeableTable! {
         didSet {
             registerCells()
+            if FeatureFlag.playlistsRebranding.enabled {
+                filtersTable.themeStyle = .primaryUi01
+                filtersTable.dragDelegate = self
+                filtersTable.dropDelegate = self
+            } else {
+                filtersTable.themeStyle = .primaryUi04
+                filtersTable.dragDelegate = nil
+                filtersTable.dropDelegate = nil
+            }
         }
     }
 
@@ -39,7 +48,8 @@ class PlaylistsViewController: PCViewController, FilterCreatedDelegate {
 
     lazy private var informationalBannerCoordinator: InformationalBannerViewCoordinator = {
         let bannerType: InformationalBannerType = FeatureFlag.playlistsRebranding.enabled ? .playlists : .filters
-        let viewModel = InformationalBannerViewModel(bannerType: bannerType)
+        let invertedColor: Bool? = FeatureFlag.playlistsRebranding.enabled ? true : nil
+        let viewModel = InformationalBannerViewModel(bannerType: bannerType, invertedColor: invertedColor)
         return InformationalBannerViewCoordinator(viewModel: viewModel)
     }()
 
@@ -47,7 +57,7 @@ class PlaylistsViewController: PCViewController, FilterCreatedDelegate {
         super.viewDidLoad()
 
         if FeatureFlag.playlistsRebranding.enabled {
-            setRighBarButtons()
+            customRightBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewFilter))
         } else {
             customRightBtn = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(editTapped))
         }
@@ -107,34 +117,6 @@ class PlaylistsViewController: PCViewController, FilterCreatedDelegate {
         super.viewDidDisappear(animated)
         removeAllCustomObservers()
         navigationController?.navigationBar.shadowImage = nil
-    }
-
-    private func setRighBarButtons() {
-        let plusButton = UIButton(type: .system)
-        plusButton.setImage(UIImage(named: "add-playlist"), for: .normal)
-        plusButton.addTarget(self, action: #selector(addNewFilter), for: .touchUpInside)
-
-        let settingsButton = UIButton(type: .custom)
-        settingsButton.setImage(UIImage(named: "more"), for: .normal)
-        settingsButton.addTarget(self, action: #selector(editTapped), for: .touchUpInside)
-
-        let stackView = UIStackView(arrangedSubviews: [plusButton, settingsButton])
-        stackView.axis = .horizontal
-        stackView.spacing = 16
-        stackView.distribution = .equalSpacing
-        stackView.alignment = .center
-
-        let container = UIView()
-        container.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
-            stackView.topAnchor.constraint(equalTo: container.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: container.bottomAnchor)
-        ])
-
-        customRightBtn = UIBarButtonItem(customView: container)
     }
 
     @objc private func editTapped() {
