68ebdeb55676bc66cacc93742a3112851a92abd0
Brandon Titus <b@titus.io>
2025-08-13 22:08:38 -0600

    Add additional padding for sign in text fields
    Better matches the old layout and current Figma designs

--- podcasts/SyncSigninView.swift
+++ podcasts/SyncSigninView.swift
@@ -81,6 +81,7 @@ struct SyncSigninView: View {
                 .onSubmit { focusedField = .password }
                 .onChange(of: model.email) { _ in model.textFieldChanged() }
         }
+        .padding(9)
         .themedTextField(hasErrored: model.errorMessage != nil)
     }
 
@@ -112,6 +113,7 @@ struct SyncSigninView: View {
             }
             .onChange(of: model.password) { _ in model.textFieldChanged() }
         }
+        .padding(9)
         .themedTextField(hasErrored: model.errorMessage != nil)
     }
 
