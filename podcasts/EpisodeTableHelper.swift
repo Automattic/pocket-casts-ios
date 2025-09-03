c3746ee5bb9cd6666559f70a826bea4ca77c76ed
Daniele Bogo <me@bogodaniele.com>
2025-09-03 18:02:32 +0100

    Add Episode Array extension

--- podcasts/EpisodeTableHelper.swift
+++ podcasts/EpisodeTableHelper.swift
@@ -5,26 +5,12 @@ import PocketCastsDataModel
 struct EpisodeTableHelper {
     static func loadEpisodes(tintColor: UIColor = AppTheme.appTintColor(), query: String, arguments: [Any]?) -> [ListEpisode] {
         let loadedEpisodes = DataManager.sharedManager.findEpisodesWhere(customWhere: query, arguments: arguments)
-
-        var newData = [ListEpisode]()
-        for episode in loadedEpisodes {
-            let isInUpNext = PlaybackManager.shared.inUpNext(episode: episode)
-            newData.append(ListEpisode(episode: episode, tintColor: tintColor, isInUpNext: isInUpNext))
-        }
-
-        return newData
+        return loadedEpisodes.toListEpisodes(tintColor: tintColor)
     }
 
     static func loadSmartPlaylistEpisodes(tintColor: UIColor = AppTheme.appTintColor(), query: String, arguments: [Any]? = nil) -> [ListEpisode] {
         let loadedEpisodes = DataManager.sharedManager.findSmartPlaylistEpisodesWhere(query: query, arguments: arguments)
-
-        var newData = [ListEpisode]()
-        for episode in loadedEpisodes {
-            let isInUpNext = PlaybackManager.shared.inUpNext(episode: episode)
-            newData.append(ListEpisode(episode: episode, tintColor: tintColor, isInUpNext: isInUpNext))
-        }
-
-        return newData
+        return loadedEpisodes.toListEpisodes(tintColor: tintColor)
     }
 
     static func loadSectionedEpisodes(tintColor: UIColor = AppTheme.appTintColor(), query: String, arguments: [Any]?, episodeShortKey: (Episode) -> String) -> [ArraySection<String, ListEpisode>] {
