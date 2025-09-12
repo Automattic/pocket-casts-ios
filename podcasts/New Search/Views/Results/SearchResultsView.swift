30445af2e975c739c4cc88f7544a55f3677530c6
Sérgio Estêvão <sergioestevao@gmail.com>
2025-09-12 07:31:11 +0100

    Allow styling with the correct size

--- podcasts/New Search/Views/Results/SearchResultsView.swift
+++ podcasts/New Search/Views/Results/SearchResultsView.swift
@@ -15,7 +15,15 @@ struct SearchResultsView: View {
 
     var body: some View {
         Group {
-            NavigationLink(destination: SearchResultsListView(displayMode: displayMode).setupDefaultEnvironment().environmentObject(searchAnalyticsHelper).environmentObject(searchResults).environmentObject(searchHistory), isActive: $showInlineResults) { EmptyView() }
+            NavigationLink(destination:
+                            SearchResultsListView(displayMode: displayMode)
+                                .setupDefaultEnvironment()
+                                .environmentObject(searchAnalyticsHelper)
+                                .environmentObject(searchResults)
+                                .environmentObject(searchHistory),
+                           isActive: $showInlineResults) {
+                EmptyView()
+            }
 
             if searchResults.episodeSearchError != nil && searchResults.podcastSearchError != nil {
                 HStack(alignment: .center) {
@@ -38,8 +46,11 @@ struct SearchResultsView: View {
                         displayMode = .podcasts
                         showInlineResults = true
                     }
-
-                    PodcastsCarouselView()
+                    if FeatureFlag.searchImprovements.enabled {
+                        podcastList
+                    } else {
+                        PodcastsCarouselView()
+                    }
 
                     episodeList()
                 }
@@ -87,6 +98,41 @@ struct SearchResultsView: View {
                                message: L10n.discoverNoPodcastsFoundMsg,
                                icon: { Image(systemName: "info.circle") })
             }
+        }
+    }
+
+    @ViewBuilder var podcastList: some View {
+        if searchResults.isSearchingForPodcasts {
+            ProgressView()
+                .frame(maxWidth: .infinity)
+                .tint(AppTheme.loadingActivityColor().color)
+            // Force the list to re-render the ProgressView by changing it's id
+                //.id(identifier)
+                .onAppear {
+                    //identifier += 1
+                }
+        } else if let _ = searchResults.podcastSearchError {
+            EmptyStateView(
+                title: L10n.discoverSearchFailed,
+                message: L10n.discoverSearchFailedMsg,
+                icon: { Image("no-connection-grey").renderingMode(.template) },
+                actions: [
+                    .init(title: L10n.tryAgain, style: SimpleTextButtonStyle(theme: .sharedTheme, textColor: .primaryInteractive01)) {
+                        searchResults.search(term: searchResults.currentSearchTerm)
+                    }
+                ]
+            )
+        } else if searchResults.podcasts.count > 0 {
+            VStack {
+                ForEach(searchResults.podcasts.prefix(Constants.maxNumberOfEpisodes), id: \.self) { podcast in
+                    PodcastTableCellView(viewModel: PodcastCellViewModel(podcastSearchResult: podcast), style: .large)
+                        .padding(.vertical, 8)
+                }
+            }.padding(20)
+        } else if !searchResults.isShowingLocalResultsOnly {
+            EmptyStateView(title: L10n.discoverNoPodcastsFound,
+                           message: L10n.discoverNoPodcastsFoundMsg,
+                           icon: { Image(systemName: "info.circle") })
         }
     }
 
--- podcasts/PodcastTableCellView.swift
+++ podcasts/PodcastTableCellView.swift
@@ -4,14 +4,37 @@ import PocketCastsDataModel
 import PocketCastsServer
 
 struct PodcastTableCellView: View {
+
+    enum Style {
+        case regular
+        case large
+
+        var size: CGFloat {
+            switch self {
+                case .regular:
+                    return 48
+                case .large:
+                    return 56
+            }
+        }
+    }
+
     @EnvironmentObject var theme: Theme
+
     let viewModel: PodcastCellViewModel
-    let onSubscribe: ((PodcastCellViewModel) -> Void)? = nil
+    let style: Style
+    let onSubscribe: ((PodcastCellViewModel) -> Void)?
+
+    init(viewModel: PodcastCellViewModel, style: Style = .regular, onSubscribe: ((PodcastCellViewModel) -> Void)? = nil) {
+        self.viewModel = viewModel
+        self.style = style
+        self.onSubscribe = onSubscribe
+    }
 
     var body: some View {
         HStack(spacing: 8) {
             PodcastImage(uuid: viewModel.uuid)
-                .frame(width: 48, height: 48)
+                .frame(width: style.size, height: style.size)
                 .clipShape(RoundedRectangle(cornerRadius: 4))
 
             VStack(alignment: .leading, spacing: 3) {
@@ -54,6 +77,14 @@ struct PodcastCellViewModel {
         self.title = discoverPodcast.title
         self.author = discoverPodcast.author
         self.datetime = datetime
+        self.onSubscribe = onSubscribe
+    }
+
+    init(podcastSearchResult: PodcastFolderSearchResult, onSubscribe: ((Self) -> Void)? = nil) {
+        self.uuid = podcastSearchResult.uuid
+        self.datetime = nil
+        self.author = podcastSearchResult.author
+        self.title = podcastSearchResult.title
         self.onSubscribe = onSubscribe
     }
 }
