2eebb0806b0c33e3a8da708f89f0c0cb94304ee2
Sérgio Estêvão <sergioestevao@gmail.com>
2025-10-06 15:00:49 +0100

    Add FF for end of year 2025

--- podcasts/MainTabBarController.swift
+++ podcasts/MainTabBarController.swift
@@ -699,7 +699,7 @@ class MainTabBarController: UITabBarController, NavigationProtocol {
     }
 
     func observersForEndOfYearStats() {
-        guard FeatureFlag.endOfYear.enabled || FeatureFlag.endOfYear2024.enabled else {
+        guard FeatureFlag.endOfYear.enabled || FeatureFlag.endOfYear2024.enabled || FeatureFlag.endOfYear2024.enabled else {
             return
         }
 
