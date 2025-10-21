13bd821ac09b5696a9c33034e7521d7eace5cafa
Sérgio Estêvão <sergioestevao@gmail.com>
2025-10-21 23:27:56 +0100

    Improve animations

--- podcasts/End of Year/Stories/2025/NumberListened2025.swift
+++ podcasts/End of Year/Stories/2025/NumberListened2025.swift
@@ -1,6 +1,23 @@
 import SwiftUI
 import PocketCastsDataModel
 import Lottie
+import Combine
+
+class StepCounter: ObservableObject {
+
+    private let interval: Double
+    private var timer: Timer?
+
+    @Published var counter: Int = 0
+
+    init(interval: TimeInterval) {
+        self.interval = interval
+        self.timer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { [weak self] _ in
+            guard let self else { return }
+            counter = counter + 1
+        }
+    }
+}
 
 struct NumberListened2025: ShareableStory {
 
@@ -9,11 +26,12 @@ struct NumberListened2025: ShareableStory {
 
     @ObservedObject private var animationViewModel = PlayPauseAnimationViewModel(duration: EndOfYear.defaultDuration)
 
+    @StateObject private var stepCounter: StepCounter = .init(interval: 2)
+
     let listenedNumbers: ListenedNumbers
     let podcasts: [Podcast]
 
-    @State var topRowXOffset: Double = 0
-    @State var bottomRowXOffset: Double = 0
+    @State var progress = CGFloat(0)
 
     private let foregroundColor = Color.white
     private let backgroundColor = Color.endOfYear2025Background
@@ -23,6 +41,7 @@ struct NumberListened2025: ShareableStory {
         ZStack {
             VStack(alignment: .center) {
                 headerView
+                Spacer()
                 podcastsAnimation
                 Spacer()
             }
@@ -49,23 +68,36 @@ struct NumberListened2025: ShareableStory {
         StoryHeader2025(title: L10n.playback2025ListenedToNumbers(listenedNumbers.numberOfPodcasts, listenedNumbers.numberOfEpisodes))
     }
 
+    let itemsCount = 7
+
+    var indices: [Int] {
+        let indices = (0..<itemsCount).map { (stepCounter.counter + $0) % podcasts.endIndex }
+        return indices
+    }
+
     @ViewBuilder var podcastsAnimation: some View {
         ZStack() {
-            let itemsCount = 7
-            let indices = (0..<itemsCount).map { ($0 % itemsCount) % podcasts.endIndex }
             ForEach(Array(zip(indices.indices, indices)), id: \.0) { (index, pos) in
-                let size = 260 - CGFloat(abs(index - 3) * 20)
-                let offset = CGFloat((index - 3) * 20)
+                let shift = index - 3
+                let size = 260 - CGFloat(abs(shift) * 30)
+                let offset = CGFloat(shift * 30)
                 podcastCover(pos, shadow: false)
-                    .frame(width: size, height: size)
-                    .offset(x: 0, y: offset)
-                    .zIndex(Double(itemsCount - abs(index - 3)))
+                    .frame(width: size * (1.0 - (0.1 * progress)), height: size * (1.0 - (0.1 * progress)))
+                    .offset(x: 0, y: offset - (5.0 * progress))
+                    .zIndex(Double(itemsCount - abs(shift)))                    
             }
         }
+        .frame(width: 320, height: 320)
         .background(.red)
         .onAppear {
             if animated {
                 animationViewModel.play()
+            }
+        }
+        .onChange(of: stepCounter.counter) { value in
+            progress = 0
+            withAnimation {
+                progress = 1
             }
         }
     }
