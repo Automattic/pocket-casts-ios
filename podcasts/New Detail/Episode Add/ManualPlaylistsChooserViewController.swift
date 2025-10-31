5d8c9befe20e8f8a9cd58e859039c70650cc14df
Daniele Bogo <me@bogodaniele.com>
2025-10-31 14:35:15 +0000

    Remove unnecessary toast message

--- podcasts/New Detail/Episode Add/ManualPlaylistsChooserViewController.swift
+++ podcasts/New Detail/Episode Add/ManualPlaylistsChooserViewController.swift
@@ -152,12 +152,7 @@ class ManualPlaylistsChooserViewController: PCViewController {
 
         manualPlaylists.forEach { playlist in
             if added.contains(playlist.uuid) {
-                let didAdd = dataManager.add(episodes: [episode], to: playlist)
-                guard didAdd else {
-                    let theme: any ToastTheme = ToastIconTheme(iconName: "option-alert", iconColor: Theme.sharedTheme.primaryIcon01)
-                    Toast.show(L10n.playlistManualAddEpisodeFullPlaylistToast, theme: theme)
-                    return
-                }
+                dataManager.add(episodes: [episode], to: playlist)
             }
             if removed.contains(playlist.uuid) {
                 dataManager.deleteEpisodes([episode.uuid], from: playlist)
