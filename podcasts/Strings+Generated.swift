42918d6936c7c339ff40093c825b9e4d339a4d7d
Daniele Bogo <me@bogodaniele.com>
2025-11-12 16:36:40 +0000

    Use Custom order

--- podcasts/Strings+Generated.swift
+++ podcasts/Strings+Generated.swift
@@ -2920,7 +2920,7 @@ internal enum L10n {
   /// Title for the options to configure badge display options.
   internal static var podcastsBadges: String { return L10n.tr("Localizable", "podcasts_badges", fallback: "Badges") }
   /// Episodes will be displayed in custom order by drag and drop
-  internal static var podcastsEpisodeSortDragAndDrop: String { return L10n.tr("Localizable", "podcasts_episode_sort_drag_and_drop", fallback: "Drag and Drop") }
+  internal static var podcastsEpisodeSortDragAndDrop: String { return L10n.tr("Localizable", "podcasts_episode_sort_drag_and_drop", fallback: "Custom order") }
   /// Episodes will be displayed in order from the longest to the shortest.
   internal static var podcastsEpisodeSortLongestToShortest: String { return L10n.tr("Localizable", "podcasts_episode_sort_longest_to_shortest", fallback: "Longest to Shortest") }
   /// Episodes will be displayed in order from the most resent to the oldest.
