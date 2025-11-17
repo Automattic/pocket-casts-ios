02bc1017cc20d522ab4e05138e0d12bd8a33d97d
Daniele Bogo <me@bogodaniele.com>
2025-11-17 13:06:07 +0000

    Reset value if tag is different

--- podcasts/PlaylistsViewController+Table.swift
+++ podcasts/PlaylistsViewController+Table.swift
@@ -31,13 +31,14 @@ extension PlaylistsViewController: UITableViewDelegate, UITableViewDataSource {
         if FeatureFlag.playlistsRebranding.enabled {
             let cell = cell(tableView, for: NewPlaylistCell.reuseIdentifier) as! NewPlaylistCell
             if let playlist = playlists[safe: indexPath.row] {
-                cell.set(playlistName: playlist.playlistName, isManualPlaylist: playlist.manual)
+                if cell.tag != indexPath.row { cell.resetAllMetadata() }
                 cell.tag = indexPath.row
+                cell.set(playlistName: playlist.playlistName, isManualPlaylist: playlist.manual)
                 Task { [weak self] in
                     await self?.playlistMetadataLoader.loadMetadata(for: playlist) { count in
                         guard let cell = tableView.cellForRow(at: indexPath) as? NewPlaylistCell else { return }
                         cell.set(count: count)
-                    } images: {  images in
+                    } images: { images in
                         guard let cell = tableView.cellForRow(at: indexPath) as? NewPlaylistCell else { return }
                         cell.set(images: images)
                     }
