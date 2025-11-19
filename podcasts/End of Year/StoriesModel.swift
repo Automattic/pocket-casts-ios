4b2f3d9597f52db5e3ef20005b559cedf7845c77
Brandon Titus <b@titus.io>
2025-11-18 21:02:06 -0700

    Only log endOfYearStoryShared if shareable

--- podcasts/End of Year/StoriesModel.swift
+++ podcasts/End of Year/StoriesModel.swift
@@ -340,13 +340,16 @@ private extension StoriesModel {
         UIApplication.userDidTakeScreenshotNotification.publisher()
             .receive(on: DispatchQueue.main)
             .sink { [weak self] _ in
-                self?.pause()
-                self?.screenshotTaken = true
+                guard let self else { return }
+                pause()
+                screenshotTaken = true
 
-                let year = EndOfYear.currentYear.literalValue
-                let story = self?.currentStoryIdentifier ?? "unknown"
-                let properties = ["story": story, "year": year, "from": "screenshot"]
-                Analytics.track(.endOfYearStoryShared, properties: properties)
+                if dataSource.shareableStory(for: currentStoryIndex) != nil {
+                    let year = EndOfYear.currentYear.literalValue
+                    let story = currentStoryIdentifier
+                    let properties = ["story": story, "year": year, "from": "screenshot"]
+                    Analytics.track(.endOfYearStoryShared, properties: properties)
+                }
             }
             .store(in: &cancellables)
 
