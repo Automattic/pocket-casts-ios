4ecc36e7cc5a98f2df38de79e5d500cbb3b5b0d5
Daniele Bogo <me@bogodaniele.com>
2025-11-20 19:03:36 +0000

    Adjust Top Podcast Story and replace animation

--- podcasts/End of Year/Stories/2025/TopSpotStory2025.swift
+++ podcasts/End of Year/Stories/2025/TopSpotStory2025.swift
@@ -12,7 +12,7 @@ struct TopSpotStory2025: ShareableStory {
 
     private let foregroundColor = Color.white
     private let backgroundColor = Color(hex: "#17423B")
-    private var scaleFactor: Double = 0.75
+    private var scaleFactor: Double = 0.72
 
     let identifier: String = "top_1_show"
 
@@ -23,28 +23,19 @@ struct TopSpotStory2025: ShareableStory {
     var body: some View {
         ZStack {
             GeometryReader { proxy in
-                VStack(alignment: .center, spacing: 24) {
-                    StoryHeader2025(title: L10n.playback2025TopSpotTitle, description: L10n.playback2025TopSpotSubtitle)
-                    ZStack {
-                        LottieView(animation: .named("playback_2025_top_spot_story"))
-                            .animationDidFinish({ completed in
-                            })
-                            .configure({ animationView in
-                                animationView.contentMode = .scaleToFill
-                            })
-                            .playbackMode(renderForSharing ? .paused(at: .progress(1)) : .playing(.fromProgress(0, toProgress: 1, loopMode: .autoReverse)))
-                            .frame(width: proxy.size.width, height: proxy.size.width)
-                            .scaleEffect(1.2)
-                            .scaledToFill()
-                        PodcastImage(uuid: topPodcast.podcast.uuid, size: .page, aspectRatio: nil, contentMode: .fill)
-                            .frame(width: proxy.size.width * scaleFactor, height: proxy.size.width * scaleFactor)
-                            .cornerRadius(8)
-                            .shadow(color: .black.opacity(0.15), radius: 4, x: 0, y: 2)
-                    }
-                    VStack {
-                        let timeString = topPodcast.totalPlayedTime.storyTimeDescriptionForSharing
-                        let numberPlayed = topPodcast.numberOfPlayedEpisodes
-                        StoryFooter2025(title: nil, description: L10n.playback2025TopSpotDescription(numberPlayed, timeString))
+                ZStack {
+                    background(size: proxy.size)
+                    VStack(alignment: .center, spacing: 0) {
+                        StoryHeader2025(title: L10n.playback2025TopSpotTitle, description: L10n.playback2025TopSpotSubtitle)
+                        Spacer()
+                        VStack {
+                            Spacer()
+                            let timeString = topPodcast.totalPlayedTime.storyTimeDescriptionForSharing
+                            let numberPlayed = topPodcast.numberOfPlayedEpisodes
+                            StoryFooter2025(title: nil, description: L10n.playback2025TopSpotDescription(numberPlayed, timeString))
+                            Spacer()
+                        }
+                        .frame(height: proxy.size.height / 3.5)
                     }
                 }
             }
@@ -69,4 +60,37 @@ struct TopSpotStory2025: ShareableStory {
             StoryShareableText(L10n.eoyStoryTopPodcastShareText("%1$@"), podcast: topPodcast.podcast, year: .y2025)
         ]
     }
+
+    @ViewBuilder func background(size: CGSize) -> some View {
+        VStack(alignment: .center, spacing: 0) {
+            Spacer()
+            ZStack {
+                LottieView(animation: .named("playback_2025_top_spot_story"))
+                    .animationDidFinish({ completed in
+                    })
+                    .configure({ animationView in
+                        animationView.contentMode = .scaleToFill
+                    })
+                    .playbackMode(renderForSharing ? .paused(at: .progress(1)) : .playing(.fromProgress(0, toProgress: 1, loopMode: .autoReverse)))
+                    .frame(width: size.width, height: size.width)
+                    .scaleEffect(UIScreen.isSmallScreen ? 1.2 : 1.25)
+                    .scaledToFill()
+                PodcastImage(uuid: topPodcast.podcast.uuid, size: .page, aspectRatio: nil, contentMode: .fill)
+                    .frame(width: size.width * scaleFactor, height: size.width * scaleFactor)
+                    .cornerRadius(8)
+                    .shadow(color: .black.opacity(0.15), radius: 4, x: 0, y: 2)
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    TopSpotStory2025(
+        topPodcast: TopPodcast(
+            podcast: Podcast(),
+            numberOfPlayedEpisodes: 10,
+            totalPlayedTime: 100000
+        )
+    )
 }
