42f14efe1521a91912c0f890196f00082c5aec69
Brandon Titus <b@titus.io>
2025-11-25 08:12:06 -0700

    Remove toolbar modifications for voiceover
    We will need to come back to this but it has broken the visibility of the toolbar in iOS 26.1

--- podcasts/New Search/Views/Results/LocalSearchView.swift
+++ podcasts/New Search/Views/Results/LocalSearchView.swift
@@ -66,14 +66,10 @@ struct LocalSearchView: View {
 
     private var navigationContent: some View {
         NavigationStack(path: $navigationPath) {
-            let heightConstant: CGFloat = 44
             podcastsView
                 .navigationDestination(for: LocalSearchRoute.self) { route in
                     destinationView(for: route)
-                    .padding(.top, heightConstant)
                 }
-                .toolbar(.hidden, for: .navigationBar)
-                .padding(.top, heightConstant)
         }
     }
 
@@ -220,7 +216,6 @@ private extension LocalSearchView {
             .background(backgroundColor.ignoresSafeArea())
             .navigationTitle(viewModel.navigationTitle)
             .navigationBarTitleDisplayMode(.inline)
-            .toolbar(.hidden, for: .navigationBar)
         case .podcast:
             LocalSearchEpisodeResultsView(
                 isLoading: viewModel.isEpisodeSearchInFlight,
@@ -240,7 +235,6 @@ private extension LocalSearchView {
             .background(backgroundColor.ignoresSafeArea())
             .navigationTitle(viewModel.navigationTitle)
             .navigationBarTitleDisplayMode(.inline)
-            .toolbar(.hidden, for: .navigationBar)
         }
     }
 
