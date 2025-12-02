788bb33dc58087dc86c30d2da1a3bc658e8cb5c8
Sérgio Estêvão <sergioestevao@gmail.com>
2025-12-02 15:05:27 +0000

    Ensure that we can start queries with at least 1 character

--- podcasts/New Search/Model/SearchResultsModel.swift
+++ podcasts/New Search/Model/SearchResultsModel.swift
@@ -70,7 +70,7 @@ class SearchResultsModel: ObservableObject {
         currentSearchTerm = term
         clearErrors()
 
-        guard term.trim().count > 1, !isTermAnURL(term) else {
+        guard !term.trim().isEmpty, !isTermAnURL(term) else {
             return
         }
 
