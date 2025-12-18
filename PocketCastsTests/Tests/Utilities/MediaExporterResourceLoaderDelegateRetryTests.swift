3acb792bdf426f4b8d063df398d5ff14062aa084
Sérgio Estêvão <sergioestevao@gmail.com>
2025-12-18 13:04:37 +0000

    Use alternative method to disable test

--- PocketCastsTests/Tests/Utilities/MediaExporterResourceLoaderDelegateRetryTests.swift
+++ PocketCastsTests/Tests/Utilities/MediaExporterResourceLoaderDelegateRetryTests.swift
@@ -153,8 +153,9 @@ final class MediaExporterResourceLoaderDelegateRetryTests: XCTestCase {
     }
 
     // MARK: - Retry Integration Tests
-    // Test was having flaky results when calling the test url
-    func disabled_testRetryWithoutUserAgent_ResetsStateAndRetries() {
+    func testRetryWithoutUserAgent_ResetsStateAndRetries() {
+        XCTExpectFailure("Test is flaky due to async timing issues", strict: false)
+
         delegate.response = HTTPURLResponse(url: testURL, statusCode: 403, httpVersion: nil, headerFields: nil)
         delegate.hasRetriedWithoutUserAgent = false
 
