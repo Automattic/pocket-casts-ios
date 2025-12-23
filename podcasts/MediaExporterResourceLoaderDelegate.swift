9670f803ceb9d24d23817e141e359e7a1f53808e
Sérgio Estêvão <sergioestevao@gmail.com>
2025-12-23 15:19:31 +0000

    Fix linting

--- podcasts/MediaExporterResourceLoaderDelegate.swift
+++ podcasts/MediaExporterResourceLoaderDelegate.swift
@@ -253,7 +253,7 @@ class MediaExporterResourceLoaderDelegate: NSObject, AVAssetResourceLoaderDelega
 
         // Is there enough data cached to fulfill the request?
         guard bytesCached > currentOffset else {
-            if FeatureFlag.streamAndDownloadReadFromMemoryBuffer.enabled, currentOffset < bufferData.count + bytesCached {                
+            if FeatureFlag.streamAndDownloadReadFromMemoryBuffer.enabled, currentOffset < bufferData.count + bytesCached {
                 let start = currentOffset - bytesCached
                 let end = min(currentOffset + requestedLength - bytesCached, bufferData.count)
                 let dataRequested = bufferData.subdata(in: start..<end)
