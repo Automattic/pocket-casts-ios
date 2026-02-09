d3b37ce37e223764eb0508258516e33d9f4d0e17
Sérgio Estêvão <sergioestevao@gmail.com>
2026-02-09 17:26:07 +0000

    Use Button cell for badge settings to avoid large inset

--- podcasts/BadgeSettingsViewController.swift
+++ podcasts/BadgeSettingsViewController.swift
@@ -7,12 +7,12 @@ class BadgeSettingsViewController: PCViewController, UITableViewDelegate, UITabl
     private let optionsSection = 0
     private let filtersSection = 1
 
-    private let cellId = "TopLevelSettingsCell"
+    private let cellId = "ButtonCell"
 
     var playlists: [EpisodeFilter]!
     @IBOutlet var optionsTable: UITableView! {
         didSet {
-            optionsTable.register(UINib(nibName: "TopLevelSettingsCell", bundle: nil), forCellReuseIdentifier: cellId)
+            optionsTable.register(UINib(nibName: "ButtonCell", bundle: nil), forCellReuseIdentifier: cellId)
             optionsTable.rowHeight = UITableView.automaticDimension
             optionsTable.estimatedRowHeight = UITableView.automaticDimension
             optionsTable.sectionHeaderHeight = UITableView.automaticDimension
@@ -52,22 +52,21 @@ class BadgeSettingsViewController: PCViewController, UITableViewDelegate, UITabl
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! TopLevelSettingsCell
-        cell.showsDisclosureIndicator = false
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ButtonCell        
 
         if indexPath.section == optionsSection {
             if indexPath.row == 0 {
-                cell.settingsLabel.text = L10n.off
+                cell.buttonTitle.text = L10n.off
             } else if indexPath.row == 1 {
-                cell.settingsLabel.text = L10n.settingsBadgeTotalUnplayed
+                cell.buttonTitle.text = L10n.settingsBadgeTotalUnplayed
             } else if indexPath.row == 2 {
-                cell.settingsLabel.text = L10n.settingsBadgeNewSinceOpened
+                cell.buttonTitle.text = L10n.settingsBadgeNewSinceOpened
             }
 
             let badgeSetting = Int(Settings.appBadge?.rawValue ?? AppBadge.off.rawValue)
             cell.accessoryType = (badgeSetting == indexPath.row) ? .checkmark : .none
         } else if indexPath.section == filtersSection, let filter = playlists[safe: indexPath.row] {
-            cell.settingsLabel.text = filter.playlistName
+            cell.buttonTitle.text = filter.playlistName
 
             let selectedFilterId = Settings.appBadgeFilterUuid
             cell.accessoryType = filter.uuid == selectedFilterId ? .checkmark : .none
