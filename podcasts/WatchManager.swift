41957979bf48b2a42eb9a3edff656493d8774896
Brandon Titus <b@titus.io>
2026-02-12 18:28:32 -0700

    fix: Handle loginDetailsRequest in WatchConnectivity session without reply handler
    When WatchConnectivity delivers loginDetailsRequest to the wrong delegate method
    (without reply handler), push data to watch instead of logging unknown message.
    This prevents excessive 'Unknown message type: loginDetailsRequest' log spam.

--- podcasts/WatchManager.swift
+++ podcasts/WatchManager.swift
@@ -204,6 +204,13 @@ class WatchManager: NSObject, WCSessionDelegate {
             if DateUtil.hasEnoughTimePassed(since: ServerSettings.lastRefreshEndTime(), time: 30.minutes) {
                 RefreshManager.shared.refreshPodcasts()
             }
+        } else if WatchConstants.Messages.LoginDetailsRequest.type == messageType {
+            // Watch is requesting login details but message was delivered without reply handler
+            // This can happen with WatchConnectivity timing issues - mark login details as updated
+            // so that updateWatchData() will push the latest login information to the watch.
+            FileLog.shared.addMessage("WatchManager: loginDetailsRequest received without reply handler, pushing data to watch")
+            Settings.setLoginDetailsUpdated()
+            updateWatchData()
         }
     }
 
