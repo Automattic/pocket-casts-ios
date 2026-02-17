1922d7f62f66d84db60388a45661fadaf99c0b5b
Brandon Titus <b@titus.io>
2026-02-16 20:09:27 -0700

    Set errorLogger

--- podcasts/AppDelegate.swift
+++ podcasts/AppDelegate.swift
@@ -42,6 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         setupAnalytics()
 
         DataManager.logger = SentryLogger()
+        ServerConfig.shared.errorLogger = SentryLogger()
 
         appInstallState = appLifecycleAnalytics.checkApplicationInstalledOrUpgraded()
 
