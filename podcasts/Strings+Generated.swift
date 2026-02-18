dac7892ee4adc1b605f57c27980ea50599472d70
Brandon Titus <b@titus.io>
2026-02-18 12:37:18 -0700

    Add watch logs to LogsView

--- podcasts/Strings+Generated.swift
+++ podcasts/Strings+Generated.swift
@@ -1732,6 +1732,8 @@ internal enum L10n {
   internal static var logs: String { return L10n.tr("Localizable", "logs", fallback: "Logs") }
   /// Message when no email account is configured to be able to send the logs
   internal static var logsNoEmailAccountConfigured: String { return L10n.tr("Localizable", "logs_no_email_account_configured", fallback: "You need to configure an email account on the device in order to send the logs") }
+  /// Message shown when watchOS logs are not available
+  internal static var logsWatchOsNotAvailable: String { return L10n.tr("Localizable", "logs_watch_os_not_available", fallback: "No watchOS logs available. Make sure you have an Apple Watch paired and the Pocket Casts watch app installed.") }
   /// Button title for manage downloads file space usage banner and modal.
   internal static var manageDownloadsAction: String { return L10n.tr("Localizable", "manage_downloads_action", fallback: "Manage downloads") }
   /// Detail for manage downloads file space usage banner and modal. %1$@ is the disk space in Mb/GB that the episodes take
