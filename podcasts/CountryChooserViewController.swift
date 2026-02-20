767e8e4181681e2994ba190e5b8d33607efcbf93
Sérgio Estêvão <sergioestevao@gmail.com>
2026-02-20 12:29:14 +0000

    Update country cell and picket for dynamic type

--- podcasts/CountryChooserViewController.swift
+++ podcasts/CountryChooserViewController.swift
@@ -57,6 +57,10 @@ class CountryChooserViewController: PCViewController, UITableViewDataSource, UIT
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        UITableView.automaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         56
     }
 
