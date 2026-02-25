525cb46fbc6e79d0d5f21606af03744d55907469
Brandon Titus <b@titus.io>
2026-02-24 20:45:59 -0700

    Prefer Show Notes to asset image

--- podcasts/EpisodeArtwork.swift
+++ podcasts/EpisodeArtwork.swift
@@ -12,9 +12,9 @@ class EpisodeArtwork {
         self.imageManager = imageManager
     }
 
-    /// Attempts to load an episode embedded artwork.
-    /// It first tries to load an embedded artwork from the AVAsset.
-    /// If nothing is found, it then request the image from Cache Server Handler.
+    /// Attempts to load episode artwork with the following priority (matching Android):
+    /// 1. Show notes image URL from the server (publisher intent takes precedence)
+    /// 2. Embedded artwork from the AVAsset (ID3 tags)
     /// If an image is retrieved, `episodeEmbeddedArtworkLoaded` notification is triggered
     /// - Parameters:
     ///   - asset: an AVAsset
@@ -25,12 +25,19 @@ class EpisodeArtwork {
             return
         }
 
-        if let assetEpisodeArtwork = loadEpisodeArtwork(from: asset) {
-            imageManager.save(assetEpisodeArtwork, for: episodeUuid)
-            return
-        }
+        Task { [weak self] in
+            guard let self else { return }
 
-        loadEpisodeArtworkFromUrl(podcastUuid: podcastUuid, episodeUuid: episodeUuid)
+            // Priority 1: Show notes image URL (publisher intent takes precedence)
+            if await self.loadArtworkFromShowNotes(podcastUuid: podcastUuid, episodeUuid: episodeUuid) {
+                return
+            }
+
+            // Priority 2: Embedded artwork from audio file metadata
+            if let assetEpisodeArtwork = self.loadEpisodeArtwork(from: asset) {
+                self.imageManager.save(assetEpisodeArtwork, for: episodeUuid)
+            }
+        }
     }
 
     func isCached(episodeUuid: String) -> Bool {
@@ -46,25 +53,27 @@ class EpisodeArtwork {
         return artworkItems.compactMap { $0.dataValue.flatMap { UIImage(data: $0) } }.first
     }
 
-    private func loadEpisodeArtworkFromUrl(podcastUuid: String, episodeUuid: String) {
-        Task { [weak self] in
-            guard let self else {
-                return
-            }
+    /// Attempts to load episode artwork from show notes URL.
+    /// - Returns: true if artwork was successfully loaded and saved, false otherwise
+    private func loadArtworkFromShowNotes(podcastUuid: String, episodeUuid: String) async -> Bool {
+        guard let imageUrl = try? await ShowInfoCoordinator.shared.loadEpisodeArtworkUrl(podcastUuid: podcastUuid, episodeUuid: episodeUuid),
+              let url = URL(string: imageUrl) else {
+            return false
+        }
 
-            guard let imageUrl = try? await ShowInfoCoordinator.shared.loadEpisodeArtworkUrl(podcastUuid: podcastUuid, episodeUuid: episodeUuid),
-                  let url = URL(string: imageUrl) else {
-                return
-            }
+        // Resize image to avoid really big images that appear
+        // super blurred on CarPlay.
+        // If the image is smaller to the given size, no downsampling is done.
+        let size = imageManager.biggestPodcastImageSize
+        let resizeProcessor = DownsamplingImageProcessor(size: .init(width: size, height: size))
 
-            // Resize image to avoid really big images that appear
-            // super blurred on CarPlay.
-            // If the image is smaller to the given size, no downsampling is done.
-            let size = self.imageManager.biggestPodcastImageSize
-            let resizeProcessor = DownsamplingImageProcessor(size: .init(width: size, height: size))
-            KingfisherManager.shared.retrieveImage(with: url, options: [.processor(resizeProcessor)]) { result in
+        return await withCheckedContinuation { continuation in
+            KingfisherManager.shared.retrieveImage(with: url, options: [.processor(resizeProcessor)]) { [weak self] result in
                 if let image = try? result.get().image {
-                    self.imageManager.save(image, for: episodeUuid)
+                    self?.imageManager.save(image, for: episodeUuid)
+                    continuation.resume(returning: true)
+                } else {
+                    continuation.resume(returning: false)
                 }
             }
         }
