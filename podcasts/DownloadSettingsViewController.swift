4e38909c1d814c497e40bb5cfbd011d7377c39bb
Sérgio Estêvão <sergioestevao@gmail.com>
2026-03-02 21:48:14 +0000

    Avoid alert of doing presentation before base VC being attached to window

--- podcasts/DownloadSettingsViewController.swift
+++ podcasts/DownloadSettingsViewController.swift
@@ -29,13 +29,17 @@ class DownloadSettingsViewController: PCViewController, UITableViewDataSource, U
         NotificationCenter.default.addObserver(self, selector: #selector(podcastUpdated(_:)), name: Constants.Notifications.podcastUpdated, object: nil)
         insetAdjuster.setupInsetAdjustmentsForMiniPlayer(scrollView: settingsTable)
         Analytics.track(.settingsAutoDownloadShown)
-        ManageDownloadsCoordinator.showModalIfNeeded(from: self, source: "auto_download")
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         settingsTable.reloadData()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        ManageDownloadsCoordinator.showModalIfNeeded(from: self, source: "auto_download")
     }
 
     // MARK: - UITableView methods
