88441b888f9e2095a6d0925892a024b5ef4ba99a
Sérgio Estêvão <sergioestevao@gmail.com>
2026-03-04 18:59:16 +0000

    Only show buffering UI when playing an episode

--- podcasts/NowPlayingPlayerItemViewController+Update.swift
+++ podcasts/NowPlayingPlayerItemViewController+Update.swift
@@ -167,7 +167,7 @@ extension NowPlayingPlayerItemViewController {
             timeSlider.currentTime = upTo
         }
 
-        timeSlider.indeterminant = PlaybackManager.shared.buffering()
+        timeSlider.indeterminant = PlaybackManager.shared.buffering() && PlaybackManager.shared.playing()
     }
 
     func updateProvisionalChapterInfoForTime(time: TimeInterval) {
