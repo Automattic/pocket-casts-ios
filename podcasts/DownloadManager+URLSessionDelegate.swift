a95f9fa1b9cb62766b44e61a8250880081de71d6
Sérgio Estêvão <sergioestevao@gmail.com>
2026-03-06 15:54:33 +0000

    Use store manager

--- podcasts/DownloadManager+URLSessionDelegate.swift
+++ podcasts/DownloadManager+URLSessionDelegate.swift
@@ -208,7 +208,7 @@ extension DownloadManager: URLSessionDelegate, URLSessionDownloadDelegate {
         } catch {
             if !copyFile {
                 // Lets try remove the file so we don't have a pending file on the tmp folder
-                try? FileManager.default.removeItem(at: location)
+                StorageManager.removeItem(at: location)
             }
             FileLog.shared.addMessage("DownloadManager: Failed to copy downloaded file from location: \(location.absoluteString) to destination:  \(destinationPath) error: \(error)")
             markEpisode(episode, asFailedWithMessage: L10n.downloadErrorNotEnoughSpace, reason: .badResponse)
