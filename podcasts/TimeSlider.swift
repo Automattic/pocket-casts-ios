e43988b50fb8a02ee8467ce1060318a9e6b1324b
Sérgio Estêvão <sergioestevao@gmail.com>
2026-03-13 10:44:52 +0000

    Remove should animate

--- podcasts/TimeSlider.swift
+++ podcasts/TimeSlider.swift
@@ -108,8 +108,7 @@ class TimeSlider: UIView {
     // MARK: - Touch handling
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let firstTouch = touches.first {
-            timeLayer().shouldAnimate = true
+        if let firstTouch = touches.first {            
             let touchPoint = firstTouch.location(in: self)
             let slightlyBiggerKnobRect = timeLayer().knobRect.insetBy(dx: -20, dy: -20)
             if slightlyBiggerKnobRect.contains(touchPoint) {
