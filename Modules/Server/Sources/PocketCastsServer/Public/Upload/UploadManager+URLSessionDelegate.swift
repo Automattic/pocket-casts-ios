2cf92bc8a31bf60c9adf6be06fcb7aa5f5501f50
Brandon Titus <b@titus.io>
2026-03-13 16:44:27 -0600

    Track data usage in UploadManager
    Records bytes transferred during custom episode uploads.

--- Modules/Server/Sources/PocketCastsServer/Public/Upload/UploadManager+URLSessionDelegate.swift
+++ Modules/Server/Sources/PocketCastsServer/Public/Upload/UploadManager+URLSessionDelegate.swift
@@ -9,6 +9,22 @@ extension UploadManager: URLSessionDelegate, URLSessionDataDelegate {
     // things smaller than 150kb are suspect, probably text, xml or html error pages
     private static let suspectEpisodeSize = 150 * 1024
 
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics) {
+        let bytesSent = task.countOfBytesSent
+        let isCellular = metrics.transactionMetrics.last?.isCellular ?? false
+
+        if bytesSent > 0 {
+            let connectionType: NetworkDataUsageManager.ConnectionType = isCellular ? .cellular : .wifi
+
+            DataManager.sharedManager.networkDataUsageManager.add(
+                bytesUploaded: bytesSent,
+                operationType: .upload,
+                connectionType: connectionType,
+                sessionType: .background
+            )
+        }
+    }
+
     public func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
         // make sure we call the completion handler on the main queue, otherwise it will crash
         DispatchQueue.main.async {
