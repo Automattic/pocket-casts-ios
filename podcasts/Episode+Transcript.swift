1c1f1301c501c641498796d3f479c29829947ff3
Leandro Alonso <contato@leandroalonso.com>
2026-04-23 07:41:07 -0300

    DEBUG: assume generated transcripts always
    This is made to test in Staging. This commit needs to be reverted once staging/prod is ready for transcripts/fingerprints.

--- podcasts/Episode+Transcript.swift
+++ podcasts/Episode+Transcript.swift
@@ -1,18 +1,35 @@
 import PocketCastsDataModel
+import PocketCastsUtils
 
 extension Episode {
     func checkTranscriptAvailability() {
         Task.init {
-            if let metadata = try? await ShowInfoCoordinator.shared.loadTranscriptsMetadata(podcastUuid: parentIdentifier(), episodeUuid: uuid) {
-                let transcriptsAvailable = !metadata.transcripts.isEmpty
-                let hasGeneratedTranscripts = metadata.hasGeneratedTranscripts
-                let userInfo = [
-                    "episodeUuid": uuid,
-                    "isAvailable": transcriptsAvailable,
-                    "hasGeneratedTranscripts": hasGeneratedTranscripts
-                ]
-                NotificationCenter.postOnMainThread(notification: Constants.Notifications.episodeTranscriptAvailabilityChanged, userInfo: userInfo)
+            let metadata = try? await ShowInfoCoordinator.shared.loadTranscriptsMetadata(podcastUuid: parentIdentifier(), episodeUuid: uuid)
+
+            let transcriptsAvailable: Bool
+            let hasGeneratedTranscripts: Bool
+
+            #if DEBUG
+            if FeatureFlag.syncedTranscripts.enabled {
+                transcriptsAvailable = true
+                hasGeneratedTranscripts = metadata?.hasGeneratedTranscripts ?? false
+            } else {
+                guard let metadata else { return }
+                transcriptsAvailable = !metadata.transcripts.isEmpty
+                hasGeneratedTranscripts = metadata.hasGeneratedTranscripts
             }
+            #else
+            guard let metadata else { return }
+            transcriptsAvailable = !metadata.transcripts.isEmpty
+            hasGeneratedTranscripts = metadata.hasGeneratedTranscripts
+            #endif
+
+            let userInfo: [AnyHashable: Any] = [
+                "episodeUuid": uuid,
+                "isAvailable": transcriptsAvailable,
+                "hasGeneratedTranscripts": hasGeneratedTranscripts
+            ]
+            NotificationCenter.postOnMainThread(notification: Constants.Notifications.episodeTranscriptAvailabilityChanged, userInfo: userInfo)
         }
     }
 }
