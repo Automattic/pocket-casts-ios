c654fac0a23b1ca5b9fccaa78dc00ac8b78fe9a8
Gio Lodi <gio@mokacoding.com>
2026-04-23 11:06:35 +1000

    Disable TestFlight upload because of build no reuse

--- .buildkite/commands/build-tvos-app-store.sh
+++ .buildkite/commands/build-tvos-app-store.sh
@@ -14,7 +14,11 @@ bundle exec fastlane run configure_apply
 echo "--- :hammer_and_wrench: Building"
 bundle exec fastlane build_app_store_connect_tvos
 
-echo "--- :rocket: Uploading to TestFlight"
-echo "NOTE: This TestFlight upload is temporary; it's here only to validate the end-to-end tvOS signing and delivery setup."
-echo "Remove it from this step once the pipeline is confirmed working."
-bundle exec fastlane upload_app_store_connect_build_to_testflight_tvos
+# Disabled while we think of a build number strategy.
+#
+# Otherwise, builds will fail because of reused build number.
+# E.g.: https://buildkite.com/automattic/pocket-casts-ios/builds/14284/steps/canvas?jid=019db7d9-940a-4937-905d-d4608d24ae2f&tab=output#019db7d9-940a-4937-905d-d4608d24ae2f
+# echo "--- :rocket: Uploading to TestFlight"
+# echo "NOTE: This TestFlight upload is temporary; it's here only to validate the end-to-end tvOS signing and delivery setup."
+# echo "Remove it from this step once the pipeline is confirmed working."
+# bundle exec fastlane upload_app_store_connect_build_to_testflight_tvos
