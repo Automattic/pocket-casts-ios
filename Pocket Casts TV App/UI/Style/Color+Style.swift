7f48d17863134192b6f64904c9ee71c81617bb5b
Sérgio Estêvão <sergioestevao@gmail.com>
2026-04-24 15:51:39 +0100

    Add color styles

---
+++ Pocket Casts TV App/UI/Style/Color+Style.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+extension Color {
+
+    static var textPrimary: Color {
+        return Color(red: 0.98, green: 0.98, blue: 0.98)
+    }
+
+    static var textPrimaryActive: Color {
+        return Color(red: 0.09, green: 0.09, blue: 0.09)
+    }
+
+    static var textSecondary: Color {
+        return Color(red: 0.69, green: 0.7, blue: 0.72)
+    }
+
+    static var textSecondaryActive: Color {
+        return Color(red: 0.24, green: 0.25, blue: 0.27)
+    }
+
+    static var textTertiary: Color {
+        return Color(red: 0.48, green: 0.49, blue: 0.51)
+    }
+
+    static var textTertiaryActive: Color {
+        return Color(red: 0.48, green: 0.49, blue: 0.51)
+    }
+
+    static var textDisabled: Color {
+        return Color(red: 0.29, green: 0.3, blue: 0.32)
+    }
+
+    static var textDisabledActive: Color {
+        return Color(red: 0.69, green: 0.70, blue: 0.72)
+    }
+}
