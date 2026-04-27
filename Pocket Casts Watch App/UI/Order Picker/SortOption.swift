53ad06d198d38fcb4e3e7f31c070aeae72c0b469
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-04-27 16:58:20 -0400

    Fix Xcode warnings

--- Pocket Casts Watch App/UI/Order Picker/SortOption.swift
+++ Pocket Casts Watch App/UI/Order Picker/SortOption.swift
@@ -6,13 +6,13 @@ protocol SortOption: Identifiable, CaseIterable, Equatable {
     var description: String { get }
 }
 
-extension PodcastEpisodeSortOrder: SortOption {
+extension PodcastEpisodeSortOrder: @retroactive Identifiable, SortOption {
     static var pickerTitle: String = L10n.sortEpisodes
 
     public var id: Int32 { rawValue }
 }
 
-extension LibrarySort: SortOption {
+extension LibrarySort: @retroactive Identifiable, SortOption {
     static var pickerTitle: String = L10n.podcastsSort
     public var id: Int { Int(rawValue) }
 
