dff023d1923d71dc1c2710d5ecf41944571349a2
Sérgio Estêvão <sergioestevao@gmail.com>
2026-04-28 12:51:04 +0100

    Add timer for sync loading state

--- Pocket Casts TV App/UI/Auth/SigningInView.swift
+++ Pocket Casts TV App/UI/Auth/SigningInView.swift
@@ -1,8 +1,26 @@
 import SwiftUI
+import Combine
 
 @Observable
 class SigningInViewModel {
+    private var cancellable: AnyCancellable?
+
+    enum State: Equatable, Hashable {
+        case waiting
+        case finished
+    }
+    var state: State = .waiting
+
     var podcasts: [MockPodcast] = MockData.makePodcasts()
+
+    func sync() {
+        cancellable = Timer.publish(every: 5.0, on: .main, in: .common)
+                    .autoconnect()
+                    .sink { [weak self] _ in
+                        guard let self else { return }
+                        state = .finished
+                    }
+    }
 }
 
 struct SigningInView: View {
@@ -27,6 +45,12 @@ struct SigningInView: View {
             Spacer()
             podcastGrid
             Spacer().frame(height: 100)
+        }
+        .task {
+            model.sync()
+        }
+        .onChange(of: model.state) {
+            viewModel.state = .signedIn
         }
     }
 
