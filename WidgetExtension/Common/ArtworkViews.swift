c63628fa9e8de0c1f8b880201a0f727bbd0482a9
Sérgio Estêvão <sergioestevao@gmail.com>
2026-04-28 14:14:00 +0100

    Add tinting to image in widget if they are active

--- WidgetExtension/Common/ArtworkViews.swift
+++ WidgetExtension/Common/ArtworkViews.swift
@@ -1,4 +1,47 @@
 import SwiftUI
+import UIKit
+
+extension UIImage {
+    /// Converts white/light pixels to transparent, useful for logos on white backgrounds
+    func addingAlphaFromLuminance() -> UIImage? {
+        guard let cgImage = self.cgImage else { return nil }
+
+        let width = cgImage.width
+        let height = cgImage.height
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bytesPerPixel = 4
+        let bytesPerRow = bytesPerPixel * width
+        let bitsPerComponent = 8
+
+        var pixelData = [UInt8](repeating: 0, count: width * height * bytesPerPixel)
+
+        guard let context = CGContext(
+            data: &pixelData,
+            width: width,
+            height: height,
+            bitsPerComponent: bitsPerComponent,
+            bytesPerRow: bytesPerRow,
+            space: colorSpace,
+            bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+        ) else { return nil }
+
+        context.draw(cgImage, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+        // Convert luminance to alpha
+        for i in stride(from: 0, to: pixelData.count, by: 4) {
+            let r = Double(pixelData[i])
+            let g = Double(pixelData[i + 1])
+            let b = Double(pixelData[i + 2])
+            let luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255.0
+
+            // Invert: dark pixels become opaque, light pixels become transparent
+            pixelData[i + 3] = UInt8((1.0 - luminance) * 255)
+        }
+
+        guard let outputCGImage = context.makeImage() else { return nil }
+        return UIImage(cgImage: outputCGImage, scale: self.scale, orientation: self.imageOrientation)
+    }
+}
 
 struct LargeArtworkView: View {
     @Environment(\.isAccentedRenderingMode) var isAccentedRenderingMode
@@ -8,9 +51,20 @@ struct LargeArtworkView: View {
 
     var showShadow: Bool = true
 
+    var imageToUse: UIImage? {
+        guard let imageData else {
+            return nil
+        }
+        if isAccentedRenderingMode {
+            return UIImage(data: imageData)?.addingAlphaFromLuminance()
+        } else {
+            return UIImage(data: imageData)
+        }
+    }
+
     var body: some View {
         ZStack {
-            if showShadow {
+            if !isAccentedRenderingMode, showShadow {
                 Rectangle()
                     .foregroundColor(Color.nowPlayingShadowColor)
                     .aspectRatio(1, contentMode: .fit)
@@ -19,11 +73,12 @@ struct LargeArtworkView: View {
                     .secondaryShadow()
                     .backwardWidgetAccentable(isAccentedRenderingMode)
             }
-
-            if let imageData, let uiImage = UIImage(data: imageData) {
+            if let uiImage = imageToUse {
                 Image(uiImage: uiImage)
                     .resizable()
-                    .backwardWidgetFullColorRenderingMode()
+                    .if(isAccentedRenderingMode) { content in
+                        content.backwardWidgetAccentedRenderingMode(isAccentedRenderingMode)
+                    }
                     .aspectRatio(1, contentMode: .fit)
                     .frame(maxHeight: size)
                     .cornerRadius(8)
@@ -33,7 +88,8 @@ struct LargeArtworkView: View {
             } else {
                 Image("no-podcast-artwork")
                     .resizable()
-                    .backwardWidgetFullColorRenderingMode()
+                    .renderingMode(.template)
+                    .backwardWidgetAccentedRenderingMode(isAccentedRenderingMode)
                     .aspectRatio(1, contentMode: .fit)
                     .frame(maxHeight: size)
                     .cornerRadius(8)
@@ -49,25 +105,42 @@ struct SmallArtworkView: View {
     @Environment(\.isAccentedRenderingMode) var isAccentedRenderingMode
 
     @State var imageData: Data?
+
+    var imageToUse: UIImage? {
+        guard let imageData else {
+            return nil
+        }
+        if isAccentedRenderingMode {
+            return UIImage(data: imageData)?.addingAlphaFromLuminance()?.withRenderingMode(.alwaysTemplate)
+        } else {
+            return UIImage(data: imageData)
+        }
+    }
+
     var body: some View {
         ZStack {
-            Rectangle()
-                .foregroundColor(Color.nowPlayingShadowColor)
-                .aspectRatio(1, contentMode: .fit)
-                .cornerRadius(5)
-                .secondaryShadow()
-                .backwardWidgetAccentable(isAccentedRenderingMode)
-            if let imageData, let uiImage = UIImage(data: imageData) {
+            if !isAccentedRenderingMode {
+                Rectangle()
+                    .foregroundColor(Color.nowPlayingShadowColor)
+                    .aspectRatio(1, contentMode: .fit)
+                    .cornerRadius(5)
+                    .secondaryShadow()
+                    .backwardWidgetAccentable(isAccentedRenderingMode)
+            }
+            if let uiImage = imageToUse {
                 Image(uiImage: uiImage)
                     .resizable()
-                    .backwardWidgetFullColorRenderingMode()
+                    .if(isAccentedRenderingMode) { content in
+                        content.backwardWidgetAccentedRenderingMode(isAccentedRenderingMode)
+                    }
                     .aspectRatio(1, contentMode: .fit)
                     .cornerRadius(4)
                     .artworkShadow()
             } else {
                 Image("no-podcast-artwork")
                     .resizable()
-                    .backwardWidgetFullColorRenderingMode()
+                    .renderingMode(.template)
+                    .backwardWidgetAccentedRenderingMode(isAccentedRenderingMode)
                     .aspectRatio(1, contentMode: .fit)
                     .cornerRadius(4)
                     .artworkShadow()
