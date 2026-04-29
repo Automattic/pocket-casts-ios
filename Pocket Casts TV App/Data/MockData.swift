24457068c8546f910acbfcd7e59e93be88f7f713
Sérgio Estêvão <sergioestevao@gmail.com>
2026-04-29 13:57:58 +0100

    Increase mock data podcasts in order to have scroll

--- Pocket Casts TV App/Data/MockData.swift
+++ Pocket Casts TV App/Data/MockData.swift
@@ -9,7 +9,7 @@ struct MockPodcast: Identifiable {
 struct MockData {
 
     static func makePodcasts() -> [MockPodcast] {
-        let numberOfPodcasts = 14
+        let numberOfPodcasts = 48
         var results = [MockPodcast]()
         for i in (0..<numberOfPodcasts) {
             results.append(MockPodcast(id: UUID().uuidString, name: "Podcast \(i+1)", image: "Covers/login-cover-\( (i % 10) + 1)"))
