1f2465b758dc52f6570c0c58db513b267829eca6
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-04-30 13:54:17 -0400

    Create PodcastHeartView lazily

--- podcasts/Podcasts/All Podcasts/Cells/PodcastListCell.swift
+++ podcasts/Podcasts/All Podcasts/Cells/PodcastListCell.swift
@@ -18,9 +18,11 @@ class PodcastListCell: ThemeableCollectionCell {
         }
     }
 
-    @IBOutlet var supporterHeart: PodcastHeartView!
     @IBOutlet var unplayedBadge: UnplayedBadge!
     @IBOutlet var unplayedHeight: NSLayoutConstraint!
+    @IBOutlet var contentStackView: UIStackView!
+
+    private var supporterHeart: PodcastHeartView?
 
     private var badgeType: BadgeType = .off
 
@@ -54,13 +56,29 @@ class PodcastListCell: ThemeableCollectionCell {
 
         unplayedBadge.updateColors()
 
-        supporterHeart.isHidden = !podcast.isPaid
         if podcast.isPaid {
-            supporterHeart.setPodcastColor(podcast: podcast)
-            supporterHeart.isShadowHidden = true
+            let heart = supporterHeart ?? makeSupporterHeart()
+            heart.isHidden = false
+            heart.setPodcastColor(podcast: podcast)
+            heart.isShadowHidden = true
+        } else {
+            supporterHeart?.isHidden = true
         }
 
         updateSize()
+    }
+
+    private func makeSupporterHeart() -> PodcastHeartView {
+        let heart = PodcastHeartView(frame: CGRect(x: 0, y: 0, width: 28, height: 28))
+        heart.translatesAutoresizingMaskIntoConstraints = false
+        let unplayedIndex = contentStackView.arrangedSubviews.firstIndex(of: unplayedBadge) ?? contentStackView.arrangedSubviews.count
+        contentStackView.insertArrangedSubview(heart, at: unplayedIndex)
+        NSLayoutConstraint.activate([
+            heart.widthAnchor.constraint(equalToConstant: 28),
+            heart.widthAnchor.constraint(equalTo: heart.heightAnchor),
+        ])
+        supporterHeart = heart
+        return heart
     }
 
     private func updateSize() {
