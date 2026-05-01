40804b6170c9cae3af13741a2a04a88305f75a26
Sérgio Estêvão <sergioestevao@gmail.com>
2026-05-01 13:19:45 +0100

    Allow back navigation in all places

--- Pocket Casts TV App/UI/Playlists/PlaylistsView.swift
+++ Pocket Casts TV App/UI/Playlists/PlaylistsView.swift
@@ -3,19 +3,19 @@ import Combine
 
 @Observable
 class PlaylistsViewModel {
-    
+
     private var cancellable: AnyCancellable?
-    
+
     enum State: Equatable, Hashable {
         case loading
         case ready
         case empty
     }
-    
+
     var state: State = .loading
-    
+
     var playlists: [MockPlaylist] = MockData.makePlaylists()
-    
+
     func load() {
         //Mock data load
         cancellable = Timer.publish(every: 1.0, on: .main, in: .common, options: nil)
@@ -32,13 +32,13 @@ class PlaylistsViewModel {
 struct PlaylistsView: View {
     @Environment(AppCoordinator.self) var coordinator
     @Environment(MainTabRouter.self) var tabRouter: MainTabRouter
-    
+
     @State private var model = PlaylistsViewModel()
-    
+
     enum Layout {
         static let gridSize = CGFloat(496)
     }
-    
+
     var body: some View {
         ZStack {
             switch model.state {
@@ -54,11 +54,11 @@ struct PlaylistsView: View {
             model.load()
         }
     }
-    
+
     var loadingView: some View {
         ProgressView()
     }
-    
+
     var playlistsView: some View {
         NavigationStack {
             ScrollView {
@@ -71,19 +71,19 @@ struct PlaylistsView: View {
             }
         }
     }
-    
+
     var emptyView: some View {
         EmptyDataView(title: L10n.tvPlaylistsEmptyTitle, subtitle: L10n.tvPlaylistsEmptySubtitle, actionTitle: L10n.tvPlaylistsEmptyActionTitle) {
             tabRouter.selectedTab = .home
         }
     }
-    
+
     private let items: [GridItem] = (0..<3).map { _ in
         GridItem(.flexible(minimum: Layout.gridSize), spacing: 48)
     }
-    
+
     @Namespace private var listNamespace
-    
+
     var playlistsCollection: some View {
         LazyVGrid(columns: items, spacing: 48, content: {
             ForEach(model.playlists) { playlist in
@@ -97,9 +97,13 @@ struct PlaylistsView: View {
         .focusScope(listNamespace)
         .navigationDestination(for: MockPlaylist.self) { playlist in
             VStack {
-                Text("Playlist (\(playlist.title) details coming soon")
-                    .font(.title2)
-                    .foregroundStyle(Color.textPrimary)
+                Button {
+
+                } label: {
+                    Text("Playlist \(playlist.title) details coming soon")
+                        .font(.title2)
+                        .foregroundStyle(Color.textPrimary)
+                }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             .padding()
