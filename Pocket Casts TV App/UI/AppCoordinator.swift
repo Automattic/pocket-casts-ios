2b694cb1f41bf9c7d1f626910b3e89632e418811
Sérgio Estêvão <sergioestevao@gmail.com>
2026-05-05 15:29:39 +0100

    Ensure credentials are available

--- Pocket Casts TV App/UI/AppCoordinator.swift
+++ Pocket Casts TV App/UI/AppCoordinator.swift
@@ -15,7 +15,15 @@ class AppCoordinator {
     var state: State = .loading
 
     func load() async {
+        // Ensure database and tables are setup before we go forward
         let _ = DataManager.sharedManager
+
+        setupCredentials()
+        
         state = .welcome
+    }
+
+    private func setupCredentials() {
+        ServerCredentials.sharing = ApiCredentials.sharingServerSecret
     }
 }
