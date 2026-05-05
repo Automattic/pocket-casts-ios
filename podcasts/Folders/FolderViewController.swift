dd2edadd26b17541d331b2e540dd71bfa2946f09
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-05-05 10:42:02 -0400

    Fix themeableCollectionView background color in Folder

--- podcasts/Folders/FolderViewController.swift
+++ podcasts/Folders/FolderViewController.swift
@@ -82,7 +82,7 @@ class FolderViewController: PCViewController, UIGestureRecognizerDelegate {
         let backgroundColor = ThemeColor.filterUi01(filterColor: folderColor)
 
         if let themeableCollectionView = mainGrid as? ThemeableCollectionView {
-            themeableCollectionView.style = Settings.libraryType() == .list ?  ThemeStyle.primaryUi04 : ThemeStyle.primaryUi02
+            themeableCollectionView.style = ThemeStyle.primaryUi02
         }
 
         changeNavTint(titleColor: titleColor, iconsColor: iconColor, backgroundColor: backgroundColor)
