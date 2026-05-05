6366e7ac22c79e9c361bc37a540f598f9aab1b74
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-05-05 17:12:24 -0400

    Remvoe unused MiniPlayerInsets

--- podcasts/Shared/MiniPlayerInsets.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-enum MiniPlayerInsets {
-    /// Returns the baseline padding above the bottom edge, adjusted for the mini player if visible.
-    /// Default baseline is 16pt.
-    static func baseline(padding: CGFloat = 8) -> CGFloat {
-        Constants.effectiveMiniPlayerOffset + padding
-    }
-}
