60eed7f637e7813c7a653c7f3de5db63f72b7b42
Sérgio Estêvão <sergioestevao@gmail.com>
2026-05-12 10:44:07 +0100

    Remove unused mock data

--- Pocket Casts TV App/UI/WelcomeViewModel.swift
+++ Pocket Casts TV App/UI/WelcomeViewModel.swift
@@ -2,5 +2,12 @@ import SwiftUI
 
 @Observable
 class WelcomeViewModel {
-    var podcasts: [MockPodcast] = MockData.makePodcasts()
+    var images: [String] = {
+        var results = [String]()
+        for i in (0..<48) {
+            let podcastImageName = "Covers/login-cover-\( (i % 10) + 1)"
+            results.append(podcastImageName)
+        }
+        return results
+    }()
 }
