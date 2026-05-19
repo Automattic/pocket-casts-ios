0fab2d08db7c33bf1d3d21d9eb024f520d4711e9
Sérgio Estêvão <sergioestevao@gmail.com>
2026-05-19 17:13:47 +0100

    Improve animation and description

--- Pocket Casts TV App/UI/Player/MediaOverlayView.swift
+++ Pocket Casts TV App/UI/Player/MediaOverlayView.swift
@@ -2,32 +2,41 @@ import SwiftUI
 
 struct MediaOverlayView: View {
 
-    @Bindable var model = NowPlayingViewModel()
+    @Bindable var model: NowPlayingViewModel
+    @Binding var isTransportBarVisible: Bool
 
     enum Layout {
         static let podcastImageSize = CGFloat(640)
+    }
+
+    var scale: CGFloat {
+        return isTransportBarVisible ? 3.0 : 2.0
     }
 
     var body: some View {
         GeometryReader() { proxy in
             if !model.isVideo, let uiImage = model.displayImage {
                 VStack(alignment: .center) {
-                    Spacer()
-                        .frame(height: 100)
+                    if isTransportBarVisible {
+                        Spacer().frame(height: 100)
+                    } else {
+                        Spacer()
+                    }
                     HStack {
                         Spacer()
                         Image(uiImage: uiImage)
                             .resizable()
-                            .frame(width: proxy.size.height / 2.0, height: proxy.size.height / 2.0)
+                            .frame(width: proxy.size.height / scale, height: proxy.size.height / scale)
                             .clipShape(RoundedRectangle(cornerRadius: 24))
-                            .blurredCoverBackground(size: proxy.size.height / 2.0) {
+                            .blurredCoverBackground(size: proxy.size.height / scale) {
                                 Image(uiImage: uiImage)
                             }
-                            .animation(.smooth, value: proxy.size.height)
+                            .animation(.smooth, value: isTransportBarVisible)
                         Spacer()
                     }
                     Spacer()
                 }
+                .ignoresSafeArea()
             }
         }
         .ignoresSafeArea()
