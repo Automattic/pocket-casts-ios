38f6971ff0b1685133f8822cd2eb8f2c3f94d2b7
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-05-19 13:57:51 -0400

    Enable this feature only on iOS 26

--- podcasts/Extensions/UIViewController+TabBar.swift
+++ podcasts/Extensions/UIViewController+TabBar.swift
@@ -5,7 +5,7 @@ extension UIViewController {
     /// On earlier OS versions, or when the view controller isn't inside a
     /// tab bar controller, this is a no-op.
     func setEnclosingTabBarHidden(_ hidden: Bool, animated: Bool) {
-        guard #available(iOS 18.0, *), let tabBarController else { return }
+        guard #available(iOS 26, *), let tabBarController else { return }
         tabBarController.setTabBarHidden(hidden, animated: animated)
     }
 }
