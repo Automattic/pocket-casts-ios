f821a7fdd9d8a3aaaa6fe222f5e1d7e5d9d6819f
pocketcasts <42823211+pocketcasts@users.noreply.github.com>
2026-05-19 11:06:51 -0300

    POC-630: [Bug] When fingerprint is not available tapping show the message to download

--- podcasts/TranscriptViewController.swift
+++ podcasts/TranscriptViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Combine
+import PocketCastsDataModel
 import PocketCastsServer
 import PocketCastsUtils
 
@@ -1002,6 +1003,8 @@ class TranscriptViewController: PlayerItemViewController, AnalyticsSourceProvide
                 "synced_state": syncedState.analyticsName
             ])
             if case .unavailable = syncedState { return }
+            let status = PlaybackManager.shared.currentEpisode().flatMap { DownloadStatus(rawValue: $0.episodeStatus) }
+            if status == .downloaded || status == .downloadedForStreaming { return }
             Toast.show(L10n.transcriptTapToSeekStreamingUnavailable)
             return
         }
