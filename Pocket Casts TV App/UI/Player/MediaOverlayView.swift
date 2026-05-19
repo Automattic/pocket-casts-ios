a46cf87a101b9368d68d87198dbdb8cb3fca3e3d
Sérgio Estêvão <sergioestevao@gmail.com>
2026-05-19 15:40:29 +0100

    Improve animation

--- Pocket Casts TV App/UI/Player/MediaOverlayView.swift
+++ Pocket Casts TV App/UI/Player/MediaOverlayView.swift
@@ -5,23 +5,32 @@ struct MediaOverlayView: View {
     @Bindable var model = NowPlayingViewModel()
 
     enum Layout {
-        static let podcastImageSize = CGFloat(418)
+        static let podcastImageSize = CGFloat(640)
     }
 
     var body: some View {
-        VStack {
-            Spacer()
-            if !model.isVideo {
-                if let uiImage = model.displayImage {
-                    Image(uiImage: uiImage)
-                        .resizable()
-                        .frame(width: Layout.podcastImageSize, height: Layout.podcastImageSize)
-                        .clipShape(RoundedRectangle(cornerRadius: 24))
+        Group {
+            if !model.isVideo, let uiImage = model.displayImage {
+                GeometryReader() { proxy in
+                    VStack(alignment: .center) {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .frame(width: proxy.size.height / 2.0, height: proxy.size.height / 2.0)
+                                .clipShape(RoundedRectangle(cornerRadius: 24))
+                                .blurredCoverBackground(size: proxy.size.height / 2.0) {
+                                    Image(uiImage: uiImage)
+                                }
+                                .animation(.smooth, value: proxy.size.height)
+                            Spacer()
+                        }
+                        Spacer()
+                    }
                 }
             }
-            Spacer()
         }
-        .padding()
         .task {
             model.load()
         }
