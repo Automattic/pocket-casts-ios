affbb6ee97bf087c3c74084d928d5903d940945b
Sérgio Estêvão <sergioestevao@gmail.com>
2026-05-27 07:51:10 +0100

    Unselect when leaving

--- Pocket Casts TV App/UI/Common/FocusStore.swift
+++ Pocket Casts TV App/UI/Common/FocusStore.swift
@@ -16,6 +16,8 @@ struct FocusObserving: ViewModifier {
             .onChange(of: isFocused) { _, newValue in
                 if newValue {
                     focusStore.focusedID = section
+                } else if focusStore.focusedID == section {
+                    focusStore.focusedID = nil
                 }
             }
     }
