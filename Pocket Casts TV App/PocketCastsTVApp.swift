a0acd2476631bb5c2d734ce11e89a3a7b28cf5a2
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-06-03 14:07:32 -0400

    Enable Analytics on tvOS

--- Pocket Casts TV App/PocketCastsTVApp.swift
+++ Pocket Casts TV App/PocketCastsTVApp.swift
@@ -2,10 +2,19 @@ import SwiftUI
 
 @main
 struct PocketCastsTVApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+
+    init() {
+        AnalyticsSetup.setupIfNeeded()
+    }
+
     var body: some Scene {
         WindowGroup {
             RootView()
                 .preferredColorScheme(.dark)
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            TVAppLifecycleAnalytics.shared.handle(scenePhase: newPhase)
         }
     }
 }
