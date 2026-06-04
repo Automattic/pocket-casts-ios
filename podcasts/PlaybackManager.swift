23e89ae24f197134e44f9cc828ca832ede8bf945
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-06-04 09:03:17 -0400

    Fix NowPlaying screen crashing when shown immediatelly after attempting to play a playlist

--- podcasts/PlaybackManager.swift
+++ podcasts/PlaybackManager.swift
@@ -1311,17 +1311,15 @@ class PlaybackManager: ServerPlaybackDelegate {
             // the user has chosen to play a single episode, and they have an up next list, so add this episode into up next and push the rest down
             switchTo(episodeToPlay: startingAtEpisode, moveExistingToUpNext: true, autoPlay: true)
         } else {
-            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-                // there's a new list of episodes to play, so clear what's currently playing and play that
-                self?.load(episode: startingAtEpisode, autoPlay: true, overrideUpNext: true)
-                NotificationCenter.postOnMainThread(notification: Constants.Notifications.playbackTrackChanged)
+            // there's a new list of episodes to play, so clear what's currently playing and play that
+            load(episode: startingAtEpisode, autoPlay: true, overrideUpNext: true)
+            NotificationCenter.postOnMainThread(notification: Constants.Notifications.playbackTrackChanged)
 
-                let filteredEpisodes = episodes!.filter { $0.uuid != startingAtEpisode.uuid }
-                if filteredEpisodes.isEmpty {
-                    return
-                }
-                self?.queue.bulkAdd(filteredEpisodes)
+            let filteredEpisodes = episodes!.filter { $0.uuid != startingAtEpisode.uuid }
+            if filteredEpisodes.isEmpty {
+                return
             }
+            queue.bulkAdd(filteredEpisodes)
         }
     }
 
