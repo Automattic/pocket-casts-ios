6713b64fc6005fc240b932a9cefb297d0c1fef49
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-06-08 15:28:39 -0400

    Fix an issue with lists sometimes losing focus when content is reloaded

--- podcasts/HomeGridItem.swift
+++ podcasts/HomeGridItem.swift
@@ -5,6 +5,21 @@ class HomeGridItem: Identifiable {
     let podcast: Podcast?
     let folder: Folder?
 
+    enum ID: Hashable {
+        case podcast(String)
+        case folder(String)
+        case empty
+    }
+
+    var id: ID {
+        if let podcast {
+            return .podcast(podcast.uuid)
+        } else if let folder {
+            return .folder(folder.uuid)
+        }
+        return .empty
+    }
+
     init(podcast: Podcast) {
         self.podcast = podcast
         folder = nil
