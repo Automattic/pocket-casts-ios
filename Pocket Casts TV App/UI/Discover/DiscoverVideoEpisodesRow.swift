f24d060a97b736c12275e692170a21e23ee3f7d4
Sérgio Estêvão <sergioestevao@gmail.com>
2026-06-14 16:25:24 +0100

    Fix focus on video row on home

--- Pocket Casts TV App/UI/Discover/DiscoverVideoEpisodesRow.swift
+++ Pocket Casts TV App/UI/Discover/DiscoverVideoEpisodesRow.swift
@@ -51,7 +51,7 @@ struct DiscoverVideoEpisodesRow: View {
                 ForEach(model.episodes, id: \.uuid) { episode in
                     DiscoverVideoEpisodeCell(episode: episode)
                         .padding(.vertical, Layout.spacing / 2)
-                        .setFocus(section: model.item?.focusStoreID ?? "")
+                        .setFocus(section: model.item?.focusStoreID ?? model.type?.rawValue ?? "")
                         .focused($focusedID, equals: episode.uuid)
                         .prefersDefaultFocus(model.episodes.first?.uuid == episode.uuid, in: focusNS)
                 }
