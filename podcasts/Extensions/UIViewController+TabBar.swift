aa8d4080d04e1c79cc5d15f1a97a565f2986c480
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-07-01 12:24:17 -0400

    Hide mini player during multi-select

--- podcasts/Extensions/UIViewController+TabBar.swift
+++ podcasts/Extensions/UIViewController+TabBar.swift
@@ -5,12 +5,15 @@ extension UIViewController {
     /// On earlier OS versions, or when the view controller isn't inside a
     /// tab bar controller, this is a no-op.
     func setEnclosingTabBarHidden(_ hidden: Bool, animated: Bool) {
-        // Workaround: `setTabBarHidden(_:animated:)` is buggy on iOS 27, so we
-        // disable tab bar hiding there for now. There's a follow-up ticket to
-        // re-enable this once Apple fixes the underlying issues.
-        guard #unavailable(iOS 27) else { return }
-
         guard #available(iOS 26, *), let tabBarController else { return }
+
         tabBarController.setTabBarHidden(hidden, animated: animated)
+
+        let miniPlayer = appDelegate()?.miniPlayer()
+        if hidden {
+            miniPlayer?.hideMiniPlayer(animated)
+        } else {
+            miniPlayer?.showMiniPlayer()
+        }
     }
 }
