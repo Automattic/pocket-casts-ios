920d4759b24a75be9bd2942ecc2f3c863f2b9d90
Sérgio Estêvão <sergioestevao@gmail.com>
2026-07-09 14:18:03 +0100

    Add featured podcast event

--- Pocket Casts TV App/UI/Discover/DiscoverFeaturedPodcastCell.swift
+++ Pocket Casts TV App/UI/Discover/DiscoverFeaturedPodcastCell.swift
@@ -87,9 +87,14 @@ struct DiscoverFeaturedPodcastCell: View {
                             Text(L10n.tvDiscoverFeaturedGoToPodcast)
                         }
                         .simultaneousGesture(TapGesture().onEnded {
-                            if let listId, let podcastUuid = podcast.uuid {
+                            guard let podcastUuid = podcast.uuid else {
+                                return
+                            }
+                            if let listId {
                                 DiscoverAnalytics.podcastTapped(listId: listId, podcastUuid: podcastUuid, source: source)
                             }
+                            Analytics.track(.discoverFeaturedPodcastTapped, properties: ["uuid": podcastUuid])
+                            AnalyticsHelper.openedFeaturedPodcast()
                         })
                         .focused($focusedButton, equals: FocusValues.goPodcast)
                     }
