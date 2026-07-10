dfce0737b5f1ae92aa4118798d81f71de3ef024b
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-07-10 09:11:49 -0400

    Add FeatureFlag.troubleshooting

--- podcasts/OnlineSupportController.swift
+++ podcasts/OnlineSupportController.swift
@@ -1,6 +1,7 @@
 import MessageUI
 import SwiftUI
 import PocketCastsServer
+import PocketCastsUtils
 import UIKit
 import WebKit
 
@@ -117,9 +118,9 @@ class OnlineSupportController: PCViewController, WKNavigationDelegate, UIAdaptiv
             UIAction(title: L10n.settingsConnectionStatus) { [weak self] _ in
                 self?.showStatusPage()
             },
-            UIAction(title: L10n.troubleshootingTitle) { [weak self] _ in
+            FeatureFlag.troubleshooting.enabled ? UIAction(title: L10n.troubleshootingTitle) { [weak self] _ in
                 self?.showTroubleshooting()
-            },
+            } : nil,
             UIAction(title: L10n.exportDatabase) { [weak self] _ in
                 guard let self, let sender = customRightBtn else { return }
                 export(sender)
@@ -128,7 +129,7 @@ class OnlineSupportController: PCViewController, WKNavigationDelegate, UIAdaptiv
                 guard let self, let sender = customRightBtn else { return }
                 viewLogs(sender)
             },
-        ])
+        ].compactMap { $0 })
     }
 
     private func showStatusPage() {
