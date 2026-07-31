04b42c576702ec574903470cc88c6caad66451c0
Sérgio Estêvão <sergioestevao@gmail.com>
2026-07-31 11:24:46 +0100

    When reading playlists the limit of 1000 does not apply
    It only applies when adding to manual playlist. Because we don't edit playlists locally this limit does not make sense

--- Pocket Casts TV App/UI/Playlists/PlaylistDetailsViewModel.swift
+++ Pocket Casts TV App/UI/Playlists/PlaylistDetailsViewModel.swift
@@ -69,7 +69,6 @@ class PlaylistDetailsViewModel {
             let query = PlaylistQueryBuilder.query(
                 clause: .episode,
                 for: playlist.playlist,
-                limit: Self.playlistEpisodeLimit,
                 shouldShowArchived: true
             )
             let playlistEpisodes = dataManager.findPlaylistEpisodesWhere(query: query, arguments: nil)
@@ -81,9 +80,6 @@ class PlaylistDetailsViewModel {
             }
         }
     }
-
-    /// Mirrors `EpisodeDataManager.Constants.Limits.maxPlaylistItems`, which is private to the data module.
-    private static let playlistEpisodeLimit = 1000
 
     func setShowArchived(_ value: Bool) {
         guard value != showArchived else { return }
