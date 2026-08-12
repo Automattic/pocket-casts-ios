68f3670e22590889c718cb60a2750eca210a3f14
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-08-12 13:33:55 -0400

    Add TranscriptEpisodeInfoProvider feedback

--- podcasts/TranscriptPlaybackManaging.swift
+++ podcasts/TranscriptPlaybackManaging.swift
@@ -23,7 +23,8 @@ extension PlaybackManager: TranscriptPlaybackManaging {
     }
 
     var isPlayingEpisode: Bool {
-        isPlaying
+        guard let episodeUUID else { return false }
+        return isActivelyPlaying(episodeUuid: episodeUUID)
     }
 
     var canSeek: Bool { true }
@@ -34,37 +35,32 @@ extension PlaybackManager: TranscriptPlaybackManaging {
 }
 
 struct TranscriptEpisodeInfoProvider: TranscriptPlaybackManaging {
-    private let episodeUuid: String
+    private let _episodeUUID: String
+
+    var episodeUUID: String? { _episodeUUID }
     let podcastUUID: String?
-
-    var episodeUUID: String? {
-        episodeUuid
-    }
-
-    var parentIdentifier: String? {
-        podcastUUID
-    }
+    var parentIdentifier: String? { podcastUUID }
 
     init(episodeUUID: String, podcastUUID: String) {
-        self.episodeUuid = episodeUUID
+        self._episodeUUID = episodeUUID
         self.podcastUUID = podcastUUID
     }
 
     func currentTime() -> TimeInterval {
-        guard PlaybackManager.shared.isCurrentEpisode(uuid: episodeUuid) else {
+        guard PlaybackManager.shared.isCurrentEpisode(uuid: _episodeUUID) else {
             return 0
         }
         return PlaybackManager.shared.currentTime()
     }
 
     func seekTo(time: TimeInterval) {
-        guard PlaybackManager.shared.isCurrentEpisode(uuid: episodeUuid) else { return }
+        guard PlaybackManager.shared.isCurrentEpisode(uuid: _episodeUUID) else { return }
         PlaybackManager.shared.seekTo(time: time)
     }
 
-    var canSeek: Bool { PlaybackManager.shared.isCurrentEpisode(uuid: episodeUuid) }
+    var canSeek: Bool { PlaybackManager.shared.isCurrentEpisode(uuid: _episodeUUID) }
 
     var isPlayingEpisode: Bool {
-        PlaybackManager.shared.isActivelyPlaying(episodeUuid: episodeUuid)
+        PlaybackManager.shared.isActivelyPlaying(episodeUuid: _episodeUUID)
     }
 }
