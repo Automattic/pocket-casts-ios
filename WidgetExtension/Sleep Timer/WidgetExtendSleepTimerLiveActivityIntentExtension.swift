7264bd0398ee10d70adbc63b59db19c1a66af502
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-08-13 11:54:00 -0400

    Log if the sleep timer widget intent stub is ever hit
    Matches the PlayEpisodeIntent placeholder pattern so a support log
    would show a trace if this unreachable extension-process stub is
    ever actually executed.

--- WidgetExtension/Sleep Timer/WidgetExtendSleepTimerLiveActivityIntentExtension.swift
+++ WidgetExtension/Sleep Timer/WidgetExtendSleepTimerLiveActivityIntentExtension.swift
@@ -1,5 +1,10 @@
 import Foundation
+import PocketCastsUtils
 
+// Placeholder so that ExtendSleepTimerLiveActivityIntent can compile in widget extension, but never actually executes
+// because it is a LiveActivityIntent which only runs in the app.
 extension ExtendSleepTimerLiveActivityIntent {
-    func extendSleepTimer(by duration: TimeInterval) {}
+    func extendSleepTimer(by duration: TimeInterval) {
+        FileLog.shared.addMessage("ExtendSleepTimerLiveActivityIntent error: In Widget intent extension")
+    }
 }
