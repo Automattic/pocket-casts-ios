baff3942358cd2c7af85dbe5d1be59595ad075ca
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-08-20 16:06:58 -0400

    Add a full screen affordance over the video in the player
    Tapping the video already opened it full screen, but nothing indicated
    that. Show a button in the bottom trailing corner of the video, on a
    dark material background so it stays legible over any frame.

--- podcasts/NowPlayingPlayerItemViewController.swift
+++ podcasts/NowPlayingPlayerItemViewController.swift
@@ -102,6 +102,10 @@ class NowPlayingPlayerItemViewController: PlayerItemViewController {
             tapGesture.numberOfTapsRequired = 1
             tapGesture.numberOfTouchesRequired = 1
             floatingVideoView.addGestureRecognizer(tapGesture)
+
+            floatingVideoView.onFullScreenTapped = { [weak self] in
+                self?.videoTapped()
+            }
         }
     }
 
@@ -606,7 +610,7 @@ class NowPlayingPlayerItemViewController: PlayerItemViewController {
     }
 
     @objc private func videoTapped() {
-        guard PlaybackManager.shared.currentEpisode != nil else { return }
+        guard PlaybackManager.shared.currentEpisode != nil, presentedViewController == nil else { return }
 
         if PlaybackManager.shared.shouldRenderVideo() {
             let videoController = VideoViewController()
