34f24c161f6382f484456b2278ff3755e8b9d160
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-08-25 15:58:53 -0400

    Use the async AVAsset load API in EpisodeFileSizeUpdater

--- podcasts/EpisodeFileSizeUpdater.swift
+++ podcasts/EpisodeFileSizeUpdater.swift
@@ -4,31 +4,28 @@ import PocketCastsDataModel
 import PocketCastsUtils
 
 class EpisodeFileSizeUpdater {
-    class func updateEpisodeDuration(episode: BaseEpisode?) {
-        guard let episode else { return }
-
+    class func updateEpisodeDuration(episode: BaseEpisode) {
         let fileLocation = episode.pathToDownloadedFile(pathFinder: DownloadManager.shared)
         let url = URL(fileURLWithPath: fileLocation)
         let asset = AVURLAsset(url: url)
 
-        asset.loadValuesAsynchronously(forKeys: ["duration"]) {
-            let status = asset.statusOfValue(forKey: "duration", error: nil)
-            if status == .loaded {
-                let calculatedDuration = CMTimeGetSeconds(asset.duration)
-                if calculatedDuration < 10 || calculatedDuration > 36000 { return } // duration is too short or too long to be a podcast eg: less than 10 seconds or more than 10 hours
+        Task {
+            guard let duration = try? await asset.load(.duration) else { return }
 
-                let currentDuration = episode.duration
-                if Int(currentDuration) == Int(calculatedDuration) { return } // we already have the correct duration
+            let calculatedDuration = CMTimeGetSeconds(duration)
+            if calculatedDuration < 10 || calculatedDuration > 36000 { return } // duration is too short or too long to be a podcast eg: less than 10 seconds or more than 10 hours
 
-                var syncChanges = true
-                if abs(currentDuration - calculatedDuration) < 30 {
-                    // only a minor change so just update the db but don't bother syncing
-                    syncChanges = false
-                }
+            let currentDuration = episode.duration
+            if Int(currentDuration) == Int(calculatedDuration) { return } // we already have the correct duration
 
-                DataManager.sharedManager.saveEpisode(duration: calculatedDuration, episode: episode, updateSyncFlag: syncChanges)
-                NotificationCenter.postOnMainThread(notification: Constants.Notifications.episodeDurationChanged, object: episode.uuid)
+            var syncChanges = true
+            if abs(currentDuration - calculatedDuration) < 30 {
+                // only a minor change so just update the db but don't bother syncing
+                syncChanges = false
             }
+
+            DataManager.sharedManager.saveEpisode(duration: calculatedDuration, episode: episode, updateSyncFlag: syncChanges)
+            NotificationCenter.postOnMainThread(notification: Constants.Notifications.episodeDurationChanged, object: episode.uuid)
         }
     }
 }
