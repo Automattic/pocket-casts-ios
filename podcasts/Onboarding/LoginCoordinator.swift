46d3a3e14ecac55253e29a71c3795a623e776f4d
Yael Rubinstein <yaelirub@gmail.com>
2026-08-26 19:18:17 -0700

    just to trigger Claude review to test new prompt

--- podcasts/Onboarding/LoginCoordinator.swift
+++ podcasts/Onboarding/LoginCoordinator.swift
@@ -214,6 +214,7 @@ extension LoginCoordinator: SyncSigninDelegate, CreateAccountDelegate {
 
     func handleAccountCreated() {
         Analytics.track(.userAccountCreated, properties: ["source": socialAuthProvider ?? "password", "flow": OnboardingFlow.shared.currentFlow])
+
         OnboardingFlow.shared.markAccountCreated()
         OnboardingFlow.shared.accountCreated?(true)
         if OnboardingFlow.shared.currentFlow.shouldDismiss {
