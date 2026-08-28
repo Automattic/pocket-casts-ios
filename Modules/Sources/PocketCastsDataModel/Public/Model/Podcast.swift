db53d2e186a855480d97e99c3bad7d437a5aeb4a
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-08-28 08:31:51 -0400

    Inline the @GRDBRecord macro expansion for the remaining models

--- Modules/Sources/PocketCastsDataModel/Public/Model/Podcast.swift
+++ Modules/Sources/PocketCastsDataModel/Public/Model/Podcast.swift
@@ -1,15 +1,12 @@
 import Foundation
 import GRDB
-import GRDBMacros
 import PocketCastsUtils
 
-@GRDBRecord(table: "SJPodcast")
 public class Podcast: NSObject, Identifiable {
     @objc public var id = 0 as Int64
     @objc public var addedDate: Date?
     @objc public var autoDownloadSetting = 0 as Int32
     @objc public var autoAddToUpNext = 0 as Int32
-    @GRDBColumn("episodeKeepSetting")
     @objc public var autoArchiveEpisodeLimit = 0 as Int32
     @objc public var backgroundColor: String?
     @objc public var detailColor: String? // dark artwork overlay
@@ -61,20 +58,16 @@ public class Podcast: NSObject, Identifiable {
     @objc public var isExplicit = false
     @objc public var fundingURL: String?
 
-    @GRDBIgnore
     public var settings = PodcastSettings.defaults
 
     // transient not saved to database
-    @GRDBIgnore
     public var cachedUnreadCount = 0
 
     // if set to an episode UUID, all podcast episodes after the given
     // UUID will be updated
-    @GRDBIgnore
     public var forceRefreshEpisodeFrom: String? = nil
 
     // transient not saved to database
-    @GRDBIgnore
     public var networkList: PodcastNetworkList? = nil
 
     override public init() {}
@@ -125,7 +118,243 @@ public class Podcast: NSObject, Identifiable {
 
         return podcast
     }
+
+    // MARK: - GRDB
+
+    public static let databaseTableName = "SJPodcast"
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case addedDate
+        case autoDownloadSetting
+        case autoAddToUpNext
+        case autoArchiveEpisodeLimit = "episodeKeepSetting"
+        case backgroundColor
+        case detailColor
+        case primaryColor
+        case secondaryColor
+        case lastColorDownloadDate
+        case imageURL
+        case latestEpisodeUuid
+        case latestEpisodeDate
+        case mediaType
+        case lastThumbnailDownloadDate
+        case thumbnailStatus
+        case podcastUrl
+        case author
+        case overrideGlobalEffects
+        case playbackSpeed
+        case boostVolume
+        case trimSilenceAmount
+        case podcastCategory
+        case podcastDescription
+        case podcastHTMLDescription
+        case sortOrder
+        case startFrom
+        case skipLast
+        case subscribed
+        case title
+        case uuid
+        case syncStatus
+        case colorVersion
+        case pushEnabled
+        case episodeSortOrder
+        case episodeGrouping
+        case showType
+        case estimatedNextEpisode
+        case episodeFrequency
+        case lastUpdatedAt
+        case excludeFromAutoArchive
+        case overrideGlobalArchive
+        case autoArchivePlayedAfter
+        case autoArchiveInactiveAfter
+        case isPaid
+        case licensing
+        case fullSyncLastSyncAt
+        case showArchived
+        case refreshAvailable
+        case folderUuid
+        case usedCustomEffectsBefore
+        case isPrivate
+        case isExplicit
+        case fundingURL
+    }
+
+    public required init(from decoder: Decoder) throws {
+        super.init()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(Int64.self, forKey: .id) ?? 0
+        addedDate = try container.decodeIfPresent(Date.self, forKey: .addedDate)
+        autoDownloadSetting = try container.decodeIfPresent(Int32.self, forKey: .autoDownloadSetting) ?? 0
+        autoAddToUpNext = try container.decodeIfPresent(Int32.self, forKey: .autoAddToUpNext) ?? 0
+        autoArchiveEpisodeLimit = try container.decodeIfPresent(Int32.self, forKey: .autoArchiveEpisodeLimit) ?? 0
+        backgroundColor = try container.decodeIfPresent(String.self, forKey: .backgroundColor)
+        detailColor = try container.decodeIfPresent(String.self, forKey: .detailColor)
+        primaryColor = try container.decodeIfPresent(String.self, forKey: .primaryColor)
+        secondaryColor = try container.decodeIfPresent(String.self, forKey: .secondaryColor)
+        lastColorDownloadDate = try container.decodeIfPresent(Date.self, forKey: .lastColorDownloadDate)
+        imageURL = try container.decodeIfPresent(String.self, forKey: .imageURL)
+        latestEpisodeUuid = try container.decodeIfPresent(String.self, forKey: .latestEpisodeUuid)
+        latestEpisodeDate = try container.decodeIfPresent(Date.self, forKey: .latestEpisodeDate)
+        mediaType = try container.decodeIfPresent(String.self, forKey: .mediaType)
+        lastThumbnailDownloadDate = try container.decodeIfPresent(Date.self, forKey: .lastThumbnailDownloadDate)
+        thumbnailStatus = try container.decodeIfPresent(Int32.self, forKey: .thumbnailStatus) ?? 1
+        podcastUrl = try container.decodeIfPresent(String.self, forKey: .podcastUrl)
+        author = try container.decodeIfPresent(String.self, forKey: .author)
+        overrideGlobalEffects = try container.decodeIfPresent(Bool.self, forKey: .overrideGlobalEffects) ?? false
+        playbackSpeed = try container.decodeIfPresent(Double.self, forKey: .playbackSpeed) ?? 1
+        boostVolume = try container.decodeIfPresent(Bool.self, forKey: .boostVolume) ?? false
+        trimSilenceAmount = try container.decodeIfPresent(Int32.self, forKey: .trimSilenceAmount) ?? 0
+        podcastCategory = try container.decodeIfPresent(String.self, forKey: .podcastCategory)
+        podcastDescription = try container.decodeIfPresent(String.self, forKey: .podcastDescription)
+        podcastHTMLDescription = try container.decodeIfPresent(String.self, forKey: .podcastHTMLDescription)
+        sortOrder = try container.decodeIfPresent(Int32.self, forKey: .sortOrder) ?? 0
+        startFrom = try container.decodeIfPresent(Int32.self, forKey: .startFrom) ?? 0
+        skipLast = try container.decodeIfPresent(Int32.self, forKey: .skipLast) ?? 0
+        subscribed = try container.decodeIfPresent(Int32.self, forKey: .subscribed) ?? 1
+        title = try container.decodeIfPresent(String.self, forKey: .title)
+        uuid = try container.decodeIfPresent(String.self, forKey: .uuid) ?? ""
+        syncStatus = try container.decodeIfPresent(Int32.self, forKey: .syncStatus) ?? 0
+        colorVersion = try container.decodeIfPresent(Int32.self, forKey: .colorVersion) ?? 1
+        pushEnabled = try container.decodeIfPresent(Bool.self, forKey: .pushEnabled) ?? false
+        episodeSortOrder = try container.decodeIfPresent(Int32.self, forKey: .episodeSortOrder) ?? 1
+        episodeGrouping = try container.decodeIfPresent(Int32.self, forKey: .episodeGrouping) ?? 0
+        showType = try container.decodeIfPresent(String.self, forKey: .showType)
+        estimatedNextEpisode = try container.decodeIfPresent(Date.self, forKey: .estimatedNextEpisode)
+        episodeFrequency = try container.decodeIfPresent(String.self, forKey: .episodeFrequency)
+        lastUpdatedAt = try container.decodeIfPresent(String.self, forKey: .lastUpdatedAt)
+        excludeFromAutoArchive = try container.decodeIfPresent(Bool.self, forKey: .excludeFromAutoArchive) ?? false
+        overrideGlobalArchive = try container.decodeIfPresent(Bool.self, forKey: .overrideGlobalArchive) ?? false
+        autoArchivePlayedAfter = try container.decodeIfPresent(Double.self, forKey: .autoArchivePlayedAfter) ?? 0
+        autoArchiveInactiveAfter = try container.decodeIfPresent(Double.self, forKey: .autoArchiveInactiveAfter) ?? 0
+        isPaid = try container.decodeIfPresent(Bool.self, forKey: .isPaid) ?? false
+        licensing = try container.decodeIfPresent(Int32.self, forKey: .licensing) ?? 0
+        fullSyncLastSyncAt = try container.decodeIfPresent(String.self, forKey: .fullSyncLastSyncAt)
+        showArchived = try container.decodeIfPresent(Bool.self, forKey: .showArchived) ?? false
+        refreshAvailable = try container.decodeIfPresent(Bool.self, forKey: .refreshAvailable) ?? false
+        folderUuid = try container.decodeIfPresent(String.self, forKey: .folderUuid)
+        usedCustomEffectsBefore = try container.decodeIfPresent(Bool.self, forKey: .usedCustomEffectsBefore) ?? false
+        isPrivate = try container.decodeIfPresent(Bool.self, forKey: .isPrivate) ?? false
+        isExplicit = try container.decodeIfPresent(Bool.self, forKey: .isExplicit) ?? false
+        fundingURL = try container.decodeIfPresent(String.self, forKey: .fundingURL)
+    }
+
+    public func encode(to container: inout PersistenceContainer) {
+        container["id"] = id
+        container["addedDate"] = addedDate?.timeIntervalSince1970
+        container["autoDownloadSetting"] = autoDownloadSetting
+        container["autoAddToUpNext"] = autoAddToUpNext
+        container["episodeKeepSetting"] = autoArchiveEpisodeLimit
+        container["backgroundColor"] = backgroundColor
+        container["detailColor"] = detailColor
+        container["primaryColor"] = primaryColor
+        container["secondaryColor"] = secondaryColor
+        container["lastColorDownloadDate"] = lastColorDownloadDate?.timeIntervalSince1970
+        container["imageURL"] = imageURL
+        container["latestEpisodeUuid"] = latestEpisodeUuid
+        container["latestEpisodeDate"] = latestEpisodeDate?.timeIntervalSince1970
+        container["mediaType"] = mediaType
+        container["lastThumbnailDownloadDate"] = lastThumbnailDownloadDate?.timeIntervalSince1970
+        container["thumbnailStatus"] = thumbnailStatus
+        container["podcastUrl"] = podcastUrl
+        container["author"] = author
+        container["overrideGlobalEffects"] = overrideGlobalEffects
+        container["playbackSpeed"] = playbackSpeed
+        container["boostVolume"] = boostVolume
+        container["trimSilenceAmount"] = trimSilenceAmount
+        container["podcastCategory"] = podcastCategory
+        container["podcastDescription"] = podcastDescription
+        container["podcastHTMLDescription"] = podcastHTMLDescription
+        container["sortOrder"] = sortOrder
+        container["startFrom"] = startFrom
+        container["skipLast"] = skipLast
+        container["subscribed"] = subscribed
+        container["title"] = title
+        container["uuid"] = uuid
+        container["syncStatus"] = syncStatus
+        container["colorVersion"] = colorVersion
+        container["pushEnabled"] = pushEnabled
+        container["episodeSortOrder"] = episodeSortOrder
+        container["episodeGrouping"] = episodeGrouping
+        container["showType"] = showType
+        container["estimatedNextEpisode"] = estimatedNextEpisode?.timeIntervalSince1970
+        container["episodeFrequency"] = episodeFrequency
+        container["lastUpdatedAt"] = lastUpdatedAt
+        container["excludeFromAutoArchive"] = excludeFromAutoArchive
+        container["overrideGlobalArchive"] = overrideGlobalArchive
+        container["autoArchivePlayedAfter"] = autoArchivePlayedAfter
+        container["autoArchiveInactiveAfter"] = autoArchiveInactiveAfter
+        container["isPaid"] = isPaid
+        container["licensing"] = licensing
+        container["fullSyncLastSyncAt"] = fullSyncLastSyncAt
+        container["showArchived"] = showArchived
+        container["refreshAvailable"] = refreshAvailable
+        container["folderUuid"] = folderUuid
+        container["usedCustomEffectsBefore"] = usedCustomEffectsBefore
+        container["isPrivate"] = isPrivate
+        container["isExplicit"] = isExplicit
+        container["fundingURL"] = fundingURL
+    }
+
+    public enum Columns {
+        public static let id = Column(CodingKeys.id)
+        public static let addedDate = Column(CodingKeys.addedDate)
+        public static let autoDownloadSetting = Column(CodingKeys.autoDownloadSetting)
+        public static let autoAddToUpNext = Column(CodingKeys.autoAddToUpNext)
+        public static let autoArchiveEpisodeLimit = Column(CodingKeys.autoArchiveEpisodeLimit)
+        public static let backgroundColor = Column(CodingKeys.backgroundColor)
+        public static let detailColor = Column(CodingKeys.detailColor)
+        public static let primaryColor = Column(CodingKeys.primaryColor)
+        public static let secondaryColor = Column(CodingKeys.secondaryColor)
+        public static let lastColorDownloadDate = Column(CodingKeys.lastColorDownloadDate)
+        public static let imageURL = Column(CodingKeys.imageURL)
+        public static let latestEpisodeUuid = Column(CodingKeys.latestEpisodeUuid)
+        public static let latestEpisodeDate = Column(CodingKeys.latestEpisodeDate)
+        public static let mediaType = Column(CodingKeys.mediaType)
+        public static let lastThumbnailDownloadDate = Column(CodingKeys.lastThumbnailDownloadDate)
+        public static let thumbnailStatus = Column(CodingKeys.thumbnailStatus)
+        public static let podcastUrl = Column(CodingKeys.podcastUrl)
+        public static let author = Column(CodingKeys.author)
+        public static let overrideGlobalEffects = Column(CodingKeys.overrideGlobalEffects)
+        public static let playbackSpeed = Column(CodingKeys.playbackSpeed)
+        public static let boostVolume = Column(CodingKeys.boostVolume)
+        public static let trimSilenceAmount = Column(CodingKeys.trimSilenceAmount)
+        public static let podcastCategory = Column(CodingKeys.podcastCategory)
+        public static let podcastDescription = Column(CodingKeys.podcastDescription)
+        public static let podcastHTMLDescription = Column(CodingKeys.podcastHTMLDescription)
+        public static let sortOrder = Column(CodingKeys.sortOrder)
+        public static let startFrom = Column(CodingKeys.startFrom)
+        public static let skipLast = Column(CodingKeys.skipLast)
+        public static let subscribed = Column(CodingKeys.subscribed)
+        public static let title = Column(CodingKeys.title)
+        public static let uuid = Column(CodingKeys.uuid)
+        public static let syncStatus = Column(CodingKeys.syncStatus)
+        public static let colorVersion = Column(CodingKeys.colorVersion)
+        public static let pushEnabled = Column(CodingKeys.pushEnabled)
+        public static let episodeSortOrder = Column(CodingKeys.episodeSortOrder)
+        public static let episodeGrouping = Column(CodingKeys.episodeGrouping)
+        public static let showType = Column(CodingKeys.showType)
+        public static let estimatedNextEpisode = Column(CodingKeys.estimatedNextEpisode)
+        public static let episodeFrequency = Column(CodingKeys.episodeFrequency)
+        public static let lastUpdatedAt = Column(CodingKeys.lastUpdatedAt)
+        public static let excludeFromAutoArchive = Column(CodingKeys.excludeFromAutoArchive)
+        public static let overrideGlobalArchive = Column(CodingKeys.overrideGlobalArchive)
+        public static let autoArchivePlayedAfter = Column(CodingKeys.autoArchivePlayedAfter)
+        public static let autoArchiveInactiveAfter = Column(CodingKeys.autoArchiveInactiveAfter)
+        public static let isPaid = Column(CodingKeys.isPaid)
+        public static let licensing = Column(CodingKeys.licensing)
+        public static let fullSyncLastSyncAt = Column(CodingKeys.fullSyncLastSyncAt)
+        public static let showArchived = Column(CodingKeys.showArchived)
+        public static let refreshAvailable = Column(CodingKeys.refreshAvailable)
+        public static let folderUuid = Column(CodingKeys.folderUuid)
+        public static let usedCustomEffectsBefore = Column(CodingKeys.usedCustomEffectsBefore)
+        public static let isPrivate = Column(CodingKeys.isPrivate)
+        public static let isExplicit = Column(CodingKeys.isExplicit)
+        public static let fundingURL = Column(CodingKeys.fundingURL)
+    }
 }
+
+extension Podcast: FetchableRecord, PersistableRecord, TableRecord, Decodable {}
 
 public enum TrimSilenceAmount: Int32, Codable, CaseIterable {
     case off = 0, low = 3, medium = 5, high = 10
