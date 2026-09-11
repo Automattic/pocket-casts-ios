67225f0f5a6edb8f8b509d9332c072fa523e9453
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-09-11 17:52:10 -0400

    Let users turn off the What's New unread dot
    Adds a synced, default-on "Show unread dot" setting under General settings. Turning it off hides the dots on the Profile tab and the What's New row without marking anything read.

--- podcasts/ProfileViewController.swift
+++ podcasts/ProfileViewController.swift
@@ -366,7 +366,7 @@ class ProfileViewController: PCViewController, UITableViewDataSource, UITableVie
         case .whatsNew:
             cell.settingsImage.image = UIImage(named: "mail")
             cell.settingsLabel.text = L10n.whatsNew
-            cell.showsUnreadIndicator = WhatsNewManager.shared.hasUnlistedMessages()
+            cell.showsUnreadIndicator = WhatsNewManager.shared.showsDotOnWhatsNewRow()
         case .allStats:
             cell.settingsImage.image = UIImage(named: "profile-stats")
             cell.settingsLabel.text = L10n.settingsStats
@@ -693,7 +693,7 @@ private extension ProfileViewController {
               let cell = profileTable.cellForRow(at: IndexPath(row: row, section: section)) as? TopLevelSettingsCell else {
             return
         }
-        cell.showsUnreadIndicator = WhatsNewManager.shared.hasUnlistedMessages()
+        cell.showsUnreadIndicator = WhatsNewManager.shared.showsDotOnWhatsNewRow()
     }
 }
 
