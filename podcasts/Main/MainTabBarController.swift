47113266fcf9cfa66bdb6e2e243ae8ec3db56f77
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-09-16 19:59:03 -0400

    Remove dead views and DiscoverCoordinator

--- podcasts/Main/MainTabBarController.swift
+++ podcasts/Main/MainTabBarController.swift
@@ -124,7 +124,7 @@ class MainTabBarController: UITabBarController, NavigationProtocol {
         let filtersViewController = PlaylistsViewController()
         filtersViewController.tabBarItem = UITabBarItem(title: L10n.playlists, image: UIImage(named: "playlists_tab"), tag: pcTabs.firstIndex(of: .filter)!)
 
-        let discoverViewController = DiscoverCollectionViewController(coordinator: DiscoverCoordinator())
+        let discoverViewController = DiscoverCollectionViewController()
 
         discoverViewController.tabBarItem = UITabBarItem(title: L10n.discover, image: UIImage(named: "discover_tab"), tag: pcTabs.firstIndex(of: .discover)!)
 
