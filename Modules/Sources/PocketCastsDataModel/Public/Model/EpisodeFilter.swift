9180b990726181eb9d707ac00998b47ec951ecc1
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-09-21 13:10:10 -0400

    Keep the full @GRDBRecord expansion for EpisodeFilter
    Restores CodingKeys, init(from:), Columns, FetchableRecord and Decodable, matching the macro output. Nothing reads EpisodeFilter through GRDB yet, but the plan is to.

--- Modules/Sources/PocketCastsDataModel/Public/Model/EpisodeFilter.swift
+++ Modules/Sources/PocketCastsDataModel/Public/Model/EpisodeFilter.swift
@@ -113,6 +113,66 @@ public class EpisodeFilter: NSObject {
 
     public static let databaseTableName = "SJFilteredPlaylist"
 
+    enum CodingKeys: String, CodingKey {
+        case id
+        case autoDownloadEpisodes
+        case customIcon
+        case filterAllPodcasts
+        case filterAudioVideoType
+        case filterDownloaded
+        case filterFinished
+        case filterNotDownloaded
+        case filterPartiallyPlayed
+        case filterStarred
+        case filterUnplayed
+        case filterHours
+        case playlistName
+        case sortPosition
+        case sortType
+        case uuid
+        case podcastUuids
+        case autoDownloadLimit
+        case filterDuration
+        case longerThan
+        case shorterThan
+        case syncStatus
+        case wasDeleted
+        case manual
+        case showArchivedEpisodes
+        case playlistUpdateDate
+    }
+
+    public required init(from decoder: Decoder) throws {
+        super.init()
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decodeIfPresent(Int64.self, forKey: .id) ?? 0
+        autoDownloadEpisodes = try container.decodeIfPresent(Bool.self, forKey: .autoDownloadEpisodes) ?? false
+        customIcon = try container.decodeIfPresent(Int32.self, forKey: .customIcon) ?? 0
+        filterAllPodcasts = try container.decodeIfPresent(Bool.self, forKey: .filterAllPodcasts) ?? false
+        filterAudioVideoType = try container.decodeIfPresent(Int32.self, forKey: .filterAudioVideoType) ?? 0
+        filterDownloaded = try container.decodeIfPresent(Bool.self, forKey: .filterDownloaded) ?? false
+        filterFinished = try container.decodeIfPresent(Bool.self, forKey: .filterFinished) ?? false
+        filterNotDownloaded = try container.decodeIfPresent(Bool.self, forKey: .filterNotDownloaded) ?? false
+        filterPartiallyPlayed = try container.decodeIfPresent(Bool.self, forKey: .filterPartiallyPlayed) ?? false
+        filterStarred = try container.decodeIfPresent(Bool.self, forKey: .filterStarred) ?? false
+        filterUnplayed = try container.decodeIfPresent(Bool.self, forKey: .filterUnplayed) ?? false
+        filterHours = try container.decodeIfPresent(Int32.self, forKey: .filterHours) ?? 0
+        playlistName = try container.decodeIfPresent(String.self, forKey: .playlistName) ?? ""
+        sortPosition = try container.decodeIfPresent(Int32.self, forKey: .sortPosition) ?? 0
+        sortType = try container.decodeIfPresent(Int32.self, forKey: .sortType) ?? 0
+        uuid = try container.decodeIfPresent(String.self, forKey: .uuid) ?? ""
+        podcastUuids = try container.decodeIfPresent(String.self, forKey: .podcastUuids) ?? ""
+        autoDownloadLimit = try container.decodeIfPresent(Int32.self, forKey: .autoDownloadLimit) ?? 0
+        filterDuration = try container.decodeIfPresent(Bool.self, forKey: .filterDuration) ?? false
+        longerThan = try container.decodeIfPresent(Int32.self, forKey: .longerThan) ?? 0
+        shorterThan = try container.decodeIfPresent(Int32.self, forKey: .shorterThan) ?? 0
+        syncStatus = try container.decodeIfPresent(Int32.self, forKey: .syncStatus) ?? 0
+        wasDeleted = try container.decodeIfPresent(Bool.self, forKey: .wasDeleted) ?? false
+        manual = try container.decodeIfPresent(Bool.self, forKey: .manual) ?? false
+        showArchivedEpisodes = try container.decodeIfPresent(Bool.self, forKey: .showArchivedEpisodes) ?? false
+        playlistUpdateDate = try container.decodeIfPresent(Date.self, forKey: .playlistUpdateDate)
+    }
+
     public func encode(to container: inout PersistenceContainer) {
         container["id"] = id
         container["autoDownloadEpisodes"] = autoDownloadEpisodes
@@ -141,6 +201,35 @@ public class EpisodeFilter: NSObject {
         container["showArchivedEpisodes"] = showArchivedEpisodes
         container["playlistUpdateDate"] = playlistUpdateDate?.timeIntervalSince1970
     }
+
+    public enum Columns {
+        public static let id = Column(CodingKeys.id)
+        public static let autoDownloadEpisodes = Column(CodingKeys.autoDownloadEpisodes)
+        public static let customIcon = Column(CodingKeys.customIcon)
+        public static let filterAllPodcasts = Column(CodingKeys.filterAllPodcasts)
+        public static let filterAudioVideoType = Column(CodingKeys.filterAudioVideoType)
+        public static let filterDownloaded = Column(CodingKeys.filterDownloaded)
+        public static let filterFinished = Column(CodingKeys.filterFinished)
+        public static let filterNotDownloaded = Column(CodingKeys.filterNotDownloaded)
+        public static let filterPartiallyPlayed = Column(CodingKeys.filterPartiallyPlayed)
+        public static let filterStarred = Column(CodingKeys.filterStarred)
+        public static let filterUnplayed = Column(CodingKeys.filterUnplayed)
+        public static let filterHours = Column(CodingKeys.filterHours)
+        public static let playlistName = Column(CodingKeys.playlistName)
+        public static let sortPosition = Column(CodingKeys.sortPosition)
+        public static let sortType = Column(CodingKeys.sortType)
+        public static let uuid = Column(CodingKeys.uuid)
+        public static let podcastUuids = Column(CodingKeys.podcastUuids)
+        public static let autoDownloadLimit = Column(CodingKeys.autoDownloadLimit)
+        public static let filterDuration = Column(CodingKeys.filterDuration)
+        public static let longerThan = Column(CodingKeys.longerThan)
+        public static let shorterThan = Column(CodingKeys.shorterThan)
+        public static let syncStatus = Column(CodingKeys.syncStatus)
+        public static let wasDeleted = Column(CodingKeys.wasDeleted)
+        public static let manual = Column(CodingKeys.manual)
+        public static let showArchivedEpisodes = Column(CodingKeys.showArchivedEpisodes)
+        public static let playlistUpdateDate = Column(CodingKeys.playlistUpdateDate)
+    }
 }
 
-extension EpisodeFilter: PersistableRecord {}
+extension EpisodeFilter: FetchableRecord, PersistableRecord, TableRecord, Decodable {}
