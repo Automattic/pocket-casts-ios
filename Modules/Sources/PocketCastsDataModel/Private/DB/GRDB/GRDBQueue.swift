acb370e2afea4af3e40efe6c99aaf4be96c8e73e
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-09-23 15:56:02 -0400

    Remove unused GRDBQueue inDatabase and inTransaction
    cleanUp() now uses write, which GRDB already wraps in a transaction.

--- Modules/Sources/PocketCastsDataModel/Private/DB/GRDB/GRDBQueue.swift
+++ Modules/Sources/PocketCastsDataModel/Private/DB/GRDB/GRDBQueue.swift
@@ -11,32 +11,6 @@ public final class GRDBQueue {
         self.logger = logger
     }
 
-    func inDatabase(_ block: (any PCDatabase) -> Void) {
-        do {
-            try dbPool.write { db in
-                let dbWrapper = GRDBDatabase(database: db)
-                block(dbWrapper)
-            }
-        } catch {
-            logger?.log(error: error, context: [:])
-        }
-    }
-
-    func inTransaction(_ block: (any PCDatabase, UnsafeMutablePointer<ObjCBool>) -> Void) {
-        do {
-            try dbPool.writeInTransaction { db in
-                let rollback = UnsafeMutablePointer<ObjCBool>.allocate(capacity: 1)
-                rollback.pointee = false
-                let dbWrapper = GRDBDatabase(database: db)
-                block(dbWrapper, rollback)
-                defer { rollback.deallocate() }
-                return rollback.pointee.boolValue ? .rollback : .commit
-            }
-        } catch {
-            logger?.log(error: error, context: [:])
-        }
-    }
-
     func read(_ block: (any PCDatabase) -> Void) {
         guard FeatureFlag.concurrentDatabaseReads.enabled else {
             write(block)
