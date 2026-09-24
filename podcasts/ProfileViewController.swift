2a79b8b2f116cf973feeb61e1a98141ad9573321
Alex Grebenyuk <grebenyuk.alexander@gmail.com>
2026-09-24 13:29:43 -0400

    Use the bell.badge symbol for the What's New dot on iOS 26
    The system badge indicator was too large next to the bell.

--- podcasts/ProfileViewController.swift
+++ podcasts/ProfileViewController.swift
@@ -700,9 +700,7 @@ private extension ProfileViewController {
         guard FeatureFlag.whatsNewFeed.enabled else { return }
 
         let showsDot = WhatsNewManager.shared.showsDotOnWhatsNewButton()
-        if #available(iOS 26.0, *) {
-            whatsNewButton.badge = showsDot ? .indicator() : nil
-        } else if showsDot {
+        if showsDot {
             let configuration = UIImage.SymbolConfiguration(paletteColors: [ThemeColor.support05(), AppTheme.navBarIconsColor()])
             whatsNewButton.image = UIImage(systemName: "bell.badge", withConfiguration: configuration)?.withRenderingMode(.alwaysOriginal)
         } else {
